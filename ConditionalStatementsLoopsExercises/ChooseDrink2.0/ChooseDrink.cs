ca5bed84afd388da08bdabb5764c427d698b5a88
pirocorp <pirocorp@abv.bg>
2017-12-31 00:33:48 +0100

    Receive on the second line the quantities of the drink and you have to print the calculated the price.

---
+++ ConditionalStatementsLoopsExercises/ChooseDrink2.0/ChooseDrink.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ChooseDrink
+{
+    class ChooseDrink
+    {
+        static void Main()
+        {
+            var profession = Console.ReadLine();
+            var quantities = int.Parse(Console.ReadLine());
+            var professionResult = profession;
+            profession = profession.ToLower();
+
+            //Prices
+            var water = 0.70m;
+            var coffee = 1.00m;
+            var beer = 1.70m;
+            var tea = 1.20m;
+
+            var totalCost = 0.0m;
+            //string preferedDrink = ""; Version 1.0
+            if (profession == "athlete")
+            {
+                //preferedDrink = "Water"; Version 1.0
+                totalCost = water * quantities; 
+            }
+            else if (profession == "businessman" || profession == "businesswoman")
+            {
+                //preferedDrink = "Coffee"; Version 1.0
+                totalCost = coffee * quantities;
+            }
+            else if (profession == "softuni student")
+            {
+                //preferedDrink = "Beer"; Version 1.0
+                totalCost = beer * quantities;
+            }
+            else
+            {
+                //preferedDrink = "Tea"; Version 1.0
+                totalCost = tea * quantities;
+            }
+            //Console.WriteLine(result); Version 1.0
+            Console.WriteLine($"The {professionResult} has to pay {totalCost}.");
+        }
+    }
+}
