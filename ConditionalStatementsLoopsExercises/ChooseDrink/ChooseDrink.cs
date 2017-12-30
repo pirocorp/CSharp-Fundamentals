f0c608efae7c0986bcf182ead5d1079ba181b1ba
pirocorp <pirocorp@abv.bg>
2017-12-30 21:09:45 +0100

    Write a program, which receives a profession (as a string) and chooses the perfect drink for the person.

---
+++ ConditionalStatementsLoopsExercises/ChooseDrink/ChooseDrink.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ChooseDrink
+{
+    class ChooseDrink
+    {
+        static void Main()
+        {
+            var profession = Console.ReadLine().ToLower();
+
+            string result = "";
+            if (profession == "athlete")
+            {
+                result = "Water";
+            }
+            else if (profession == "businessman" || profession == "businesswoman")
+            {
+                result = "Coffee";
+            }
+            else if (profession == "softuni student")
+            {
+                result = "Beer";
+            }
+            else
+            {
+                result = "Tea";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
