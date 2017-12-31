66b8568542658c169389edb170de853f12497608
pirocorp <pirocorp@abv.bg>
2017-12-31 16:58:31 +0100

    Write a program, which finds all possible combinations in the interval between two numbers. The program should also find the last combination, in which a number’s digits are equal to a given magical number.

---
+++ ConditionalStatementsLoopsExercises/GameNumbers/GameNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GameNumbers
+{
+    class GameNumbers
+    {
+        static void Main()
+        {
+            var start = int.Parse(Console.ReadLine());
+            var end = int.Parse(Console.ReadLine());
+            var magicNumber = int.Parse(Console.ReadLine());
+
+            var combinations = 0;
+            bool no = true;
+            var lastI = 0;
+            var lastJ = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    combinations++;
+                    if (magicNumber == i + j)
+                    {
+                        no = false;
+                        lastI = i;
+                        lastJ = j;
+                    }
+                }
+            }
+
+            if (no)
+            {
+                Console.WriteLine($"{combinations} combinations - neither equals {magicNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"Number found! {lastI} + {lastJ} = {magicNumber}");
+            }
+        }
+    }
+}
