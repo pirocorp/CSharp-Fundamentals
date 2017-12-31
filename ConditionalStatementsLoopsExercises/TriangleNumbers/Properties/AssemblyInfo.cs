0da7595aa61882c13e574f2a12ee17e46ca1653f
pirocorp <pirocorp@abv.bg>
2017-12-31 16:16:57 +0100

    Program, which receives a number – n, and prints a triangle from 1 to n

--- ConditionalStatementsLoopsExercises/TriangleNumbers/Properties/AssemblyInfo.cs
+++ ConditionalStatementsLoopsExercises/TriangleNumbers/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CountIntegers")]
+[assembly: AssemblyTitle("TriangleNumbers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CountIntegers")]
+[assembly: AssemblyProduct("TriangleNumbers")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("62d03345-237a-465f-b29d-fd9efa3e59f3")]
+[assembly: Guid("a717fcf9-8a70-4e17-be3f-d8edb3d91095")]
 
 // Version information for an assembly consists of the following four values:
 //
