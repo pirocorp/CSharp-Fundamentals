f0ffd8bc3e2304090d0c5d0647d52e8c4354362e
pirocorp <pirocorp@abv.bg>
2017-12-31 16:11:29 +0100

    Program, which can receive any type of input, but upon receiving anything other than an integer – stops

---
+++ ConditionalStatementsLoopsExercises/CountIntegers/CountIntegers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CountIntegers
+{
+    class CountIntegers
+    {
+        static void Main()
+        {
+            int counter = 0;
+
+            try
+            {
+                while (true)
+                {               
+                    var n = int.Parse(Console.ReadLine());
+                    counter++;
+                }
+            }
+            catch
+            {
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
