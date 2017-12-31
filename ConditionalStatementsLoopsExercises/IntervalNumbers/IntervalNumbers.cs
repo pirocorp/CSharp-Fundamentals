65191a4180e96d179168a2ceb5c7c012a49cfe01
pirocorp <pirocorp@abv.bg>
2017-12-31 15:40:00 +0100

    Takes two numbers as input and prints the interval of numbers between them.

---
+++ ConditionalStatementsLoopsExercises/IntervalNumbers/IntervalNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IntervalNumbers
+{
+    class IntervalNumbers
+    {
+        static void Main()
+        {
+            var numberOne = int.Parse(Console.ReadLine());
+            var numberTwo = int.Parse(Console.ReadLine());
+
+            for (int i = Math.Min(numberOne, numberTwo); i <= Math.Max(numberOne, numberTwo); i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
