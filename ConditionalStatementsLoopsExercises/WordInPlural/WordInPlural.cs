dd450e164f640c12be3dd67c86aa96ea4c54d84c
pirocorp <pirocorp@abv.bg>
2017-12-31 15:34:47 +0100

    Receives a noun and prints the noun in plural.

---
+++ ConditionalStatementsLoopsExercises/WordInPlural/WordInPlural.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WordInPlural
+{
+    class WordInPlural
+    {
+        static void Main()
+        {
+            string noun = Console.ReadLine().ToLower();
+
+            char lastChar = noun[noun.Length - 1];
+            char charBeforLast = noun[noun.Length - 2];
+
+            var newNoun = "";
+            if (lastChar == 'y')
+            {
+                newNoun = noun.Substring(0, noun.Length - 1);
+                newNoun += "ies";
+            }
+            else if (lastChar == 'o' || lastChar == 's' || lastChar == 'x' || lastChar == 'z' )
+            {
+                newNoun = noun;
+                newNoun += "es";
+            }
+            else if (lastChar == 'h' && (charBeforLast == 'c' || charBeforLast == 's'))
+            {
+                newNoun = noun;
+                newNoun += "es";
+            }
+            else
+            {
+                newNoun = noun;
+                newNoun += "s";
+            }
+
+                Console.WriteLine(newNoun);
+        }
+    }
+}
