560287a5fb3066559e698c6127329aed4a962ecb
pirocorp <pirocorp@abv.bg>
2017-12-31 16:42:48 +0100

    Write a program, which finds all the possible combinations between two numbers – N and M. The first digit decreases from N to 1, and the second digit increases from 1 to M. The two digits form a number. Multiply the two digits, then multiply their product by 3. Add the result to the total sum. You will also be given a third number, which will be the maximum boundary of the sum. If the sum is equal or greater than this number you should stop the program. See the examples for further information.

---
+++ ConditionalStatementsLoopsExercises/TestNumbers/TestNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TestNumbers
+{
+    class TestNumbers
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            var maxSumBoundary = int.Parse(Console.ReadLine());
+
+            var totalSum = 0;
+            var count = 0;
+            bool br = false;
+
+            for (int i = n; i >= 1; i--)
+            {
+                if (br) break;
+
+                for (int j = 1; j <= m; j++)
+                {
+                    totalSum += i * j * 3;
+                    count++;
+
+                    if (totalSum >= maxSumBoundary)
+                    {
+                        br = true;
+                        break;
+                    }
+                }
+            }
+
+            if (br)
+            {
+                Console.WriteLine($"{count} combinations");
+                Console.WriteLine($"Sum: {totalSum} >= {maxSumBoundary}");
+            }
+            else
+            {
+                Console.WriteLine($"{count} combinations");
+                Console.WriteLine($"Sum: {totalSum}");
+            }
+        }
+    }
+}
