b31eb7baa40e1023f5074828b2855595a1dd91c4
pirocorp <pirocorp@abv.bg>
2017-12-31 18:07:05 +0100

    When one of the player’s health is below or equal to zero you should stop any other further operations and print who the winner is, how much health points he has and in which turn he won.

---
+++ ConditionalStatementsLoopsExercises/NeighbourWars/NeighbourWars.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace NeighbourWars
+{
+    class NeighbourWars
+    {
+        static void Main()
+        {
+            var peshoDamage = int.Parse(Console.ReadLine());
+            var goshoDamage = int.Parse(Console.ReadLine());
+
+            var round = 0;
+            var peshoHealth = 100;
+            var goshoHealth = 100;
+            bool peshoWin;
+
+            while (true)
+            {
+                if (round %2 == 0)
+                {
+                    goshoHealth -= peshoDamage;
+                    round++;
+                    if (goshoHealth <= 0)
+                    {
+                        peshoWin = true;
+                        break;
+                    }
+                    Console.WriteLine($"Pesho used Roundhouse kick and reduced Gosho to {goshoHealth} health.");
+                    
+                }
+                else
+                {
+                    peshoHealth -= goshoDamage;
+                    round++;
+                    if (peshoHealth <= 0)
+                    {
+                        peshoWin = false;
+                        break;
+                    }
+                    Console.WriteLine($"Gosho used Thunderous fist and reduced Pesho to {peshoHealth} health.");
+
+                }
+
+                if (round % 3 == 0 && round > 0)
+                {
+                    peshoHealth += 10;
+                    goshoHealth += 10;
+                }
+            }
+
+            if (peshoWin)
+            {
+                Console.WriteLine($"Pesho won in {round}th round.");
+            }
+            else
+            {
+                Console.WriteLine($"Gosho won in {round}th round.");
+            }
+        }
+    }
+}
