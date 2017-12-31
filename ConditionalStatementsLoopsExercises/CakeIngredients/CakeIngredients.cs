abe216ae7c24e797676095035fd917513f2ae215
pirocorp <pirocorp@abv.bg>
2017-12-31 15:51:06 +0100

    “Adding ingredient {name of the ingredient}.”. When you receive the command “Bake!”

---
+++ ConditionalStatementsLoopsExercises/CakeIngredients/CakeIngredients.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CakeIngredients
+{
+    class CakeIngredients
+    {
+        static void Main(string[] args)
+        {
+            string ingredients;
+            var ingredientsCount = 0;
+            
+            do
+            {
+                ingredients = Console.ReadLine();
+                if (ingredients == "Bake!") continue;
+                ingredientsCount++;
+                Console.WriteLine($"Adding ingredient {ingredients}.");
+            } while (ingredients != "Bake!");
+
+            Console.WriteLine($"Preparing cake with {ingredientsCount} ingredients.");
+        }
+    }
+}
