d22655b2214b01b51d862a6562959a627b9c66e6
pirocorp <pirocorp@abv.bg>
2018-01-01 19:09:17 +0100

    Refactor Special Numbers

---
+++ DataTypesAndVariablesLab/RefactorSpecialNumbers/RefactorSpecialNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace RefactorSpecialNumbers
+{
+    class RefactorSpecialNumbers
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int num = 1; num <= n; num++)
+            {
+                int currentNum = num;
+                int sum = 0;
+
+                while (currentNum > 0)
+                {
+                    sum += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }
+                bool specialNum = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{num} -> {specialNum}");
+            }
+        }
+    }
+}
