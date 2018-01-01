ca30fae4abd0b2025583f70510899515a2dd42c8
pirocorp <pirocorp@abv.bg>
2018-01-01 19:00:36 +0100

    Refactor Volume of Pyramid

---
+++ DataTypesAndVariablesLab/RefactorVolumePyramid/RefactorVolumePyramid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RefactorVolumePyramid
+{
+    class RefactorVolumePyramid
+    {
+        static void Main()
+        {
+            Console.Write("Length: ");
+            var Length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            var Width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            var Height = double.Parse(Console.ReadLine());
+            var volume = (Length * Width * Height) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+        }
+    }
+}
