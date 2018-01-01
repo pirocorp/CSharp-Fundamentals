a17cd91e583fb2bd4bb2e4563d8c3b0224f08a1c
pirocorp <pirocorp@abv.bg>
2018-01-01 18:21:44 +0100

    Program to enter an integer number of centuries and convert it to years, days, hours and minutes.

--- ConditionalStatementsLoopsExercises/SMSTyping/SMSTyping.cs
+++ ConditionalStatementsLoopsExercises/SMSTyping/SMSTyping.cs
@@ -8,30 +8,34 @@ public class SMS_Typing
 
         var SMS = string.Empty;
 
-        for (int i = 0; i < n; i++)
-        {
-            var currentMessageCharacter = Console.ReadLine();
+        //for (int i = 0; i < n; i++)
+        //{
+        //    var currentMessageCharacter = Console.ReadLine();
 
-            if (currentMessageCharacter == "0")
-            {
-                SMS += " ";
-            }
-            else
-            {
-                var mainDigit = int.Parse(currentMessageCharacter[0].ToString());
-                var numberOfDigits = currentMessageCharacter.Length;
-                var offset = (mainDigit - 2) * 3;
+        //    if (currentMessageCharacter == "0")
+        //    {
+        //        SMS += " ";
+        //    }
+        //    else
+        //    {
+        //        var mainDigit = int.Parse(currentMessageCharacter[0].ToString());
+        //        var numberOfDigits = currentMessageCharacter.Length;
+        //        var offset = (mainDigit - 2) * 3;
 
-                if (mainDigit == 8 || mainDigit == 9)
-                {
-                    offset++;
-                }
+        //        if (mainDigit == 8 || mainDigit == 9)
+        //        {
+        //            offset++;
+        //        }
 
-                var letterIndex = offset + numberOfDigits - 1;
-                var currentMessage = (char)(letterIndex + 97);
-                SMS += currentMessage;
-            }
-        }
+        //        var letterIndex = offset + numberOfDigits - 1;
+        //        var currentMessage = (char)(letterIndex + 97);
+        //        SMS += currentMessage;
+        //    }
+        //}
+        //Solution with array
+
+
+
         Console.WriteLine(SMS);
     }
 }
