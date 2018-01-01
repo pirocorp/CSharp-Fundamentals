feca3757233370df9c99525cc65e221739a451ce
pirocorp <pirocorp@abv.bg>
2018-01-01 01:20:16 +0100

    Program, which, given an image resolution (width and height), calculates its megapixels.

---
+++ ConditionalStatementsLoopsExercises/Megapixels/Megapixels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Megapixels
+{
+    class Megapixels
+    {
+        static void Main()
+        {
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+
+            long pixels = width * height;
+            double megaPixels = pixels / 1000000.0;
+            Console.WriteLine($"{width}x{height} => {Math.Round(megaPixels,1)}MP");
+        }
+    }
+}
