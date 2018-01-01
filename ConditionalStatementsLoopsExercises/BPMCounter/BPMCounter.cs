d939317ecca5398b50b1c368186689327cbe1e3a
pirocorp <pirocorp@abv.bg>
2018-01-01 02:16:02 +0100

    Program, which receives BPM (beats per minute) and number of beats from the console and calculates how many bars (1 bar == 4 beats) the beats equal to, then calculates the length of the sequence in minutes and seconds.

---
+++ ConditionalStatementsLoopsExercises/BPMCounter/BPMCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BPMCounter
+{
+    class BPMCounter
+    {
+        static void Main()
+        {
+            int bpm = int.Parse(Console.ReadLine());
+            double nBeats = double.Parse(Console.ReadLine());
+
+            var bars = Math.Round(nBeats / 4.0, 1);
+            double timeMin = nBeats / bpm;
+            double timeSec = timeMin - Math.Truncate(timeMin);
+            timeMin = Math.Truncate(timeMin);
+            timeSec *= 60;
+            timeSec = Math.Floor(timeSec);
+            Console.WriteLine($"{bars} bars - {timeMin}m {timeSec}s");
+        }
+    }
+}
