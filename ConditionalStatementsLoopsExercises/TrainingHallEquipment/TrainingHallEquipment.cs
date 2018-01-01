b51a5c2232a1f4dfcd8ade14d025b744b2f84f76
pirocorp <pirocorp@abv.bg>
2018-01-01 03:05:08 +0100

    As the new intern in SoftUni, you’re tasked with equipping the new training halls with all the necessary items to lead quality technical trainings. You’ll be given a budget and a list of items to buy.

---
+++ ConditionalStatementsLoopsExercises/TrainingHallEquipment/TrainingHallEquipment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TrainingHallEquipment
+{
+    class TrainingHallEquipment
+    {
+        static void Main(string[] args)
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+            int items = int.Parse(Console.ReadLine());
+
+            var sum = 0.0m;
+
+            for (int i = 0; i < items; i++)
+            {
+                var name = Console.ReadLine();
+                var price = decimal.Parse(Console.ReadLine());
+                var itemCounts = int.Parse(Console.ReadLine());
+
+                if (itemCounts != 1) name += "s";
+                sum += price * itemCounts;
+
+                Console.WriteLine($"Adding {itemCounts} {name} to cart.");
+            }
+
+            Console.WriteLine($"Subtotal: ${sum:f2}");
+
+            if (sum > budget)
+            {
+                var moneyNeeded = sum - budget;
+                Console.WriteLine($"Not enough. We need ${moneyNeeded:f2} more.");
+            }
+            else
+            {
+                var moneyLeft = budget - sum;
+                Console.WriteLine($"Money left: ${moneyLeft:f2}");
+            }
+        }
+    }
+}
