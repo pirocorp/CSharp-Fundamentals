ee15a558967509585519ae5b320fa53c6df16669
pirocorp <pirocorp@abv.bg>
2018-01-01 18:52:55 +0100

    Greetings

---
+++ DataTypesAndVariablesLab/Greeting/Greeting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Greeting
+{
+    class Greeting
+    {
+        static void Main()
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string ageStr = Console.ReadLine();
+            int age = int.Parse(ageStr); // Parse string int
+            Console.WriteLine($"Hello, {firstName} {lastName}.\r\nYou are {age} years old.");
+
+        }
+    }
+}
