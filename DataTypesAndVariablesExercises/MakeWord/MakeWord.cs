63ff12aebbd66a2a9d73aab5dc18a2617ecfe84e
pirocorp <pirocorp@abv.bg>
2018-01-03 00:52:57 +0100

    Program, which combines n characters and prints on a single line the combinations of these characters.

---
+++ DataTypesAndVariablesExercises/MakeWord/MakeWord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MakeWord
+{
+    class MakeWord
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            string word = "";
+            for (int i = 0; i < n; i++)
+            {
+                var letter = Console.ReadLine();
+                word += letter;
+            }
+
+            Console.WriteLine($"The word is: {word}");
+        }
+    }
+}
