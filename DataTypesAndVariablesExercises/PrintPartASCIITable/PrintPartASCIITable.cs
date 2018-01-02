f447d8fe70e64a17e73bcb63201c13b140bfeb81
pirocorp <pirocorp@abv.bg>
2018-01-02 13:14:43 +0100

    Prints part of the ASCII table

---
+++ DataTypesAndVariablesExercises/PrintPartASCIITable/PrintPartASCIITable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PrintPartASCIITable
+{
+    class PrintPartASCIITable
+    {
+        static void Main(string[] args)
+        {
+            var startAscii = int.Parse(Console.ReadLine());
+            var endAscii = int.Parse(Console.ReadLine());
+
+            for (char i = (char)startAscii; i <= endAscii; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
