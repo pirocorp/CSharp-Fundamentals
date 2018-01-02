9bac2406b3e7a53cc66ac12636b83dd99f887bdf
pirocorp <pirocorp@abv.bg>
2018-01-02 12:41:48 +0100

    Program to convert a decimal number to hexadecimal and binary number and print it.

---
+++ DataTypesAndVariablesExercises/IntegerToHexAndBinary/IntegerToHexAndBinary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntegerToHexAndBinary
+{
+    class IntegerToHexAndBinary
+    {
+        static void Main()
+        {
+            var decNum = int.Parse(Console.ReadLine());
+
+            string binNum = Convert.ToString(decNum, 2);
+            string hexNum = Convert.ToString(decNum, 16);
+            hexNum = hexNum.ToUpper();
+
+            Console.WriteLine(hexNum);
+            Console.WriteLine(binNum);
+        }
+    }
+}
