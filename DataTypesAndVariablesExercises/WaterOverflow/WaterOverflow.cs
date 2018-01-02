0f64df9e1835c15ac75adaf2c5985c77431021a3
pirocorp <pirocorp@abv.bg>
2018-01-02 23:21:31 +0100

    You have a water tank with capacity of 255 liters.

---
+++ DataTypesAndVariablesExercises/WaterOverflow/WaterOverflow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class WaterOverflow
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var waterInTank = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                var water = int.Parse(Console.ReadLine());
+                if (waterInTank + water > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    waterInTank += water;
+                }
+            }
+
+            Console.WriteLine(waterInTank);
+        }
+    }
+}
