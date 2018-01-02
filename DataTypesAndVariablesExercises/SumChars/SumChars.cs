3ecd05d7652927f8c6a312e17e65c099446f305b
pirocorp <pirocorp@abv.bg>
2018-01-03 00:56:59 +0100

    Program, which sums the ASCII codes of n characters and prints the sum on the console.

---
+++ DataTypesAndVariablesExercises/SumChars/SumChars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumChars
+{
+    class SumChars
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            int totalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int letter = char.Parse(Console.ReadLine());
+                totalSum += letter;
+            }
+
+            Console.WriteLine($"The sum equals: {totalSum}");
+        }
+    }
+}
