7fc26f4dd65df916e086b8546ab5ec78e9c70599
pirocorp <pirocorp@abv.bg>
2018-01-02 12:23:22 +0100

    Program to check if given symbol is digit, vowel or any other symbol.

---
+++ DataTypesAndVariablesExercises/VowelDigit/VowelDigit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VowelDigit
+{
+    class VowelDigit
+    {
+        static void Main()
+        {
+            char symbol = char.Parse(Console.ReadLine().ToLower());
+
+            if (symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u')
+            {
+                Console.WriteLine("vowel");
+            }
+            else if (symbol >= 48 && symbol <= 57)
+            {
+                Console.WriteLine("digit");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
