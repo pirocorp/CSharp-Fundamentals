dfc64534c38745b7c094fcae1ee740b311b3c80b
pirocorp <pirocorp@abv.bg>
2018-01-03 00:29:17 +0100

    His sentence equals to the times his id overflows the numerical type sbyte. Round the years to the nearest larger integer value

---
+++ DataTypesAndVariablesExercises/SentenceThief/SentenceThief.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SentenceThief
+{
+    class SentenceThief
+    {
+        static void Main()
+        {
+            string numericType = Console.ReadLine();
+            long n = int.Parse(Console.ReadLine());
+
+            long maxValue = 0;
+
+            switch (numericType)
+            {
+                case "sbyte": maxValue = sbyte.MaxValue; break;
+                case "int": maxValue = int.MaxValue; break;
+                case "long": maxValue = long.MaxValue; break;
+                default: break;
+            }
+
+            long thief = long.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                long currentId = long.Parse(Console.ReadLine());
+
+                if (currentId > thief && currentId <= maxValue)
+                {
+                    thief = currentId;
+                }
+            }
+
+            long yearsInPrison;
+
+            if (thief >= 0)
+            {
+                yearsInPrison = (long)Math.Ceiling(thief / 127.0);
+            }
+            else
+            {
+                yearsInPrison = (long)Math.Ceiling(thief / -128.0);
+            }
+
+            string years;
+
+            if (yearsInPrison > 1) years = "years";
+            else years = "year";
+
+            Console.WriteLine($"Prisoner with id {thief} is sentenced to {yearsInPrison} {years}");
+        }
+    }
+}
