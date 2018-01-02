34ac17d8316bcd5dadfe3241abb11c372e04d389
pirocorp <pirocorp@abv.bg>
2018-01-02 12:13:43 +0100

    Program to calculate rectangle’s perimeter, area and diagonal by given its width and height.

--- DataTypesAndVariablesExercises/RectangleProperties/RectangleProperties.cs
+++ DataTypesAndVariablesExercises/RectangleProperties/RectangleProperties.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RectangleProperties
 {
-    class Program
+    class RectangleProperties
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            var perimeter = 2 * width + 2 * height;
+            var area = width * height;
+            var diagonal = Math.Sqrt(width * width + height * height);
+
+            Console.WriteLine(perimeter);
+            Console.WriteLine(area);
+            Console.WriteLine(diagonal);
         }
     }
 }
