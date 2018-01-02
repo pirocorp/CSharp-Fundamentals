0f3bd6aa9146301aac6bbf662aa9f28d7d5b194d
pirocorp <pirocorp@abv.bg>
2018-01-03 00:10:09 +0100

    On the first line, you will receive the numeral type of thief’s id. On the second line, you will receive n – the number of ids you will receive. The person who has an id closest to the maximum value of the given numeral type without overflowing it is the thief’s id.

---
+++ DataTypesAndVariablesExercises/CatchThief/CatchThief.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CatchThief
+{
+    class CatchThief
+    {
+        static void Main()
+        {
+            string numericType = Console.ReadLine();
+            long n = int.Parse(Console.ReadLine());
+
+            long maxValue = 0;
+
+            switch (numericType)
+            {
+                case "sbyte": maxValue = sbyte.MaxValue; break;
+                case "int": maxValue = int.MaxValue; break;
+                case "long": maxValue = long.MaxValue; break;
+                default: break;
+            }
+
+            long thief = long.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                long currentId = long.Parse(Console.ReadLine());
+
+                if (currentId > thief && currentId <= maxValue)
+                {
+                    thief = currentId;
+                }
+            }
+
+            Console.WriteLine(thief);
+        }
+    }
+}
