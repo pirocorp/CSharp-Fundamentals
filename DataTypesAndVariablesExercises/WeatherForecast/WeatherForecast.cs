ed881c492ff3d9b5278c973173f2c4114fbce859
pirocorp <pirocorp@abv.bg>
2018-01-02 23:54:00 +0100

    Always print the smallest possible option.

---
+++ DataTypesAndVariablesExercises/WeatherForecast/WeatherForecast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WeatherForecast
+{
+    class WeatherForecast
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+
+            try
+            {
+                long number = long.Parse(inputString);
+
+                if (number >= sbyte.MinValue && number <= sbyte.MaxValue)
+                {
+                    Console.WriteLine("Sunny");
+                }
+                else if (number >= int.MinValue && number <= int.MaxValue)
+                {
+                    Console.WriteLine("Cloudy");
+                }
+                else if (number >= long.MinValue && number <= long.MaxValue)
+                {
+                    Console.WriteLine("Windy");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Rainy");
+            }
+        }
+    }
+}
