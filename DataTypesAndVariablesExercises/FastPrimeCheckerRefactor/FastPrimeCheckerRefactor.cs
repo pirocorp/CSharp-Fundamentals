5dca84a970e6aecc1459b49185f36631df1c5b2b
pirocorp <pirocorp@abv.bg>
2018-01-02 12:50:47 +0100

    Fast Prime Checker - Refactor

---
+++ DataTypesAndVariablesExercises/FastPrimeCheckerRefactor/FastPrimeCheckerRefactor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FastPrimeCheckerRefactor
+{
+    class FastPrimeCheckerRefactor
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int currentNum = 2; currentNum <= n; currentNum++)
+            {
+                bool prime = true;
+
+                for (int divisor = 2; divisor <= Math.Sqrt(currentNum); divisor++)
+                {
+                    if (currentNum % divisor == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"{currentNum} -> {prime}");
+            }
+        }
+    }
+}
