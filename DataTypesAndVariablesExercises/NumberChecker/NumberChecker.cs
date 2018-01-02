00fd52a00cd28871af1a84593bdb7239b23d3202
pirocorp <pirocorp@abv.bg>
2018-01-02 23:09:14 +0100

    Program, which checks if a number is an integer or a floating-point number and prints either “floating-point” or “integer”, depending on the case.

---
+++ DataTypesAndVariablesExercises/NumberChecker/NumberChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumberChecker
+{
+    class NumberChecker
+    {
+        static void Main()
+        {
+            var stringLine = Console.ReadLine();
+
+            try
+            {
+                long intNum = long.Parse(stringLine);
+                Console.WriteLine("integer");
+            }
+            catch
+            {
+                double floatNum = double.Parse(stringLine);
+                Console.WriteLine("floating-point");
+            }
+        }
+    }
+}
