3d92ce66cf67cf16a5a0604875a861df0929cba1
pirocorp <pirocorp@abv.bg>
2018-01-02 12:59:21 +0100

    Comparing Floats

---
+++ DataTypesAndVariablesExercises/ComparingFloats/ComparingFloats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ComparingFloats
+{
+    class ComparingFloats
+    {
+        static void Main()
+        {
+            decimal numberA = decimal.Parse(Console.ReadLine());
+            decimal numberB = decimal.Parse(Console.ReadLine());
+
+            decimal difrence = numberA - numberB;
+
+            if (difrence < 0.000001m && difrence > -0.000001m)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
