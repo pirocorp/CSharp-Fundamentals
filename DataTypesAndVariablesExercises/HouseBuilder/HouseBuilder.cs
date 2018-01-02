c7084b2af19da9a12b4020dc984ab68f5cad07fa
pirocorp <pirocorp@abv.bg>
2018-01-03 00:46:58 +0100

    Print the total price for the materials.

---
+++ DataTypesAndVariablesExercises/HouseBuilder/HouseBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HouseBuilder
+{
+    class HouseBuilder
+    {
+        static void Main()
+        {
+            var countSbyte = 4;
+            var countInt = 10;
+
+            long numberOne = long.Parse(Console.ReadLine());
+            long numberTwo = long.Parse(Console.ReadLine());
+
+            long total;
+            if (numberOne < numberTwo) total = numberOne * countSbyte + numberTwo * countInt;
+            else total = numberTwo * countSbyte + numberOne * countInt;
+
+            Console.WriteLine(total);
+        }
+    }
+}
