35b3b7a43ab5ff71298848390f2082e77132c831
pirocorp <pirocorp@abv.bg>
2018-01-03 12:10:26 +0100

    Program, which calculates the volume of n beer kegs.

---
+++ DataTypesAndVariablesExercises/BeerKegs/BeerKegs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BeerKegs
+{
+    class BeerKegs
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            string biggestModel = "";
+            double biggestModelVolume = -1;
+            for (int i = 0; i < n; i++)
+            {
+                var currentModel = Console.ReadLine();
+                var currentRadius = double.Parse(Console.ReadLine());
+                var currentHeight = double.Parse(Console.ReadLine());
+                var currentVolume = Math.PI * currentRadius * currentRadius * currentHeight;
+                if (currentVolume > biggestModelVolume)
+                {
+                    biggestModelVolume = currentVolume;
+                    biggestModel = currentModel;
+                }
+            }
+
+            Console.WriteLine(biggestModel);
+        }
+    }
+}
