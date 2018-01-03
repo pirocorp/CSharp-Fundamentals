5df4e33801ce47b8f3982113c3871114b1e9c2b9
pirocorp <pirocorp@abv.bg>
2018-01-03 12:17:52 +0100

    You will receive a key (integer) and n characters afterward. Add the key to each of the characters and append them to message. At the end print the message, which you decrypted.

---
+++ DataTypesAndVariablesExercises/DecryptingMessages/DecryptingMessages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DecryptingMessages
+{
+    class DecryptingMessages
+    {
+        static void Main()
+        {
+            var key = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+
+            string result = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                letter += (char)key;
+                result += letter;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
