1b6dd27bf22cb5b27bc986f0002cf9dd039d9f62
pirocorp <pirocorp@abv.bg>
2018-01-03 17:04:31 +0100

    Program to enter a real number of terabytes and convert it to bits

---
+++ DataTypesAndVariablesExercises/FromTerabytesBits/FromTerabytesBits.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace FromTerabytesBits
+{
+    class FromTerabytesBits
+    {
+        static void Main()
+        {
+            double teraByte = double.Parse(Console.ReadLine());
+            double bits = teraByte * 1024 * 1024 * 1024 * 1024 * 8;
+            Console.WriteLine(bits);
+        }
+    }
+}
