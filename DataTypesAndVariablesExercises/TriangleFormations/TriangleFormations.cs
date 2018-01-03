c6a1c7f7a7c0ad67e089a8e273004a44971f86f9
pirocorp <pirocorp@abv.bg>
2018-01-03 18:00:15 +0100

    You are given 3 integer numbers: a, b and c, which will represent the 3 sides of a triangle. Your task is to check whether the triangle is valid. If it is valid, you have to check if it is a right triangle.

---
+++ DataTypesAndVariablesExercises/TriangleFormations/TriangleFormations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace TriangleFormations
+{
+    class TriangleFormations
+    {
+        static void Main()
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int sideB = int.Parse(Console.ReadLine());
+            int sideC = int.Parse(Console.ReadLine());
+
+            bool isValid = (sideA < (sideB + sideC) && sideB < (sideA + sideC) && sideC < (sideA + sideB));
+            bool sideAIshypotenuse = (Math.Pow(sideA, 2) == Math.Pow(sideB, 2) + Math.Pow(sideC, 2));
+            bool sideBIshypotenuse = (Math.Pow(sideB, 2) == Math.Pow(sideA, 2) + Math.Pow(sideC, 2));
+            bool sideCIshypotenuse = (Math.Pow(sideC, 2) == Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+
+            if (isValid)
+            {
+                Console.WriteLine("Triangle is valid.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Triangle.");
+                return;
+            }
+
+            if (sideAIshypotenuse)
+            {
+                Console.WriteLine("Triangle has a right angle between sides b and c");
+            }
+            else if (sideBIshypotenuse)
+            {
+                Console.WriteLine("Triangle has a right angle between sides a and c");
+            }
+            else if (sideCIshypotenuse)
+            {
+                Console.WriteLine("Triangle has a right angle between sides a and b");
+            }
+            else
+            {
+                Console.WriteLine("Triangle has no right angles");
+            }
+        }
+    }
+}
