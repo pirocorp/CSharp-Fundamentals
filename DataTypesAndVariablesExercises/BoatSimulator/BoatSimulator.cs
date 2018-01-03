009732f670a4b4b122d882aa438444bb95f97a39
pirocorp <pirocorp@abv.bg>
2018-01-03 12:34:07 +0100

    You have the task to write a simulator of a boat race. You will receive two characters, which will represent the two boats.

---
+++ DataTypesAndVariablesExercises/BoatSimulator/BoatSimulator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BoatSimulator
+{
+    class BoatSimulator
+    {
+        static void Main()
+        {
+            char firstBoat = char.Parse(Console.ReadLine());
+            char secondBoat = char.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            var firstBoatMovedDistance = 0;
+            var secondBoatMovedDistance = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (firstBoatMovedDistance >= 50 || secondBoatMovedDistance >= 50)
+                {
+                    break;
+                }
+
+                string line = Console.ReadLine();
+
+                if (i % 2 == 1)
+                {
+                    firstBoatMovedDistance += line.Length;
+                }
+                else
+                {
+                    secondBoatMovedDistance += line.Length;
+                }
+
+                if (line == "UPGRADE")
+                {
+                    firstBoat += (char)3;
+                    secondBoat += (char)3;
+                }
+            }
+
+            if (firstBoatMovedDistance > secondBoatMovedDistance)
+            {
+                Console.WriteLine(firstBoat);
+            }
+            else if (secondBoatMovedDistance > firstBoatMovedDistance)
+            {
+                Console.WriteLine(secondBoat);
+            }
+            else
+            {
+                Console.WriteLine("Draw");
+            }
+        }
+    }
+}
