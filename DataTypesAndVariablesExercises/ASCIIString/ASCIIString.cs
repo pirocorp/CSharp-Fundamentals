da23c452334678e0610398bac354bed5dec7ac10
pirocorp <pirocorp@abv.bg>
2018-01-03 18:51:47 +0100

    Program that reads a number N. On the next N lines, it reads integers from the ASCII table. The task is to concatenate everything in string format.

---
+++ DataTypesAndVariablesExercises/ASCIIString/ASCIIString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ASCIIString
+{
+    class ASCIIString
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string result = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                int numLetter = int.Parse(Console.ReadLine());
+                result += (char)numLetter;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
