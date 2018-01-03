5c0a0f4eda44a88fa5a14198fbf5906c43a4385a
pirocorp <pirocorp@abv.bg>
2018-01-03 19:04:23 +0100

    Performs the operation between the operands. The left and right operands will always be integers.

---
+++ DataTypesAndVariablesExercises/Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main()
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            char operatorChar = char.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            int result = 0;
+
+            switch (operatorChar)
+            {
+                case '+': result = numberOne + numberTwo; Console.WriteLine($"{numberOne} {operatorChar} {numberTwo} = {result}"); break;
+                case '-': result = numberOne - numberTwo; Console.WriteLine($"{numberOne} {operatorChar} {numberTwo} = {result}"); break;
+                case '*': result = numberOne * numberTwo; Console.WriteLine($"{numberOne} {operatorChar} {numberTwo} = {result}"); break;
+                case '/': result = numberOne / numberTwo; Console.WriteLine($"{numberOne} {operatorChar} {numberTwo} = {result}"); break;
+                default: break;
+            }
+        }
+    }
+}
