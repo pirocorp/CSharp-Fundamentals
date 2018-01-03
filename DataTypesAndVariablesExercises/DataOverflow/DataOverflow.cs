9f60d0519d333af647ae8e83b61d0a4895f4e800
pirocorp <pirocorp@abv.bg>
2018-01-03 18:28:07 +0100

    You will be given two numbers. Your task is to find the lowest one by value, find the numerical type it fits in from the following (byte, ushort, uint, ulong) and check how many times the greater one by value overflows the type of the smaller one (rounded to the nearest whole integer).

---
+++ DataTypesAndVariablesExercises/DataOverflow/DataOverflow.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace DataOverflow
+{
+    class DataOverflow
+    {
+        static void Main()
+        {
+            ulong numOne = ulong.Parse(Console.ReadLine());
+            ulong numTwo = ulong.Parse(Console.ReadLine());
+
+            var numMax = Math.Max(numOne, numTwo);
+            var numMin = Math.Min(numOne, numTwo);
+
+            string biggerType = "";
+            string smallerType = "";
+
+            ulong numMinTypeMaxValue = 0;
+
+            if (numMin <= byte.MaxValue)
+            {
+                smallerType = "byte";
+                numMinTypeMaxValue = byte.MaxValue;
+            }
+            else if (numMin <= ushort.MaxValue)
+            {
+                smallerType = "ushort";
+                numMinTypeMaxValue = ushort.MaxValue;
+
+            }
+            else if (numMin <= uint.MaxValue)
+            {
+                smallerType = "uint";
+                numMinTypeMaxValue = uint.MaxValue;
+
+            }
+            else if (numMin <= ulong.MaxValue)
+            {
+                smallerType = "ulong";
+                numMinTypeMaxValue = ulong.MaxValue;
+            }
+
+            if (numMax <= byte.MaxValue)
+            {
+                biggerType = "byte";
+            }
+            else if (numMax <= ushort.MaxValue)
+            {
+                biggerType = "ushort";
+            }
+            else if (numMax <= uint.MaxValue)
+            {
+                biggerType = "uint";
+            }
+            else if (numMax <= ulong.MaxValue)
+            {
+                biggerType = "ulong";
+            }
+
+            ulong overflow = (ulong)Math.Round((double) numMax / numMinTypeMaxValue);
+
+            Console.WriteLine($"bigger type: {biggerType}");
+            Console.WriteLine($"smaller type: {smallerType}");
+            Console.WriteLine($"{numMax} can overflow {smallerType} {overflow} times");
+        }
+    }
+}
