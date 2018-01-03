b71eba39536d076fe562bf7245662288e8401371
pirocorp <pirocorp@abv.bg>
2018-01-03 11:35:13 +0100

    Program, which reads three lines from the console. On the first line, there will be delimiter (char) – you have to separate the strings by this delimiter.

---
+++ DataTypesAndVariablesExercises/StringConcatenation/StringConcatenation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StringConcatenation
+{
+    class StringConcatenation
+    {
+        static void Main()
+        {
+            var delimiter = Console.ReadLine();
+            var numberType = Console.ReadLine();
+            var n = int.Parse(Console.ReadLine());
+
+            var resultString = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                var stringInput = Console.ReadLine();
+                if (numberType == "odd")
+                {
+                    if (i % 2 == 1) resultString += stringInput + delimiter;
+                }
+                else
+                {
+                    if (i % 2 == 0) resultString += stringInput + delimiter;
+                }
+            }
+
+            resultString = resultString.Remove((resultString.Length) -1);
+            Console.WriteLine(resultString);
+        }
+    }
+}
