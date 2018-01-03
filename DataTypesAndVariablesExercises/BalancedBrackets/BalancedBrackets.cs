ca904ed6fb70272cbe584c66fb06f7251fef42da
pirocorp <pirocorp@abv.bg>
2018-01-03 13:01:47 +0100

    Your task is to find out if the brackets are balanced. That means after every closing bracket should follow an opening one. Nested parentheses are not valid, and if two consecutive opening brackets exist, the expression should be marked as unbalanced.

---
+++ DataTypesAndVariablesExercises/BalancedBrackets/BalancedBrackets.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BalancedBrackets
+{
+    class BalancedBrackets
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            bool openBracked = false;
+            bool balancedBraked = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "(")
+                {
+                    if (openBracked)
+                    {
+                        balancedBraked = false;
+                        break;
+                    }
+                    openBracked = true;
+                    balancedBraked = false;
+                }
+
+                if (line == ")")
+                {
+                    balancedBraked = true;
+
+                    if (!openBracked)
+                    {
+                        balancedBraked = false;
+                        break;
+                    }
+
+                    openBracked = false;
+                }
+            }
+
+            if (balancedBraked)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
