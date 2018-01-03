decebd508b8006274faa05ec60fe7d40b47b0725
pirocorp <pirocorp@abv.bg>
2018-01-03 16:57:23 +0100

    Write a program to increment a byte variable (starting at 0) n times and print the result. If you detect an overflow (or several), print how many times there was an overflow alongside the variable value.

---
+++ DataTypesAndVariablesExercises/IncrementVariable/IncrementVariable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IncrementVariable
+{
+    class IncrementVariable
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            int overflow = 0;
+            byte num = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                num++;
+                Console.Write(num + "\r");
+                if (num == 0) overflow++;
+            }
+
+            Console.WriteLine(num);
+            if (overflow > 0) Console.WriteLine($"Overflowed {overflow} times");
+        }
+    }
+}
