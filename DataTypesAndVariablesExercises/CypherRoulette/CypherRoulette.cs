4dcc1381196ab2063239d1cae96a4e4e9ba71d37
pirocorp <pirocorp@abv.bg>
2018-01-03 21:02:44 +0100

    You will be given an integer N. On the next N lines, you will receive some strings. If they are normal random characters, you should append them to one another in the cypher string. If the command "spin" is entered, every string entered after it should be appended at the start of the cypher string, if the command "spin" is entered again after that, you should again begin to append them at the end of the cypher string. And so, the append direction changes each time you enter the command "spin". If two equal strings are entered two consecutive times, the cypher resets - emptying the cypher string. This rule also applies to the "spin" command. Note: the "spin" commands do not count towards the N count.

---
+++ DataTypesAndVariablesExercises/CypherRoulette/CypherRoulette.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CypherRoulette
+{
+    class CypherRoulette
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string resultString = "";
+            bool appendedAtTheStart = false;
+            string lastString = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentString = Console.ReadLine();
+
+                if (currentString == "spin")
+                {
+                    appendedAtTheStart = !appendedAtTheStart;
+                    i--;
+
+                    if (lastString == currentString)
+                    {
+                        resultString = "";
+                        appendedAtTheStart = !appendedAtTheStart;
+                    }
+
+                    lastString = currentString;
+                    continue;
+                }
+
+                if (appendedAtTheStart)
+                {
+                    resultString = currentString + resultString;
+                }
+                else
+                {
+                    resultString += currentString;
+                }
+
+                if (lastString == currentString)
+                {
+                    resultString = "";
+                }
+
+                lastString = currentString;
+            }
+
+            Console.WriteLine(resultString);
+        }
+    }
+}
