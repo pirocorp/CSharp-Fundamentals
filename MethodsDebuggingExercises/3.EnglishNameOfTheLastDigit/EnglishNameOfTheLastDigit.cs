504208c7d4d0cc09e21e3a6905709e2b82b0712d
pirocorp <pirocorp@abv.bg>
2018-01-04 18:37:08 +0100

    Write a method that returns the English name of the last digit of a given number. Write a program that reads an integer and prints the returned value from this method.

---
+++ MethodsDebuggingExercises/3.EnglishNameOfTheLastDigit/EnglishNameOfTheLastDigit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EnglishNameOfTheLastDigit
+{
+    class EnglishNameOfTheLastDigit
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            Console.WriteLine(LastDigit(number));
+
+        }
+
+        static string LastDigit(string num)
+        {
+            int lastDigit = num[num.Length - 1];
+            string digit = string.Empty;
+
+            switch (lastDigit)
+            {
+                case '0': digit = "zero"; break;
+                case '1': digit = "one"; break;
+                case '2': digit = "two"; break;
+                case '3': digit = "three"; break;
+                case '4': digit = "four"; break;
+                case '5': digit = "five"; break;
+                case '6': digit = "six"; break;
+                case '7': digit = "seven"; break;
+                case '8': digit = "eight"; break;
+                case '9': digit = "nine"; break;
+                default: digit = "error"; break;
+            }
+
+            return digit;
+        }
+    }
+}
