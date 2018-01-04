ad905be1c8ebc42442e5bd09893f2494a2fc5218
pirocorp <pirocorp@abv.bg>
2018-01-04 12:29:55 +0100

    Create a method that prints a blank cash receipt.

---
+++ MethodsDebuggingLab/BlankReceipt/BlankReceipt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BlankReceipt
+{
+    class BlankReceipt
+    {
+        static void Main()
+        {
+            PrintReceipt();
+        }
+
+        static void PrintReceipt()
+        {
+            PrintHeader();
+            PrintBody();
+            PrintFooter();
+        }
+
+        static void PrintHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+        static void PrintBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+        static void PrintFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("© SoftUni");
+        }
+
+    }
+}
