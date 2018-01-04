6da20541f507d109b70356053c7389d63c4b3048
pirocorp <pirocorp@abv.bg>
2018-01-04 18:00:40 +0100

    Write a method that receives a name as parameter and prints on the console. “Hello, <name>!”

---
+++ MethodsDebuggingExercises/HelloName!/HelloName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloName
+{
+    class HelloName
+    {
+        static void Main()
+        {
+            var name = Console.ReadLine();
+
+            PrintName(name);
+        }
+
+        private static void PrintName(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
