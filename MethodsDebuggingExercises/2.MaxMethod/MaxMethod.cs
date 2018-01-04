19486407979a0c5196ecfb4e3e54855fc50cd372
pirocorp <pirocorp@abv.bg>
2018-01-04 18:09:54 +0100

    Create a method GetMax(int a, int b), that returns the largest of two numbers. Write a program that reads three numbers from the console and prints the biggest of them. Use the GetMax(…) method you just created.

---
+++ MethodsDebuggingExercises/2.MaxMethod/MaxMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MaxMethod
+{
+    class MaxMethod
+    {
+        static void Main()
+        {
+            double number1 = double.Parse(Console.ReadLine());
+            double number2 = double.Parse(Console.ReadLine());
+            double number3 = double.Parse(Console.ReadLine());
+
+            double result = GetMax(GetMax(number1, number2), number3);
+            Console.WriteLine(result);
+        }
+
+        static double GetMax(double number1, double number2)
+        {
+            if (number1 > number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+        }
+    }
+}
