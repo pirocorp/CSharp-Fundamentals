ed10f54ecaa2af272b7413841792b2c5e86de64d
pirocorp <pirocorp@abv.bg>
2018-01-04 12:54:37 +0100

    Create a method that calculates and returns the area of a triangle by given base and height:

---
+++ MethodsDebuggingLab/CalculateTriangleArea/CalculateTriangleArea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculateTriangleArea
+{
+    class CalculateTriangleArea
+    {
+        static void Main()
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine(CalcTriangleArea(width, height));
+        }
+
+        static double CalcTriangleArea(double width, double height)
+        {
+            return width * height / 2;
+        }
+    }
+}
