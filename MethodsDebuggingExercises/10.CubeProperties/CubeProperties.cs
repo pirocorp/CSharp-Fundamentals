b8692771c4b53968a19d842c9234ab17aa3772ab
pirocorp <pirocorp@abv.bg>
2018-01-04 23:40:04 +0100

    Write a program that can calculate the length of the face diagonals, space diagonals, volume and surface area of a cube (http://www.mathopenref.com/cube.html) by a given side. On the first line you will get the side of the cube. On the second line is given the parameter (face, space, volume or area). Output should be rounded to the second digit after the decimal point:

---
+++ MethodsDebuggingExercises/10.CubeProperties/CubeProperties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CubeProperties
+{
+    class CubeProperties
+    {
+        static void Main()
+        {
+            double cubeSide = double.Parse(Console.ReadLine());
+            string propertiOfCube = Console.ReadLine().ToLower().Trim();
+
+            switch (propertiOfCube)
+            {
+                case "face": Console.WriteLine($"{Math.Sqrt(2 * cubeSide * cubeSide):f2}"); break;
+                case "space": Console.WriteLine($"{Math.Sqrt(3 * cubeSide * cubeSide):f2}"); break;
+                case "volume": Console.WriteLine($"{cubeSide * cubeSide * cubeSide:f2}"); break;
+                case "area": Console.WriteLine($"{6 * cubeSide * cubeSide:f2}"); break;
+            }
+        }
+    }
+}
