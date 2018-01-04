c0b352aeaed8adf2d2a809a5f3abcae1aeafce86
pirocorp <pirocorp@abv.bg>
2018-01-04 18:45:14 +0100

    Write a method that prints the digits of a given decimal number in a reversed order. My method reverse string.

---
+++ MethodsDebuggingExercises/04. Numbers in Reversed Order/NumbersInReversedOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumbersInReversedOrder
+{
+    class NumbersInReversedOrder
+    {
+        static void Main()
+        {
+            string number = Console.ReadLine();
+
+            string reverseNumber = NumbersInReverse(number);
+            Console.WriteLine(reverseNumber);
+        }
+
+        static string NumbersInReverse(string number)
+        {
+            string result = string.Empty;
+            for (int i = number.Length-1; i >= 0; i--)
+            {
+                result += number[i];
+            }
+
+            return result;
+        }
+    }
+}
