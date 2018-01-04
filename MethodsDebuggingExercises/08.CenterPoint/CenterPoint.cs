51263c55e1c859ae580627f3206507dfa60728bb
pirocorp <pirocorp@abv.bg>
2018-01-04 19:52:03 +0100

    You are given the coordinates of two points on a Cartesian coordinate system - X1, Y1, X2 and Y2. Create a method that prints the point that is closest to the center of the coordinate system (0, 0) in the format (X, Y). If the points are on a same distance from the center, print only the first one.

---
+++ MethodsDebuggingExercises/08.CenterPoint/CenterPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CenterPoint
+{
+    class CenterPoint
+    {
+        static void Main()
+        {
+            var x1 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine());
+            var x2 = double.Parse(Console.ReadLine());
+            var y2 = double.Parse(Console.ReadLine());
+
+            PrintClosestToCenterPoint(x1, y1, x2, y2);
+        }
+
+        static void PrintClosestToCenterPoint(double x1, double y1, double x2, double y2)
+        {
+            var distanceToCenterPoint1 = Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
+            var distanceToCenterPoint2 = Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2, 2));
+            if (distanceToCenterPoint1 <= distanceToCenterPoint2) Console.WriteLine($"({x1}, {y1})");
+            else Console.WriteLine($"({x2}, {y2})");          
+        }
+    }
+}
