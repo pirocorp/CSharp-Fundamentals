0036c3647fa47d54227166150da8cffaa0985a64
pirocorp <pirocorp@abv.bg>
2018-01-04 18:58:32 +0100

    Define a method Fib(n) that calculates the nth Fibonacci number. Examples:

---
+++ MethodsDebuggingExercises/05.FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FibonacciNumbers
+{
+    class FibonacciNumbers
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int nthFiboNum = ReturnNthFibo(n);
+            Console.WriteLine(nthFiboNum);
+        }
+
+        static int ReturnNthFibo(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+
+            int zeroFibo = 1;
+            int firstFibo = 1;
+            int nFibo = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                nFibo = zeroFibo + firstFibo;
+                zeroFibo = firstFibo;
+                firstFibo = nFibo;
+            }
+
+            return nFibo;
+        }
+    }
+}
