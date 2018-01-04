d25031b24897af4ab74cdc61404fa8db67e77afb
pirocorp <pirocorp@abv.bg>
2018-01-04 12:58:48 +0100

    Create a method that calculates and returns the value of a number raised to a given power:

---
+++ MethodsDebuggingLab/MathPower/MathPower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MathPower
+{
+    class MathPower
+    {
+        static void Main()
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            var result = RaiseToPower(number, power);
+            Console.WriteLine(result);
+        }
+
+        static double RaiseToPower(double number, int power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+                result *= number;
+            return result;
+        }
+    }
+}
