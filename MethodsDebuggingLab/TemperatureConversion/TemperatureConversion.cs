a1ee1ac874f91fb0b3f915ce07f1191f14940bca
pirocorp <pirocorp@abv.bg>
2018-01-04 12:51:41 +0100

    Create a method that converts a temperature from Fahrenheit to Celsius. Format the result to the 2nd decimal point. Use the formula: (fahrenheit - 32) * 5 / 9.

---
+++ MethodsDebuggingLab/TemperatureConversion/TemperatureConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TemperatureConversion
+{
+    class TemperatureConversion
+    {
+        static void Main()
+        {
+            //Console.Write("Temperature in Fahrenheit: ");
+            double fahrenheit = double.Parse(Console.ReadLine());
+            double celsius = FahrenheitToCelsius(fahrenheit);
+            //Console.Write("Temperature in Celsius: {0:F2}", celsius);
+            Console.WriteLine($"{celsius:f2}");
+        }
+
+        static double FahrenheitToCelsius(double degrees)
+        {
+            double celsius = (degrees - 32) * 5 / 9;
+            return celsius;
+        }
+    }
+}
