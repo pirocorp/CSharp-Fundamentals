d0e75642ec8279af8d72495b8d3b98df9d9b75d1
pirocorp <pirocorp@abv.bg>
2018-01-05 18:58:33 +0100

    Write 2 methods: which prints a message in the following format:

---
+++ MethodsDebuggingExercises/20.Notifications/Notifications.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _20.Notifications
+{
+    class Notifications
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string result = Console.ReadLine().ToLower();
+                if (result == "success")
+                {
+                    string operation = Console.ReadLine();
+                    string message = Console.ReadLine();
+                    ShowSuccess(operation, message);
+                }
+                else if (result == "error")
+                {
+                    string operation = Console.ReadLine();
+                    int code = int.Parse(Console.ReadLine());
+                    ShowError(operation, code);
+                }
+            }
+        }
+
+        static void ShowSuccess(string operation, string message)
+        {
+            Console.WriteLine($"Successfully executed {operation}.");
+            Console.WriteLine("==============================");
+            Console.WriteLine($"Message: {message}.");
+        }
+
+        static void ShowError(string operation, int code)
+        {
+            string reason = String.Empty;
+            if (code < 0)
+            {
+                reason = "Internal System Failure";
+            }
+            else
+            {
+                reason = "Invalid Client Data";
+            }
+
+            Console.WriteLine($"Error: Failed to execute {operation}.");
+            Console.WriteLine("==============================");
+            Console.WriteLine($"Error Code: {code}.");
+            Console.WriteLine($"Reason: {reason}.");
+        }
+
+    }
+}
