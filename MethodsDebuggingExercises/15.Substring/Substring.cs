f9de54d9624622150e6169ab8bf36c974e4a6618
pirocorp <pirocorp@abv.bg>
2018-01-05 12:43:30 +0100

    You are given a text and a number count. Your program should search through the text for the letter 'p' (ASCII code 112) and print 'p' along with count letters to its right.  Each match should be printed on a separate line. If there are no matches of 'p' in the text, we print "no".

---
+++ MethodsDebuggingExercises/15.Substring/Substring.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Substring
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        int count = int.Parse(Console.ReadLine());
+
+        const char searchedChar = 'p';
+        bool charIsFound = false;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == searchedChar)
+            {
+                charIsFound = true;
+                int lenght = count + 1;
+                if (lenght + i > text.Length - 1)
+                {
+                    lenght -= (lenght + i - text.Length);
+                }
+
+                string matchedString = text.Substring(i, lenght);
+                Console.WriteLine(matchedString);
+
+                i += lenght -1;
+            }
+        }
+
+        if (!charIsFound)
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
