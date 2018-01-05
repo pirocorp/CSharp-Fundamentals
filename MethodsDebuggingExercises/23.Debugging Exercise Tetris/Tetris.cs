e64869caa60eb5f6da43bb2f83b248884b5787c5
pirocorp <pirocorp@abv.bg>
2018-01-05 23:39:46 +0100

    Debugging Exercise:You will receive an integer N. On the next several lines, you will receive commands in the form of directions – left, right, up and down Your task is to print the famous Tetris block – the T-shape, facing the given direction, with a size of N. When you receive the command “exit”, you should stop the program execution.

---
+++ MethodsDebuggingExercises/23.Debugging Exercise Tetris/Tetris.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Tetris
+{
+    class Tetris
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string currentDirection = string.Empty;
+
+            while (currentDirection != "exit")
+            {
+                currentDirection = Console.ReadLine();
+                switch (currentDirection)
+                {
+                    case "up":
+                        Up(n);
+                        break;
+                    case "right":
+                        Right(n);
+                        break;
+                    case "down":
+                        Down(n);
+                        break;
+                    case "left":
+                        Left(n);
+                        break;
+                }
+            }
+        }
+
+        static void Left(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0}{1}", new string('.', n), new string('*', n));
+            }
+
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(new string('*', 2 * n));
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0}{1}", new string('.', n), new string('*', n));
+            }
+        }
+
+        static void Right(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}{1}", new string('*', n), new string('.', n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', 2 * n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}{1}", new string('*', n), new string('.', n));
+            }
+        }
+
+        static void Up(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}{1}{2}", new string('.', n), new string('*', n), new string('.', n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}", new string('*', n * 3));
+            }
+        }
+
+        static void Down(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}", new string('*', n * 3));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}{1}{2}", new string('.', n), new string('*', n), new string('.', n));
+            }
+        }
+    }
+}
