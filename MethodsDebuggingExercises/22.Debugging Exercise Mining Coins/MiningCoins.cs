425145f66cbecb9d0728112f077ee7e7ee213384
pirocorp <pirocorp@abv.bg>
2018-01-05 23:56:02 +0100

    Debugging Exercise: In this problem you have to decrypt encrypted data and calculate its value. You will receive an integer N. On the next N lines, you will receive 3-digit integers. The input integers will be indexed - starting from 1 to N. The integers represent characters of an encrypted message. You must take the first and the last digit and form a 2-digit number from them, depending on the current index of the input number: • if the index is an odd number – subtract the middle digit from the 2-digit number. • If the index is even number – add the middle digit to the 2-digit number. If the resulting number is an English alphabet letter (Lower or Uppercase) append it to a string. If it is not, ignore it. After that, calculate the sum of the 3 digits and divide it by N. The result from this operation should be a floating-point number, rounded to the 7-th digit after the decimal point. Do the same for all input integers and sum the results. The sum is the value of the decrypted data. The final result should be the decrypted string (the letters, appended together), and the value.

---
+++ MethodsDebuggingExercises/22.Debugging Exercise Mining Coins/MiningCoins.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _22.Debugging_Exercise_Mining_Coins
+{
+    class MiningCoins
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string decrypted = string.Empty;
+            float totalValue = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                int digit1 = number / 100;
+                int digit2 = (number % 100) / 10;
+                int digit3 = number % 10;
+
+                totalValue += (digit1 + digit2 + digit3) / (float)n;
+                int aSCIIcode;
+
+                if (i % 2 == 0)
+                {
+                    aSCIIcode = ((digit1 * 10) + digit3) + digit2;
+                }
+                else
+                {
+                    aSCIIcode = ((digit1 * 10) + digit3) - digit2;
+                }
+
+                if ((aSCIIcode >= 65 && aSCIIcode <= 90)
+                    || (aSCIIcode >= 97 && aSCIIcode <= 122))
+                {
+                    decrypted += (char)aSCIIcode;
+                }
+            }
+
+            Console.WriteLine("Message: {0}", decrypted);
+            Console.WriteLine("Value: {0:F7}", totalValue);
+        }
+    }
+}
