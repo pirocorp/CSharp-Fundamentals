594056485e28e86af0a59b6be950a5745bd2b1d5
pirocorp <pirocorp@abv.bg>
2018-01-05 20:31:59 +0100

    Problem Description You are given a delimiter. On the next line, you will receive a number N. On the next N lines, you will receive strings on each line. Your task is to print the strings, separated by the delimiter. Note: the delimiter and strings could be anything: whitespace and empty strings are acceptable input!

---
+++ MethodsDebuggingExercises/21.Debugging Exercise Tricky Strings/TrickyStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class TrickyStrings
+{
+    static void Main(string[] args)
+    {
+        var delimiter = Console.ReadLine();
+        var numberOfStrings = int.Parse(Console.ReadLine());
+
+        var result = string.Empty;
+        var currentString = string.Empty;
+        for (int i = 0; i < numberOfStrings; i++)
+        {
+            currentString += Console.ReadLine();
+            result += currentString + delimiter;
+            currentString = string.Empty;
+        }
+
+        if (delimiter.Length != 0)
+        {
+            result = result.Remove(result.Length - delimiter.Length);
+        }
+
+        Console.WriteLine(result);
+    }
+}
