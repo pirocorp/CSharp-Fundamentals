63998300b3516487424d96e2e4cfdb202d83ed94
pirocorp <pirocorp@abv.bg>
2018-01-05 16:16:49 +0100

    Debugging Exercise You are given a program that reads a n numbers and a sequence of commands to be executed over these numbers. Input • The first line holds an integer n – the count of numbers. • The second line holds n numbers – integers separated by space. • Each of the next few lines hold commands in format: “[action] [i-th element] [value]”. • The commands sequence end with a command “stop”. Commands Commands are given in format “[action] [i-th element] [value]”. Elements are indexed from 1 to n. The action can be “multiply”, “add”, “subtract”, “rshift” or “lshift”. • The actions “multiply”, “add” and “subtract” have parameters. The first parameter is the index of the element that needs to be changed (in range [1...n]). The second parameter is the value with which we manipulate the element. • The command “lshift” moves the first element last. E.g. “lshift” over {1, 2, 3} will produce {2, 3, 1}. • The command “rshift” moves the last element first. E.g. “rshift” over {1, 2, 3} will produce {3, 1, 2}. Output Print the values of the n elements after the execution of each command (except the last “stop” command).

---
+++ MethodsDebuggingExercises/18.SequenceCommands/SequenceCommands.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+
+public class SequenceOfCommands
+{
+    private const char ArgumentsDelimiter = ' ';
+
+    public static void Main()
+    {
+        int countOfNumbers = int.Parse(Console.ReadLine());
+
+        long[] array = Console.ReadLine()
+            .Split(ArgumentsDelimiter)
+            .Select(long.Parse)
+            .ToArray();
+
+        string command ="";
+
+        while (!command.Equals("stop"))
+        {
+            string[] input = Console.ReadLine().Trim().Split(' ');
+
+             command = input[0];
+
+            int[] args = new int[2];
+
+            if (command.Equals("add") ||
+                command.Equals("subtract") ||
+                command.Equals("multiply"))
+            {
+                try
+                {
+                    args[0] = int.Parse(input[1]);
+                    args[1] = int.Parse(input[2]);
+                }
+                catch
+                {
+                    Console.WriteLine("Parsa na argumentite gurmi");
+                }
+
+                array = PerformAction(array, command, args);
+            }
+            else
+            {
+                PerformAction(array, command, args);
+            }
+            if (!command.Equals("stop")) PrintArray(array);
+        }
+    }
+
+    static long[] PerformAction(long[] array, string action, int[] args)
+    {
+        int pos = args[0] -1;
+        int value = args[1];
+        //Console.WriteLine(action + " " + pos + " " + value);
+        switch (action)
+        {
+            case "multiply":
+                array[pos] *= value;
+                break;
+            case "add":
+                array[pos] += value;
+                break;
+            case "subtract":
+                array[pos] -= value;
+                break;
+            case "lshift":
+                ArrayShiftLeft(array);
+                break;
+            case "rshift":
+                ArrayShiftRight(array);
+                break;
+            default:  break;
+        }
+
+        return array;
+    }
+
+    private static void ArrayShiftRight(long[] array)
+    {
+        long a = array[array.Length - 1];
+        for (int i = array.Length - 1; i >= 1; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[0] = a;
+    }
+
+    private static void ArrayShiftLeft(long[] array)
+    {
+        long a = array[0];
+        for (int i = 1; i <= array.Length - 1; i++)
+        {
+            array[i - 1] = array[i];
+        }
+        array[array.Length - 1] = a;
+    }
+
+    private static void PrintArray(long[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
