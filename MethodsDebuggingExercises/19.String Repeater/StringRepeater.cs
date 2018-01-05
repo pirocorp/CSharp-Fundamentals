89c1f1e8329fc1c05f4a610e2196ba0bb1ba15f2
pirocorp <pirocorp@abv.bg>
2018-01-05 18:38:06 +0100

    Write a RepeatString(str, count) method that receives a string and an integer N as parameters and returns the string, repeated N times. After that, print the result on the console.

---
+++ MethodsDebuggingExercises/19.String Repeater/StringRepeater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _19.String_Repeater
+{
+    class StringRepeater
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+
+            string repeatedString = RepeatString(str, count);
+            Console.WriteLine(repeatedString);
+        }
+
+        static string RepeatString(string str, int count)
+        {
+            string repeatedString = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                repeatedString += str;
+            }
+                   
+            return repeatedString;
+        }
+
+    }
+}
