3857d757e9df6ae9c1fb7b9ddb805f40afde25f2
pirocorp <pirocorp@abv.bg>
2018-01-05 11:15:34 +0100

    Write a program that calculates and prints the n! for any n in the range [1…1000].

---
+++ MethodsDebuggingExercises/13.Factorial/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factorial
+{
+    class Factorial
+    {
+        static void Main()
+        {
+            int factor = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+            for (int i = 1; i <= factor; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
