727cab06ef17339e911245e868b81711bfeb6295
pirocorp <pirocorp@abv.bg>
2018-01-06 12:48:24 +0100

    You will receive 2 input lines – each containing an integer. • The first is N – the number of websites which are down. • The second is the security key. On the next N lines you will receive data about websites in the following format: {siteName} {siteVisits} {siteCommercialPricePerVisit} You must calculate the site loss by the following formula: siteVisits * siteCommercialPricePerVisit When you finish reading all data, you must print the affected sites’ names – each on a new line. Then you must print the total money loss – sum of all site loss, on a new line. Finally you must print the security token, which is the security key, POWERED by the COUNT of affected sites.

--- MethodsDebuggingExercises/25.Anonymous Downsite/AnonymousDownsite.cs
+++ MethodsDebuggingExercises/25.Anonymous Downsite/AnonymousDownsite.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace _25.Anonymous_Downsite
 {
-    class Program
+    class AnonymousDownsite
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+            int securityKey = int.Parse(Console.ReadLine());
+
+            string allAffectedWebsites = string.Empty;
+            decimal totalMoneyLoss = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char[] charSeparators =  { ' ' };
+                string[] websiteData = Console.ReadLine().Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+                allAffectedWebsites += websiteData[0];
+                allAffectedWebsites += "\n";
+                totalMoneyLoss += LossFromSite(websiteData[1], websiteData[2]);
+            }
+
+            BigInteger securityToken = BigInteger.Pow(securityKey, n);
+            if (allAffectedWebsites.Length > 0)
+            {
+                allAffectedWebsites = allAffectedWebsites.Remove(allAffectedWebsites.Length - 1);
+            }
+
+            Console.WriteLine(allAffectedWebsites);
+            Console.WriteLine($"Total Loss: {totalMoneyLoss:f20}");
+            Console.WriteLine($"Security Token: {securityToken}");
+        }
+
+        private static decimal LossFromSite(string siteVisitsString, string siteCommercialPricePerVisitString)
+        {
+            long siteVisits = long.Parse(siteVisitsString);
+            decimal siteCommercialPricePerVisit = decimal.Parse(siteCommercialPricePerVisitString);
+
+            return siteVisits * siteCommercialPricePerVisit;
         }
     }
 }
