2bc57386077a798c268c0651857e50345765d879
pirocorp <pirocorp@abv.bg>
2018-01-06 23:29:07 +0100

    Write a program to read an array of integers and find all triples of elements a, b and c, such that a + b == c (where a stays to the left from b). Print “No” if no such triples exist.

--- Arrays - Lab/04. Triple Sum/Properties/AssemblyInfo.cs
+++ Arrays - Lab/04. Triple Sum/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("03. Last K Numbers Sums")]
+[assembly: AssemblyTitle("04. Triple Sum")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("03. Last K Numbers Sums")]
+[assembly: AssemblyProduct("04. Triple Sum")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("673daf0f-9d22-4510-917d-a413a5cd5920")]
+[assembly: Guid("cbf4149c-126b-4047-bf3e-c5dd30cfb687")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Arrays - Lab/04. Triple Sum/Triple Sum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Triple_Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            string values = Console.ReadLine();
+            char[] delimeterList = { ' ' };
+            long[] numbers = values.Split(delimeterList, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+
+            bool tripleSumFind = false;
+            for (long a = 0; a < numbers.Length; a++)
+            {
+                for (long b = a + 1; b < numbers.Length; b++)
+                {
+                    for (long c = 0; c < numbers.Length; c++)
+                    {
+                        if (numbers[a] + numbers[b] == numbers[c] /*&& c != a && c != b*/)
+                        {
+                            Console.WriteLine($"{numbers[a]} + {numbers[b]} == {numbers[c]}");
+                            tripleSumFind = true;
+                            break;
+                        }
+                        else
+                        {
+                            //Console.WriteLine($"{numbers[a]} + {numbers[b]} == {numbers[c]} -> false");
+                        }
+                    }
+
+                }
+
+            }
+
+            if (!tripleSumFind)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
