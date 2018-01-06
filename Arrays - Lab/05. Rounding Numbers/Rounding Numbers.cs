45c4382831ca6d55f841acab8d39d1b8d7e22ce3
pirocorp <pirocorp@abv.bg>
2018-01-06 23:59:33 +0100

    Write a program to read an array of real numbers (space separated values), round them to the nearest integer in “away from 0” style and print the output as in the examples below.

---
+++ Arrays - Lab/05. Rounding Numbers/Rounding Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05.Rounding_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string values = Console.ReadLine();
+            char[] delimeterList = { ' ' };
+            string[] numberStrings = values.Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            double[] numbers = new double[numberStrings.Length];
+
+            for (int i = 0; i < numberStrings.Length; i++)
+            {
+                numbers[i] = double.Parse(numberStrings[i]);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numberStrings[i]} => {Math.Round(numbers[i], MidpointRounding.AwayFromZero)}");
+            }
+
+        }
+    }
+}
