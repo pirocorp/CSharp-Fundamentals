2da2ad247c483a2368494f76c4eb9ec6dd2fafd2
pirocorp <pirocorp@abv.bg>
2018-01-07 00:31:57 +0100

    Write a program to read an array of strings, reverse it and print its elements. The input consists of a sequence of space separated strings. Print the output on a single line (space separated).

---
+++ Arrays - Lab/06. Reverse Array of Strings/Reverse Array of Strings.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string consoleLine = Console.ReadLine();
+            char[] delimeterList = {' '};
+            string[] inputStrings = consoleLine.Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            string[] reverseStrings = new string[inputStrings.Length];
+
+            for (int i = 0; i < inputStrings.Length; i++)
+            {
+                reverseStrings[inputStrings.Length - i - 1] = inputStrings[i];
+            }
+
+            for (int i = 0; i < reverseStrings.Length; i++)
+            {
+                Console.Write(reverseStrings[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            var reverse = inputStrings.Reverse();
+
+            for (int i = 0; i < reverse.Count(); i++)
+            {
+                Console.Write(reverse.ElementAt(i) + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < inputStrings.Length / 2; i++)
+            {
+                var x = inputStrings[i];
+                inputStrings[i] = inputStrings[inputStrings.Length - 1 - i];
+                inputStrings[inputStrings.Length - 1 - i] = x;
+            }
+
+            for (int i = 0; i < inputStrings.Length; i++)
+            {
+                Console.Write(inputStrings[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
