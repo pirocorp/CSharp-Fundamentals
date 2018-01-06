11ffa8ff061a0d65408c9ab03a437a67ab563296
pirocorp <pirocorp@abv.bg>
2018-01-06 22:20:54 +0100

    Enter two integers n and k. Generate and print the following sequence of n elements: • The first element is: 1 • All other elements = sum of the previous k elements (if less than k are available, sum all of them) • Example: n = 9, k = 5  120 = 4 + 8 + 16 + 31 + 61

---
+++ Arrays - Lab/03. Last K Numbers Sums/Last K Numbers Sums.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03.Last_K_Numbers_Sums
+{
+    class Program
+    {
+        static void Main()
+        {
+            long n = long.Parse(Console.ReadLine());
+            long k = long.Parse(Console.ReadLine());
+
+            long[] sequence = new long[n];
+
+            sequence[0] = 1;
+            for (long i = 1; i < n; i++)
+            {
+                long sum = 0;
+                long count = 0;
+                if (i >= k) count = i - k;
+
+                for (long j = count; j < i; j++)
+                {
+                    sum += sequence[j];
+                }
+
+                sequence[i] = sum;
+            }
+
+            for (long i = 0; i < n; i++)
+            {
+                Console.Write($"{sequence[i]} ");
+            }
+        }
+    }
+}
