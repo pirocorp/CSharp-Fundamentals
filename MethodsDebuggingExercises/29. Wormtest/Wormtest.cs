919e5b49933ed112546ada9cc71ccae265fa6df3
pirocorp <pirocorp@abv.bg>
2018-01-06 15:07:38 +0100

    On the first line of input you will get the worm’s length in meters (m), which will be an integer. On the second line of input you will get the worm’s width, in centimeters (cm), which will be a floating-point number. You should convert the length in centimeters (multiply it by 100). Then you should divide the length and the width and find the remainder of the division. If it is zero or cannot be calculated, you should print the product of the length and the width (length * width), rounded to the second digit after the decimal point. If the remainder is NOT zero, you should print what percentage is the length of the width. Print it rounded to the second digit after the decimal point. Example: length = 1m ; width = 30cm. percentage = 333.33 %.

---
+++ MethodsDebuggingExercises/29. Wormtest/Wormtest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _29.Wormtest
+{
+    class Wormtest
+    {
+        static void Main()
+        {
+            long lengthInMeters = long.Parse(Console.ReadLine());
+            decimal widthInCentimeters = decimal.Parse(Console.ReadLine());
+
+            decimal widthInMeters = widthInCentimeters / 100;
+
+            decimal remainder = 0;
+            if (widthInMeters > 0 && lengthInMeters > 0)
+            {
+                remainder = lengthInMeters % widthInMeters;
+            }
+
+            if (remainder == 0)
+            {
+                var result = lengthInMeters * 100 * widthInCentimeters;
+                Console.WriteLine($"{result:f2}");
+            }
+            else
+            {
+                var percentage = (lengthInMeters / widthInMeters) * 100;
+                Console.WriteLine($"{percentage:f2}%");
+            }
+        }
+         
+    }
+}
