5800ee3eaee29a203f887b583dd72039fefb6da6
pirocorp <pirocorp@abv.bg>
2018-01-06 13:49:10 +0100

    You will be given N – an integer indicating the wing flaps, a contestant has chosen to do. After that, you will receive M – a floating-point number indicating the distance, in meters, the hornet travels for 1000 wing flaps. Then you will receive P – an integer indicating the endurance of the contestant, or how many wing flaps he can make, before he stops to take a break and rest. A hornet rests for 5 seconds. You can assume that a hornet makes 100 wing flaps per second. Your task is to calculate how much distance will the hornet travel, after it flaps its wings N times, and how much time it took him, to travel it. The distance is measured in meters and the time – in seconds.

---
+++ MethodsDebuggingExercises/26.Hornet Wings/HornetWings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _26.Hornet_Wings
+{
+    class HornetWings
+    {
+        static void Main()
+        {
+            int wingFlapsToDo = int.Parse(Console.ReadLine());
+            decimal distanceTraveldPer1000WingFlaps = decimal.Parse(Console.ReadLine());
+            int wingFlapsBeforeRest = int.Parse(Console.ReadLine());
+
+            long timeForRestInSec = 5;
+            long hornetFlapsPerSec = 100;
+
+            decimal distanceTraveld = (wingFlapsToDo / 1000.0m) * distanceTraveldPer1000WingFlaps;
+            long timeForRest = (wingFlapsToDo / wingFlapsBeforeRest) * timeForRestInSec;
+            decimal timeForFlapsInSec = (wingFlapsToDo / (decimal) hornetFlapsPerSec);
+            decimal totalTime = timeForRest + timeForFlapsInSec;
+
+            Console.WriteLine($"{distanceTraveld:f2} m.");
+            Console.WriteLine($"{totalTime} s.");
+        }
+    }
+}
