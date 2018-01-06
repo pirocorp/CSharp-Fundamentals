6bf47cbcdae01526ca751c0ac7c06003965ed14a
pirocorp <pirocorp@abv.bg>
2018-01-06 14:28:41 +0100

    You will be given the poke power the Poke Mon has, N – an integer. Then you will be given the distance between the poke targets, M – an integer. Then you will be given the exhaustionFactor Y – an integer. Your task is to start subtracting M from N until N becomes less than M, i.e. the Poke Mon does not have enough power to reach the next target. Every time you subtract M from N that means you’ve reached a target and poked it successfully. COUNT how many targets you’ve poked – you’ll need that count. The Poke Mon becomes gradually more exhausted. IF N becomes equal to EXACTLY 50 % of its original value, you must divide N by Y, if it is POSSIBLE. This DIVISION is between integers. If a division is not possible, you should NOT do it. Instead, you should continue subtracting. After dividing, you should continue subtracting from N, until it becomes less than M. When N becomes less than M, you must take what has remained of N and the count of targets you’ve poked, and print them as output.

---
+++ MethodsDebuggingExercises/27.Poke Mon/Poke Mon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _27.Poke_Mon
+{
+    class PokeMon
+    {
+        static void Main(string[] args)
+        {
+            long pokePower = long.Parse(Console.ReadLine());
+            long powerNeedForPoke = long.Parse(Console.ReadLine());
+            long exhaustionFactor = long.Parse(Console.ReadLine());
+
+            long pokeCounter = 0;
+            long pokeing = pokePower;
+            while (pokeing >= powerNeedForPoke)
+            {
+                pokeing -= powerNeedForPoke;
+                pokeCounter++;
+                if (pokeing == pokePower * 0.5 && exhaustionFactor > 0)
+                {
+                    pokeing /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(pokeing);
+            Console.WriteLine(pokeCounter);
+        }
+    }
+}
