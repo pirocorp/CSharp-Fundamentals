53b21a9d9394ad421e6797cc9a88a0c7b0f74312
pirocorp <pirocorp@abv.bg>
2018-01-06 14:43:45 +0100

    You will receive N, an integer – the amount of phoenixes. For each phoenix, you will receive 3 input lines: • On the first input line you will receive an integer – the total length of the body of the phoenix. • On the second input line you will receive a floating-point number – the total width of the body of the phoenix. • On the third input line you will receive an integer – the length of 1 wing of the phoenix. For each phoenix, you must print the years it will take for it to reincarnate, which is calculated by the following formula: The totalLength powered by 2, multiplied by the sum of the totalWidth and the totalWingLength (2 * wingLength). totalYears = {totalLength} ^ 2 * ({totalWidth} + 2 * {wingLength})

---
+++ MethodsDebuggingExercises/28.Resurrection/Resurrection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _28.Resurrection
+{
+    class Resurrection
+    {
+        static void Main()
+        {
+            long n = long.Parse(Console.ReadLine());
+            for (long i = 0; i < n; i++)
+            {
+                long totalLength = long.Parse(Console.ReadLine());
+                decimal totalWidth = decimal.Parse(Console.ReadLine());
+                long wingLength = long.Parse(Console.ReadLine());
+
+                decimal totalYears = GetTotalYears(totalLength, totalWidth, wingLength);
+                Console.WriteLine(totalYears);
+            }
+        }
+
+        private static decimal GetTotalYears(long totalLength, decimal totalWidth, long wingLength)
+        {
+            decimal totalYears = totalLength * totalLength * (totalWidth + 2 * wingLength);
+
+            return totalYears;
+        }
+    }
+}
