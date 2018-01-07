0779706ac8d02e2666b6b9c364531b30b769f12e
pirocorp <pirocorp@abv.bg>
2018-01-07 16:16:44 +0100

    Program to read an array of integers, find the smallest element and print it.

---
+++ Arrays - Lab/12. Smallest Element in Array/Smallest Element in Array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _12.Smallest_Element_in_Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = {' '};
+            int[] numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            if (numbers.Length <= 0)
+            {
+                return;
+            }
+
+            var smallest = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < smallest)
+                {
+                    smallest = numbers[i];
+                }
+
+            }
+
+            Console.WriteLine(smallest);
+
+        }
+    }
+}
