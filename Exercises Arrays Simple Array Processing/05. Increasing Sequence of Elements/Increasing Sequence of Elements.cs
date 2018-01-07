229f29ea7037a251a881926903900e69c65c1883
pirocorp <pirocorp@abv.bg>
2018-01-07 17:31:24 +0100

    Read an array of integers and find out if it is an increasing sequence. Print Yes if it does and No if it doesn’t.

---
+++ Exercises Arrays Simple Array Processing/05. Increasing Sequence of Elements/Increasing Sequence of Elements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.Increasing_Sequence_of_Elements
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeter = {' '};
+            int[] numbers = Console.ReadLine()
+                .Split(delimeter, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            bool isIncreasingSsequence = true;
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                if (numbers[i] > numbers[i+1])
+                {
+                    isIncreasingSsequence = false;
+                    break;
+                }
+            }
+
+            if (isIncreasingSsequence)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
