44488aa06e7a2fc8862562445242674e28bbadcf
pirocorp <pirocorp@abv.bg>
2018-01-07 17:23:41 +0100

    Read an array of real numbers and a number p. Find how many elements are bigger than p in the array and print the count.

---
+++ Exercises Arrays Simple Array Processing/04. Count Occurrences of Larger Numbers/Count Occurrences of Larger Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Count_Occurrences_of_Larger_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimeterList = {' '};
+            double[] numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            double p = double.Parse(Console.ReadLine());
+
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > p) count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
