0da7273e99d4af512903954843177d36ecbf3d84
pirocorp <pirocorp@abv.bg>
2018-01-07 22:48:01 +0100

    You are given two lines of input: a string and an int array. Modify the elements in the first array by the second array, following these conditions: • If the element at the position is even, subtract its value to the ASCII code of the character on the same position. • If the element at the position is odd, add its value to the ASCII code of the character on the same position. After that, print the resulting string.

---
+++ Exercises Arrays Simple Array Processing/15. Char Rotation/Char Rotation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _15.Char_Rotation
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = {' '};
+            string inputString = Console.ReadLine();
+            int[] numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            char[] result = new char[inputString.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    result[i] = (char)(inputString[i] - numbers[i]);
+                }
+                else
+                {
+                    result[i] = (char)(inputString[i] + numbers[i]);
+                }
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- Exercises Arrays Simple Array Processing/15. Char Rotation/Properties/AssemblyInfo.cs
+++ Exercises Arrays Simple Array Processing/15. Char Rotation/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("14. Phone")]
+[assembly: AssemblyTitle("15. Char Rotation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("14. Phone")]
+[assembly: AssemblyProduct("15. Char Rotation")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1778b504-84b8-446b-867a-53e002196b2e")]
+[assembly: Guid("59c62b57-e6b5-4485-ae59-dcc5e9184c3b")]
 
 // Version information for an assembly consists of the following four values:
 //
