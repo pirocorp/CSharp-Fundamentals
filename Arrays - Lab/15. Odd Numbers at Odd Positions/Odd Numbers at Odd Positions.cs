f25ff83b2dea7097d25dcd1edbb4e714eb966cc8
pirocorp <pirocorp@abv.bg>
2018-01-07 16:37:21 +0100

    Read an array of integers and find how many odd numbers at odd positions the array holds. If there are no numbers which match this criteria, do not print anything

---
+++ Arrays - Lab/15. Odd Numbers at Odd Positions/Odd Numbers at Odd Positions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _15.Odd_Numbers_at_Odd_Positions
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = {' '};
+            var numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 == 1 && Math.Abs(numbers[i] % 2) == 1)
+                {
+                    Console.WriteLine($"Index {i} -> {numbers[i]}");
+                }
+            }
+        }
+    }
+}
