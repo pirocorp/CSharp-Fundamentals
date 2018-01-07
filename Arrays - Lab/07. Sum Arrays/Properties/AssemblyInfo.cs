e95af9a3e9f258c1af590e3692a132b04593406e
pirocorp <pirocorp@abv.bg>
2018-01-07 11:57:13 +0100

    Write a program that reads two arrays of integers and sums them. When the arrays are not of the same size, duplicate the smaller array a few times.

--- Arrays - Lab/07. Sum Arrays/Properties/AssemblyInfo.cs
+++ Arrays - Lab/07. Sum Arrays/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("06. Reverse Array of Strings")]
+[assembly: AssemblyTitle("07. Sum Arrays")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("06. Reverse Array of Strings")]
+[assembly: AssemblyProduct("07. Sum Arrays")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("720e637b-c59e-4447-b90c-aaedb325b1e9")]
+[assembly: Guid("1d2ca2ff-1b5b-4810-ba20-f9c3f2c17d6d")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Arrays - Lab/07. Sum Arrays/Sum Arrays.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Sum_Arrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Input and processing Data
+            string consoleLine = Console.ReadLine();
+            char[] delimeterList = {' '};
+            string[] elements = consoleLine.Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            int[] arrayOfNumbers1 = elements.Select(int.Parse).ToArray();
+            consoleLine = Console.ReadLine();
+            elements = consoleLine.Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            int[] arrayOfNumbers2 = elements.Select(int.Parse).ToArray();
+
+            //Calculations
+            int[] sumOfArrays = SumArrays(arrayOfNumbers1, arrayOfNumbers2);
+
+            //Output
+            for (int i = 0; i < sumOfArrays.Length; i++)
+            {
+                Console.Write($"{sumOfArrays[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static int[] SumArrays(int[] arrayOfNumbers1, int[] arrayOfNumbers2)
+        {
+            if (arrayOfNumbers2.Length > arrayOfNumbers1.Length)
+            {
+                int[] arraySwap = arrayOfNumbers1;
+                arrayOfNumbers1 = arrayOfNumbers2;
+                arrayOfNumbers2 = arraySwap;
+            }
+
+            int[] sumOfArrays = new int[arrayOfNumbers1.Length];
+
+            for (int i = 0; i < sumOfArrays.Length; i++)
+            {
+                sumOfArrays[i] = arrayOfNumbers1[i] + arrayOfNumbers2[i % arrayOfNumbers2.Length];
+            }
+
+            return sumOfArrays;
+        }
+    }
+}
