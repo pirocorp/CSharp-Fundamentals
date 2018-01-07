3d7e6f5a317220083351bd43ca3e02d99ad99901
pirocorp <pirocorp@abv.bg>
2018-01-07 17:49:06 +0100

    Read an array of strings and find out how many of them are capital English letters (such as A, B, C etc.). Print the count on the console.

---
+++ Exercises Arrays Simple Array Processing/07. Count of Capital Letters in Array/Count of Capital Letters in Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Count_of_Capital_Letters_in_Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = {' '};
+            string[] inputStrings = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            int count = 0;
+            for (int i = 0; i < inputStrings.Length; i++)
+            {
+                if (inputStrings[i].Length == 1 && inputStrings[0][0] >= 'A' && inputStrings[0][0] <= 'Z')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
