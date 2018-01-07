96b562aafec46a6c4663475d14b8afd224e84c45
pirocorp <pirocorp@abv.bg>
2018-01-07 15:54:34 +0100

    Write a program which reads an array of integers, calculates its sum and prints it. The input consists of a number n (the number of elements) + n integers, each as a separate line.

---
+++ Arrays - Lab/10. Sum Array Elements/Sum Array Elements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.Sum_Array_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfElements = int.Parse(Console.ReadLine());
+            var array = new int[numberOfElements];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            var sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            sum = array.Sum();
+            Console.WriteLine(sum);
+
+        }
+    }
+}
