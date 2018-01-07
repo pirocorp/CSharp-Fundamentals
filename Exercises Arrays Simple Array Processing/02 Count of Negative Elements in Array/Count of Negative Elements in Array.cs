b4496c3a367055c0262d2c19293aae9eca01672e
pirocorp <pirocorp@abv.bg>
2018-01-07 17:09:15 +0100

    Read an integer N and on the next N lines read an array of integers. Then, find the count of negative elements in the array.

---
+++ Exercises Arrays Simple Array Processing/02 Count of Negative Elements in Array/Count of Negative Elements in Array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.Largest_Element_in_Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 0) count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
