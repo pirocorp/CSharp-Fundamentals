1134018e014677313b71c373f420e52e7cf47957
pirocorp <pirocorp@abv.bg>
2018-01-07 18:25:39 +0100

    You are an airplane pilot, trying to maneuver your airplane to safety from an unknown danger. An array holds a sequence of up / down commands and numbers. Its first element always holds the initial altitude. The command up increases the altitude by the next number, while the command down decreases the altitude by the next number. If at any point the altitude becomes either zero or negative, print “crashed” and end the program. If by the end, the altitude is positive, however, print “got through safely. current altitude: {altitude}m”.

---
+++ Exercises Arrays Simple Array Processing/09. Altitude/Altitude.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _09.Altitude
+{
+    class Altitude
+    {
+        static void Main()
+        {
+            char[] delimeterList = {' '};
+            string[] commandStrings = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            int altiude = int.Parse(commandStrings[0]);
+            int currentAltitude = 0;
+            for (int i = 1; i < commandStrings.Length; i +=2)
+            {
+                if (commandStrings[i] == "up")
+                {
+                    currentAltitude = 1;
+                }
+                else
+                {
+                    currentAltitude = -1;
+                }
+
+                currentAltitude *= int.Parse(commandStrings[i + 1]);
+                altiude += currentAltitude;
+                if (altiude <= 0)
+                {
+                    Console.WriteLine("crashed");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"got through safely. current altitude: {altiude}m");
+        }
+    }
+}
