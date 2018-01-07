aa0be7a87168df3cbc146cd12458fa3dcff9ea7f
pirocorp <pirocorp@abv.bg>
2018-01-07 17:17:48 +0100

    Read an array of integers and print how many times a given element exists in it.

---
+++ Exercises Arrays Simple Array Processing/03 Count of Given Element in Array/Count of Given Element in Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_Count_of_Given_Element_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimeterList = {' '};
+            int[] numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int x = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == x) count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
--- Exercises Arrays Simple Array Processing/03 Count of Given Element in Array/Properties/AssemblyInfo.cs
+++ Exercises Arrays Simple Array Processing/03 Count of Given Element in Array/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("02 Count of Negative Elements in Array")]
+[assembly: AssemblyTitle("03 Count of Given Element in Array")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("02 Count of Negative Elements in Array")]
+[assembly: AssemblyProduct("03 Count of Given Element in Array")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9f6449c8-7b07-4872-9af9-4ea5dc5c382c")]
+[assembly: Guid("7f726e5b-21b8-4520-906c-6419e4c6e244")]
 
 // Version information for an assembly consists of the following four values:
 //
