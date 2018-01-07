bf626f91e6145304ffd1d785152e7819dc935867
pirocorp <pirocorp@abv.bg>
2018-01-07 12:21:52 +0100

    program to read an array of integers and condense them by summing adjacent couples of elements until a single integer is obtained.

---
+++ Arrays - Lab/08. Condense Array to Number/Condense Array to Number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Data input and processing
+            string consoleLine = Console.ReadLine();
+            char[] delimiterList = {' '};
+            string[] inputStrings = consoleLine.Split(delimiterList, StringSplitOptions.RemoveEmptyEntries);
+
+            int[] nums = inputStrings.Select(int.Parse).ToArray();
+            while (nums.Length > 1)
+            {
+                int[] condensed = new int[nums.Length - 1];
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    condensed[i] = nums[i] + nums[i + 1];
+                }
+
+                nums = condensed;
+            }
+
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
