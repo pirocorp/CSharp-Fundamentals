a3ef5bc32dee84f9bf184341677aa85685c9a39b
pirocorp <pirocorp@abv.bg>
2018-01-07 16:25:26 +0100

    Program to read an array of strings, rotate it to the right and print its rotated elements.

---
+++ Arrays - Lab/13. Rotate Array of Strings/Rotate Array of Strings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _13.Rotate_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimeterList = {' '};
+            var array = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var rotatedArray = new string[array.Length];
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                rotatedArray[i + 1] = array[i];
+            }
+
+            var lastElement = array[rotatedArray.Length - 1];
+            rotatedArray[0] = lastElement;
+            
+            Console.WriteLine(string.Join(" ", rotatedArray));
+        }
+    }
+}
