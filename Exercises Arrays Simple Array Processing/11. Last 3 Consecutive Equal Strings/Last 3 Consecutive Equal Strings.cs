79e7c19de8d7b7be2ee8cde301a5eac6545ac622
pirocorp <pirocorp@abv.bg>
2018-01-07 20:14:08 +0100

    Read an array of strings and find the last three consecutive equal strings.

--- Exercises Arrays Simple Array Processing/11. Last 3 Consecutive Equal Strings/Last 3 Consecutive Equal Strings.cs
+++ Exercises Arrays Simple Array Processing/11. Last 3 Consecutive Equal Strings/Last 3 Consecutive Equal Strings.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _11.Last_3_Consecutive_Equal_Strings
 {
@@ -10,7 +6,32 @@ namespace _11.Last_3_Consecutive_Equal_Strings
     {
         static void Main()
         {
+            char[] delimeterList = {' '};
+            string[] words = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
 
+            string lastFoundString = string.Empty;
+            string preveusWord = words[0];
+            int count = 1;
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (preveusWord == words[i])
+                {
+                    count++;
+                    if (count >= 3)
+                    {
+                        lastFoundString = words[i];
+                        preveusWord = words[i];
+                    }
+                }
+                else
+                {
+                    count = 1;
+                    preveusWord = words[i];
+                }
+            }
+
+            Console.WriteLine($"{lastFoundString} {lastFoundString} {lastFoundString}");
         }
     }
 }
