ffd5f25491bcd5066a38543706dd41048e93706a
pirocorp <pirocorp@abv.bg>
2018-01-07 17:39:27 +0100

    Read an array of integers and find out if all the elements in the array are the same.

---
+++ Exercises Arrays Simple Array Processing/06. Equal Sequence of Elements in Array/Equal Sequence of Elements in Array.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.Equal_Sequence_of_Elements_in_Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = {' '};
+            int[] numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            bool isFromEqualElements = true;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] != numbers[j])
+                    {
+                        isFromEqualElements = false;
+                        break;
+                    }
+                }
+
+            }
+
+            if (isFromEqualElements)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
