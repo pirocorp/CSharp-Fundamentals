7e071d1468e0b26811032a64a3269f8cc2342e84
pirocorp <pirocorp@abv.bg>
2018-01-07 20:40:02 +0100

    You will be given an array of phone numbers (strings) and an array of names (strings). After which, you will be given strings on new lines, until you receive the command “done”. Find the numbers, which correspond to the names and print them on the console in the following format: • {name} -> {number} A number corresponds to a name when it’s located on the same position as its corresponding name in both arrays

---
+++ Exercises Arrays Simple Array Processing/13. Phonebook/Phonebook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _13.Phonebook
+{
+    class Phonebook
+    {
+        static void Main()
+        {
+            char[] delimeterList = {' '};
+            string[] phoneNumber = Console.ReadLine().Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            string[] names = Console.ReadLine().Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            string name = Console.ReadLine();
+            while (name != "done")
+            {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (name == names[i] && i < phoneNumber.Length)
+                    {
+                        Console.WriteLine($"{name} -> {phoneNumber[i]}");
+                    }
+                }
+
+                name = Console.ReadLine();
+            }
+        }
+    }
+}
