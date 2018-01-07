763a2f945a6fdf3cc50beff09f7b926354114f6d
pirocorp <pirocorp@abv.bg>
2018-01-07 17:01:38 +0100

    Read an integer N and on the next N lines read an array of integers. Then, find its largest element.

---
+++ Exercises Arrays Simple Array Processing/01. Largest Element in Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.Largest_Element_in_Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+
+            int largest = int.MinValue;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+                if (largest < numbers[i])
+                {
+                    largest = numbers[i];
+                }
+            }
+
+            Console.WriteLine(largest);
+        }
+    }
+}
