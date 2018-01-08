9f2e54f2213ec902f2153cb630afb7bf624f7680
pirocorp <pirocorp@abv.bg>
2018-01-08 20:27:39 +0100

    Write a program that finds the most frequent number in a given sequence of numbers. • Numbers will be in the range [0…65535]. • In case of multiple numbers with the same maximal frequency, print the leftmost of them.

---
+++ Exercises Arrays Simple Array Processing/25. Most Frequent Number/Most Frequent Number.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25.Most_Frequent_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimeterList = { ' ' };
+            int[] numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int bestNumber = numbers[0];
+            int bestCount = 1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        count++;
+                    }
+                }
+
+                if (count > bestCount)
+                {
+                    bestCount = count;
+                    bestNumber = numbers[i];
+                }
+            }
+
+            Console.WriteLine(bestNumber);
+        }
+    }
+}
