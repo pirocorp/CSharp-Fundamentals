ae3607d52c259324ee4c285903b72d300734ff79
pirocorp <pirocorp@abv.bg>
2018-01-08 11:17:10 +0100

    You will receive an array of integers, holding the plants’ power level. If a plant’s power reaches 0, it dies. The plants get more and more withered each day, and so – their power decreases by 1, except for one special plant, which blooms instead. The special plant which blooms is the one whose index corresponds to the current day. Its power is increased by 1, but only if it isn’t dead (but since it has withered the same day, realistically its power stays the same). When a season ends, all plants which are still alive bloom. The season ends when the Nth day is reached, N being the total number of plants. Your task is to calculate how many days and seasons it will take for all the plants to die. Print them on the console in the following format: “survived {days} days ({seasons} seasons)”

---
+++ Exercises Arrays Simple Array Processing/16. Power Plants/Power Plants.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+
+namespace _16.Power_Plants
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = {' '};
+            int[] plantsPowerLevel = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int days = 0;
+
+            while (true)
+            {
+                days += AllDaysInSeason(plantsPowerLevel);
+                
+                if (IfAllDeath(plantsPowerLevel))
+                {
+                    break;
+                }
+
+                EndOfSeason(plantsPowerLevel);
+            }
+
+            int seasons = days / plantsPowerLevel.Length;
+
+            if (days % plantsPowerLevel.Length == 0)
+            {
+                seasons--;
+            }
+
+            string seasonString = String.Empty;
+
+            if (seasons == 1)
+            {
+                seasonString = "season";
+            }
+            else
+            {
+                seasonString = "seasons";
+            }
+
+            Console.WriteLine($"survived {days} days ({seasons} {seasonString})");
+        }
+
+        private static void EndOfSeason(int[] plantsPowerLevel)
+        {
+            for (int plant = 0; plant < plantsPowerLevel.Length; plant++)
+            {
+                if (plantsPowerLevel[plant] > 0) plantsPowerLevel[plant]++;
+            }
+        }
+
+        private static int AllDaysInSeason(int[] plantsPowerLevel)
+        {
+            int days = -1;
+            for (int dayInSeason = 0; dayInSeason < plantsPowerLevel.Length; dayInSeason++)
+            {
+                days++;
+                for (int plant = 0; plant < plantsPowerLevel.Length; plant++)
+                {
+                    if (plant != dayInSeason) plantsPowerLevel[plant]--;
+                }
+
+                if (IfAllDeath(plantsPowerLevel)) return days + 1;
+            }
+
+            return days + 1;
+        }
+
+        private static bool IfAllDeath(int[] plantsPowerLevel)
+        {
+            bool isAllDeath = true;
+            for (int i = 0; i < plantsPowerLevel.Length; i++)
+            {
+                if (plantsPowerLevel[i] > 0)
+                {
+                    isAllDeath = false;
+                    return isAllDeath;
+                }
+            }
+
+            return isAllDeath;
+        }
+    }
+}
