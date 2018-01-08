757a9d0a28b99edbd039bf836e5c118a2122c494
pirocorp <pirocorp@abv.bg>
2018-01-08 19:31:58 +0100

    Write a program to find all prime numbers in range [1…n]. Implement the algorithm called “Sieve of Eratosthenes”: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes. Steps in the “Sieve of Eratosthenes” algorithm: 1. Assign primes[0…n] = true 2. Assign primes[0] = primes[1] = false 3. Find the smallest p, which holds primes[p] = true • Print p (it is prime) • Assign primes[2*p] = primes[3*p] = primes[4*p] = … = false 4. Repeat for the next smallest p < n.

---
+++ Exercises Arrays Simple Array Processing/21. Sieve of Eratosthenes/Sieve of Eratosthenes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _21.Sieve_of_Eratosthenes
+{
+    class Program
+    {
+        static void Main()
+        {
+            long n = long.Parse(Console.ReadLine());
+            n++;
+            bool[] primes = new bool[n];
+            InitializeBoolArray(primes);
+            primes[0] = false;
+            primes[1] = false;
+            FindAllPrimes(n, primes);
+            PrintAllPrimes(primes);
+            Console.WriteLine();
+        }
+
+        private static void PrintAllPrimes(bool[] primes)
+        {
+            for (long i = 2; i < primes.Length; i++)
+            {
+                if(primes[i])
+                Console.Write(i + " ");
+            }
+        }
+
+        private static void FindAllPrimes(long n, bool[] primes)
+        {
+            for (long i = 2; i < Math.Sqrt(n); i++)
+            {
+                if (primes[i])
+                {
+                    for (long j = i * i; j < n; j += i)
+                    {
+                        primes[j] = false;
+                    }
+                }
+            }
+        }
+
+        private static void InitializeBoolArray(bool[] primes)
+        {
+            for (long i = 0; i < primes.Length; i++)
+            {
+                primes[i] = true;
+            }
+        }
+    }
+}
