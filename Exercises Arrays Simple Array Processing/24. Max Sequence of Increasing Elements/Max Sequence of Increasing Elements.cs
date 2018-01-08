248ba816cc2d0a3654487bc0e83c06936541310e
pirocorp <pirocorp@abv.bg>
2018-01-08 20:18:31 +0100

    Write a program that finds the longest increasing subsequence in an array of integers. The longest increasing subsequence is a portion of the array (subsequence) that is strongly increasing and has the longest possible length. If several such subsequences exist, find the left most of them.

---
+++ Exercises Arrays Simple Array Processing/24. Max Sequence of Increasing Elements/Max Sequence of Increasing Elements.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23.Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimeterList = { ' ' };
+            int[] numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int lenth = 1;
+            int position = 0;
+            int bestLenth = 1;
+            int bestPosition = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1] > numbers[i])
+                {
+                    lenth++;
+                }
+                else
+                {
+                    position = i;
+                    lenth = 1;
+                }
+
+                if (lenth > bestLenth)
+                {
+                    bestLenth = lenth;
+                    bestPosition = position;
+
+                }
+            }
+
+            for (int i = bestPosition; i < bestPosition + bestLenth; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
