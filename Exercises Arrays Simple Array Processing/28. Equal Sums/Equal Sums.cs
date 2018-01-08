eb010a380adbe1c09e213549bb0c22406b07ad45
pirocorp <pirocorp@abv.bg>
2018-01-08 21:06:12 +0100

    Write a program that determines if there exists an element in the array such that the sum of the elements on its left is equal to the sum of the elements on its right. If there are no elements to the left / right, their sum is considered to be 0. Print the index that satisfies the required condition or “no” if there is no such index.

---
+++ Exercises Arrays Simple Array Processing/28. Equal Sums/Equal Sums.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28.Equal_Sums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimeterList = { ' ' };
+            int[] numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int leftSum = LeftSum(i, numbers);
+                int rightSum = RightSum(i, numbers);
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+
+            Console.WriteLine("no");
+        }
+
+        private static int RightSum(int position, int[] numbers)
+        {
+            int sum = 0;
+            for (int i = numbers.Length - 1; i > position; i--)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+
+        private static int LeftSum(int position, int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < position; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+    }
+}
