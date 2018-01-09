e1a5ee93da58ad394a9a175422021d375c8c7d24
pirocorp <pirocorp@abv.bg>
2018-01-09 15:58:52 +0100

    For this task, you can use your solution from Inventory Matcher. You will again receive 3 arrays – one with strings, one with longs and one with decimals. Again, the price and quantity correspond to a name, which is located on same index as the name. This time only the arrays containing the names and the array containing the prices will have the same length. If in the quantities array there is no index, which corresponds to the name, you should assume the quantity is 0. On top of that the products, which you receive after the arrays will contain not only a string for the name, but also a long, which is the quantity that must be ordered. If you have enough quantity, calculate the total price by multiplying the ordered quantity times the price and print it in the following format: {product name} x {quantity ordered} costs {total price of the order} Format the price to the 2nd decimal place. Do not forget to decrease the quantity of the product. If you do not have enough quantities print: We do not have enough {product name}

---
+++ Exercises Arrays Simple Array Processing/36. Upgraded Matcher/Upgraded Matcher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _36.Upgraded_Matcher
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+
+            string[] names = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            long[] quantities = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToArray();
+
+            decimal[] prices = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToArray();
+
+            string[] product = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            while (product[0] != "done")
+            {
+                long index = Array.IndexOf(names, product[0]);
+                long currentQuantaty = long.Parse(product[1]);
+
+                if (index >= quantities.Length || index < 0)
+                {
+                    Console.WriteLine($"We do not have enough {product[0]}");
+                }
+                else
+                {
+                    if (quantities[index] < currentQuantaty)
+                    {
+                        Console.WriteLine($"We do not have enough {product[0]}");
+                    }
+                    else
+                    {
+                        quantities[index] -= currentQuantaty;
+                        decimal price = currentQuantaty * prices[index];
+                        Console.WriteLine($"{product[0]} x {currentQuantaty} costs {price:f2}");
+                    }
+                }
+                product = Console.ReadLine()
+                    .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
