a3b913edf41ed6a4867be8d4d6b9a71268123435
pirocorp <pirocorp@abv.bg>
2018-01-09 16:13:12 +0100

    You will receive an integer array from the console. You start from the beginning of the array and try to move right by a step, equal to the value at position 0. If that is possible you should collect the value from the index on which you landed, and try to move to the right by its value, if that is not possible – try to move to the left. If that is also not possible stop the program and print the sum of the collected values. Example: Example: We have the array [3 7 12 2 10]. We start from 3 and move 3 indices to 2. We have to move 2 indices, but we can’t move to the right, so we move to the left to 7. From there we cannot move anywhere and we stop the program and we print the sum of the collected cells: 3 + 2 + 7 = 12

---
+++ Exercises Arrays Simple Array Processing/37. Jump Around/Jump Around.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _37.Jump_Around
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            int[] numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sumOfPositions = 0;
+            int currentIndex = 0;
+
+            while (true)
+            {
+                sumOfPositions += numbers[currentIndex];
+                bool moveRight = currentIndex + numbers[currentIndex] < numbers.Length &&
+                                 currentIndex + numbers[currentIndex] >= 0;
+
+                bool moveLeft = currentIndex - numbers[currentIndex] < numbers.Length &&
+                                currentIndex - numbers[currentIndex] >= 0;
+                if (moveRight)
+                {
+                    currentIndex += numbers[currentIndex];
+                }
+                else if (moveLeft)
+                {
+                    currentIndex -= numbers[currentIndex];
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(sumOfPositions);
+        }
+    }
+}
