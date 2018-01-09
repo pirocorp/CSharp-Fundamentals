daa8c71ba63dafc2d0cabffe0e3901b336ddb6f9
pirocorp <pirocorp@abv.bg>
2018-01-09 12:40:39 +0100

    You will receive an array of strings and you have to execute some command upon it. You can receive three types of commands: • Reverse – reverse the array • Distinct – delete all non-unique elements from the array • Replace {index} {string} – replace the element at the given index with the string, which will be given to you

---
+++ Exercises Arrays Simple Array Processing/30. Manipulate Array/Manipulate Array.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30.Manipulate_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimeterList = { ' ' };
+            string[] array = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            int n = int.Parse(Console.ReadLine());
+            string[] command;
+
+            for (int i = 0; i < n; i++)
+            {
+                command = Console.ReadLine().Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+                switch (command[0])
+                {
+                    case "Reverse":
+                        Reverse(array);
+                        break;
+                    case "Distinct":
+                        Distinct(array);
+                        break;
+                    case "Replace":
+                        Replace(array, int.Parse(command[1]), command[2]);
+                        break;
+                }
+            }
+
+            string result = String.Join(" ", array);
+            array = result.Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            result = String.Join(", ", array);
+            Console.WriteLine(result);
+        }
+
+        private static void Replace(string[] array, int index, string element)
+        {
+            array[index] = element;
+        }
+
+        private static void Distinct(string[] array)
+        {
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j] && j != i)
+                    {
+                        array[j] = string.Empty;
+                    }
+                }
+            }
+        }
+
+        private static void Reverse(string[] array)
+        {
+            string currentElement = string.Empty;
+            int n = array.Length / 2;
+            for (int i = 0; i < n; i++)
+            {
+                currentElement = array[i];
+                array[i] = array[array.Length - (1 + i)];
+                array[array.Length - (1 + i)] = currentElement;
+            }
+        }
+    }
+}
