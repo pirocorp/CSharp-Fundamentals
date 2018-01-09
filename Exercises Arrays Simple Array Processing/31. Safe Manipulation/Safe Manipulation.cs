bd4c3a943b4e11a072258d43dd9bd9fa03f327f7
pirocorp <pirocorp@abv.bg>
2018-01-09 13:20:08 +0100

    Now we need to make our program safer and more user-friendly. Make the program print “Invalid input!” if we try to replace an element at a non-existent index or an invalid command is written on the console. Also make the program work until the command “END” is given as an input.
    Both solutions with and without integrated methods;

---
+++ Exercises Arrays Simple Array Processing/31. Safe Manipulation/Safe Manipulation.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31.Safe_Manipulation
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            string[] array = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            //int n = int.Parse(Console.ReadLine());
+            string[] command = Console.ReadLine().Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            while(command[0] != "END")
+            {
+                switch (command[0])
+                {
+                    case "Reverse":
+                        Reverse(array);
+                        //array = array.Reverse().ToArray();
+                        break;
+                    case "Distinct":
+                        array = Distinct(array);
+                        //array = array.Distinct().ToArray();
+                        break;
+                    case "Replace":
+                        int index = int.Parse(command[1]);
+
+                        if (index < 0 || index >= array.Length)
+                        {
+                            Console.WriteLine("Invalid input!");
+                            break;
+                        }
+
+                        Replace(array, index, command[2]);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input!");
+                        break;
+                }
+
+                command = Console.ReadLine().Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            string result = String.Join(" ", array);
+            array = result.Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            result = String.Join(", ", array);
+            Console.WriteLine(result);
+        }
+        
+        private static void Replace(string[] array, int index, string element)
+        {
+            array[index] = element;
+        }
+
+        private static string[] Distinct(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j] && j != i)
+                    {
+                        array[j] = string.Empty;
+                    }
+                }
+            }
+
+            char[] delimeterList = { ' ' };
+            string result = String.Join(" ", array);
+            array = result.Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            return array;
+        }
+
+        private static void Reverse(string[] array)
+        {
+            string currentElement = string.Empty;
+            int n = array.Length / 2;
+            for (int i = 0; i < n; i++)
+            {
+                currentElement = array[i];
+                array[i] = array[array.Length - (1 + i)];
+                array[array.Length - (1 + i)] = currentElement;
+            }
+        }
+    }
+}
