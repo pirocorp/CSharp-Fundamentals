7ee246b763651be56b03376eb8580623322a12b8
pirocorp <pirocorp@abv.bg>
2018-01-09 14:50:51 +0100

    You manage your own pizza restaurant and you are in charge of the orders. Your pizza is made only from ingredients, which have a specific length. On the first line, you will receive an array of strings with the possible ingredients. On the next line, you will receive an integer, which represents the length of the strings, which we will used in the recipe. Your recipe should not use more than 10 ingredients. If you collect 10 ingredients stop the program and print the result.

---
+++ Exercises Arrays Simple Array Processing/33. Pizza Ingredients/Pizza Ingredients.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33.Pizza_Ingredients
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            string[] array = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            int length = int.Parse(Console.ReadLine());
+            int ingridiants = 0;
+            string stringResult = string.Empty;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length == length)
+                {
+                    Console.WriteLine($"Adding {array[i]}.");
+                    ingridiants++;
+                    stringResult += array[i] + " ";
+                    if (ingridiants == 10)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            string[] result = stringResult.Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            string resultedString = String.Join(", ", result);
+            Console.WriteLine($"Made pizza with total of {ingridiants} ingredients.");
+            Console.WriteLine($"The ingredients are: {resultedString}.");
+        }
+    }
+}
