0b102d185c03a20fe6df981d2bf405d5f631257d
pirocorp <pirocorp@abv.bg>
2018-01-09 15:11:49 +0100

    You are the main accountant for a group of bandits, whose main line of work is robbing banks and stores. Your job is to calculate the score from the heist, calculating the price of the loot and taking the expenses into account. On the first line, you will receive an array with two elements. The first element represents the price of the jewels and the second – the price of the gold. On each of the next lines, you will receive input in the format “{loot} {heist expenses}” until you receive the command “Jail Time”. The loot will be a string containing random characters. The jewels will be represented with the character “%” and the gold – with the character “$”. If you find either from the symbols it means you have found one of the goodies. Upon receiving “Jail Time” you have to calculate the total earnings and the total expenses from the heists. If the total earnings are more or equal to the total expenses print: “Heists will continue. Total earnings: {money earned}.”. Otherwise print: “Have to find another job. Lost: {money lost}.”.

---
+++ Exercises Arrays Simple Array Processing/34. Heists/Heists.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34.Heists
+{
+    class Heists
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            long[] numbers = Console.ReadLine() // jewels - % gold - $
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToArray();
+
+            string[] command = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            long totalExpenses = 0;
+            long totalIncome = 0;
+
+            while (command[0] != "Jail" && command[1] != "Time")
+            {
+                totalExpenses += long.Parse(command[1]);
+
+                for (int i = 0; i < command[0].Length; i++)
+                {
+                    if (command[0][i] == '%')
+                    {
+                        totalIncome += numbers[0];
+                    }
+                    else if (command[0][i] == '$')
+                    {
+                        totalIncome += numbers[1];
+                    }
+                }
+
+                command = Console.ReadLine()
+                    .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (totalIncome >= totalExpenses)
+            {
+                long total = totalIncome - totalExpenses;
+                Console.WriteLine($"Heists will continue. Total earnings: {total}.");
+            }
+            else
+            {
+                long lost = totalExpenses - totalIncome;
+                Console.WriteLine($"Have to find another job. Lost: {lost}.");
+            }
+        }
+    }
+}
