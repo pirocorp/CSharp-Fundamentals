7a26484d8a4ad9fab41f393edc32b258d333772a
pirocorp <pirocorp@abv.bg>
2018-01-09 14:36:53 +0100

    Write a program, which receives an array of integers and an integer as input. Find the last occurrence of the integer in the given array and print the sum of all elements before the number. Example: if we receive the array 10 20 30 40 20 30 40 and we receive on the next line the integer 20 we have to print the sum the elements 10 20 30 40, which is 100. If no such number exists in the array – print “No occurrences were found!”.

---
+++ Exercises Arrays Simple Array Processing/32. Grab and Go/Grab and Go.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _32.Grab_and_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimeterList = { ' ' };
+            long[] numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToArray();
+
+            long lastOccurr = long.Parse(Console.ReadLine());
+            int lastOccurIndex = Array.LastIndexOf(numbers, lastOccurr);
+            if (lastOccurIndex > 0)
+            {
+                var newArray = numbers.Take(lastOccurIndex).ToArray();
+                var sumOfElements = newArray.Sum();
+                Console.WriteLine(sumOfElements);
+            }
+            else
+            {
+                Console.WriteLine("No occurrences were found!");
+            }
+        }
+    }
+}
