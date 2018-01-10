614ece44a1a3c2ee7ac96382ffc60e27c90854ef
pirocorp <pirocorp@abv.bg>
2018-01-10 18:04:57 +0100

    Read a list of integers in range [0…1000] and print them in ascending order along with their number of occurrences

--- 07. Lists - Lab/07. Count Numbers/Count Numbers.cs
+++ 07. Lists - Lab/07. Count Numbers/Count Numbers.cs
@@ -10,7 +10,28 @@ namespace _07.Count_Numbers
     {
         static void Main()
         {
+            char[] delimeterList = { ' ' };
+            var numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
 
+            numbers.Sort();
+            int count = 1;
+            for (int i = 0; i < numbers.Count-1; i++)
+            {
+                if (numbers[i] == numbers[i+1] && i < numbers.Count -1)
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine($"{numbers[i]} -> {count}");
+                    count = 1;
+                }
+            }
+
+            Console.WriteLine($"{numbers[numbers.Count - 1]} -> {count}");
         }
     }
 }
