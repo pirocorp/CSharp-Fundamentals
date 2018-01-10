350d8d2691bbdc4f0bb8279cfa54b1806213f826
pirocorp <pirocorp@abv.bg>
2018-01-11 00:17:01 +0100

    On the first line, you will receive a list of integers. On the next line, you will receive an array with exactly three numbers. First number represents the number of elements you have to take from the list (starting from the first one). Second number represents the number of elements you have to delete from the numbers you took (starting from the first one). Last number is the number we search in our collection after the manipulations. If it is present print: “YES!”, otherwise print “NO!”.

---
+++ 07. Lists - Exercises/03. Search for a Number/Search for a Number.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Search_for_a_Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var command = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> sublist = numbers.GetRange(0, command[0]);
+
+            while (command[1] > 0)
+            {
+                sublist.RemoveAt(command[1] -1);
+                command[1]--;
+            }
+
+            if (sublist.Exists(x => x == command[2]))
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                Console.WriteLine("NO!");
+            }
+        }
+    }
+}
