dbd662c926bd9729b72248a15088aeab5333029d
pirocorp <pirocorp@abv.bg>
2018-01-10 16:44:07 +0100

    Write a program to append several lists of numbers.  Lists are separated by ‘|’.  Values are separated by spaces (‘ ’, one or several)  Order the lists from the last to the first, and their values from left to right.

---
+++ 07. Lists - Lab/02. Append Lists/Append Lists.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Append_Lists
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { '|' };
+            char[] delimeterList2 = { ' ' };
+            var stringsOfLists = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            List <List<int>> listOfLists = new List<List<int>>();
+
+            for (int i = stringsOfLists.Length - 1; i >= 0; i--)
+            {
+                listOfLists.Add(stringsOfLists[i].Split(delimeterList2, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+
+            string result = String.Empty;
+
+            for (int i = 0; i < listOfLists.Count; i++)
+            {
+                result += String.Join(" ", listOfLists[i]);
+                result += " ";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
