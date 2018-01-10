e8f1943456f779ecf7c26dab30f8127b90053874
pirocorp <pirocorp@abv.bg>
2018-01-10 19:46:12 +0100

    Read a list of integers and find the longest sequence of equal elements. If several exist, print the leftmost.

---
+++ 07. Lists - Exercises/01. Max Sequence of Equal Elements/Max Sequence of Equal Elements.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int lenth = 1;
+            int position = 0;
+            int bestLenth = 1;
+            int bestPosition = 0;
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i - 1] == numbers[i])
+                {
+                    lenth++;
+                }
+                else
+                {
+                    position = i;
+                    lenth = 1;
+                }
+
+                if (lenth > bestLenth)
+                {
+                    bestLenth = lenth;
+                    bestPosition = position;
+
+                }
+            }
+
+            string result = String.Join(" ", numbers.Select(i => i.ToString()).ToArray(), bestPosition, bestLenth);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
