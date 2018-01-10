26dc2a538709defa524f532a3a381b3836b533fd
pirocorp <pirocorp@abv.bg>
2018-01-10 17:08:25 +0100

    Write a program to sum all adjacent equal numbers in a list of decimal numbers, starting from left to right.  After two numbers are summed, the obtained result could be equal to some of its neighbors and should be summed as well (see the examples below).  Always sum the leftmost two equal neighbors (if several couples of equal neighbors are available).

---
+++ 07. Lists - Lab/03. Sum Adjacent Equal Numbers/Sum Adjacent Equal Numbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Sum_Adjacent_Equal_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToList();
+
+            while (true)
+            {
+                int indexOfFirstNumber = GetAdjacentIndex(numbers);
+                if (indexOfFirstNumber > -1)
+                {
+                    numbers.RemoveAt(indexOfFirstNumber + 1);
+                    numbers[indexOfFirstNumber] += numbers[indexOfFirstNumber];
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        private static int GetAdjacentIndex(List<decimal> numbers)
+        {
+            for (int i = 0; i < numbers.Count -1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
