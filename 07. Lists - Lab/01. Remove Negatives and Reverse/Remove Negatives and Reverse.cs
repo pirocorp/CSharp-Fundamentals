36a36b5e377580c6e486c810f22fad7738f9ce79
pirocorp <pirocorp@abv.bg>
2018-01-10 16:20:01 +0100

    Read a list of integers, remove all negative numbers from it and print the remaining elements in reversed order. In case of no elements left in the list, print “empty”.

--- 07. Lists - Lab/01. Remove Negatives and Reverse/Remove Negatives and Reverse.cs
+++ 07. Lists - Lab/01. Remove Negatives and Reverse/Remove Negatives and Reverse.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01.Remove_Negatives_and_Reverse
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            char[] delimeterList = {' '};
+            var numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            numbers.RemoveAll(IsNegative);
+            numbers.Reverse();
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(" ", numbers));
+            }
+        }
+
+        private static bool IsNegative(int number)
+        {
+            if (number < 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
