cbd42f59b302436c62aeaad4db3301d0d178f27b
pirocorp <pirocorp@abv.bg>
2018-01-10 17:32:01 +0100

    Read a text, split it into words and distribute them into 3 lists.  Lower-case words like “programming”, “at” and “databases” – consist of lowercase letters only.  Upper-case words like “PHP”, “JS” and “SQL” – consist of uppercase letters only.  Mixed-case words like “C#”, “SoftUni” and “Java” – all others. Use the following separators between the words: , ; : . ! ( ) " ' \ / [ ] space Print the 3 lists as shown in the example below.

---
+++ 07. Lists - Lab/04. Split by Word Casing/Split by Word Casing.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.Split_by_Word_Casing
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = {',', ';', ':', '.', '!', '(', ')', '"', '\'', '\\', '/', '[', ']', ' ' };
+            var words = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            var lowerCaseWords = new List<string>();
+            var mixedCaseWords = new List<string>();
+            var upperCaseWords = new List<string>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (AllUpperCase(words[i]))
+                {
+                    upperCaseWords.Add(words[i]);
+                }
+                else if (AllLowerCase(words[i]))
+                {
+                    lowerCaseWords.Add(words[i]);
+                }
+                else
+                {
+                    mixedCaseWords.Add(words[i]);
+                }
+            }
+
+            Console.WriteLine($"Lower-case: {String.Join(", ", lowerCaseWords)}");
+            Console.WriteLine($"Mixed-case: {String.Join(", ", mixedCaseWords)}");
+            Console.WriteLine($"Upper-case: {String.Join(", ", upperCaseWords)}");
+        }
+
+        private static bool AllLowerCase(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] < 'a' || word[i] > 'z')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool AllUpperCase(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] < 'A' || word[i] > 'Z')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
