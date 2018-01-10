78db6b60233eb4cb2c2d808117f833f631f5cf5d
pirocorp <pirocorp@abv.bg>
2018-01-10 17:36:42 +0100

    Read a list of decimal numbers and sort them in increasing order. Print the output as shown in the examples below.

---
+++ 07. Lists - Lab/05. Sort Numbers/Sort Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.Sort_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToList();
+
+            numbers.Sort();
+
+            Console.WriteLine($"{String.Join(" <= ", numbers)}");
+        }
+    }
+}
