0324bf52f10177396fba1c826a4e5bbfa43d6c04
pirocorp <pirocorp@abv.bg>
2018-01-10 17:43:53 +0100

    Read a list of integers and extract all square numbers from it and print them in descending order. A square number is an integer which is the square of any integer. For example, 1, 4, 9, 16 are square numbers.

--- 07. Lists - Lab/06. Square Numbers/Program.cs
+++ 07. Lists - Lab/06. Square Numbers/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _06.Square_Numbers
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            char[] delimeterList = { ' ' };
+            var numbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var result = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (Math.Sqrt(numbers[i]) == (int) Math.Sqrt(numbers[i]))
+                {
+                    result.Add(numbers[i]);
+                }
+            }
+
+            result.Sort();
+            result.Reverse();
+            Console.WriteLine(String.Join(" ", result));
         }
     }
 }
