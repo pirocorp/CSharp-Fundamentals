db06153a763b2a7eeedd08fbd23d62dca35e3714
pirocorp <pirocorp@abv.bg>
2018-01-11 20:08:45 +0100

    Write a program that reads sequence of numbers and special bomb number with a certain power. Your task is to detonate every occurrence of the special bomb number and according to its power his neighbors from left and right. Detonations are performed from left to right and all detonated numbers disappear. Finally print the sum of the remaining elements in the sequence.

---
+++ 07. Lists - Exercises/07. Bomb Numbers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Bomb_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var numbersSequence = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var bombSequence = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int bomb = bombSequence[0];
+            int bombPower = bombSequence[1];
+
+            int bombIndex = numbersSequence.IndexOf(bomb);
+
+            while (bombIndex >= 0)
+            {
+                int bombStartIndex = bombIndex - bombPower;
+                if (bombStartIndex < 0)
+                {
+                    bombStartIndex = 0;
+                }
+
+                int bombEndIndex = bombIndex + bombPower;
+                if (bombEndIndex >= numbersSequence.Count)
+                {
+                    bombEndIndex = numbersSequence.Count - 1;
+                }
+
+                int boom = bombEndIndex - bombStartIndex + 1;
+                while (boom > 0)
+                {
+                    numbersSequence.RemoveAt(bombStartIndex);
+                    boom--;
+                }
+
+                bombIndex = numbersSequence.IndexOf(bomb);
+            }
+
+            Console.WriteLine(numbersSequence.Sum());
+        }
+    }
+}
