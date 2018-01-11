6472b144158fa57635ae47737ea7ca75d88ceb6b
pirocorp <pirocorp@abv.bg>
2018-01-11 19:49:38 +0100

    Write a program that reads sequence of numbers, reverses their digits, and prints their sum.

---
+++ 07. Lists - Exercises/06. Sum Reversed Numbers/Sum Reversed Numbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Sum_Reversed_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            List<int> nums = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> listOfResults = new List<int>();
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                int number = nums[i];
+                int result = 0;
+                
+
+                while (number != 0)
+                {
+                    result *= 10;
+                    result += number % 10;
+                    number /= 10;
+                }
+
+                listOfResults.Add(result);
+            }
+
+            Console.WriteLine(listOfResults.Sum());
+        }
+    }
+}
