9d352995e2e1f12142bedfc4cdd513c797322495
pirocorp <pirocorp@abv.bg>
2018-01-11 19:37:26 +0100

    Write a program that reads an array of integers from the console and set of commands and executes them over the array. The commands are as follows: • add <index> <element> – adds element at the specified index (elements right from this position inclusively are shifted to the right). • addMany <index> <element 1> <element 2> … <element n> – adds a set of elements at the specified index. • contains <element> – prints the index of the first occurrence of the specified element (if exists) in the array or -1 if the element is not found. • remove <index> – removes the element at the specified index. • shift <positions> – shifts every element of the array the number of positions to the left (with rotation). o For example, [1, 2, 3, 4, 5] -> shift 2 -> [3, 4, 5, 1, 2] • sumPairs – sums the elements in the array by pairs (first + second, third + fourth, …). o For example, [1, 2, 4, 5, 6, 7, 8] -> [3, 9, 13, 8]. • print – stop receiving more commands and print the last state of the array.

---
+++ 07. Lists - Exercises/05. Array Manipulator/Array Manipulator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace _05.Array_Manipulator
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var nums = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var command = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "print")
+            {
+                switch (command[0])
+                {
+                    case "add":
+                        nums.Insert(int.Parse(command[1]), int.Parse(command[2]));
+                        break;
+                    case "addMany":
+                        AddMany(command, nums);
+                        break;
+                    case "contains":
+                        Console.WriteLine(nums.IndexOf(int.Parse(command[1])));
+                        break;
+                    case "remove":
+                        nums.RemoveAt(int.Parse(command[1]));
+                        break;
+                    case "shift":
+                        ShiftAtLeft(int.Parse(command[1]), nums);
+                        break;
+                    case "sumPairs":
+                        nums = SumPairs(nums);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command");
+                        break;
+                }
+
+                command = Console.ReadLine()
+                    .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            }
+            
+            Console.WriteLine($"[{String.Join(", ", nums)}]");
+        }
+
+        private static List<int> SumPairs(List<int> nums)
+        {
+            List<int> result = new List<int>();
+
+            if (nums.Count % 2 == 0)
+            {
+                for (int i = 0; i < nums.Count; i += 2)
+                {
+                    result.Add(nums[i] + nums[i + 1]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < nums.Count - 1; i += 2)
+                {
+                    result.Add(nums[i] + nums[i + 1]);
+                }
+
+                result.Add(nums[nums.Count-1]);
+            }
+
+            return result;
+        }
+
+        private static void ShiftAtLeft(int positions, List<int> nums)
+        {
+            for (int i = 0; i < positions % nums.Count; i++)
+            {
+                int swap = nums[0];
+                nums.RemoveAt(0);
+                nums.Insert(nums.Count, swap);
+            }
+        }
+
+        private static void AddMany(string[] command, List<int> nums)
+        {
+            int index = int.Parse(command[1]);
+            for (int i = command.Length -1; i >= 2; i--)
+            {
+                int element = int.Parse(command[i]);
+                nums.Insert(index, element);
+            }
+        }
+    }
+}
