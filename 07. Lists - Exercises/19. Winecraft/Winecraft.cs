d5b1b725b3b021c9d61fbd4ab8b685f186d880bd
pirocorp <pirocorp@abv.bg>
2018-01-12 18:58:04 +0100

    You will be given a sequence of integers, which will represent grapes. On the next line you will be given N - an integer, indicating the growth days. You must increment every integer in the list by 1 N times. However, if one of the grapes’ value is greater than the grape to its left and is also greater than the one to his right, it should steal the values, which would have been incremented to its neighbors, and add them to itself, instead of being incremented by 1 like normal (note: if any of the greater grapes’ neighboring grapes have a value of 0, DON’T decrement it and DON’T add its value to the greater grape). Lesser grapes don’t get incremented, but instead they have their values decremented by 1 by their neighboring greater grapes, therefore their values get added to the greater grapes. After you're done with the growing (processed the grapes N times), every grape which has a value, lower than N should be removed. The process should then repeat, again incrementing everything N times, where the greater grapes steal from the lesser grapes, until your list contains less than N grapes. After that, print the remaining grapes on the console (one line, space-separated).

---
+++ 07. Lists - Exercises/19. Winecraft/Winecraft.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Winecraft
+{
+    class Winecraft
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var elements = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int growthDays = int.Parse(Console.ReadLine());
+            var elementsMap = new int[elements.Count];
+
+            while (elements.Count >= growthDays)
+            {
+                for (int i = 0; i < growthDays; i++)
+                {
+                    elementsMap = MapElements(elements);
+                    CalculateResultElements(elements, elementsMap);
+                }
+
+                RemoveAllElements(elements, growthDays);
+            }
+
+            Console.WriteLine(String.Join(" ", elements));
+
+        }
+
+        private static void RemoveAllElements(List<int> elements, int growthDays)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i] <= growthDays)
+                {
+                    elements.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        private static void CalculateResultElements(List<int> elements, int[] elementsMap)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elementsMap[i] == 1)
+                {
+                    elements[i] += 1;
+                    if (elements[i - 1] > 0)
+                    {
+                        elements[i - 1] -= 1;
+                        elements[i] += 1;
+                    }
+
+                    if (elements[i + 1] > 0)
+                    {
+                        elements[i + 1] -= 1;
+                        elements[i] += 1;
+                    }
+                }
+                else if (elementsMap[i] == 0)
+                {
+                    elements[i]++;
+                }
+            }
+        }
+
+        private static int[] MapElements(List<int> elements)
+        {
+            var elementsMap = new int [elements.Count];
+            for (int i = 0; i < elements.Count; i++)
+            {
+                bool leftElementExist = i - 1 >= 0;
+                bool rightElementExist = i + 1 < elements.Count;
+
+                if (leftElementExist && rightElementExist && elements[i - 1] < elements[i] &&
+                    elements[i + 1] < elements[i])
+                {
+                    elementsMap[i - 1] = -1;
+                    elementsMap[i] = 1;
+                    elementsMap[i + 1] = -1;
+                }
+
+            }
+
+            return elementsMap;
+        }
+    }
+}
