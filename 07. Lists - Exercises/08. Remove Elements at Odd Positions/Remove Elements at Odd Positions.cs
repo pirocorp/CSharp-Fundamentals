63acb75ae174a4dedb3d2d1a004e2b6f25dc0557
pirocorp <pirocorp@abv.bg>
2018-01-12 10:09:13 +0100

    Write a program which reads a list of space-separated strings and removes the elements at positions [1…3…5…7…etc.]. After that, print the elements with no delimiter. Note that positions are counted from 1.

--- 07. Lists - Exercises/08. Remove Elements at Odd Positions/Remove Elements at Odd Positions.cs
+++ 07. Lists - Exercises/08. Remove Elements at Odd Positions/Remove Elements at Odd Positions.cs
@@ -10,7 +10,21 @@ namespace _08.Remove_Elements_at_Odd_Positions
     {
         static void Main()
         {
-             
+            char[] delimeterList = { ' ' };
+            var inputStrings = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            var resultListOfStrings = new List<string>();
+
+            for (int i = 0; i < inputStrings.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    resultListOfStrings.Add(inputStrings[i]);
+                }
+            }
+
+            Console.WriteLine(String.Join(string.Empty, resultListOfStrings));
         }
     }
 }
