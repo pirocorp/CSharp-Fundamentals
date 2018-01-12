d5263c3b4f3a60049fa484f2653ef3dae7641d69
pirocorp <pirocorp@abv.bg>
2018-01-12 14:47:12 +0100

    You will be given a list of integers on the first line of the input (space-separated). Remove all repeating elements from the list.

---
+++ 07. Lists - Exercises/14. Distinct List/Distinct List.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Distinct_List
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var listOfIntegers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List <int> disticntListOfIntegers = listOfIntegers.Distinct().ToList();
+            Console.WriteLine(String.Join(" ", disticntListOfIntegers));
+        }
+    }
+}
