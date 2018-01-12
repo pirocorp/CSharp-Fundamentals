09c93502b1645f0824515729aa2ea82b984a5c34
pirocorp <pirocorp@abv.bg>
2018-01-12 16:30:13 +0100

    In music, certain frequencies correspond to certain musical notes (example: A -> 440hz, C# -> 554.37hz). You will be given frequencies as real numbers on the first line of the input (space-separated). Your task is to convert the numbers to their musical note representation, then separate them into naturals (C, B, F and etc.) and sharp notes (C#, F#, A#, etc.). After that, print both lists in the console in the format described in the examples. After you print them, sum each list’s frequencies and print it on the console, rounded to the second decimal place. Constraints The frequencies in the input will be constrained to this chart:

---
+++ 07. Lists - Exercises/18. Note Statistics/Note Statistics.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Note_Statistics
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            string[] notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+            List<double> notesInHz = new List<double>{261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88 };
+
+            var inputFreq = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            var notesResultList = new List<string>();
+            var stringResultNatural = new List<string>();
+            var stringResultSharp = new List<string>();
+            var naturalsSum = 0.0;
+            var sharpsSum = 0.0;
+
+            for (int i = 0; i < inputFreq.Count; i++)
+            {
+                var currentInputNoteFreq = inputFreq[i];
+                var index = notesInHz.IndexOf(currentInputNoteFreq);
+                var currentNote = notes[index];
+                notesResultList.Add(currentNote);
+
+                //Console.Beep((int)inputFreq[i], 100);
+
+                if (currentNote.Contains("#"))
+                {
+                    stringResultSharp.Add(currentNote);
+                    sharpsSum += inputFreq[i];
+                }
+                else
+                {
+                    stringResultNatural.Add(currentNote);
+                    naturalsSum += inputFreq[i];
+                }
+            }
+
+            Console.WriteLine($"Notes: {String.Join(" ", notesResultList)}");
+            Console.WriteLine($"Naturals: {String.Join(", ", stringResultNatural)}");
+            Console.WriteLine($"Sharps: {String.Join(", ", stringResultSharp)}");
+            Console.WriteLine($"Naturals sum: {naturalsSum}");
+            Console.WriteLine($"Sharps sum: {sharpsSum}");
+            
+        }
+    }
+}
