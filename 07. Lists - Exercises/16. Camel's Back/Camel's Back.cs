cc07a564611ad4a78b834c31ce67628d7c95319c
pirocorp <pirocorp@abv.bg>
2018-01-12 15:16:42 +0100

    The city is breaking down on a camel back. You will receive a sequence of N integers, (space-separated), which will represent the buildings in the city.  You will then receive an integer M, indicating the camel back's size. The camel back is a linear structure standing below the city, in such a way that it has an equal amount of buildings to its left and right. The idea is, if every round – one building falls from the left side of the city, and one from the right side, how many rounds will it take for the city to stop breaking down? As output you must print how many rounds it took before the city stopped breaking down as “{rounds} rounds”. On the next line, print what’s left of the city (space-separated). Format: “remaining: {buildings (space-separated)}” If no buildings have fallen, print “already stable: {buildings (space-separated)}” Example: city with 9 elements; Camel back size: 5 Constraints N and M will always be odd numbers. [1-1000] and M will always be smaller or equal to N.

---
+++ 07. Lists - Exercises/16. Camel's Back/Camel's Back.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.Camel_s_Back
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var listOfIntegers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int camelbackSize = int.Parse(Console.ReadLine());
+            int breakingDown = listOfIntegers.Count - camelbackSize;
+
+            if (breakingDown == 0)
+            {
+                Console.WriteLine($"already stable: {String.Join(" ", listOfIntegers)}");
+                return;
+            }
+
+            int rounds = breakingDown / 2;
+            for (int i = 0; i < rounds; i++)
+            {
+                listOfIntegers.RemoveAt(0);
+                listOfIntegers.RemoveAt(listOfIntegers.Count - 1);
+            }
+
+            Console.WriteLine($"{rounds} rounds");
+            Console.WriteLine($"remaining: {String.Join(" ", listOfIntegers)}");
+        }
+    }
+}
