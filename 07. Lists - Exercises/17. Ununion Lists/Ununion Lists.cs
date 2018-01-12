847783bb13f2a5fdda089ba01dc5af27cb549b17
pirocorp <pirocorp@abv.bg>
2018-01-12 15:33:11 +0100

    You will be given a sequence of integers, separated by a space. This is the primal list. Then you will receive an integer N. On the next N lines, you will receive sequences of integers. Your task is to add all elements that the primal list does not contain from the current sequence to the primal list and then remove from the primal list, all elements which are contained in the current sequence of integers. If there are several occurrences, remove all occurrences you find. After you are done receiving lists and manipulating the primal list, sort the primal list and print it.

---
+++ 07. Lists - Exercises/17. Ununion Lists/Ununion Lists.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Ununion_Lists
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var primalList = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var integersList = Console.ReadLine()
+                    .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+                
+                for (int j = 0; j < integersList.Count; j++)
+                {
+                    var currentItemOfIntegersList = integersList[j];
+                    if (primalList.Contains(currentItemOfIntegersList))
+                    {
+                        primalList.RemoveAll(x => x == currentItemOfIntegersList);
+                    }
+                    else
+                    {
+                        primalList.Add(currentItemOfIntegersList);
+                    }
+                }
+            }
+
+            primalList.Sort();
+            Console.WriteLine(String.Join(" ", primalList));
+        }
+    }
+}
