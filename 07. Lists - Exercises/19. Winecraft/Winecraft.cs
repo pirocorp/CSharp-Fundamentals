1f96989f4520894364132bd06c3e0805a39d92de
pirocorp <pirocorp@abv.bg>
2018-01-12 23:21:18 +0100

    V1.02

--- 07. Lists - Exercises/19. Winecraft/Winecraft.cs
+++ 07. Lists - Exercises/19. Winecraft/Winecraft.cs
@@ -9,33 +9,30 @@
         {
             var grapes = Console.ReadLine()
                 .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(x => Convert.ToInt16(x)).ToArray();
-
-            var rounds = Convert.ToInt16(Console.ReadLine());
+                .Select(x => Convert.ToInt32(x)).ToArray();
+            var rounds = Convert.ToInt32(Console.ReadLine());
             var grapesCounter = grapes.Length;
+            var grapesKind = new Int32[grapesCounter];
 
-            //---spin for each season until the grapes are less than rounds---
+            //---spin for each season until the grapes are less or equal than rounds---
             while (grapesCounter >= rounds)
             {
                 //---count the rounds---
                 for (int counter = 0; counter < rounds; counter++)
                 {
-                    // -1 => lesser, 0 => normal, 1 => greater
-                    var grapesKind = new int[grapes.Length];
-                    for (int index = 1; index < grapes.Length - 1; index++)
-                    {
-                        if (grapes[index] > grapes[index - 1] && grapes[index] > grapes[index + 1])
-                        {
-                            grapesKind[index] = 1;
-                            grapesKind[index - 1] = -1;
-                            grapesKind[index + 1] = -1;
-                            index++;
-                        }
-                    }
-
                     //---calculate the grapes---
                     for (int index = 0; index < grapes.Length; index++)
                     {
+                        // -1 => lesser, 0 => normal, 1 => greater 
+                        for (int kindIndex = 1; kindIndex < grapes.Length - 1; kindIndex++)
+                        {
+                            if (grapes[kindIndex] > grapes[kindIndex - 1] && grapes[kindIndex] > grapes[kindIndex + 1])
+                            {
+                                grapesKind[kindIndex] = 1;
+                                grapesKind[kindIndex - 1] = -1;
+                                grapesKind[kindIndex + 1] = -1;
+                            }
+                        }
                         if (grapesKind[index] == 0 && grapes[index] > 0)
                         {
                             grapes[index]++;
@@ -45,21 +42,19 @@
                             grapes[index]++;
                             if (grapes[index - 1] > 0)
                             {
-                                grapes[index]++;
                                 grapes[index - 1]--;
+                                grapes[index]++;
                             }
                             if (grapes[index + 1] > 0)
                             {
-                                grapes[index]++;
                                 grapes[index + 1]--;
+                                grapes[index]++;
                             }
                             index++;
                         }
                     }
                 }
-
                 grapesCounter = grapes.Length;
-
                 for (int index = 0; index < grapes.Length; index++)
                 {
                     if (grapes[index] <= rounds)
@@ -69,7 +64,7 @@
                     }
                 }
             }
-            Console.WriteLine(string.Join(" ", grapes.Where(x => x > rounds)));
+            Console.WriteLine(string.Join(" ", grapes.Where(x => x > 0)));
         }
     }
 }
