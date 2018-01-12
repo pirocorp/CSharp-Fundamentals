77f1ed6f48f534734eac8eb489c885942968b57f
pirocorp <pirocorp@abv.bg>
2018-01-12 10:55:38 +0100

    You will be given two integer lists on the first two lines (space-separated). Remove the elements in the first list from the elements in the second list. If an element from the first list occurs more than once in the second list, remove all occurrences. After you remove the elements, check the sum of both lists. If the sum of both of them is equal, print “Yes. Sum: {sum}”. If not print the absolute difference between the two lists in the format “No. Diff: {sum}”.

---
+++ 07. Lists - Exercises/10. Equal Sum After Extraction/Equal Sum After Extraction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Equal_Sum_After_Extraction
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var firstList = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var secondList = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            
+            foreach (var currentElementFirstList in firstList)
+            {
+                secondList.Remove(currentElementFirstList);
+            }
+
+            var sumFirstList = firstList.Sum();
+            var sumSecondList = secondList.Sum();
+
+            if (sumFirstList == sumSecondList)
+            {
+                Console.WriteLine($"Yes. Sum: {sumFirstList}");
+            }
+            else
+            {
+                var difference = Math.Abs(sumFirstList - sumSecondList);
+                Console.WriteLine($"No. Diff: {difference}");
+            }
+        }
+    }
+}
