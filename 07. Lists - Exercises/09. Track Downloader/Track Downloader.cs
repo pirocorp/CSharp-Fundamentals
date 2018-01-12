e96ff709dc11d4b190e2183f5abf985b924c1095
pirocorp <pirocorp@abv.bg>
2018-01-12 10:41:42 +0100

    You will receive a list of blacklisted words (on the same line, separated by one space). On the next lines, you will start receiving a list of filenames (as strings) until you receive the string “end”. Your task is to add the filenames to a list and sort them. Ignore the tracks which contain the text in the blacklist.

---
+++ 07. Lists - Exercises/09. Track Downloader/Track Downloader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Track_Downloader
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var blackList = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+
+            string filename = Console.ReadLine();
+            var filenameList = new List<string>();
+
+            while (filename != "end")
+            {
+                if (NotContain(blackList, filename))
+                {
+                    filenameList.Add(filename);
+                }
+
+                filename = Console.ReadLine();
+            }
+
+            filenameList.Sort();
+            Console.WriteLine(String.Join(Environment.NewLine, filenameList));
+        }
+
+        private static bool NotContain(string[] blackList, string filename)
+        {
+            for (int i = 0; i < blackList.Length; i++)
+            {
+                string currentWord = blackList[i];
+                for (int j = 0; j < filename.Length; j++)
+                {
+                    if (filename.Contains(currentWord))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
