566cd25e573896b9de204074a0d97a81d0b5c9b6
pirocorp <pirocorp@abv.bg>
2018-01-12 11:22:42 +0100

    You will receive a list of integers on the first input line (space-separated). Your task is to separate the list into two parts and perform the following operations:

---
+++ 07. Lists - Exercises/12. Tear List in Half/Tear List in Half.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Tear_List_in_Half
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = {' '};
+            var listOfNumbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var leftHalf = listOfNumbers.GetRange(0, listOfNumbers.Count / 2);
+            var rightHalf = listOfNumbers.GetRange(listOfNumbers.Count / 2, listOfNumbers.Count / 2);
+
+            var resultList = new List<int>();
+
+            for (int i = 0; i < leftHalf.Count; i++)
+            {
+                resultList.Add(rightHalf[i] / 10);
+                resultList.Add(leftHalf[i]);
+                resultList.Add(rightHalf[i] % 10);
+            }
+
+            Console.WriteLine(String.Join(" ", resultList));
+        }
+    }
+}
