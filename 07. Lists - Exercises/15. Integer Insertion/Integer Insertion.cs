dfaba138b9a00edd9f46dcf3b366b048841fd73f
pirocorp <pirocorp@abv.bg>
2018-01-12 15:05:02 +0100

    You will receive a list of integers on the same line (separated by one space). On the next lines, you will start receiving a list of strings, until you receive the string “end”. Your task is to insert each string (converted to integer) at a specific index in the list. The index is determined by the first digit of the number. Example: 514  first digit – 5  insert 514 at the 5th index of the list. After you insert all the elements, print the list, separated by single spaces. The input will always be valid and you don’t need to explicitly check if you’re inserting an element into a valid index.

---
+++ 07. Lists - Exercises/15. Integer Insertion/Integer Insertion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Integer_Insertion
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var listOfIntegers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string inputString = Console.ReadLine();
+
+            while (inputString != "end")
+            {
+                string indexOfInsertString = new string(inputString[0], 1);
+                int indexOfInsert = int.Parse(indexOfInsertString);
+                listOfIntegers.Insert(indexOfInsert, int.Parse(inputString));
+                inputString = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(" ", listOfIntegers));
+        }
+    }
+}
