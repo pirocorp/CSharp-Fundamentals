06901dd97293abfcfc2c08179e578817549155b1
pirocorp <pirocorp@abv.bg>
2018-01-12 11:06:51 +0100

    You will receive an integer list on the first line of the input (space-separated). Leave the first, middle and last elements as they are. For every other element, exchange it with its opposite indexed element (list[1]  list[length-2] and so on…). After that, print the list on the console (space-separated).

---
+++ 07. Lists - Exercises/11. Flip List Sides/Flip List Sides.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Flip_List_Sides
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var list = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 1; i < list.Count / 2; i++)
+            {
+                int swap = list[i];
+                list[i] = list[list.Count - 1 - i];
+                list[list.Count - 1 - i] = swap;
+            }
+
+            Console.WriteLine(String.Join(" ", list));
+        }
+    }
+}
