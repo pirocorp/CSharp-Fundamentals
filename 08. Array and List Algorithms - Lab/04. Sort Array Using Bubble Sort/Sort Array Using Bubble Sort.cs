2dd28cc6d679214210be8c62db09f638b1748d2d
pirocorp <pirocorp@abv.bg>
2018-01-13 11:34:47 +0100

    Optimized bubble sort. Read a list of integers on the first line of the console. After that, sort the array, using the Bubble Sort algorithm.

---
+++ 08. Array and List Algorithms - Lab/04. Sort Array Using Bubble Sort/Sort Array Using Bubble Sort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Sort_Array_Using_Bubble_Sort
+{
+    class Program
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            bool swapped;
+            do
+            {
+                swapped = false;
+                int n = arr.Length - 1;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        Swap(arr, i, i + 1); 
+                        swapped = true;
+                    }
+                }
+
+                n = n - 1;
+
+            } while (swapped);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void Swap(int[] arr, int i, int i1)
+        {
+            var temp = arr[i];
+            arr[i] = arr[i1];
+            arr[i1] = temp;
+        }
+    }
+}
