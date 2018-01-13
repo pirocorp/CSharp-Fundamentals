2c5f43e2e4e31a6d85dd85bb1709bcb753904b06
pirocorp <pirocorp@abv.bg>
2018-01-13 11:40:38 +0100

    Read a list of integers on the first line of the console. After that, sort the array, using the Insertion Sort algorithm.

---
+++ 08. Array and List Algorithms - Lab/05. Sort Array Using Insertion Sort/Sort Array Using Insertion Sort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Sort_Array_Using_Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int firstUnsorted = 0; firstUnsorted < arr.Length - 1; firstUnsorted++)
+            {
+                var i = firstUnsorted + 1;
+                while (i > 0)
+                {
+                    if (arr[i - 1] > arr[i])
+                        Swap(arr, i, i - 1);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void Swap(int[] arr, int i, int i1)
+        {
+            var temp = arr[i];
+            arr[i] = arr[i1];
+            arr[i1] = temp;
+        }
+    }
+}
