27f5c5b8c3ebd2079c29c712d37fa2ebb5b553a2
pirocorp <pirocorp@abv.bg>
2018-01-13 11:15:13 +0100

    Read a list of integers on the first line of the console. After that, find the smallest element in the array and print it on the console.

---
+++ 08. Array and List Algorithms - Lab/02. Smallest Element in Array/Smallest Element in Array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Smallest_Element_in_Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine()
+                .Split().Select(int.Parse).ToArray();
+
+            var min = int.MaxValue;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            Console.WriteLine(min);
+
+        }
+    }
+}
