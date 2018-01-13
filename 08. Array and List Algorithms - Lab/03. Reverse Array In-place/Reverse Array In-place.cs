809e3f7d13cc065f01379f80aa0175e6bacba904
pirocorp <pirocorp@abv.bg>
2018-01-13 11:17:08 +0100

    Read a list of integers on the first line of the console. After that, reverse the array in-place (as in, don’t create a new collection to hold the result, reverse it using only the original array). After you are done, print the reversed array on the console. Note: You are not allowed to iterate over the list backwards and just print it,

---
+++ 08. Array and List Algorithms - Lab/03. Reverse Array In-place/Reverse Array In-place.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Reverse_Array_In_place
+{
+    class Program
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                var temp = arr[i];
+                arr[i] = arr[arr.Length - 1 - i];
+                arr[arr.Length - 1 - i] = temp;
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
