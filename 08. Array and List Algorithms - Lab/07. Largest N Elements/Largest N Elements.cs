28141f46c7688a668d7e498f8c3c235812f0b6ef
pirocorp <pirocorp@abv.bg>
2018-01-13 12:32:50 +0100

    Read a list of integers on the first line of the console. On the next line, you will receive an integer N. After that, find and print the largest N elements the array, sorted in descending order.

---
+++ 08. Array and List Algorithms - Lab/07. Largest N Elements/Largest N Elements.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Largest_N_Elements
+{
+    class Program
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine()
+                .Split().Select(int.Parse).ToArray();
+            var n = int.Parse(Console.ReadLine());
+
+            SortArrayUsingInsertionSort(arr);
+            ReverseArray(arr);
+            var largestNElements = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                largestNElements.Add(arr[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", largestNElements));
+        }
+
+        private static void ReverseArray(int[] arr)
+        {
+            var n = arr.Length / 2;
+            var end = arr.Length - 1;
+            for (int i = 0; i < n; i++)
+            {
+               Swap(arr, i, end - i); 
+            }
+        }
+
+        private static void SortArrayUsingInsertionSort(int[] arr)
+        {
+            for (int arrIndex = 0; arrIndex < arr.Length - 1; arrIndex++)
+            {
+                var firstUnsorted = arrIndex + 1;
+
+                while (firstUnsorted > 0)
+                {
+                    if (arr[firstUnsorted - 1] > arr[firstUnsorted])
+                    {
+                        Swap(arr, firstUnsorted - 1, firstUnsorted);
+                    }
+
+                    firstUnsorted--;
+                }
+            }
+        }
+
+        private static void Swap(int[] arr, int x, int y)
+        {
+            var swap = arr[x];
+            arr[x] = arr[y];
+            arr[y] = swap;
+        }
+    }
+}
