71d400c9232df908ece0c6f09c8ee654e514207e
pirocorp <pirocorp@abv.bg>
2018-01-13 17:38:36 +0100

    You are given an array of strings (one line, space-delimited). Sort the array using the Bubble sort or Insertion sort algorithms. Instead of comparing if one string is smaller than the other (which you can’t do), you can use the string.CompareTo(str) method. Once the array is sorted, print it on the console, separating the elements by spaces.

---
+++ 08. Array and List Algorithms - Exercises/03. Sort Array of Strings/Sort Array of Strings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Sort_Array_of_Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var originalStrings = Console.ReadLine().Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            SortArrayOfStrings(originalStrings);
+            Console.WriteLine($"{String.Join(" ", originalStrings)}");
+        }
+
+        private static void SortArrayOfStrings(string[] originalStrings)
+        {
+            var swapped = true;
+            var n = originalStrings.Length;
+            while (swapped)
+            {
+                swapped = false;
+
+                for (int i = 1; i < n; i++)
+                {
+                    var currentElement = originalStrings[i];
+                    var previusElement = originalStrings[i - 1];
+
+                    if (currentElement.CompareTo(previusElement) < 0)
+                    {
+                        originalStrings[i - 1] = currentElement;
+                        originalStrings[i] = previusElement;
+                        swapped = true;
+                    }
+                }
+
+                n -= 1;
+            }
+        }
+    }
+}
