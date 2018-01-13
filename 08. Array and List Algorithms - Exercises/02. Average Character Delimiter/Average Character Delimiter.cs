c55748ea1ea26249f9a6321961c941caacc2dbba
pirocorp <pirocorp@abv.bg>
2018-01-13 16:15:30 +0100

    You will receive an array of strings as input. Your task is to find the average character within every string in the array. As in, take every character’s ASCII code, sum it and divide the result by the sum of the count of all the letters in the array.

--- 08. Array and List Algorithms - Exercises/02. Average Character Delimiter/Average Character Delimiter.cs
+++ 08. Array and List Algorithms - Exercises/02. Average Character Delimiter/Average Character Delimiter.cs
@@ -8,8 +8,44 @@ namespace _02.Average_Character_Delimiter
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            char[] delimeterList = {' '};
+            var originalStrings = Console.ReadLine().Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            var allCharacters = String.Empty;
+
+            allCharacters = AppendStrings(originalStrings, allCharacters);
+            int sumOfAllCharacters = SumStringCharacters(allCharacters);
+            char delimeter = (char)Math.Floor(sumOfAllCharacters / (double)allCharacters.Length);
+
+            if (delimeter >= 'a' && delimeter <= 'z')
+            {
+                delimeter -= (char)32;
+            }
+
+            Console.WriteLine(String.Join($"{delimeter}", originalStrings));
+        }
+
+        private static int SumStringCharacters(string allCharacters)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < allCharacters.Length; i++)
+            {
+                sum += allCharacters[i];
+            }
+
+            return sum;
+        }
+
+        private static string AppendStrings(string[] originalStrings, string allCharacters)
+        {
+            for (int i = 0; i < originalStrings.Length; i++)
+            {
+                allCharacters += originalStrings[i];
+            }
+
+            return allCharacters;
         }
     }
 }
