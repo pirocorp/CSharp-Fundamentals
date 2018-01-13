be8ac82df245f5412ae90d6ff62e2a510a8cb00f
pirocorp <pirocorp@abv.bg>
2018-01-13 19:25:57 +0100

    You are given an array of doubles (one line, space-separated). Your task is to convert the double values to their character representations and insert them into a list like so: • Example: 83.105 • Extract the left part of the number and convert it to its ASCII code representation: 83  S • Extract the right part of the number and convert I to its ASCII code representation: 105  i • Insert the left character at the position equal to the index of the double number in the original array.  Index of double: 0  insert S at index 0 • Insert the right character at the position equal to the index of the double number in the original array, counted in reverse: index of the double: 0  insert “i” at index 0, counted in reverse (so, the end of the list) Repeat the aforementioned algorithm for each element of the double array, until you run out of elements. When you do, print the list with no delimiter. If any of the parts of the double number are 0 (such as 86.0 or 0.97), ignore them and do not insert them anywhere.

--- 08. Array and List Algorithms - Exercises/05. Decode Radio Frequencies/Decode Radio Frequencies.cs
+++ 08. Array and List Algorithms - Exercises/05. Decode Radio Frequencies/Decode Radio Frequencies.cs
@@ -13,13 +13,30 @@ namespace _05.Decode_Radio_Frequencies
             char[] delimeterList = { ' ' };
             var listOfDoubles = Console.ReadLine()
                 .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
-                .Select(double.Parse)
                 .ToList();
+
+            var resultListOfStrings = new List<string>();
 
             for (int i = 0; i < listOfDoubles.Count; i++)
             {
-                
+                var currentDouble = listOfDoubles[i].Split('.');
+                var intPartOfDouble = int.Parse(currentDouble[0]);
+                var floatPartOfDouble = int.Parse(currentDouble[1]);
+
+                if (floatPartOfDouble != 0)
+                {
+                    var resut = new string((char) floatPartOfDouble, 1);
+                    resultListOfStrings.Insert(i, resut);
+                }
+
+                if (intPartOfDouble !=0)
+                {
+                    var resut = new string((char)intPartOfDouble, 1);
+                    resultListOfStrings.Insert(i, resut);
+                }
             }
+
+            Console.WriteLine(String.Join("", resultListOfStrings));
         }
     }
 }
