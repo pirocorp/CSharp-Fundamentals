38840b9269e43a1dda8fcf00c1d3eab7f65ac4b0
pirocorp <pirocorp@abv.bg>
2018-01-13 13:53:42 +0100

    Until you receive the command "stop", you will either receive an integer, or the command bang on every line. If you receive an integer, place it at the front of the list. If, however, you receive the command bang, remove the first element whose value is smaller than the average of the elements of the list, print "shot {element}" and then decrement every element in the list by 1. If you receive "bang" and there are no elements left in the list, print "nobody left to shoot! last one was {lastRemovedInt}" and end the program. When you finally do receive the command "end", either print "survivors: {elements, separated by space}" if there are any, or "you shot them all. last one standing was {lastRemovedInt}".

---
+++ 08. Array and List Algorithms - Exercises/01. Shoot List Elements/01. Shoot List Elements.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Shoot_List_Elements
+{
+    class Program
+    {
+        static double lastRemovedInt = double.NaN;
+        private static bool bang = true;
+
+        static void Main()
+        {
+            var resutList = new List<int>();
+            var command = Console.ReadLine();
+
+            while (command != "stop")
+            {
+                if (command == "bang")
+                {
+                    BangProcessing(resutList);
+                    if (!bang)
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    resutList.Insert(0, int.Parse(command));
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (resutList.Count > 0)
+            {
+                Console.WriteLine($"survivors: {String.Join(" ", resutList)}");
+            }
+            else
+            {
+                Console.WriteLine($"you shot them all. last one was {lastRemovedInt}");
+            }
+        }
+
+        private static void BangProcessing(List<int> resutList)
+        {
+            if (resutList.Count > 0)
+            {
+                double average = resutList.Average();
+                lastRemovedInt = RemoveFirstElementSmallerThanAverage(average, resutList);
+                DecrementEveryElementInList(1, resutList);
+            }
+            else
+            {
+                Console.WriteLine($"nobody left to shoot!last one was {lastRemovedInt}");
+                bang = false;
+            }
+
+            bang = true;
+        }
+
+        private static void DecrementEveryElementInList(int i, List<int> resutList)
+        {
+            for (int j = 0; j < resutList.Count; j++)
+            {
+                resutList[j] -= i;
+            }
+        }
+
+        private static double RemoveFirstElementSmallerThanAverage(double average, List<int> resutList)
+        {
+            for (int i = 0; i < resutList.Count; i++)
+            {
+                if (resutList[i] <= average)
+                {
+                    int lastBangElement = resutList[i];
+                    resutList.RemoveAt(i);
+                    Console.WriteLine($"shot {lastBangElement}");
+                    return lastBangElement;
+                }
+            }
+
+            return Double.NaN;
+        }
+    }
+}
