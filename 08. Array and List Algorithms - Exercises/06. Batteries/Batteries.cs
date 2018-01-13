bd8333021135166d86e73f696206fcfa3620b397
pirocorp <pirocorp@abv.bg>
2018-01-13 19:53:16 +0100

    You are in a battery manufacturing plant. Your task is to stress test the latest batch of batteries for longevity. You will receive an array of doubles on the console (first line, space-separated), indicating the capacities of the different batteries in the batch (in mAh). On the next line, you will receive the usage per hour for each battery as an array of doubles (second line, space-separated). Next, you will receive the amount of hours you have to stress test each battery for (as an integer). Each of the batteries drains by its capacity until either the testing hours are over, or the battery dies (reaches 0 capacity). Print the status of all the batteries at the end of the testing session (in percentage). If any batteries die, along with the percentage, print how many hours it took. The capacity and percentage are rounded to the 2nd decimal point.

---
+++ 08. Array and List Algorithms - Exercises/06. Batteries/Batteries.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Batteries
+{
+    class Batteries
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var capacitiesOfBatteriesInmAh = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            var usagePerHour = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            var hoursOfTest = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < capacitiesOfBatteriesInmAh.Count; i++)
+            {
+                var canLastHours = Math.Ceiling(capacitiesOfBatteriesInmAh[i] / (usagePerHour[i]));
+
+                if (canLastHours <= hoursOfTest)
+                {
+                    Console.WriteLine($"Battery {i + 1}: dead (lasted {canLastHours} hours)");
+                }
+                else
+                {
+                    var leftCharge = capacitiesOfBatteriesInmAh[i] - (usagePerHour[i] * hoursOfTest);
+                    var percentage = (leftCharge / capacitiesOfBatteriesInmAh[i]) * 100.0;
+                    Console.WriteLine($"Battery {i + 1}: {leftCharge:f2} mAh ({percentage:f2})%");
+                }
+            }
+        }
+    }
+}
