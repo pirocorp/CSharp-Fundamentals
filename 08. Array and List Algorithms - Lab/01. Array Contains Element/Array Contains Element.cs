01e60ee54cc85cb4788d021ca3c90eb20d52bcd7
pirocorp <pirocorp@abv.bg>
2018-01-13 11:11:29 +0100

    Read a list of integers on the first line of the console and an integer N from the second line of the console and print whether the element is contained in the array. If it is, print “yes”, otherwise print “no”.

---
+++ 08. Array and List Algorithms - Lab/01. Array Contains Element/Array Contains Element.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Array_Contains_Element
+{
+    class Program
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine()
+                .Split().Select(int.Parse).ToArray();
+            var element = int.Parse(Console.ReadLine());
+
+
+            var containsElement = false;
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] == element)
+                {
+                    containsElement = true; break;
+                }
+
+            if (containsElement)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+
+        }
+    }
+}
