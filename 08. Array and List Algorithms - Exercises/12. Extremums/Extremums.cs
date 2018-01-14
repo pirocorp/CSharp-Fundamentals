7d462413059a676adaa22e78a40676a8e8d2a844
pirocorp <pirocorp@abv.bg>
2018-01-14 23:07:55 +0100

    You are given a sequence of non-negative integers and a command (Min or Max). Depending on the command, you must find the maximum or the minimum sum of numbers formed by the rotations of the digits of every element in the sequence. You are allowed to use only rotations to form the numbers (you cannot swap digits, only rotations). These rotations are similar to the array rotations e.g.:

---
+++ 08. Array and List Algorithms - Exercises/12. Extremums/Extremums.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.Extremums
+{
+    class Extremums
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+
+            var inputNumbers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command = Console.ReadLine();
+            string result;
+
+            if (command == "Max")
+            {
+                result = GetMaxNumbers(inputNumbers);
+            }
+            else
+            {
+                result = GetMinNumbers(inputNumbers);
+            }
+
+            var resultedList = result
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Console.WriteLine(String.Join(", ", resultedList));
+            Console.WriteLine(resultedList.Sum());
+        }
+
+        private static string GetMinNumbers(List<string> inputNumbers)
+        {
+            var choosenNumbers = string.Empty;
+
+            for (int i = 0; i < inputNumbers.Count; i++)
+            {
+                choosenNumbers += GetMinVersionOfNumber(inputNumbers[i]) + " ";
+            }
+
+            return choosenNumbers;
+        }
+
+        private static long GetMinVersionOfNumber(string inputNumber)
+        {
+            var minNumber = long.MaxValue;
+            var numberString = inputNumber;
+
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                var currentNumber = long.Parse(numberString);
+
+                if (minNumber > currentNumber)
+                {
+                    minNumber = currentNumber;
+                }
+
+                var newNumberString = RotateString(numberString);
+                numberString = newNumberString;
+            }
+
+            return minNumber;
+        }
+
+        private static string GetMaxNumbers(List<string> inputNumbers)
+        {
+            var choosenNumbers = string.Empty;
+
+            for (int i = 0; i < inputNumbers.Count; i++)
+            {
+                choosenNumbers += GetMaxVersionOfNumber(inputNumbers[i]) + " ";
+            }
+
+            return choosenNumbers;
+        }
+
+        private static long GetMaxVersionOfNumber(string inputNumber)
+        {
+            var minNumber = long.MinValue;
+            var numberString = inputNumber;
+
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                var currentNumber = long.Parse(numberString);
+
+                if (minNumber < currentNumber)
+                {
+                    minNumber = currentNumber;
+                }
+
+                var newNumberString = RotateString(numberString);
+                numberString = newNumberString;
+            }
+
+            return minNumber;
+        }
+
+        private static string RotateString(string numberString)
+        {
+            var newNumberString = string.Empty;
+            var swap = string.Empty;
+            swap += numberString[0];
+
+            for (var i = 1; i < numberString.Length; i++)
+            {
+                newNumberString += numberString[i];
+            }
+
+            newNumberString += swap;
+
+            return newNumberString;
+        }
+    }
+}
