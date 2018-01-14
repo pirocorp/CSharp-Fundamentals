d0d6516ae5d3c24c5e48208ed61adfca1819463c
pirocorp <pirocorp@abv.bg>
2018-01-14 12:15:41 +0100

    In most of our tasks we use linear search, when we are looking for an element. However, another simple to implement and widely used algorithm is the binary search. Its idea is to work on sorted collections and to use the principle of divide and conquer. Your task is to write two methods, which receive an array and an element, which we will be looking for. Perform the linear search on the unmodified collection and start from the first element. For the binary search order the collection in ascending order, so you receive a viable result. Print “Yes” if the element can be found in the collection or “No” if it’s not present. After that print the count of iterations, which the linear search made in format:  “Linear search made {countOfIterations} iterations” And the count of iterations, which binary search made: “Binary search made {countOfIterations} iterations”

--- 08. Array and List Algorithms - Exercises/08. Binary Search/Binary Search.cs
+++ 08. Array and List Algorithms - Exercises/08. Binary Search/Binary Search.cs
@@ -8,9 +8,100 @@ namespace _08.Binary_Search
 {
     class Program
     {
+        private static bool found = false;
         static void Main()
         {
+            char[] delimeterList = { ' ' };
+            var uniqueIntegers = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
 
+            var searchedInt = int.Parse(Console.ReadLine());
+
+            var linearCount = LinearSearch(searchedInt, uniqueIntegers);
+            var binaryCount = BinarySearch(searchedInt, uniqueIntegers);
+
+            if (!found)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+            Console.WriteLine($"Linear search made {linearCount} iterations");
+            Console.WriteLine($"Binary search made {binaryCount} iterations");
+        }
+
+        private static int BinarySearch(int searchedInt, List<int> uniqueIntegers)
+        {
+            SortList(uniqueIntegers);
+
+            var lowerBound = 0;
+            var upperBound = uniqueIntegers.Count -1;
+            var count = 0;
+
+            while (true)
+            {
+                if (upperBound < lowerBound)
+                {
+                    found = false;
+                    return count;
+                }
+
+                count++;
+
+                var midPoint = lowerBound + (upperBound - lowerBound) / 2;
+
+                if (uniqueIntegers[midPoint] < searchedInt)
+                {
+                    lowerBound = midPoint + 1;
+                }
+                else if (uniqueIntegers[midPoint] > searchedInt)
+                {
+                    upperBound = midPoint - 1;
+                }
+                else if (uniqueIntegers[midPoint] == searchedInt)
+                {
+                    found = true;
+                    return count;
+                }
+            }
+        }
+
+        private static void SortList(List<int> uniqueIntegers)
+        {
+            for (int firstUnsorted = 0; firstUnsorted < uniqueIntegers.Count - 1; firstUnsorted++)
+            {
+                var i = firstUnsorted + 1;
+                while (i > 0)
+                {
+                    if (uniqueIntegers[i - 1] > uniqueIntegers[i])
+                    {
+                        var swap = uniqueIntegers[i - 1];
+                        uniqueIntegers[i - 1] = uniqueIntegers[i];
+                        uniqueIntegers[i] = swap;
+                    }
+                    i--;
+                }
+            }
+        }
+
+        private static int LinearSearch(int searchedInt, List<int> uniqueIntegers)
+        {
+            var count = 0;
+            for (int i = 0; i < uniqueIntegers.Count; i++)
+            {
+                count++;
+                if (searchedInt == uniqueIntegers[i])
+                {
+                    found = true;
+                    return count;
+                }
+            }
+
+            return count;
         }
     }
 }
