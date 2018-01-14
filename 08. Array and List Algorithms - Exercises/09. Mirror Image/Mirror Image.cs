788fccf0df94ab74ed24ade3f39c640b9f9437c8
pirocorp <pirocorp@abv.bg>
2018-01-14 12:54:24 +0100

    You will receive a collection of elements from the console. Until you receive the command “Print” you will receive on every line a single number - n, which will represent index from your collection. Your task is to reverse the elements from index 0 to index n-1 and from n+1 to the last element of the collection. You should not change he position of the element at the given index.

---
+++ 08. Array and List Algorithms - Exercises/09. Mirror Image/Mirror Image.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Mirror_Image
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] delimeterList = { ' ' };
+            var listOfElements = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command = Console.ReadLine();
+            while (command != "Print")
+            {
+                var index = int.Parse(command);
+                ReverseListOfElementsByN(index, listOfElements);
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(" ", listOfElements));
+        }
+
+        private static void ReverseListOfElementsByN(int reverseDelimiter, List<string> listOfElements)
+        {
+            for (int i = 0; i < reverseDelimiter / 2; i++)
+            {
+                string swap = listOfElements[i];
+                listOfElements[i] = listOfElements[reverseDelimiter - 1 - i];
+                listOfElements[reverseDelimiter - 1 - i] = swap;
+            }
+
+            var midpoint = (listOfElements.Count - reverseDelimiter + 1) / 2;
+            midpoint += reverseDelimiter;
+            var count = 0;
+
+            for (int i = reverseDelimiter + 1; i < midpoint; i++)
+            {
+                string swap = listOfElements[i];
+                listOfElements[i] = listOfElements[listOfElements.Count - 1 - count];
+                listOfElements[listOfElements.Count - 1 - count] = swap;
+                count++;
+            }
+        }
+    }
+}
