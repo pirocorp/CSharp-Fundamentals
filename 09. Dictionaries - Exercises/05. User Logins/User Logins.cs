ad2ec72833bb44c16cbb936b29d64e3402d53217
pirocorp <pirocorp@abv.bg>
2018-01-15 14:41:52 +0100

    Write a program that receives a list of username-password pairs in the format “{username} -> {password}”. If there’s already a user with that username, replace their password. After you receive the command “login”, login requests start coming in, using the same format. Your task is to print the status of user login, using different messages as per the conditions below: • If the password matches with the user’s password, print “{username}: logged in successfully”. • If the user doesn’t exist, or the password doesn’t match the user, print “{username}: login failed”. When you receive the command “end”, print the count of unsuccessful login attempts, using the format “unsuccessful login attempts: {count}”.

---
+++ 09. Dictionaries - Exercises/05. User Logins/User Logins.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.User_Logins
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var users = new Dictionary<string, string>();
+
+            while (input != "login")
+            {
+                var inputStrings = input.Split(new[] {'-', '>'}, StringSplitOptions.RemoveEmptyEntries);
+                var userName = inputStrings[0].Trim();
+                var password = inputStrings[1].Trim();
+                users[userName] = password;
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+            var ununsuccessfulLoginAttemptsCounter = 0;
+
+            while (input != "end")
+            {
+                var inputStrings = input.Split(new[] { '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+                var userName = inputStrings[0].Trim();
+                var password = inputStrings[1].Trim();
+
+                if (!users.ContainsKey(userName))
+                {
+                    Console.WriteLine($"{userName}: login failed");
+                    ununsuccessfulLoginAttemptsCounter++;
+                }
+                else if (users[userName] != password)
+                {
+                    Console.WriteLine($"{userName}: login failed");
+                    ununsuccessfulLoginAttemptsCounter++;
+                }
+                else
+                {
+                    Console.WriteLine($"{userName}: logged in successfully");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (ununsuccessfulLoginAttemptsCounter > 0)
+            {
+            Console.WriteLine($"unsuccessful login attempts: {ununsuccessfulLoginAttemptsCounter}");
+            }
+        }
+    }
+}
