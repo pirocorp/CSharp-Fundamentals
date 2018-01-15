6d80d2b2cc3160095ecf8a954b196285248abea0
pirocorp <pirocorp@abv.bg>
2018-01-15 11:10:11 +0100

    Read a list of real numbers and print them in ascending order along with their number of occurrences.

---
+++ 09. Dictionaries - Lab/01. Count Real Numbers/Count Real Numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            var result = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!result.ContainsKey(number))
+                {
+                    result.Add(number, 0);
+                }
+
+                result[number]++;
+            }
+
+            foreach (var number in result)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
