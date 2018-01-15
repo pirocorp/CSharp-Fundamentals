8819dadf9a411052b69857a8edbcd79a9dd30caa
pirocorp <pirocorp@abv.bg>
2018-01-15 13:17:02 +0100

    You have been tasked to create a referenced dictionary, or in other words a dictionary, which knows how to reference itself. You will be given several input lines, in one of the following formats: • {name} = {value} • {name} = {secondName} The names will always be strings, and the values will always be integers. In case you are given a name and a value, you must store the given name and its value. If the name already EXISTS, you must CHANGE its value with the given one. In case you are given a name and a second name, you must store the given name with the same value as the value of the second name. If the given second name DOES NOT exist, you must IGNORE that input. When you receive the command “end”, you must print all entries with their value, by order of input, in the following format: {entry} === {value}

---
+++ 09. Dictionaries - Exercises/02. Dict-Ref/Dict-Ref.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Dict_Ref
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var result = new Dictionary<string, int>();
+
+            while (input != "end")
+            {
+                var inputStrings = input.Split('=');
+                var key = inputStrings[0].Trim();
+                var valueString = inputStrings[1].Trim();
+                var success = int.TryParse(valueString, out var value);
+
+                if (success)
+                {
+                    result[key] = value;
+                }
+                else
+                {
+                    if (result.ContainsKey(valueString))
+                    {
+                    result[key] = result[valueString];
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var value in result)
+            {
+                Console.WriteLine($"{value.Key} === {value.Value}");
+            }
+        }
+    }
+}
