974a43951a2b43663fd179e4a8f7d02471b35830
pirocorp <pirocorp@abv.bg>
2018-01-15 18:33:43 +0100

    You will receive a single input line containing STRINGS separated by spaces. The strings may contain any ASCII character except whitespace. You will then begin receiving commands in one of the following formats: •	merge {startIndex} {endIndex} •	divide {index} {partitions} Every time you receive the merge command, you must merge all elements from the startIndex, till the endIndex. In other words, you should concatenate them. Example: {abc, def, ghi} -> merge 0 1 -> {abcdef, ghi} If any of the given indexes is out of the array, you must take ONLY the range that is INSIDE the array and merge it. Every time you receive the divide command, you must DIVIDE the element at the given index, into several small substrings with equal length. The count of the substrings should be equal to the given partitions. Example: {abcdef, ghi, jkl} -> divide 0 3 -> {ab, cd, ef, ghi, jkl} If the string CANNOT be exactly divided into the given partitions, make all partitions except the LAST with EQUAL LENGTHS, and make the LAST one – the LONGEST. Example: {abcd, efgh, ijkl} -> divide 0 3 -> {a, b, cd, efgh, ijkl} The input ends when you receive the command “3:1”. At that point you must print the resulting elements, joined by a space.

---
+++ Exam Preparation/01. Anonymous Threat/Anonymous Threat.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Anonymous_Threat
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputStrings = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var commands = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var result = inputStrings;
+            string currentCommand = commands[0];
+
+            while (commands[0] != "3:1")
+            {
+                if (currentCommand == "merge")
+                {
+                    var startIndex = int.Parse(commands[1]);
+                    var endIndex = int.Parse(commands[2]);
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+
+                    if (endIndex >= result.Count)
+                    {
+                        endIndex = result.Count - 1;
+                    }
+                    if(startIndex <= endIndex)
+                    result = ConcatenateStrings(startIndex, endIndex, result);
+
+                }
+                else if (currentCommand == "divide")
+                {
+                    var index = int.Parse(commands[1]);
+                    var partitions = int.Parse(commands[2]);
+                    if (index < 0)
+                    {
+                        index = 0;
+                    }
+
+                    if (index >= result.Count)
+                    {
+                        index = result.Count - 1;
+                    }
+                    result = DivideString(index, partitions, result);
+                }
+
+                commands = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                currentCommand = commands[0];
+            }
+
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+        private static List<string> DivideString(int index, int partitions, List<string> inputStrings)
+        {
+            var resultedList = new List<string>();
+
+            for (int i = 0; i < index; i++)
+            {
+                resultedList.Add(inputStrings[i]);
+            }
+
+            List<string> parts = MakeStringIntoSubStrings(inputStrings[index], partitions);
+
+            for (int i = 0; i < parts.Count; i++)
+            {
+                resultedList.Add(parts[i]);
+            }
+
+            for (int i = index + 1; i < inputStrings.Count; i++)
+            {
+                resultedList.Add(inputStrings[i]);
+
+            }
+            return resultedList;
+        }
+
+        private static List<string> MakeStringIntoSubStrings(string inputString, int partitions)
+        {
+            var resultedList = new List<string>();
+            var elementLenght = inputString.Length / partitions;
+            var lastPartitionLenght = elementLenght + inputString.Length % partitions;
+
+            for (int i = 0; i < inputString.Length - lastPartitionLenght; i+= elementLenght)
+            {
+                var currentSubString = inputString.Substring(i, elementLenght);
+                resultedList.Add(currentSubString);
+            }
+
+            var currentString = inputString.Substring(inputString.Length - lastPartitionLenght, lastPartitionLenght);
+            resultedList.Add(currentString);
+
+            return resultedList;
+        }
+
+        private static List<string> ConcatenateStrings(int startIndex, int endIndex, List<string> inputStrings)
+        {
+            var resultedList = new List<string>();
+
+            for (int i = 0; i < startIndex; i++)
+            {
+                resultedList.Add(inputStrings[i]);
+            }
+
+            var resultString = string.Empty;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                resultString += inputStrings[i];
+            }
+
+            resultedList.Add(resultString);
+
+            for (int i = endIndex + 1; i < inputStrings.Count; i++)
+            {
+                resultedList.Add(inputStrings[i]);
+            }
+
+            return resultedList;
+        }
+    }
+}
