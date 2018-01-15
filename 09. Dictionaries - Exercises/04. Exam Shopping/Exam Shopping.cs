3e19c317f091124cfd8dc2e96ee4c1e8c273a66f
pirocorp <pirocorp@abv.bg>
2018-01-15 14:03:33 +0100

    A supermarket has products which have quantities. Your task is to stock the shop before Exam Sunday. Until you receive the command “shopping time”, add the various products to the shop’s inventory, keeping track of their quantity (for inventory purposes). When you receive the aforementioned command, the students start pouring in before the exam and buy various products. The format for stocking a product is: “stock {product} {quantity}”. The format for buying a product is: “buy {product} {quantity}”. If a student tries to buy a product, which doesn’t exist, print “{product} doesn't exist”. If it does exist, but it’s out of stock, print “{product} out of stock”. If a student tries buying more than the quantity of that product, sell them all the quantity of the product (the product is left out of stock, don’t print anything). When you receive the command “exam time”, your task is to print the remaining inventory in the following format: “{product} -> {quantity}”. If a product is out of stock, do not print it.

---
+++ 09. Dictionaries - Exercises/04. Exam Shopping/Exam Shopping.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Exam_Shopping
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var input = Console.ReadLine();
+            var inventory = new Dictionary<string, int>();
+            while (input != "shopping time")
+            {
+                var inputStrings = input.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                //var command = inputStrings[0];
+                var key = inputStrings[1];
+                var value = int.Parse(inputStrings[2]);
+
+                if (!inventory.ContainsKey(key))
+                {
+                    inventory.Add(key, 0);
+                }
+
+                inventory[key] += value;
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "exam time")
+            {
+                var inputStrings = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                //var command = inputStrings[0];
+                var product = inputStrings[1];
+                var value = int.Parse(inputStrings[2]);
+
+                if (!inventory.ContainsKey(product))
+                {
+                    Console.WriteLine($"{product} doesn't exist");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                if (inventory[product] == 0)
+                {
+                    Console.WriteLine($"{product} out of stock");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                var result = inventory[product] - value;
+                inventory[product] = Math.Max(result, 0);
+                input = Console.ReadLine();
+            }
+
+            foreach (var product in inventory)
+            {
+                if (product.Value > 0)
+                {
+                    Console.WriteLine($"{product.Key} -> {product.Value}");
+                }
+            }
+        }
+    }
+}
