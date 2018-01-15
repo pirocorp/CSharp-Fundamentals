f83aaed4cccaa4006d8c295e56a75812ed31313a
pirocorp <pirocorp@abv.bg>
2018-01-16 00:00:38 +0100

    The dataSet and dataKey are both strings. The dataSize is an integer. The dataSets hold dataKeys and their dataSizes. If you receive only a dataSet you should add it. If you receive a dataKey and a dataSize, you should add them to the given dataSet. And here’s where the fun begins. If you receive a dataKey and a dataSize, but the given dataSet does NOT exist, you should STORE those keys and values in a cache. When the corresponding dataSet is added, you should check if the cache holds any keys and values referenced to it, and you should add them to the dataSet. You should end your program when you receive the command “thetinggoesskrra”.  At that point you should extract the dataSet from the data with the HIGHEST dataSize (SUM of all its dataSizes), and you should print it. NOTE: Elements in the cache, should be CONSIDERED NON-EXISTANT. You should NOT count them in the final output. In case there are NO dataSets in the data, you should NOT do anything.

---
+++ Exam Preparation/02. Anonymous Cache/Anonymous Cache.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Anonymous_Cache
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = string.Empty;
+
+            var dataDict = new Dictionary<string, Dictionary<string, int>>();
+            var cacheDict = new Dictionary<string, Dictionary<string, int>>();
+
+            while (input != "thetinggoesskrra")
+            {
+                input = Console.ReadLine();
+                if (input == "thetinggoesskrra") break;
+                var tokens = input.Split(new[] { ' ', '-', '>', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens.Length == 1)
+                {
+                    var currentDataSet = tokens[0];
+                    dataDict[currentDataSet] = new Dictionary<string, int>();
+                    foreach (var cashData in cacheDict)
+                    {
+                        if (dataDict.ContainsKey(cashData.Key))
+                        {
+                            dataDict[cashData.Key] = cashData.Value;
+                            cacheDict.Remove(cashData.Key);
+                            break;
+                        }
+                    }
+                    continue;
+                }
+
+                var dataKey = tokens[0];
+                var dataSize = int.Parse(tokens[1]);
+                var dataSet = tokens[2];
+
+                if (!dataDict.ContainsKey(dataSet))
+                {
+                    if (!cacheDict.ContainsKey(dataSet))
+                    {
+                        cacheDict[dataSet] = new Dictionary<string, int>();
+                    }
+                    cacheDict[dataSet].Add(dataKey, dataSize);
+                    continue;
+                }
+
+                //testList[key[index]].Add(value[index]);
+                dataDict[dataSet].Add(dataKey, dataSize);
+
+                foreach (var cashData in cacheDict)
+                {
+                    if (dataDict.ContainsKey(cashData.Key))
+                    {
+                        dataDict[cashData.Key] = cashData.Value;
+                        cacheDict.Remove(cashData.Key);
+                        break;
+                    }
+                }
+            }
+
+            var maxSum = 0;
+            var maxDataSet = string.Empty;
+            var maxSet = string.Empty;
+
+            foreach (var data in dataDict)
+            {
+                var currentSum = 0;
+                var currentString = string.Empty;
+                foreach (var kvp in data.Value)
+                {
+                    currentSum += kvp.Value;
+                    currentString += $"$.{kvp.Key}" + " ";
+                }
+
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                    maxDataSet = currentString;
+                    maxSet = data.Key;
+                }
+            }
+
+            var result = maxDataSet.Trim().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            
+            Console.WriteLine($"Data Set: {maxSet}, Total Size: {maxSum}");
+            Console.WriteLine($"{String.Join(Environment.NewLine, result)}");
+        }
+    }
+}
