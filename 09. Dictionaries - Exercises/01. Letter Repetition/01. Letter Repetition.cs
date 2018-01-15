c334b628927a3a74fe74f4ba12e5a88f86d90e3d
pirocorp <pirocorp@abv.bg>
2018-01-15 12:50:17 +0100

    You will be given a single string, containing random ASCII character. Your task is to print every character, and how many times it has been used in the string.

---
+++ 09. Dictionaries - Exercises/01. Letter Repetition/01. Letter Repetition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Letter_Repetition
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+
+            var charDict = new Dictionary<char, int>();
+
+            foreach (var character in inputString)
+            {
+                if (!charDict.ContainsKey(character))
+                {
+                    charDict.Add(character, 0);
+                }
+
+                charDict[character]++;
+            }
+
+            foreach (var character in charDict)
+            {
+                Console.WriteLine($"{character.Key} -> {character.Value}");
+            }
+        }
+    }
+}
