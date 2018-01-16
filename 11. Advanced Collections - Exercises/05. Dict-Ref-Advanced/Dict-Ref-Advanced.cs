f51448e5b135f9cec5e2c75e74edbbed7c149107
pirocorp <pirocorp@abv.bg>
2018-01-16 19:25:10 +0100

    Remember the Dict-Ref Problem from the previous exercise? Well this one is an Advanced Version. You will begin receiving input lines containing information in one of the following formats: • {key} -> {value 1, value 2, …, value n} • {key} -> {otherKey} The keys will always be strings, and the values will always be integers, separated by a comma and a space. If you are given a key and values, you must store the values to the given key. If the key already exists, you must add the given values to the old ones. If you are given a key and another key, you must copy the values of the other key to the first one. If the other key does not exist, this input line must be IGNORED. When you receive the command “end”, you must stop reading input lines, and you must print all keys with their values, in the following format: • {key} === {value1, value2, value3. . .}

---
+++ 11. Advanced Collections - Exercises/05. Dict-Ref-Advanced/Dict-Ref-Advanced.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Dict_Ref_Advanced
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputData = Console.ReadLine();
+            var personsData = new Dictionary<string, List<int>>();
+
+            while (inputData != "end")
+            {
+                var tokens = inputData.Split(new[] {" -> "}, StringSplitOptions.RemoveEmptyEntries);
+                var key = tokens[0];
+                var valuesStrings = tokens[1].Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries);
+                var values = new List<int>();
+
+                if (valuesStrings.Length == 1)
+                {
+                    int value = 0;
+                    if (int.TryParse(valuesStrings[0], out value))
+                    {
+                        values.Add(value);
+                    }
+                    else
+                    {
+                        var element = valuesStrings[0];
+                        if (personsData.ContainsKey(element))
+                        {
+                            if (!personsData.ContainsKey(key))
+                            {
+                                personsData[key] = new List<int>();
+                            }
+                            personsData[key].AddRange(personsData[element]);
+                        }
+
+                        inputData = Console.ReadLine();
+                        continue;
+                    }
+                }
+                else
+                {
+                    values = valuesStrings.Select(int.Parse).ToList();
+                }
+
+                if (!personsData.ContainsKey(key))
+                {
+                    personsData[key] = new List<int>();
+                }
+
+                personsData[key].AddRange(values);
+                inputData = Console.ReadLine();
+            }
+
+            foreach (var person in personsData)
+            {
+                var personName = person.Key;
+                var personDataList = person.Value;
+
+                Console.WriteLine($"{personName} === {String.Join(", ", personDataList)}");
+            }
+        }
+    }
+}
