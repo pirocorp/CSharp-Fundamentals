124b40901ad20ce710d2b3d1c66a5ed3c3e77faf
pirocorp <pirocorp@abv.bg>
2018-01-16 17:32:35 +0100

    • On the first line, you will receive a key. • On the second line, you will receive a value. • On the third line, you will receive N. • On the next N lines, you will receive strings in the following format: “key => {value 1};{value 2};…{value X}” After you receive N key -> values pairs, your task is to go through them and print only the keys, which contain the key and the values, which contain the value. Print them in the following format:

---
+++ 11. Advanced Collections - Exercises/02. Key-Key Value-Value/Key-Key Value-Value.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Key_Key_Value_Value
+{
+    class Program
+    {
+        static void Main()
+        {
+            var globalKey = Console.ReadLine();
+            var globalValue = Console.ReadLine();
+            var n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split(new[] {" => "}, StringSplitOptions.RemoveEmptyEntries);
+                var key = tokens[0];
+                var listOfStrings = tokens[1].Split(';');
+
+                if (!dict.ContainsKey(key))
+                {
+                    dict[key] = new List<string>();
+                }
+
+                dict[key].AddRange(listOfStrings);
+            }
+
+            foreach (var shit in dict)
+            {
+                var key = shit.Key;
+                var listOfStrings = shit.Value;
+                if (key.Contains(globalKey))
+                {
+                    Console.WriteLine($"{key}:");
+                    for (int i = 0; i < listOfStrings.Count; i++)
+                    {
+                        var currentString = listOfStrings[i];
+                        if (currentString.Contains(globalValue))
+                        {
+                            Console.WriteLine($"-{currentString}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
