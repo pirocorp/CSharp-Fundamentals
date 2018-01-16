b14da3e6d66f62fd6fb5c04948b752d3cfb94c5a
pirocorp <pirocorp@abv.bg>
2018-01-16 23:28:16 +0100

    You have been tasked to store a few forum topics, and filter them by several given tags. You will be given several input lines, containing data about topics in the following format: {topic} -> {tag1, tag2, tag3...} The topic and tags will be strings. They will NOT contain spaces or ‘-’, ‘>’ symbols. If you receive an existing topic, you must add the new tags to it. There should be NO duplicate tags. When you receive the command “filter”, you must end the input sequence. On the next line (after “filter”), you will receive a sequence of tags, separated by a comma and a space. You must print ONLY those topics, which contain all tags in the given sequence. The topics must be printed in the following format: {topic} | {#tag1, #tag2, …, #tagN} NOTE: The tags have a number sign (‘#’) as a prefix.

---
+++ 11. Advanced Collections - Exercises/06. Forum Topics/Forum Topics.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Forum_Topics
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputData = Console.ReadLine();
+            var topicDict = new Dictionary<string, HashSet<string>>();
+
+            while (inputData != "filter")
+            {
+                var tokens = inputData.Split(new[] {" -> "}, StringSplitOptions.RemoveEmptyEntries);
+                var key = tokens[0];
+                var listOfTags = tokens[1].Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries);
+
+                if (!topicDict.ContainsKey(key))
+                {
+                    topicDict[key] = new HashSet<string>();
+                }
+
+                topicDict[key].UnionWith(listOfTags);
+                inputData = Console.ReadLine();
+            }
+
+            inputData = Console.ReadLine();
+            var listOfSearchedTags = inputData.Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var topic in topicDict)
+            {
+                var topicName = topic.Key;
+                var topicTagsList = topic.Value;
+                bool contained = true;
+
+                for (int i = 0; i < listOfSearchedTags.Length; i++)
+                {
+                    if (!topicTagsList.Contains(listOfSearchedTags[i]))
+                    {
+                        contained = false;
+                        break;
+                    }
+                }
+
+                if (contained)
+                {
+                    Console.WriteLine($"{topicName} | #{String.Join(", #", topicTagsList)}");
+                }
+            }
+        }
+    }
+}
+
