7f591bccecdb1264aa3ed9d81d08e426950313c9
pirocorp <pirocorp@abv.bg>
2018-01-16 18:13:04 +0100

    Write a program, which categorizes information about a travel company. Companies have various vehicles planned for different cities. Every city has prepared several types of vehicles. Each vehicle type has a different capacity. Until you receive the command “ready”, you will receive all the cities the company offers holiday packages for, and their respective vehicle types + capacities in the format: • “{city}:{type}-{capacity},{type}-{capacity}…” An example city with its transportation options would look like this: • “Athens:bus-40,airplane-300,train-150” If a city is entered a second time, add all transport which hasn’t already been added and replace existing transports’ capacities with the new ones. After the “ready” command, you will start receiving groups for various cities in the format: “{city} {peopleCount}” until you receive “travel time!”. After that, calculate whether the group will have enough seats to accommodate the passengers and print the status per these conditions: If the group’s size is smaller than or equal to the combines seats in all the vehicles, print: •  “{city} -> all {peopleCount} accommodated” If the group’s size is larger than the combines seats in all the vehicles, print: • “{city} -> all except {peopleCount - transportCapacities} accommodated” Constraints • There will be no redundant whitespaces anywhere in the input. • The input will always be in the format specified. • The group cities will always be existing cities. • The group sizes will always be positive.

---
+++ 11. Advanced Collections - Exercises/03. Travel Company/Travel Company.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Travel_Company
+{
+    class Program
+    {
+        static void Main()
+        {
+            var travelData = new Dictionary<string, Dictionary<string, int>>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "ready")
+            {
+                var tokens = inputData.Split(':');
+                var city = tokens[0];
+                var vehicalsCapacity = tokens[1].Split(',');
+
+                if (!travelData.ContainsKey(city))
+                {
+                    travelData[city] = new Dictionary<string, int>();
+                }
+
+                for (int i = 0; i < vehicalsCapacity.Length; i++)
+                {
+                    var currentElement = vehicalsCapacity[i].Split('-');
+                    var vehical = currentElement[0];
+                    var capacity = int.Parse(currentElement[1]);
+
+                    if (!travelData[city].ContainsKey(vehical))
+                    {
+                        travelData[city][vehical] = 0;
+                    }
+
+                    travelData[city][vehical] = capacity;
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+            inputData = Console.ReadLine();
+
+            while (inputData != "travel time!")
+            {
+                var tokens = inputData.Split(' ');
+                var city = tokens[0];
+                var turists = int.Parse(tokens[1]);
+                var transportCapacities = GetTransportCapacity(travelData, city);
+                if (turists <= transportCapacities)
+                {
+                    Console.WriteLine($"{city} -> all {turists} accommodated");
+                }
+                else
+                {
+                    Console.WriteLine($"{city} -> all except {turists - transportCapacities} accommodated");
+                }
+
+                inputData = Console.ReadLine();
+            }
+        }
+
+        private static int GetTransportCapacity(Dictionary<string, Dictionary<string, int>> travelData, string city)
+        {
+            var sum = 0;
+
+            foreach (var vehical in travelData[city])
+            {
+                sum += vehical.Value;
+            }
+
+            return sum;
+        }
+    }
+}
