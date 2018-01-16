5aad178574af8ba9b8359b3aaadc817669f59e99
pirocorp <pirocorp@abv.bg>
2018-01-17 00:40:18 +0100

    You have been tasked to create a Console Social Media Database. You will receive several input lines in one of the following formats: • post {postName} • like {postName} • dislike {postName} • comment {postName} {commenterName} {content} If you receive the post command, you must create a post with the given name. If you receive the like command you must add a like to the given post. If you receive the dislike command you must add a dislike to the given post. If you receive the comment command, you must add a comment to the given post. The comment’s writer must be the given commentator name, and the content of the comment must be the given content. By default, the posts have 0 likes, 0 dislikes and 0 comments when created. When you receive the command “drop the media”, you must end the input sequence, and you must print every post with its likes, dislikes and comments in the following format: If a certain post does not have any comments, you should just print “None”. The comments have a prefix of a single asterisk (‘*’) and 2 spaces. There is NO space between the commentator’s name and the colon. Constraints • The post name will be a string of letters and digits. • The commentator’s name will be a string of letters. • The comment’s CONTENT, may contain ANY ASCII character. • There will be NO invalid input data.

---
+++ 11. Advanced Collections - Exercises/07. Social Media Posts/Social Media Posts.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Social_Media_Posts
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Dictionary <post, comment>, [like, dislike]>
+            var posts = new Dictionary<string, int[]>();
+            var comments = new Dictionary<string, Dictionary<string, List<string>>>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "drop the media")
+            {
+                var tokens = inputData.Split(' ').ToList();
+                var command = tokens[0];
+                var key = tokens[1];
+
+                switch (command)
+                {
+                    case "post":
+                        posts[key] = new int[2];
+                        break;
+                    case "like":
+                        posts[key][0]++;
+                        break;
+                    case "dislike":
+                        posts[key][1]++;
+                        break;
+                    case "comment":
+                        var name = tokens[2];
+                        tokens.RemoveRange(0, 3);
+                        var comment = String.Join(" ", tokens);
+
+                        if (!comments.ContainsKey(key))
+                        {
+                            comments[key] = new Dictionary<string, List<string>>();
+                        }
+
+                        if (!comments[key].ContainsKey(name))
+                        {
+                            comments[key][name] = new List<string>();
+                        }
+
+                        comments[key][name].Add(comment);
+                        break;
+
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+            foreach (var post in posts)
+            {
+                var postName = post.Key;
+                var likes = post.Value[0];
+                var dislike = post.Value[1];
+                Console.WriteLine($"Post: {postName} | Likes: {likes} | Dislikes: {dislike}");
+                Console.WriteLine($"Comments:");
+
+                if (comments.ContainsKey(postName))
+                {
+                    var commentEntries = comments[postName];
+
+                    foreach (var user in commentEntries)
+                    {
+                        var userName = user.Key;
+                        var userComments = user.Value;
+                        Console.WriteLine($"*  {userName}: {String.Join(" ", userComments)}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("None");
+                }
+
+            }
+        }
+    }
+}
