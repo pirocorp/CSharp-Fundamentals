2dc6a473faf687fdb05a325b119ec839b5b67768
pirocorp <pirocorp@abv.bg>
2018-01-16 17:12:24 +0100

    On the first line of the input, you will receive n –  the number of lines of clothes, which came prepackaged for the wardrobe. On the next n lines, you will receive the clothes for each color in the format: •	“{color} -> {item1},{item2},{item3}…” If a color is added a second time, add all items from it and count the duplicates. Finally, you will receive the color and item of the clothing, that you need to look for.

---
+++ 11. Advanced Collections - Exercises/01. Wardrobe/Wardrobe.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Wardrobe
+{
+    class Wardrobe
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            // <color <clothing, count>>
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine()
+                    .Split(new[] {" -> "}, StringSplitOptions.RemoveEmptyEntries);
+                var color = tokens[0];
+                var clothingItems = tokens[1].Split(',');
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe[color] = new Dictionary<string, int>();
+                }
+
+                for (int j = 0; j < clothingItems.Length; j++)
+                {
+                    var clothing = clothingItems[j];
+                    if (!wardrobe[color].ContainsKey(clothing))
+                    {
+                        wardrobe[color][clothing] = 0;
+                    }
+
+                    wardrobe[color][clothing]++;
+                }
+            }
+
+            var inputCommand = Console.ReadLine().Split(' ');
+            var searchedColor = inputCommand[0];
+            var searchedItem = inputCommand[1];
+
+            foreach (var kvp in wardrobe)
+            {
+                var color = kvp.Key;
+                var items = kvp.Value;
+                Console.WriteLine($"{color} clothes:");
+                foreach (var clothing in items)
+                {
+                    var item = clothing.Key;
+                    var itemNumbers = clothing.Value;
+                    Console.Write($"* {item} - {itemNumbers}");
+                    if (color == searchedColor && item == searchedItem)
+                    {
+                        Console.Write(" (found!)");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
