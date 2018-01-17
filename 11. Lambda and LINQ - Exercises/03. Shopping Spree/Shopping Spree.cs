3c55e61c9cd324806c5d419d97947fa774fb21cb
pirocorp <pirocorp@abv.bg>
2018-01-17 19:03:20 +0100

    Pesho has a problem with taking decisions and mostly with deciding on what to eat for dinner. He asked you to write a program which chooses dinner for him. On the first line, you will receive Pesho’s budget as decimal number. On the next lines, until you receive the command “end”, you will receive strings with the name of the product and the price of the product. Save all products and if you receive the same product and its price is lower than the already saved one – rewrite the old value. At the end, sum all values of the received products. If the sum is bigger than Pesho’s budget – print: “Need more money... Just buy banichka” If Pesho has enough money – print all purchased products, ordered by price in descending order, then by the count of the characters in its name in ascending order. Use the following format for each product: “{productName} costs {productPrice}” Print all prices rounded to the 2nd decimal sign.

---
+++ 11. Lambda and LINQ - Exercises/03. Shopping Spree/Shopping Spree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Shopping_Spree
+{
+    class Program
+    {
+        static void Main()
+        {
+            var productPrices = new Dictionary<string, decimal>();
+            var budget = decimal.Parse(Console.ReadLine());
+            var indputData = Console.ReadLine();
+
+            while (indputData != "end")
+            {
+                var tokens = indputData.Split(' ').ToArray();
+                var key = tokens[0];
+                var value = decimal.Parse(tokens[1]);
+
+                if (!productPrices.ContainsKey(key) || (productPrices.ContainsKey(key) && productPrices[key] > value))
+                {
+                    productPrices[key] = value;
+                }
+
+                indputData = Console.ReadLine();
+            }
+
+            var sum = productPrices.Sum(x => x.Value);
+
+            if (budget >= sum)
+            {
+                var result = productPrices.OrderByDescending(x => x.Value).ThenBy(x => x.Key.Length);
+
+                foreach (var item in result)
+                {
+                    var product = item.Key;
+                    var price = item.Value;
+                    Console.WriteLine($"{product} costs {price:f2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Need more money... Just buy banichka");
+            }
+        }
+    }
+}
