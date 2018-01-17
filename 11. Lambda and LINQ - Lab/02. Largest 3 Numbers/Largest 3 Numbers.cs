502f9de266a370c7706375c937408a8b17f412c0
pirocorp <pirocorp@abv.bg>
2018-01-17 14:16:50 +0100

    Read a list of real numbers and print largest 3 of them. If less than 3 numbers exit, print all of them.

---
+++ 11. Lambda and LINQ - Lab/02. Largest 3 Numbers/Largest 3 Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] strings = Console.ReadLine().Split(' ');
+
+            List<int> nums = strings
+                .Select(int.Parse)
+                .ToList();
+
+            var sortedNums = nums.OrderByDescending(x => x);
+            var largest3Nums = sortedNums.Take(3);
+
+            Console.WriteLine(string.Join(" ", largest3Nums));
+
+        }
+    }
+}
