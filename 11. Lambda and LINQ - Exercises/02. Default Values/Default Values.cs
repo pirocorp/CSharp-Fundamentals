3870f564a08d46bb1c591ad16c4e6253bce01296
pirocorp <pirocorp@abv.bg>
2018-01-17 18:36:12 +0100

    You will be given several key-value pairs in the following format: “{key} -> {value}” Your task is to store all these pairs until you receive the command “end”. When you do receive the ending command, you must read one last line, containing the default value. Then you must change all values, which are equal to “null” with the given default value. At the end, you must print all key-value pairs, which have NOT been replaced with the default value, ordered in descending order, by their value’s length. After them, you must print all key-value pairs which HAVE been replaced with the default value, in order of input. Each key-value pair, must be printed in the following format: “{key} <-> {value}”. All variables in the program logic are strings. Note: If you receive the same key twice or more times, you should update its value every time.

---
+++ 11. Lambda and LINQ - Exercises/02. Default Values/Default Values.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Default_Values
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dict = new Dictionary<string, string>();
+
+            var inputData = Console.ReadLine();
+
+            while (inputData != "end")
+            {
+                var tokens = inputData.Split(new []{" -> "}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var key = tokens[0];
+                var value = tokens[1];
+                dict[key] = value;
+                inputData = Console.ReadLine();
+            }
+
+            inputData = Console.ReadLine();
+
+            var notNullElements = dict.Where(x => x.Value != "null")
+                .OrderByDescending(x => x.Value.Length)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var element in notNullElements)
+            {
+                var key = element.Key;
+                var value = element.Value;
+                Console.WriteLine($"{key} <-> {value}");
+            }
+
+            dict = dict.Where(x => x.Value == "null")
+                .OrderByDescending(x => x.Value.Length)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            var nullElements = dict.Select(x => x.Value == "null" ? new KeyValuePair<string, string>(x.Key, inputData) : new KeyValuePair<string, string>(x.Key, x.Value)).ToList();
+
+            foreach (var element in nullElements)
+            {
+                var key = element.Key;
+                var value = element.Value;
+
+                Console.WriteLine($"{key} <-> {value}");
+            }
+        }
+    }
+}
