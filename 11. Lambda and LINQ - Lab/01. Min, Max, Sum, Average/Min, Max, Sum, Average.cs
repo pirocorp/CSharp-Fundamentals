7e3025b78646441b973c94d7edf61d1df6a29fa1
pirocorp <pirocorp@abv.bg>
2018-01-17 14:13:20 +0100

    Write a program to read n integers and print their sum, min, max, first, last and average values.

---
+++ 11. Lambda and LINQ - Lab/01. Min, Max, Sum, Average/Min, Max, Sum, Average.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Min__Max__Sum__Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
