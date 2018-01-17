d3558d826a1caa412115ce7a82b76f6ec0a28a0f
pirocorp <pirocorp@abv.bg>
2018-01-17 17:40:21 +0100

    You will be given several usernames and dates of registry, in the following format: {username} -> {date} The username will be a string and the date will be a valid date in format “dd/MM/yyyy”. You must read input lines until you receive the command “end”. You need to store every user, with his date of registry. Then you must print the LAST 5 registered names, or in other words the OLDEST registered users, in order of registry – from the latest registered, to the oldest registered. If there are LESS than 5 people, print them in the order, specified above. if any entries have the SAME date, print the LAST entered one, FIRST, and then the other (in input).

---
+++ 11. Lambda and LINQ - Exercises/01. Register Users/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Register_Users
+{
+    class Program
+    {
+        static void Main()
+        {
+            var users = new Dictionary<string, DateTime>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "end")
+            {
+                var tokens = inputData.Split(new[] {" -> "}, StringSplitOptions.RemoveEmptyEntries);
+                var user = tokens[0];
+                var format = "dd/MM/yyyy";
+
+                if (!users.ContainsKey(user))
+                {
+                    users[user] = new DateTime();
+                }
+
+                var dateOfRegistration = DateTime.ParseExact(tokens[1], format, CultureInfo.InvariantCulture);
+                users[user] = dateOfRegistration;
+
+                inputData = Console.ReadLine();
+            }
+
+            var result = users.Reverse().OrderBy(x => x.Value).Take(5).OrderByDescending(x => x.Value).Select(x => x.Key).ToList();
+            Console.WriteLine(String.Join(Environment.NewLine, result));
+        }
+    }
+}
