59ccdc5dd533165a302e6a7e74a18faf9f518354
pirocorp <pirocorp@abv.bg>
2018-01-17 16:16:36 +0100

    Heroes III is the best game ever. Everyone loves it and everyone plays it all the time. Stamat is no exclusion to this rule. His favorite units in the game are all types of dragons – black, red, gold, azure etc… He likes them so much that he gives them names and keeps logs of their stats: damage, health and armor. The process of aggregating all the data is quite tedious, so he would like to have a program doing it. Since he is no programmer, it’s your task to help him You need to categorize dragons by their type. For each dragon, identified by name, keep information about his stats. Type is preserved as in the order of input, but dragons are sorted alphabetically by name. For each type, you should also print the average damage, health and armor of the dragons. For each dragon, print his own stats. There may be missing stats in the input, though. If a stat is missing you should assign it default values. Default values are as follows: health 250, damage 45, and armor 10. Missing stat will be marked by null. The input is in the following format {type} {name} {damage} {health} {armor}. Any of the integers may be assigned null value. See the examples below for better understanding of your task. If the same dragon is added a second time, the new stats should overwrite the previous ones. Two dragons are considered equal if they match by both name and type. Input • On the first line, you are given number N -> the number of dragons to follow • On the next N lines, you are given input in the above described format. There will be single space separating each element. Output • Print the aggregated data on the console • For each type, print average stats of its dragons in format {Type}::({damage}/{health}/{armor}) • Damage, health and armor should be rounded to two digits after the decimal separator • For each dragon, print its stats in format -{Name} -> damage: {damage}, health: {health}, armor: {armor} Constraints • N is in range [1…100] • The dragon type and name are one word only, starting with capital letter. • Damage health and armor are integers in range [0 … 100000] or null

--- Exam Preparation/03. Dragon Army/Dragon Army.cs
+++ Exam Preparation/03. Dragon Army/Dragon Army.cs
@@ -8,8 +8,99 @@ namespace _03.Dragon_Army
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var dragons = new Dictionary<string, SortedDictionary<string, int[]>>(); // <Type, List of DragonData <Name Stats>
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                var inputTokens = Console.ReadLine().Split(' ').ToArray();
+                var type = inputTokens[0];
+                var name = inputTokens[1];
+                var dragonStatsStrings = inputTokens.Skip(2).Take(3).ToArray();
+                int[] dragonStats = GetDragonStats(dragonStatsStrings);
+
+                if (!dragons.ContainsKey(type))
+                {
+                    dragons[type] = new SortedDictionary<string, int[]>();
+                }
+
+                if (!dragons[type].ContainsKey(name))
+                {
+                    dragons[type][name] = new int[3];
+                }
+
+                dragons[type][name] = dragonStats;
+            }
+
+            foreach (var type in dragons)
+            {
+                var typeAverageDamage = 0.0;
+                var typeAverageHealth = 0.0;
+                var typeAverageArmor = 0.0;
+                var typeList = type.Value;
+
+                foreach (var dragon in typeList)
+                {
+                    typeAverageDamage += dragon.Value[0];
+                    typeAverageHealth += dragon.Value[1];
+                    typeAverageArmor += dragon.Value[2];
+                }
+
+                typeAverageDamage /= typeList.Count;
+                typeAverageHealth /= typeList.Count;
+                typeAverageArmor /= typeList.Count;
+                var typeName = type.Key;
+
+                Console.WriteLine($"{typeName}::({typeAverageDamage:f2}/{typeAverageHealth:f2}/{typeAverageArmor:f2})");
+
+                foreach (var dragon in typeList)
+                {
+                    var dragonName = dragon.Key;
+                    var dragonStats = dragon.Value;
+                    var dragonDamage = dragonStats[0];
+                    var dragonHealth = dragonStats[1];
+                    var dragonArmor = dragonStats[2];
+
+                    Console.WriteLine($"-{dragonName} -> damage: {dragonDamage}, health: {dragonHealth}, armor: {dragonArmor}");
+                }
+            }
+        }
+
+        private static int[] GetDragonStats(string[] dragonStatsStrings)
+        {
+            var dragonStatsResult = new int[3];
+            int currentStat;
+
+            if (int.TryParse(dragonStatsStrings[0], out currentStat))
+            {
+                dragonStatsResult[0] = currentStat;
+            }
+            else
+            {
+                dragonStatsResult[0] = 45;
+            }
+
+            if (int.TryParse(dragonStatsStrings[1], out currentStat))
+            {
+                dragonStatsResult[1] = currentStat;
+            }
+            else
+            {
+                dragonStatsResult[1] = 250;
+            }
+
+            if (int.TryParse(dragonStatsStrings[2], out currentStat))
+            {
+                dragonStatsResult[2] = currentStat;
+            }
+            else
+            {
+                dragonStatsResult[2] = 10;
+            }
+
+            return dragonStatsResult;
         }
     }
 }
