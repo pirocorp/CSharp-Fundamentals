7f90cca7c2fcee0d17a85c1332260fadc3548030
pirocorp <pirocorp@abv.bg>
2018-01-18 21:18:53 +0100

    You will receive a list of integers, each separated with a single space. On the next line, you will receive one of the following commands: “Min”, “Max” or “All”. Your task is to remove all numbers, which are smaller than the average of the initial list and depending on the next command, to print an additional line: • If the command is “Min” – print the smallest element from the remaining numbers. • If the command is “Max” – print the largest element from the remaining numbers. • If the command is “All” – print all of the remaining numbers ordered in ascending order and separated by a single whitespace. Constraints • The elements of the array will be in the interval [-2147483648…2147483647]

---
+++ 11. Lambda and LINQ - Exercises/07. Array Data/Array Data.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Array_Data
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var command = Console.ReadLine();
+
+            numbers = numbers.Where(x => x >= numbers.Average()).ToList();
+
+            switch (command)
+            {
+                case "Min":
+                    Console.WriteLine($"{numbers.Min()}");
+                    break;
+                case "Max":
+                    Console.WriteLine($"{numbers.Max()}");
+                    break;
+                case "All":
+                    Console.WriteLine($"{String.Join(" ", numbers.OrderBy(x => x))}");
+                    break;
+            }
+        }
+    }
+}
