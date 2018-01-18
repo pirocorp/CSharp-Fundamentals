587851ab7898c5559a62e1a4327b95e44c11a713
pirocorp <pirocorp@abv.bg>
2018-01-18 21:39:13 +0100

    On the first line, you will receive an array of integers with exactly two elements – m and n: • m will be the elements you have to skip from the beginning. • n will be the element you have to take, after the skipped elements. On the second line, you will receive an array of integers. Output Your task is to take only the numbers, which are in the capital latin letter ASCII range (65…90 inclusive). From the filtered numbers, skip the next m elements (starting from 0) and take the next n elements. After that, convert each number into ASCII characters. Print the resulting string of ASCII characters on the console.

---
+++ 11. Lambda and LINQ - Exercises/08. String Decryption/String Decryption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.String_Decryption
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputMN = Console.ReadLine().Split(' ');
+            var skip = int.Parse(inputMN[0]);
+            var take = int.Parse(inputMN[1]);
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList().Where(x => x > 64 && x < 91).Skip(skip).Take(take);
+            Console.WriteLine(string.Join("", numbers.Select(x => (char)x)));
+        }
+    }
+}
