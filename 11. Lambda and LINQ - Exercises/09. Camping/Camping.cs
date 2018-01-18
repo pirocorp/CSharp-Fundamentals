5e8c33e54d77210a9af46c7c66b0b6bd8e7b4bda
pirocorp <pirocorp@abv.bg>
2018-01-18 22:29:19 +0100

    As a young and ambitious manager of a camping ground you want to invent a new system, which will keep track of the registered camper vans (RVs). Until you receive the command “end”, you will receive commands in the format: • {nameOfThePerson} {camperModel} {timeToStay} If you receive already existing name of a person  add the camper (only if it is new) to the already owned by the person campers and add the days to the already accumulated days. Your task is to print all people in the following format: *{nameOfThePerson}: {countOfCampers} ***{1st camperModel} ***{2nd camperModel} … ***{nth camperModel} Total stay: {countOfNights} nights Print all the people, ordered by the count of their campers. If two people have an equal count of campers, order them by the length of their names in ascending order. Print the campers for each person in order of receiving.

--- 11. Lambda and LINQ - Exercises/09. Camping/Camping.cs
+++ 11. Lambda and LINQ - Exercises/09. Camping/Camping.cs
@@ -8,8 +8,51 @@ namespace _09.Camping
 {
     class Camping
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var campers = new Dictionary<string, Dictionary<string, int>>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "end")
+            {
+                var tokens = inputData.Split(' ').ToArray();
+                var nameOfThePerson = tokens[0];
+                var camperModel = tokens[1];
+                var timeToStay = int.Parse(tokens[2]);
+
+                if (!campers.ContainsKey(nameOfThePerson))
+                {
+                    campers[nameOfThePerson] = new Dictionary<string, int>();
+                }
+
+                if (!campers[nameOfThePerson].ContainsKey(camperModel))
+                {
+                    campers[nameOfThePerson][camperModel] = 0;
+                }
+
+                campers[nameOfThePerson][camperModel] += timeToStay;
+
+                inputData = Console.ReadLine();
+            }
+
+            campers = campers.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key.Length).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var person in campers)
+            {
+                var personName = person.Key;
+                var rvDays = person.Value;
+                var rvCount = rvDays.Count;
+                Console.WriteLine($"{personName}: {rvCount}");
+
+                foreach (var rv in rvDays)
+                {
+                    var rvName = rv.Key;
+                    Console.WriteLine($"***{rvName}");
+                }
+
+                var days = rvDays.Sum(x => x.Value);
+                Console.WriteLine("Total stay: {0} nights", days);
+            }
         }
     }
 }
