57809b0470b47a0b70ec47b29f76c467711f4c25
pirocorp <pirocorp@abv.bg>
2018-01-18 17:08:24 +0100

    You will be given several input lines containing info about a key, an inner key and inner value, separated by SPACES. Your task is to store every inner key and inner value, in every key’s value. Check the Examples for more info. If you receive the command “flatten {key}”, you must flatten all the inner keys and inner values at the given key, or in other words, CONCATENATE them. When you receive the command “end”, the input ends. You must print all keys and their inner-keys and inner-values. Flattened inner keys and inner values, must be printed AFTER all else. The keys must be printed, ordered by length in descending order. All inner-keys must be printed ordered by length in ascending order. Flattened Values must be printed in order of input.

--- 11. Lambda and LINQ - Exercises/05. Flatten Dictionary/Flatten Dictionary.cs
+++ 11. Lambda and LINQ - Exercises/05. Flatten Dictionary/Flatten Dictionary.cs
@@ -8,8 +8,78 @@ namespace _05.Flatten_Dictionary
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var dict = new Dictionary <string, Dictionary <string, string>>();
+            var result = new Dictionary<string, Dictionary<string, string>>();
+
+            var inputData = Console.ReadLine();
+
+            while (inputData != "end")
+            {
+                var tokens = inputData.Split(' ');
+                if (tokens.Length == 2)
+                {
+                    var command = tokens[0];
+                    var argument = tokens[1];
+
+                    if (command == "flatten")
+                    {
+                        result = dict.Where(x => x.Key == argument).ToDictionary(x => x.Key, x => x.Value);
+                        result[argument] = result[argument].ToDictionary(x => x.Key + x.Value, x => string.Empty);
+                        dict.Remove(argument);
+                        dict[argument] = result[argument];
+                    }
+                }
+                else
+                {
+                    var key = tokens[0];
+                    var innerKey = tokens[1];
+                    var innerValue = tokens[2];
+
+                    if (!dict.ContainsKey(key))
+                    {
+                        dict[key] = new Dictionary<string, string>();
+                    }
+
+                    if (!dict[key].ContainsKey(innerKey))
+                    {
+                        dict[key][innerKey] = innerValue;
+                    }
+
+                    dict[key][innerKey] = innerValue;
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+            dict = dict.OrderByDescending(x => x.Key.Length).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var element in dict)
+            {
+                var key = element.Key;
+                var innerDict = element.Value;
+                Console.WriteLine(key);
+                var innerDictOnlyNotEmptyValue = innerDict.Where(x => x.Value != string.Empty).OrderBy(x => x.Key.Length);
+                var count = 0;
+
+                foreach (var kvp in innerDictOnlyNotEmptyValue)
+                {
+                    count++;
+                    var innerKey = kvp.Key;
+                    var innerValue = kvp.Value;
+                    Console.WriteLine($"{count}. {innerKey} - {innerValue}");
+                }
+
+                var innerDictOnlyEmptyValues = innerDict.Where(x => x.Value == string.Empty);
+
+                foreach (var kvp in innerDictOnlyEmptyValues)
+                {
+                    count++;
+                    var innerKey = kvp.Key;
+                    Console.WriteLine($"{count}. {innerKey}");
+                }
+            }
         }
     }
 }
