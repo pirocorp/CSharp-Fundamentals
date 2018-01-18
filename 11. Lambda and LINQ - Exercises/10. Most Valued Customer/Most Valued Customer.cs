c286f965f3c559f58a41bfab420e557d727cc2bc
pirocorp <pirocorp@abv.bg>
2018-01-18 23:42:18 +0100

    Input Until you receive the command “Shop is open”, you will receive products in the format: {productName} {productPrice} After you receive “Shop is open”, you will start receiving customers and products, which they bought in the following format: {nameOfCustomer}: {firstBoughtProduct}, {secondBoughtProduct}, … {nthBoughtProduct} Every customer’s total spending is equal to the sum of the prices of all products which they bought. If you receive a product, which you do not sell – ignore it. A customer can buy one product more than once. You might also receive the command “Discount”. When you receive it – decrease the prices of the top 3 most expensive products by 10%. Calculate the total sum, using the prices after all discounts are made. When you receive the command “Print”, stop taking input. Output After you receive “Print”, print the customer, which spent the most money and all of the products they bought (for the price, use the discounted one) in the following format: Biggest spender: {nameOfTheCustomer} ^Products bought: ^^^{1st Product}: {priceOfTheProduct} ^^^{2nd Product}: {priceOfTheProduct} ... ^^^{nth Product}: {priceOfTheProduct} Total: {priceOfAllProducts} Print the products ordered by price in descending order. Print each product only once. Format all prices to the 2nd decimal place. Constraints • When you are receiving the prices of the products, you will not receive duplicate product names. • There will never be two people with the same total sum.

---
+++ 11. Lambda and LINQ - Exercises/10. Most Valued Customer/Most Valued Customer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Most_Valued_Customer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inventory = new Dictionary<string, decimal>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "Shop is open")
+            {
+                var tokens = inputData.Split(' ');
+                var product = tokens[0];
+                var price = decimal.Parse(tokens[1]);
+
+                if (!inventory.ContainsKey(product))
+                {
+                    inventory[product] = price;
+                }
+
+                inventory[product] = price;
+                inputData = Console.ReadLine();
+            }
+
+            inputData = Console.ReadLine();
+            var clients = new Dictionary<string, List<string>>();
+
+            while (inputData != "Print")
+            {
+                var tokens = inputData.Split(new[] {": "}, StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens.Length > 1)
+                {
+                    var clientName = tokens[0];
+                    var productsBuyed = tokens[1].Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                    if (!clients.ContainsKey(clientName))
+                    {
+                        clients[clientName] = new List<string>();
+                    }
+
+                    foreach (var product in productsBuyed)
+                    {
+                        if (inventory.ContainsKey(product))
+                        {
+                            clients[clientName].Add(product);
+                        }
+                    }
+                }
+                else
+                {
+                    var result = inventory
+                        .OrderByDescending(x => x.Value)
+                        .Take(3)
+                        .ToDictionary(x => x.Key, x => x.Value * 0.9m);
+
+                    inventory = result.Concat(inventory.OrderByDescending(x => x.Value).Skip(3)).ToDictionary(x => x.Key, x => x.Value);
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+            var biggestSpender = string.Empty;
+            var max = -1m;
+
+            foreach (var client in clients)
+            {
+                var clientName = client.Key;
+                var productsbought = client.Value;
+                var sum = productsbought.Sum(x => inventory[x]);
+
+                if (sum > max)
+                {
+                    max = sum;
+                    biggestSpender = clientName;
+                }
+            }
+
+            var biggestSpenderTotalSpend = clients[biggestSpender].Sum(x => inventory[x]);
+            Console.WriteLine($"Biggest spender: {biggestSpender}");
+            Console.WriteLine($"^Products bought:");
+            clients[biggestSpender]
+                .Distinct()
+                .OrderByDescending(x => inventory[x])
+                .ToList()
+                .ForEach(x => Console.WriteLine($"^^^{x}: {inventory[x]:f2}"));
+            Console.WriteLine($"Total: {biggestSpenderTotalSpend:f2}");
+        }
+    }
+}
