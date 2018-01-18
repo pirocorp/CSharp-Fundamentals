31e51c6c8c0c061929849849859f3aca283aa269
pirocorp <pirocorp@abv.bg>
2018-01-18 11:24:33 +0100

    • All contestants are divided into teams • Every team should have exactly 3 participants • After a team has a total of 3 participants, any further attempt to add a participant should be ignored. • If a team has less than 3 participants – it gets disqualified and should not be printed. Until you receive the command “stop the game” you will receive lines of input in the following format: “team|player|pointsMade” The total score of the team is the sum of the points made from every player. Print every team, which has enough players in the following format: {teamPosition}. {teamName}; Players: ###{firstPlayerName}: {points} ###{secondPlayerName}: {points} ###{thirdPlayerName}: {points} Order the teams by total points in descending order and order each team’s players by the amount of points made, again in descending order. Constraints • You will not receive a player’s name, which already exists in the team it’s trying to be inserted into. • Points will be in the interval [0…2147483647]. • The total points of the teams and the points of the players in the same team will not be equal.

---
+++ 11. Lambda and LINQ - Exercises/04 SoftUni Beer Pong/SoftUni Beer Pong.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_SoftUni_Beer_Pong
+{
+    class Program
+    {
+        static void Main()
+        {
+            var teamPoints = new Dictionary<string, Dictionary<string, long>>(); //TeamName<PlayerName, PointsMade>
+            var inputData = Console.ReadLine();
+
+            while (inputData != "stop the game")
+            {
+                var tokens = inputData.Split('|').ToArray();
+                var playerName = tokens[0];
+                var teamName = tokens[1];
+                var points = long.Parse(tokens[2]);
+
+                if (!teamPoints.ContainsKey(teamName))
+                {
+                    teamPoints[teamName] = new Dictionary<string, long>();
+                }
+
+                if (teamPoints[teamName].Count < 3)
+                {
+                    teamPoints[teamName][playerName] = points;
+                }
+                
+                inputData = Console.ReadLine();
+            }
+            
+            teamPoints = teamPoints
+                .Where(x => x.Value.Count == 3)
+                .ToDictionary(x => x.Key, x => x.Value);
+            var result = teamPoints
+                .OrderByDescending(x => x.Value.Values.Sum())
+                .ToDictionary(x => x.Key, x => x.Value);
+            var count = 0;
+
+            foreach (var team in result)
+            {
+                count++;
+                var teamName = team.Key;
+                var teamPlayersList = team.Value;
+                teamPlayersList = teamPlayersList
+                    .OrderByDescending(x => x.Value)
+                    .ToDictionary(x => x.Key, x => x.Value);
+                Console.WriteLine($"{count}. {teamName}; Players:");
+
+                foreach (var player in teamPlayersList)
+                {
+                    var playerName = player.Key;
+                    var playerScore = player.Value;
+                    Console.WriteLine($"###{playerName}: {playerScore}");
+                }
+            }
+        }
+    }
+}
