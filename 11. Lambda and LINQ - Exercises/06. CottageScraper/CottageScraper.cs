4e38634ef68fd398eb82ef6451857aa2d34e5e73
pirocorp <pirocorp@abv.bg>
2018-01-18 18:33:16 +0100

    You’re a carpenter at the local woodworking shop. Your client wants you to build a wooden skyscraper/cottage. A CottageScraper, he calls it. But in order to accomplish this task, he needs the trees to be taller than a certain height. The problem is that he doesn’t know what type of tree he’ll use yet, and doesn’t know how tall he wants the CottageScraper to be. You’ve obviously got nothing better to do, so you go to work chopping down logs until he calls you with the details. Write a program which receives tree types and their lengths in the format “{type} -> {height}”. When you receive the command “chop chop”, it’s time to get paid. On the next line, you will receive the type of tree that needs to be used to build the CottageScraper. On the final input line, you will receive the minimum length per tree, needed to accomplish the task. Filter the trees based on type and minimum length, making sure that you’ll only use the trees of the specified type and minimum length. After which, calculate the total price of the CottageScraper, which includes the price of all the trees you collected up to that point. The price is calculated as being the average meters of all logs you collected, per meter of log, rounded to the second decimal place. You’re going to charge the client 100% of the price per log for logs you’ll use in the skyscraper, and 25% of the price per log for logs you won’t use for the CottageScraper. Both fees are rounded to the second decimal place. After you make the calculations, print them on the console in the following format: • On the first line of the console, print “Price per meter: ${pricePerMeter:F2}”. • On the second, print “Used logs price: ${usedLogsPrice:F2}”. • On the third line, print “Unused logs price: ${unusedLogsPrice:F2}”. • On the final line of the output, print “CottageScraper subtotal: ${subTotal:F2}”.

---
+++ 11. Lambda and LINQ - Exercises/06. CottageScraper/CottageScraper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.CottageScraper
+{
+    class CottageScraper
+    {
+        static void Main()
+        {
+            var trees = new Dictionary<string, List<long>>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "chop chop")
+            {
+                var tokens = inputData.Split(new[] {" -> "}, StringSplitOptions.RemoveEmptyEntries);
+                var logsType = tokens[0];
+                var lenght = long.Parse(tokens[1]);
+
+                if (!trees.ContainsKey(logsType))
+                {
+                    trees[logsType] = new List<long>();
+                }
+
+                trees[logsType].Add(lenght);
+                inputData = Console.ReadLine();
+            }
+
+            var neededTreeType = Console.ReadLine();
+            var neededHeightType = long.Parse(Console.ReadLine());
+
+            var pricePerMeter = Math.Round((trees.Sum(x => x.Value.Sum()) / (double)trees.Sum(x => x.Value.Count)), 2);
+            var usedLogs = trees[neededTreeType].Where(x => x >= neededHeightType).ToList();
+            var unUsedLogsOfNeededTreeType = trees[neededTreeType].Where(x => x < neededHeightType).ToList().Sum();
+            var usedLogsPrice = Math.Round((usedLogs.Sum() * pricePerMeter), 2);
+            var unUsedLogs = trees.Where(x => x.Key != neededTreeType).ToDictionary(x => x.Key, x => x.Value.Sum());
+            unUsedLogs[neededTreeType] = unUsedLogsOfNeededTreeType;
+            var unUsedLogsSum = unUsedLogs.Sum(x => x.Value);
+            var unUsedLogsPrice = Math.Round((unUsedLogsSum * pricePerMeter * 0.25), 2);
+            var totalPrice = unUsedLogsPrice + usedLogsPrice;
+
+            Console.WriteLine($"Price per meter: ${pricePerMeter:f2}");
+            Console.WriteLine($"Used logs price: ${usedLogsPrice:f2}");
+            Console.WriteLine($"Unused logs price: ${unUsedLogsPrice:f2}");
+            Console.WriteLine($"CottageScraper subtotal: ${totalPrice:f2}");
+        }
+    }
+}
