014f7fe776f96a3358e35da5799368e23feb01ef
pirocorp <pirocorp@abv.bg>
2018-01-19 19:34:00 +0100

    You’ve beaten all the content and the last thing left to accomplish is own a legendary item. However, it’s a tedious process and requires quite a bit of farming. Anyway, you are not too pretentious – any legendary will do. The possible items are: • Shadowmourne – requires 250 Shards; • Valanyr – requires 250 Fragments; • Dragonwrath – requires 250 Motes; Shards, Fragments and Motes are the key materials, all else is junk. You will be given lines of input, such as 2 motes 3 ores 15 stones. Keep track of the key materials - the first that reaches the 250 mark wins the race. At that point, print the corresponding legendary obtained. Then, print the remaining shards, fragments, motes, ordered by quantity in descending order, then by name in ascending order, each on a new line. Finally, print the collected junk items, in alphabetical order. Input • Each line of input is in format {quantity} {material} {quantity} {material} … {quantity} {material} Output • On the first line, print the obtained item in format {Legendary item} obtained! • On the next three lines, print the remaining key materials in descending order by quantity o If two key materials have the same quantity, print them in alphabetical order • On the final several lines, print the junk items in alphabetical order o All materials are printed in format {material}: {quantity} o All output should be lowercase, except the first letter of the legendary Constraints • The quantity-material pairs are between 1 and 25 per line. • The number of lines is in range [1..10] • All materials are case-insensitive. • Allowed working time: 0.25s • Allowed memory: 16 MB

---
+++ 11. Lambda and LINQ - Exercises/22. Legendary Farming/Legendary Farming.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Legendary_Farming
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inventory = new Dictionary<string, int>();
+            inventory["shards"] = 0;
+            inventory["fragments"] = 0;
+            inventory["motes"] = 0;
+
+            while (true)
+            {
+                var inputData = Console.ReadLine().ToLower().Split(' ');
+                bool found = false;
+
+                for (int i = 0; i < inputData.Length; i += 2)
+                {
+                    var quantity = int.Parse(inputData[i]);
+                    var material = inputData[i + 1];
+
+                    if (!inventory.ContainsKey(material))
+                    {
+                        inventory[material] = 0;
+                    }
+
+                    inventory[material] += quantity;
+
+                    if (inventory["shards"] >= 250)
+                    {
+                        inventory["shards"] -= 250;
+                        Console.WriteLine($"Shadowmourne obtained!");
+                        found = true;
+                        break;
+                    }
+                    else if (inventory["fragments"] >= 250)
+                    {
+                        inventory["fragments"] -= 250;
+                        Console.WriteLine($"Valanyr obtained!");
+                        found = true;
+                        break;
+                    }
+                    else if (inventory["motes"] >= 250)
+                    {
+                        inventory["motes"] -= 250;
+                        Console.WriteLine($"Dragonwrath obtained!");
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    break;
+                }
+            }
+
+            var result = new Dictionary<string, int>();
+
+            result["shards"] = inventory["shards"];
+            inventory.Remove("shards");
+            result["fragments"] = inventory["fragments"];
+            inventory.Remove("fragments");
+            result["motes"] = inventory["motes"];
+            inventory.Remove("motes");
+
+            result.OrderByDescending(x => x.Value).ThenBy(y => y.Key).ToList().ForEach(c => Console.WriteLine($"{c.Key}: {c.Value}"));
+            inventory.OrderBy(x => x.Key).ToList().ForEach(c => Console.WriteLine($"{c.Key}: {c.Value}"));
+        }
+    }
+}
