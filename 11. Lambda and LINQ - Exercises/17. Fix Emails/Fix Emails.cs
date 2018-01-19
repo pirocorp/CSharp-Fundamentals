7f41f2c0d05ceed4b301241c8102dbfcc21350de
pirocorp <pirocorp@abv.bg>
2018-01-19 15:20:42 +0100

    You are given a sequence of strings, each on a new line, until you receive the “stop” command. The first string is the name of a person. On the second line, you will receive their email. Your task is to collect their names and emails, and remove emails whose domain ends with "us" or "uk" (case insensitive).

---
+++ 11. Lambda and LINQ - Exercises/17. Fix Emails/Fix Emails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Fix_Emails
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            Dictionary<string, string> emails = new Dictionary<string, string>();
+
+            while (name != "stop")
+            {
+                string email = Console.ReadLine();
+                string subEmail = email.Substring(email.Length - 2);
+
+                if (subEmail != "us" && subEmail != "uk")
+                {
+                    emails.Add(name, email);
+                }
+                else
+                {
+                    name = Console.ReadLine();
+                }
+
+                name = Console.ReadLine();
+            }
+
+            emails.ToList().ForEach(x => Console.WriteLine($"{x.Key} -> {x.Value}"));
+
+        }
+    }
+}
