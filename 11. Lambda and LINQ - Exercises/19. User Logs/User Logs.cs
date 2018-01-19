d859e50e83f6224c10b690910a57c9b7e38720fd
pirocorp <pirocorp@abv.bg>
2018-01-19 17:06:21 +0100

    You are given an input in the format: • IP=(IP.Address) message=(A&sample&message) user=(username) Your task is to parse the IP and the username from the input and for every user, you have to display every IP from which the corresponding user has sent a message and the count of the messages sent with the corresponding IP. In the output, the usernames must be sorted alphabetically while their IP addresses should be displayed in the order of their first appearance. The output should be in the following format: username: IP => count, IP => count. For example, given the following input: • “IP=192.23.30.40 message='Hello&derps.' user=destroyer”, You will have to get the username destroyer and the IP 192.23.30.40 and display it in the following format: destroyer: 192.23.30.40 => 1. The username destroyer has sent a message from IP 192.23.30.40 once. Check the examples below. They will further clarify the assignment. Input The input comes from the console as varying number of lines. You have to parse every command until the command that follows is end. The input will be in the format displayed above, there is no need to check it explicitly. Output For every user found, you have to display each log in the format: username: IP => count, IP => count… The IP addresses must be split with a comma, and each line of IP addresses must end with a dot. Constraints • The number of commands will be in the range [1..50] • The IP addresses will be in the format of either IPv4 or IPv6. • The messages will be in the format: This&is&a&message • The username will be a string with length in the range [3..50] • Time limit: 0.3 sec. Memory limit: 16 MB.

---
+++ 11. Lambda and LINQ - Exercises/19. User Logs/User Logs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.User_Logs
+{
+    class Program
+    {
+        static void Main()
+        {
+            var logsData = new Dictionary<string, Dictionary<string, int>>(); //<username, <ip, count>
+            var inputData = Console.ReadLine();
+
+            while (inputData != "end")
+            {
+                var tokens = inputData.Split(' ');
+                var ip = tokens[0].Split(new[] {"IP="}, StringSplitOptions.RemoveEmptyEntries).First();
+                var user = tokens[2].Split(new[] {"user="}, StringSplitOptions.RemoveEmptyEntries).First();
+
+                if (!logsData.ContainsKey(user))
+                {
+                    logsData[user] = new Dictionary<string, int>();
+                }
+
+                if (!logsData[user].ContainsKey(ip))
+                {
+                    logsData[user][ip] = 0;
+                }
+
+                logsData[user][ip]++;
+                inputData = Console.ReadLine();
+            }
+
+
+            logsData.OrderBy(x => x.Key).ToList().ForEach(x =>
+            {
+                Console.WriteLine($"{x.Key}: ");
+                x.Value
+                    .ToList()
+                    .Take(x.Value.Count - 1)
+                    .ToList()
+                    .ForEach(y => Console.Write($"{y.Key} => {y.Value}, "));
+                x.Value
+                    .ToList()
+                    .Skip(x.Value.Count - 1)
+                    .ToList()
+                    .ForEach(z => Console.Write($"{z.Key} => {z.Value}."));
+            });
+        }
+    }
+}
