3912d2bd38b6a1b08248a0ad6e2c2a23232dd310
pirocorp <pirocorp@abv.bg>
2018-01-19 10:28:52 +0100

    Input You will be given several input lines in the following format: {selector} => {selectorObject}.{property} All elements of the input are strings. You need to store every lambada expression, as it is given. In some rare cases, you will receive the input “dance”, which indicates that the lambada expressions are starting to dance. When that happens, you must COPY the selectorObject ONCE with a dot (“.”) between it., so that it becomes “{selectorObject}.{selectorObject}” in every lambada expression’s condition. In other words, if you have “x => x.Value”, and you say “dance”, you should get “x => x.x.Value”. Check the examples for more info. If you meet a selectorObject which already exists, you must replace its property with the given new one. When you receive the command “lambada”, you must stop reading input. Output Print all lambada expressions in order of insertion.

---
+++ 11. Lambda and LINQ - Exercises/11. Lambada Expressions/Lambada Expressions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Lambada_Expressions
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lamdaDict = new Dictionary<string, string>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "lambada")
+            {
+                var tokens = inputData.Split(new[] { " => " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                if (tokens.Length > 1)
+                {
+                    var key = tokens[0];
+                    var value = tokens[1];
+
+                    if (!lamdaDict.ContainsKey(key))
+                    {
+                        lamdaDict[key] = value;
+                    }
+
+                    lamdaDict[key] = value;
+                }
+                else
+                {
+                    lamdaDict = lamdaDict.ToDictionary(x => x.Key, x => x.Key + "." + x.Value);
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+            lamdaDict.ToList().ForEach(x => Console.WriteLine($"{x.Key} => {x.Value}"));
+        }
+    }
+}
