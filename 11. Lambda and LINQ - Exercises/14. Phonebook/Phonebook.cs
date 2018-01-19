c0da453b2f4b84329fb898a846b55fad6683d43b
pirocorp <pirocorp@abv.bg>
2018-01-19 14:23:16 +0100

    Write a program that receives some info from the console about people and their phone numbers. Each entry should have just one name and one number (both of them strings). On each line, you will receive some of the following commands: • A {name} {phone} – adds entry to the phonebook. In case of trying to add a name that is already in the phonebook you should change the existing phone number with the new one provided. • S {name} – searches for a contact by given name and prints it in format "{name} -> {number}". In case the contact isn't found, print "Contact {name} does not exist.". • END – stop receiving more commands.

---
+++ 11. Lambda and LINQ - Exercises/14. Phonebook/Phonebook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Phonebook
+{
+    class Phonebook
+    {
+        static void Main()
+        {
+            var phoneBook = new Dictionary<string, string>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "END")
+            {
+                var tokens = inputData.Split(' ');
+                var command = tokens[0];
+                var name = tokens[1];
+
+                switch (command)
+                {
+                    case "A":
+                        var phoneNumber = tokens[2];
+                        phoneBook[name] = phoneNumber;
+                        break;
+                    case "S":
+                        if (phoneBook.ContainsKey(name))
+                        {
+                            Console.WriteLine($"{name} -> {phoneBook[name]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Contact {name} does not exist.");
+                        }
+                        break;
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+        }
+    }
+}
