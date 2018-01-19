08d01db7063704bd7294af3665a180ae40f2eb17
pirocorp <pirocorp@abv.bg>
2018-01-19 14:30:18 +0100

    Add functionality to the phonebook from the previous task to print all contacts ordered lexicographically when receive the command “ListAll”.

---
+++ 11. Lambda and LINQ - Exercises/15. Phonebook Upgrade/Phonebook Upgrade.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Phonebook_Upgrade
+{
+    class Program
+    {
+        static void Main()
+        {
+            var phoneBook = new SortedDictionary<string, string>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "END")
+            {
+                var tokens = inputData.Split(' ');
+                var command = tokens[0];
+
+                switch (command)
+                {
+                    case "A":
+                        var name = tokens[1];
+                        var phoneNumber = tokens[2];
+                        phoneBook[name] = phoneNumber;
+                        break;
+                    case "S":
+                        name = tokens[1];
+
+                        if (phoneBook.ContainsKey(name))
+                        {
+                            Console.WriteLine($"{name} -> {phoneBook[name]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Contact {name} does not exist.");
+                        }
+                        break;
+                    case "ListAll":
+                        phoneBook.ToList().ForEach(x => Console.WriteLine($"{x.Key} -> {x.Value}"));
+                        break;
+                }
+
+                inputData = Console.ReadLine();
+            }
+        }
+    }
+}
