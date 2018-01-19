01229dc97c9cd808e5280a9828a0e0e735b26b2f
pirocorp <pirocorp@abv.bg>
2018-01-19 10:30:39 +0100

    You will be given several input lines in the following format: {bank} -> {account} -> {balance} The bank and the account are strings, and the balance is a VARIABLE TYPE which is SUITABLE for storing MONEY. You should store every bank, each of its accounts, and their balances. If you are given a bank which already exists, you must ADD the new account to it. If EVEN the account which already exists, you must INCREASE the old balance, with the new given one. When you receive the command “end”, you must end the input sequence. Then you must print the data in a specified ordered format. Output You must print each bank’s accounts and balances, ordering the BANKS by the sum of all account balances, in descending order. If 2 banks have the same sum, you should print the one with the highest maximum balance in its accounts, first. The bank’s accounts must be ordered in descending order, by their balance. The balances, must be printed, as they are given. “500” = “500” … “500.00” = “500.00”.

---
+++ 11. Lambda and LINQ - Exercises/12. Ordered Banking System/Ordered Banking System.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Ordered_Banking_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
