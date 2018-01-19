ad64a8c1413d9880b6d9f3b6155a4c8ba81bd30c
pirocorp <pirocorp@abv.bg>
2018-01-19 11:04:01 +0100

    Input You will be given several input lines in the following format: {bank} -> {account} -> {balance} The bank and the account are strings, and the balance is a VARIABLE TYPE which is SUITABLE for storing MONEY. You should store every bank, each of its accounts, and their balances. If you are given a bank which already exists, you must ADD the new account to it. If EVEN the account which already exists, you must INCREASE the old balance, with the new given one. When you receive the command “end”, you must end the input sequence. Then you must print the data in a specified ordered format. Output You must print each bank’s accounts and balances, ordering the BANKS by the sum of all account balances, in descending order. If 2 banks have the same sum, you should print the one with the highest maximum balance in its accounts, first. The bank’s accounts must be ordered in descending order, by their balance. The balances, must be printed, as they are given. “500” = “500” … “500.00” = “500.00”.

--- 11. Lambda and LINQ - Exercises/12. Ordered Banking System/Ordered Banking System.cs
+++ 11. Lambda and LINQ - Exercises/12. Ordered Banking System/Ordered Banking System.cs
@@ -8,8 +8,43 @@ namespace _12.Ordered_Banking_System
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var banks = new Dictionary<string, Dictionary<string, decimal>>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "end")
+            {
+                var tokens = inputData.Split(new[] {" -> "}, StringSplitOptions.RemoveEmptyEntries);
+                var bankName = tokens[0];
+                var account = tokens[1];
+                var balance = decimal.Parse(tokens[2]);
+
+                if (!banks.ContainsKey(bankName))
+                {
+                    banks[bankName] = new Dictionary<string, decimal>();
+                }
+
+                if (!banks[bankName].ContainsKey(account))
+                {
+                    banks[bankName][account] = 0;
+                }
+
+                banks[bankName][account] += balance;
+                inputData = Console.ReadLine();
+            }
+
+            banks = banks
+                .OrderByDescending(x => x.Value.Values.Sum())
+                .ThenByDescending(y => y.Value.Values.Max())
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            banks
+                .ToList()
+                .ForEach(x => x.Value
+                    .OrderByDescending(y => y.Value)
+                    .ToList()
+                    .ForEach(y => Console.WriteLine($"{y.Key} -> {y.Value} ({x.Key})")));
         }
     }
 }
