1987d25124ba216cf6749f3a3e174f9c15c7bcf1
pirocorp <pirocorp@abv.bg>
2018-01-19 16:15:44 +0100

    You are given a sequence of people and for every person what cards he draws from the deck. The input will be separate lines in the format: • {personName}: {PT, PT, PT,… PT} Where P (2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A) is the power of the card and T (S, H, D, C) is the type. The input ends when a "JOKER" is drawn. The name can contain any ASCII symbol except ':'. The input will always be valid and in the format described, there is no need to check it. A single person cannot have more than one card with the same power and type, if they draw such a card they discard it. The people are playing with multiple decks. Each card has a value that is calculated by the power multiplied by the type. Powers 2 to 10 have the same value and J to A are 11 to 14. Types are mapped to multipliers the following way (S -> 4, H-> 3, D -> 2, C -> 1). Finally print out the total value each player has in his hand in the format: • {personName}: {value}

---
+++ 11. Lambda and LINQ - Exercises/18. Hands of Cards/Hands of Cards.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Hands_of_Cards
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cards = new Dictionary<string, List<string>>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "JOKER")
+            {
+                var tokens = inputData.Split(':');
+                var playerName = tokens[0];
+                var listOfCards = tokens[1].Trim().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (!cards.ContainsKey(playerName))
+                {
+                    cards[playerName] = new List<string>();
+                }
+
+                cards[playerName].AddRange(listOfCards);
+                cards[playerName] = cards[playerName].Distinct().ToList();
+                inputData = Console.ReadLine();
+            }
+
+            cards.ToList().ForEach(x => Console.WriteLine($"{x.Key}: {x.Value.Sum(y => GetCardPower(y))}"));
+        }
+
+        private static int GetCardPower(string card)
+        {
+            var powerOfCard = 0;
+
+            if (card.Length == 2)
+            {
+                if (!int.TryParse(card[0].ToString(), out powerOfCard))
+                {
+                    switch (card[0])
+                    {
+                        case 'J':
+                            powerOfCard = 11;
+                            break;
+                        case 'Q':
+                            powerOfCard = 12;
+                            break;
+                        case 'K':
+                            powerOfCard = 13;
+                            break;
+                        case 'A':
+                            powerOfCard = 14;
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                powerOfCard = int.Parse(card.Substring(0, 2));
+            }
+
+            var typeOfCard = 0;
+
+            switch (card[card.Length - 1])
+            {
+                case 'C':
+                    typeOfCard = 1;
+                    break;
+                case 'D':
+                    typeOfCard = 2;
+                    break;
+                case 'H':
+                    typeOfCard = 3;
+                    break;
+                case 'S':
+                    typeOfCard = 4;
+                    break;
+            }
+            
+            return powerOfCard * typeOfCard;
+        }
+    }
+}
