589d4fed2edffb16570b48ba9322e543f2c4c014
pirocorp <pirocorp@abv.bg>
2018-01-19 18:21:51 +0100

    Write a program to aggregate the logs data and print for each user the total duration of his sessions and a list of unique IP addresses in format "<user>: <duration> [<IP1>, <IP2>, …]". Order the users alphabetically. Order the IPs alphabetically. In our example, the output should be the following: • alex: 62 [10.10.17.33, 212.50.118.81] • peter: 303 [10.10.17.34, 10.10.17.35, 192.168.0.11] Input The input comes from the console. At the first line a number n stays which says how many log lines will follow. Each of the next n lines holds a log information in format <IP> <user> <duration>. The input data will always be valid and in the format described. There is no need to check it explicitly. Output Print one line for each user (order users alphabetically). For each user print its sum of durations and all of his sessions' IPs, ordered alphabetically in format <user>: <duration> [<IP1>, <IP2>, …]. Remove any duplicated values in the IP addresses and order them alphabetically (like we order strings). Constraints • The count of the order lines n is in the range [1…1000]. • The <IP> is a standard IP address in format a.b.c.d where a, b, c and d are integers in the range [0…255]. • The <user> consists of only of Latin characters, with length of [1…20]. • The <duration> is an integer number in the range [1…1000]. • Time limit: 0.3 sec. Memory limit: 16 MB.

---
+++ 11. Lambda and LINQ - Exercises/21. Logs Aggregator/Logs Aggregator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Logs_Aggregator
+{
+    class Program
+    {
+        static void Main()
+        {
+            var ipLogs = new Dictionary<string, Dictionary<string, int>>();
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split(' ');
+                var user = tokens[1];
+                var ip = tokens[0];
+                var duration = int.Parse(tokens[2]);
+
+                if (!ipLogs.ContainsKey(user))
+                {
+                    ipLogs[user] = new Dictionary<string, int>();
+                }
+
+                if (!ipLogs[user].ContainsKey(ip))
+                {
+                    ipLogs[user][ip] = 0;
+                }
+
+                ipLogs[user][ip] += duration;
+            }
+
+            ipLogs.OrderBy(x => x.Key).ToList().ForEach(x =>
+            {
+                Console.Write($"{x.Key}: {x.Value.Sum(s => s.Value)} [");
+                x.Value.ToList().OrderBy(o => o.Key).Take(x.Value.Count - 1).ToList().ForEach(v => Console.Write($"{v.Key}, "));
+                x.Value.ToList().OrderBy(o => o.Key).Skip(x.Value.Count - 1).ToList().ForEach(v => Console.Write($"{v.Key}]"));
+                Console.WriteLine();
+            });
+        }
+    }
+}
