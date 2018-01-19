945d9678db4eddc3a7cbdbab445c6fd64fba9d88
pirocorp <pirocorp@abv.bg>
2018-01-19 12:54:29 +0100

    Input You will be given several input lines containing information about collections, and LINQ methods that have been called on them, in the following format: {collection}.{method1}().{method2}()....{methodN}() The count of methods may vary. Your task is to store every collection and the methods that have been executed on it. If the collection already exists, you must ADD the new methods to it. Duplicate methods should be REMOVED. Output If you are given only a collection name, you must print the methods that have been executed on the collection, ordered by their length in descending order. If 2 methods have the same length, order them by the count of unique symbols they have in their names in descending order. Each method must be printed on a new line, with a prefixed asterisk and space (“* ”). If the collection name does NOT exist, you should IGNORE that line of input. If you are given only a digit, you must take the collection which has the most methods, and print the first N methods, with the lowest length (N being the digit given in the input). If there are less than N methods you must print all of them in the same order. NOTE: When printing, you must print only the method name, without its brackets (e.g. “First”, not “First()”). The input sequence ends when you receive the command “exit”. After the ending command, you will receive one last line in the following format: {method} {selection} You must take all collections, which CONTAIN the given method, and print them. The selection will either be “collection” or “all”. If you have “collection”, you must print only the collections’ names in the final output. If you have “all”, you must print the collections and their methods in the following format: {collection}  * {method1}  * {method2} ... The collections must be printed ordered by the count of their methods in descending order. If 2 collections have the same amount of methods, print the one whose shortest method name is longer than the other one’s shortest method name. The methods must be printed, ordered by their length in descending order.

---
+++ 11. Lambda and LINQ - Exercises/13. LINQuistics/LINQuistics.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.LINQuistics
+{
+    class LINQuistics
+    {
+        static void Main()
+        {
+            var methods = new Dictionary<string, List<string>>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "exit")
+            {
+                var tokens = inputData.Split(new[] {"().", ".", "()" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (tokens.Count > 1)
+                {
+                    var collection = tokens[0];
+                    tokens.RemoveAt(0);
+                    var listOfMethods = tokens;
+
+                    if (!methods.ContainsKey(collection))
+                    {
+                        methods[collection] = new List<string>();
+                    }
+
+                    methods[collection].AddRange(listOfMethods);
+                    methods[collection] = methods[collection].Distinct().ToList();
+                }
+                else
+                {
+                    var parsed = 0;
+
+                    if (int.TryParse(tokens[0], out parsed))
+                    {
+                        var result = methods
+                            .OrderByDescending(x => x.Value.Count)
+                            .First()
+                            .Value
+                            .OrderBy(x => x.Length)
+                            .Take(parsed)
+                            .ToList();
+
+                        Console.WriteLine("* " + String.Join($"{Environment.NewLine}* ", result));
+                    }
+                    else
+                    {
+                        var collectionName = tokens[0];
+
+                        if (methods.ContainsKey(collectionName))
+                        {
+                            var result = methods[collectionName]
+                                .OrderByDescending(x => x.Length)
+                                .ThenByDescending(x => x.Distinct().ToList().Count)
+                                .ToList();
+                            Console.WriteLine("* " + String.Join($"{Environment.NewLine}* ", result));
+                        }
+                    }
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+            inputData = Console.ReadLine();
+            var filters = inputData.Split(' ');
+            var method = filters[0];
+            var selection = filters[1];
+
+            var resultedMethodsDict =
+                methods.Where(x => x.Value.Contains(method)).ToDictionary(x => x.Key, x => x.Value);
+
+            if (selection == "all")
+            {
+                resultedMethodsDict
+                    .OrderByDescending(x => x.Value.Count)
+                    .ThenByDescending(y => y.Value.Min(z => z.Length))
+                    .ToList()
+                    .ForEach(x =>
+                    {
+                        Console.WriteLine($"{x.Key}");
+                        Console.WriteLine("* " + String.Join($"{Environment.NewLine}* ", x.Value.OrderByDescending(y => y.Length)));
+                    });
+            }
+            else
+            {
+                resultedMethodsDict
+                    .OrderByDescending(x => x.Value.Count)
+                    .ToList()
+                    .ForEach(x => Console.WriteLine($"{x.Key}"));
+            }
+        }
+    }
+}
