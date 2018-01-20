c433d8076e1021ff6ae57ef72a74b171d215d55e
pirocorp <pirocorp@abv.bg>
2018-01-20 14:54:07 +0100

    Write a program, which reads a string and skips through it, extracting a hidden message. The algorithm you have to implement is as follows: Let’s take the string “skipTest_String044170” as an example. Take every digit from the string and store it somewhere. After that, remove all the digits from the string. After this operation, you should have two lists of items: the numbers list and the non-numbers list: • Numbers list: [0, 4, 4, 1, 7, 0] • Non-numbers: [s, k, i, p, T, e, s, t, _, S, t, r, i, n, g] After that, take every digit in the numbers list and split it up into a take list and a skip list, depending on whether the digit is in an even or an odd index: • Numbers list: [0, 4, 4, 1, 7, 0] • Take list: [0, 4, 7] • Skip list: [4, 1, 0] Afterwards, iterate over both of the lists and skip {skipCount} characters from the non-numbers list, then take {takeCount} characters and store it in a result string. Note that the skipped characters are summed up as they go. The process would look like this on the aforementioned non-numbers list: 1. Skip 4 characters (total 0), take 0 characters  “skipTest_String”  Taken: “”  Result: “” 2. Skip 1 characters (total 4), take 4 characters  “skipTest_String”  Taken: “Test”  Result: “Test” 3. Skip 0 characters (total 9), take 7 characters  “skipTest_String”  Taken: “String”  Result: “TestString” After that, just print the result string on the console. Input • First line: The encrypted message as a string Output • First line: The decrypted message as a string Constraints • The count of digits in the input string will always be even. • The encrypted message will contain any printable ASCII character.

---
+++ 11. Lambda and LINQ - Exercises/30. Take Skip Rope/Take Skip Rope.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _30.Take_Skip_Rope
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var digits = input.Where(c => char.IsDigit(c)).Select(x => int.Parse(x.ToString())).ToList();
+            var stringToDecode = new string(input.Where(c => !(char.IsDigit(c))).ToArray());
+            var takeList = new List<int>();
+            var skipList = new List<int>();
+
+            for (int i = 0; i < digits.Count; i += 2)
+            {
+                takeList.Add(digits[i]);
+                skipList.Add(digits[i + 1]);
+            }
+
+            var result = string.Empty;
+            var skip =  0;
+            var take = 0;
+
+            for (int i = 0; i < takeList.Count; i++)
+            {
+                int currentSkip = skipList[i];
+                take = takeList[i];
+                result += new string (stringToDecode.Skip(skip).Take(take).ToArray());
+                skip += currentSkip + take;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
