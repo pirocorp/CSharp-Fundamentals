5c68d616fae0aaf2392db9794d32566598af52a9
pirocorp <pirocorp@abv.bg>
2018-01-20 20:56:18 +0100

    Write a program to read n points and find the closest two of them. Input The input holds the number of points n and n lines, each holding a point {X and Y coordinate}. Output • The output holds the shortest distance and the closest two points. • If several pairs of points are equally close, print the first of them (from top to bottom).

--- 12. Objects and Classes - Lab/05. Closest Two Points/Closest Two Points.cs
+++ 12. Objects and Classes - Lab/05. Closest Two Points/Closest Two Points.cs
@@ -8,9 +8,84 @@ namespace _05.Closest_Two_Points
 {
     class Program
     {
+        class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
         static void Main()
         {
+            Point[] points = ReadPoints();
+            Point[] closestPoints = FindClosestTwoPoints(points);
 
+            PrintDistance(closestPoints);
+
+            PrintPoint(closestPoints[0]);
+            PrintPoint(closestPoints[1]);
+        }
+
+        static Point[] ReadPoints()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Point[] points = new Point[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                points[i] = ReadPoint();
+            }
+
+            return points;
+        }
+
+        static void PrintPoint(Point point)
+        {
+            Console.WriteLine("({0}, {1})", point.X, point.Y);
+        }
+
+        static void PrintDistance(Point[] points)
+        {
+            var distance = CalcDistance(points[0], points[1]);
+            Console.WriteLine("{0:f3}", distance);
+        }
+
+        static Point[] FindClosestTwoPoints(Point[] points)
+        {
+            var minDistance = double.MaxValue;
+            Point[] closestTwoPoints = null;
+
+            for (int p1 = 0; p1 < points.Length; p1++)
+            {
+                for (int p2 = p1 + 1; p2 < points.Length; p2++)
+                {
+                    var distance = CalcDistance(points[p1], points[p2]);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closestTwoPoints = new Point[] {points[p1], points[p2]};
+                    }
+                }
+            }
+
+            return closestTwoPoints;
+        }
+
+        static Point ReadPoint()
+        {
+            int[] pointInfo = Console.ReadLine().Split()
+                .Select(int.Parse).ToArray();
+            var point = new Point();
+            point.X = pointInfo[0];
+            point.Y = pointInfo[1];
+            return point;
+        }
+
+        static double CalcDistance(Point p1, Point p2)
+        {
+            var deltaX = p2.X - p1.X;
+            var deltaY = p2.Y - p1.Y;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
         }
     }
 }
