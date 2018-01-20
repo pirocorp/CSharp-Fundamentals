d01077527e6d5cf109b18735ad3009dc5f4ece72
pirocorp <pirocorp@abv.bg>
2018-01-20 13:38:18 +0100

    Write a program, which validates parking for an online service. Users can register to park and unregister to leave. The system supports license plate validation. A valid license plate has the following 3 distinct characteristics: • It is always exactly 8 characters long. • Its first 2 and last 2 characters are always uppercase Latin letters • The 4 characters in the middle are always digits If any of the aforementioned conditions fails, the license plate is invalid. The program receives 2 commands: • “register {username} {licensePlateNumber}”: o The system only supports one car per user at the moment, so if a user tries to register another license plate, using the same username, the system should print: “ERROR: already registered with plate number {licensePlateNumber}” o If the license plate is invalid, the system should print: “ERROR: invalid license plate {licensePlateNumber}“ o If the user tries to register someone else’s license plate, the system should print: “ERROR: license plate {licensePlateNumber} is busy” o If the aforementioned checks pass successfully, the plate can be registered, so the system should print: “{username} registered {licensePlateNumber} successfully” • “unregister {username}”: o If the user is not present in the database, the system should print: “ERROR: user {username} not found” o If the aforementioned check passes successfully, the system should print: “user {username} unregistered successfully” After you execute all of the commands, print all the currently registered users and their license plates in the format: • “{username} => {licensePlateNumber}” Input • First line: n – number of commands – integer • Next n lines: commands in one of two possible formats: o Register: “register {username} {licensePlateNumber}” o Unregister: “unregister {username}” The input will always be valid and you do not need to check it explicitly.

---
+++ 11. Lambda and LINQ - Exercises/28. Parking Validation/Parking Validation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28.Parking_Validation
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var parkingLotDataBase = new Dictionary<string, string>(); //<username, licensePlateNumber>
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                switch (tokens[0])
+                {
+                    case "register":
+                        var username = tokens[1];
+                        var licensePlateNumber = tokens[2];
+                        ProcessRegistration(parkingLotDataBase, username, licensePlateNumber);
+                        break;
+                    case "unregister":
+                        username = tokens[1];
+                        DeleteRegistration(parkingLotDataBase, username);
+                        break;
+                }
+                
+            }
+
+            foreach (var user in parkingLotDataBase)
+            {
+                var username = user.Key;
+                var licensePlate = user.Value;
+                Console.WriteLine($"{username} => {licensePlate}");
+            }
+        }
+
+        private static void DeleteRegistration(Dictionary<string, string> parkingLotDataBase, string username)
+        {
+            if (!parkingLotDataBase.ContainsKey(username))
+            {
+                Console.WriteLine($"ERROR: user {username} not found");
+                return;
+            }
+
+            parkingLotDataBase.Remove(username);
+            Console.WriteLine($"user {username} unregistered successfully");
+        }
+
+        private static void ProcessRegistration(Dictionary<string, string> parkingLotDataBase, string username, string licensePlateNumber)
+        {
+            if (parkingLotDataBase.ContainsKey(username))
+            {
+                var currentLicenseNumber = parkingLotDataBase[username];
+                Console.WriteLine($"ERROR: already registered with plate number {currentLicenseNumber}");
+                return;
+            }
+
+            if (!LicensePlateValidation(licensePlateNumber))
+            {
+                Console.WriteLine($"ERROR: invalid license plate {licensePlateNumber}“");
+                return;
+            }
+
+            if (parkingLotDataBase.ContainsValue(licensePlateNumber))
+            {
+                Console.WriteLine($"ERROR: license plate {licensePlateNumber} is busy");
+                return;
+            }
+
+            parkingLotDataBase[username] = licensePlateNumber;
+            Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+        }
+
+        private static bool LicensePlateValidation(string licensePlateNumber)
+        {
+            if (licensePlateNumber.Length != 8)
+            {
+                return false;
+            }
+
+            var firstTwoCharacters = licensePlateNumber.Substring(0, 2);
+            var lastTwoCharacters = licensePlateNumber.Substring(licensePlateNumber.Length - 2, 2);
+
+            if (!(firstTwoCharacters.All(c => char.IsUpper(c)) && lastTwoCharacters.All(c => char.IsUpper(c))))
+            {
+                return false;
+            }
+
+            var midleCharacters = new string(licensePlateNumber.Skip(2).Take(4).ToArray());
+
+            if (!(midleCharacters.All(c => char.IsDigit(c))))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
