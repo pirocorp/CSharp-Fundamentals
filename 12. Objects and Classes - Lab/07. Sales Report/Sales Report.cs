7288b43bd2156a51fdbb71519cd577a63da2871d
pirocorp <pirocorp@abv.bg>
2018-01-20 21:23:56 +0100

    Write a class Sale holding the following data: town, product, price, quantity. Read a list of sales and calculate and print the total sales by town as shown in the output. Order alphabetically the towns in the output.

---
+++ 12. Objects and Classes - Lab/07. Sales Report/Sales Report.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Sales_Report
+{
+    class Program
+    {
+        class Sale
+        {
+            public string Town { get; set; }
+            public string Product { get; set; }
+            public decimal Price { get; set; }
+            public decimal Quantity { get; set; }
+        }
+
+        static Sale ReadSale()
+        {
+            string[] items = Console.ReadLine().Split();
+
+            return new Sale()
+            {
+                Town = items[0],
+                Product = items[1],
+                Price = decimal.Parse(items[2]),
+                Quantity = decimal.Parse(items[3]),
+            };
+        }
+
+        static void Main()
+        {
+            Sale[] sales = ReadSales();
+            var towns = sales.Select(s => s.Town).Distinct().OrderBy(t => t);
+            foreach (string town in towns)
+            {
+                var salesByTown = sales.Where(s => s.Town == town)
+                    .Select(s => s.Price * s.Quantity);
+                Console.WriteLine("{0} -> {1:f2}", town, salesByTown.Sum());
+            }
+
+        }
+
+        static Sale[] ReadSales()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Sale[] sales = new Sale[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                sales[i] = ReadSale();
+            }
+
+            return sales;
+        }
+    }
+}
