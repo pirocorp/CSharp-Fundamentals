48711f810a0ec436497d62711dce590ee9a6ec74
pirocorp <pirocorp@abv.bg>
2018-01-20 12:14:49 +0100

    The immune system can calculate the virus’ strength before it fights it. It is the sum of all the virus name’s letters’ ASCII codes, divided by 3. The immune system can also calculate the time it takes to defeat a virus in seconds. It is equal to the virus strength, multiplied by the length of the virus’ name. When you calculate the time to defeat the virus, convert it to minutes and seconds (500  8m 20s). Do not use any leading zeroes for the minutes and seconds. The virus is fought according to these conditions: • If the immune system defeats the virus, print: “{virusName} defeated in {virusDefeatMinutes}m {virusDefeatSeconds}s.” • If the virus’ strength is more than the immune system’s strength, print “Immune System Defeated.” and exit the program. After a virus is defeated, the immune system regains 20% of its strength. If the 20 percent exceeds the initial health of the immune system, set it to the initial health instead. Example: The virus “flu1”: • Virus Strength: 102 (f) + 108 (l) + 117 (u) + 49 (1) = 376 / 3 = 125.33 = 125. • Time to defeat: 125 * 4 (virus name length) = 500 seconds  8m 20s. Example 2: Encountering “flu1” a second time: • Time to defeat: (125 * 4) / 3 = 166.66  166 seconds If you encounter a virus any subsequent times, do not decrease its time to defeat further. When you receive the line “end”, print the status of the immune system in the format “Final Health: {finalHealth}”. Input • First line: the initial health of the immune system • On new lines, until you receive “end”: virus names Output A defeated virus’ output looks like this: • First line: “Virus {virusName}: {virusStrength} => {virusDefeatSeconds}” • Second line: “{virusName} defeated in {defeatMins}m {defeatSecs}s.” • Third line: “Remaining health: {remainingHealth}”. The remaining health is printed before its regeneration.

---
+++ 11. Lambda and LINQ - Exercises/26. Immune System/Immune System.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26.Immune_System
+{
+    class Program
+    {
+        static void Main()
+        {
+            var viruses = new List<string>();
+            var health = int.Parse(Console.ReadLine());
+            var inputData = Console.ReadLine();
+            double remainingHealth = health;
+
+            while (inputData != "end")
+            {
+                var virusName = inputData;
+                double virusStrengthOriginal = (int)(inputData.Sum(x => x) / 3.0);
+                var virusStrength = virusStrengthOriginal;
+                if (viruses.Contains(virusName))
+                {
+                    virusStrength = (virusStrength / 3.0);
+                }
+                else
+                {
+                    viruses.Add(virusName);
+                }
+
+                var TimeToDefeatInSec = (int)(virusStrength * virusName.Length);
+                Console.WriteLine($"Virus {virusName}: {virusStrengthOriginal} => {TimeToDefeatInSec}");
+
+                if (TimeToDefeatInSec < remainingHealth)
+                {
+                    var defeatMins = (int)TimeToDefeatInSec / 60;
+                    var defeatSecs = (int)TimeToDefeatInSec % 60;
+                    Console.WriteLine($"{virusName} defeated in {defeatMins}m {defeatSecs}s.");
+                    remainingHealth -= TimeToDefeatInSec;
+                    Console.WriteLine($"Remaining health: {remainingHealth}");
+                }
+                else
+                {
+                    Console.WriteLine("Immune System Defeated.");
+                    return;
+                }
+
+                remainingHealth = Math.Min(health, (int)Math.Floor(remainingHealth * 1.2));
+                inputData = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Final Health: {remainingHealth}");
+        }
+    }
+}
