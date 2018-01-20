5c9b42a63e2c229308369b24e394d02ea873371b
pirocorp <pirocorp@abv.bg>
2018-01-20 20:48:31 +0100

    Write a method to calculate the distance between two points p1 {x1, y1} and p2 {x2, y2}. Write a program to read two points (given as two integers) and print the Euclidean distance between them.

--- 12. Objects and Classes - Lab/04. Distance between Points/Distance between Points.cs
+++ 12. Objects and Classes - Lab/04. Distance between Points/Distance between Points.cs
@@ -8,8 +8,40 @@ namespace _04.Distance_between_Points
 {
     class Program
     {
-        static void Main(string[] args)
+        class Point
         {
+            public int X { get; set; }
+            public int Y { get; set; }
         }
+        
+        static void Main()
+        {
+            // Reads both points separately
+            Point p1 = ReadPoint();
+            Point p2 = ReadPoint();
+
+            // Calculate the distance between them
+            double distance = CalcDistance(p1, p2);
+
+            // Print the distance
+            Console.WriteLine("Distance: {0:f3}", distance);
+        }
+
+        static Point ReadPoint()
+        {
+            int[] pointInfo = Console.ReadLine().Split()
+                .Select(int.Parse).ToArray();
+            Point point = new Point();
+            point.X = pointInfo[0];
+            point.Y = pointInfo[1];
+            return point;
+        }
+        static double CalcDistance(Point p1, Point p2)
+        {
+            int deltaX = p2.X - p1.X;
+            int deltaY = p2.Y - p1.Y;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+
     }
 }
