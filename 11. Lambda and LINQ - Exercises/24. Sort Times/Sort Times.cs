aaac15c2b371b953b2ebe2071f55c1b7117423b2
pirocorp <pirocorp@abv.bg>
2018-01-20 10:56:22 +0100

    Write a program, which receives a list of times (space-separated, 24-hour format) and sorts them in ascending order. Print the sorted times comma-separated. Example: 06:55, 02:30, 23:11  02:30, 06:55, 21:11

---
+++ 11. Lambda and LINQ - Exercises/24. Sort Times/Sort Times.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Sort_Times
+{
+    class Program
+    {
+        static void Main()
+        {
+            var timeList = Console.ReadLine().Split(' ').ToList();
+            timeList.Sort();
+            Console.WriteLine(String.Join(", ", timeList));
+        }
+    }
+}
