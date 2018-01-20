5d778765cee7e3d10fedd54d986f5b37f59d289d
pirocorp <pirocorp@abv.bg>
2018-01-20 20:41:05 +0100

    You are given a list of words in one line. Randomize their order and print each word at a separate line.

--- 12. Objects and Classes - Lab/02. Randomize Words/Randomize Words.cs
+++ 12. Objects and Classes - Lab/02. Randomize Words/Randomize Words.cs
@@ -8,8 +8,20 @@ namespace _02.Randomize_Words
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string[] words = Console.ReadLine().Split(' ');
+            var rnd = new Random();
+
+            for (int pos1 = 0; pos1 < words.Length; pos1++)
+            {
+                int pos2 = rnd.Next(words.Length);
+                var swap = words[pos1];
+                words[pos1] = words[pos2];
+                words[pos2] = swap;
+            }
+            Console.WriteLine(string.Join("\r\n", words));
+
         }
     }
 }
