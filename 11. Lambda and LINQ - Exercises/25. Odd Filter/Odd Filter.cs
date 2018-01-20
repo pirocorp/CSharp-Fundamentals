f13d36c1c23efea76c26f3b8205908712571fdf9
pirocorp <pirocorp@abv.bg>
2018-01-20 11:05:08 +0100

    Write a program, which receives an array of integers (space-separated), removes all the odd numbers, then converts the remaining numbers to odd numbers, based on these conditions: • If the number is larger than the average of the collection of remaining numbers, add 1 to it. • If the number is smaller than the average of the collection of remaining numbers, subtract 1 from it. After you convert all of the elements to odd numbers, print them on the console (space-separated).

---
+++ 11. Lambda and LINQ - Exercises/25. Odd Filter/Odd Filter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _25.Odd_Filter
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .ToList();
+
+            var average = numbers.Average();
+            numbers = numbers.Select(x => x <= average ? x - 1 : x + 1).ToList();
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
--- 11. Lambda and LINQ - Exercises/25. Odd Filter/Properties/AssemblyInfo.cs
+++ 11. Lambda and LINQ - Exercises/25. Odd Filter/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("24. Sort Times")]
+[assembly: AssemblyTitle("25. Odd Filter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("24. Sort Times")]
+[assembly: AssemblyProduct("25. Odd Filter")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("18cf6bee-ad53-423e-b08c-1e0de6564459")]
+[assembly: Guid("4019b4a9-63dd-4bbd-8b40-2f44c3cbb0dc")]
 
 // Version information for an assembly consists of the following four values:
 //
