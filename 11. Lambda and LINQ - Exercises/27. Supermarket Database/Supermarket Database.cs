019c4f0604d3e3e27082d6ad32c82c4b4935385b
pirocorp <pirocorp@abv.bg>
2018-01-20 12:36:43 +0100

    Write a program, which keeps information about products and their prices. Each product has a name, a price and its quantity. If the product doesn’t exist in the database yet, add it with its starting quantity. If you receive a product, which already exists in the database, increase its quantity by the input quantity and if its price is different, replace the price as well. You will receive products’ names, prices and quantities on new lines. Until you receive the command “stocked”, keep adding items to the database. When you do receive the command “stocked”, print the items with their names, prices, quantities and total price of all the products with that name. When you’re done printing the items, print the grand total price of all the items. Note: The grand total is calculated, based on the latest price of the products. Input • Until you receive “stocked”, the products come in the format: “{name} {price} {quantity}”. • The product data is always delimited by a single space. Output • Print information about each product, following the format: “{name}: ${price:F2} * {quantity} = ${total:F2}” • On the next line, print 30 dashes. • On the final line, print the grand total in the following format: “Grand Total: ${grandTotal:F2}”

---
+++ 11. Lambda and LINQ - Exercises/27. Supermarket Database/Supermarket Database.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27.Supermarket_Database
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputData = Console.ReadLine();
+            var inventory = new Dictionary<string, decimal[]>();
+
+            while (inputData != "stocked")
+            {
+                var tokens = inputData.Split(new []{ ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var item = tokens[0];
+                var price = decimal.Parse(tokens[1]);
+                var quantaty = int.Parse(tokens[2]);
+
+                if (!inventory.ContainsKey(item))
+                {
+                    inventory[item] = new decimal [] {0, 0};
+                }
+
+                inventory[item][0] = price;
+                inventory[item][1] += quantaty;
+                inputData = Console.ReadLine();
+            }
+
+            foreach (var item in inventory)
+            {
+                var itemName = item.Key;
+                var itemPrice = item.Value[0];
+                var itemQuantaty = item.Value[1];
+                var total = itemPrice * itemQuantaty;
+                Console.WriteLine($"{itemName}: ${itemPrice:f2} * {itemQuantaty} = ${total:f2}");
+            }
+
+            Console.WriteLine(new string('-', 30));
+            var grandTotal = inventory.Sum(x => x.Value[0] * x.Value[1]);
+            Console.WriteLine($"Grand Total: ${grandTotal:F2}");
+        }
+    }
+}
