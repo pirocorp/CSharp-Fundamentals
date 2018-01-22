268a2a3b5e982b45c27b215adee5f97a11145b1f
pirocorp <pirocorp@abv.bg>
2018-01-22 11:24:40 +0100

    Remember the Ordered Banking System problem? Well, now you have to optimize it with classes. Create a class BankAccount which has a Name (string), Bank (string) and Balance (decimal). You will receive several input lines, containing information in the following way: {bank} | {accountName} | {accountBalance} You need to store every given Account. When you receive the command “end” you must stop the input sequence. Then you must print all Accounts, ordered by their balance, in descending order, and then by length of the bank name, in ascending order. The accounts must be printed in the following way “{accountName} -> {balance} ({bank})”. Note: Numbers must be printed, exactly as they are entered. No formatting is required.

---
+++ 12. Objects and Classes - Exercises/02. Optimized Banking System/Optimized Banking System.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Optimized_Banking_System
+{
+    class Program
+    {
+        public class BankAccount
+        {
+            public string Bank { get; set; }
+            public string Name { get; set; }
+            public decimal Balance { get; set; }
+
+            public static BankAccount Parse(string inputData)
+            {
+                var tokens = inputData.Split(new[] {" | "}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var bank = tokens[0];
+                var accountName = tokens[1];
+                var balance = decimal.Parse(tokens[2]);
+
+                var result = new BankAccount()
+                {
+                    Bank = bank,
+                    Name = accountName,
+                    Balance = balance
+                };
+
+                return result;
+            }
+        }
+
+        static void Main()
+        {
+            var inputData = Console.ReadLine();
+            var accountsList = new List<BankAccount>();
+
+            while (inputData != "end")
+            {
+                var currentAccount = BankAccount.Parse(inputData);
+                accountsList.Add(currentAccount);
+                inputData = Console.ReadLine();
+            }
+
+            accountsList = accountsList.OrderByDescending(x => x.Balance).ThenBy(x => x.Bank.Length).ToList();
+            accountsList.ForEach(x => Console.WriteLine($"{x.Name} -> {x.Balance} ({x.Bank})"));
+        }
+    }
+}
