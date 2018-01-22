dd7392fc875888819516c2387ed07e44d6fdf808
pirocorp <pirocorp@abv.bg>
2018-01-22 10:57:27 +0100

    Exercises are fun … Especially when they represent a problem from your exercises. Implement a class Exercise, which has a Topic (string), a CourseName (string), a JudgeContestLink (string), and a Problems (collection of strings). You will receive several input lines containing information about a single exercise in the following format: {topic} -> {courseName} -> {judgeContestLink} -> {problem1}, {problem2}. . . You need to store every exercise in a Collection of Exercises. When you receive the command “go go go”, you end the input sequence. You must print every exercise, in the following format: “Exercises: {topic}  Problems for exercises and homework for the “{courseName}” course @ SoftUni.  Check your solutions here: {judgeContestLink}.  1. {problem1}  2. {problem2}  . . .”

---
+++ 12. Objects and Classes - Exercises/01. Exercises/Exercises.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Exercises
+{
+    using System;
+
+    public class Exercises
+    {
+        public class Exercise
+        {
+            public string Topic { get; set; }
+            public string CourseName { get; set; }
+            public string JudgeContestLink { get; set; }
+            public List<string> Problems { get; set; }
+
+            public static Exercise Parse(string inputData)
+            {
+                //{topic} -> {courseName} -> {judgeContestLink} -> {problem1}, {problem2}
+                var tokens = inputData.Split(new[] {" -> "}, StringSplitOptions.RemoveEmptyEntries);
+                var topic = tokens[0];
+                var courseName = tokens[1];
+                var judgeContestLink = tokens[2];
+                var problems = tokens[3].Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                var result = new Exercise()
+                {
+                    Topic = topic,
+                    CourseName = courseName,
+                    JudgeContestLink = judgeContestLink,
+                    Problems = problems
+                };
+
+                return result;
+            }
+        }
+
+        public static void Main()
+        {
+            var listOfExercises = new List<Exercise>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "go go go")
+            {
+                var currentExercise = Exercise.Parse(inputData);
+                listOfExercises.Add(currentExercise);
+                inputData = Console.ReadLine();
+            }
+
+            foreach (var exercise in listOfExercises)
+            {
+                var topic = exercise.Topic;
+                var courseName = exercise.CourseName;
+                var judgeContestLink = exercise.JudgeContestLink;
+                var problems = exercise.Problems;
+                Console.WriteLine($"Exercises: {topic}");
+                Console.WriteLine($"Problems for exercises and homework for the \"{courseName}\" course @ SoftUni.");
+                Console.WriteLine($"Check your solutions here: {judgeContestLink}");
+
+                for (int i = 0; i < problems.Count; i++)
+                {
+                Console.WriteLine($"{i + 1}. {problems[i]}");
+
+                }
+            }
+        }
+    }
+}
