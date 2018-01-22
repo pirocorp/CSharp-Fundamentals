46d5bb4e976ddfabce11f15b60201ed6abff9c55
pirocorp <pirocorp@abv.bg>
2018-01-22 14:34:22 +0100

    Create a class Box, which will represent a rectangular box. The Box should have UpperLeft (Point), UpperRight (Point), BottomLeft (Point), BottomRight (Point). Create, or use from the Lab, the class Point which has X (int) and Y (int) – coordinates in 2D space. Move the CalculateDistance() method in the Point class, exactly as it is. Then use “Point.CalculateDistance(point1, point2)” signature, to use the method. Create 2 methods in the Box class: public static int CalculatePerimeter(int width, int height) public static int CalculateArea(int width, int height). Make them return an integer, representing the perimeter and area of the box. The formulas are respectively – (2 * Width + 2 * Height) and (Width * Height). The Width is the distance between the UpperLeft and the UpperRight Points, and ALSO – the Bottomleft and the BottomRight Points. The Height is the distance between the UpperLeft and the BottomLeft Points, and ALSO – the UpperRight and the BottomRight Points. You will receive several input lines in the following format: {X1}:{Y1} | {X2}:{Y2} | {X3}:{Y3} | {X4}:{Y4} Those will be the coordinates to UpperLeft, UpperRight, BottomLeft and BottomRight (IN THE SAME ORDER). When you receive the command “end”. You must print all Boxes in the following format: “Box: {width}, {height}  Perimeter: {perimeter}  Area: {area}”

---
+++ 12. Objects and Classes - Exercises/05. Boxes/Boxes.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Boxes
+{
+    class Boxes
+    {
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public static double CalculateDistance(Point p1, Point p2)
+            {
+                var deltaX = p2.X - p1.X;
+                var deltaY = p2.Y - p1.Y;
+                return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            }
+
+            public static Point Parse(string inputData)
+            {
+                var pointInfo = inputData.Split(new []{ ':' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var point = new Point()
+                {
+                    X = pointInfo[0],
+                    Y = pointInfo[1]
+                };
+
+                return point;
+            }
+        }
+
+        public class Box
+        {
+            public Point UpperLeft { get; set; }
+            public Point UpperRight { get; set; }
+            public Point BottomLeft { get; set; }
+            public Point BottomRight { get; set; }
+
+            public double Width
+            {
+                get
+                {
+                    return Point.CalculateDistance(UpperLeft, UpperRight);
+                }
+            }
+
+            public double Height
+            {
+                get
+                {
+                    return Point.CalculateDistance(UpperLeft, BottomLeft);
+                }
+            }
+
+            public static Box Parse(string inputData)
+            {
+                var tokens = inputData
+                    .Split(new[] {'|'}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+                var upperLeft = Point.Parse(tokens[0]);
+                var upperRight = Point.Parse(tokens[1]);
+                var bottomLeft = Point.Parse(tokens[2]);
+                var bottomRight = Point.Parse(tokens[3]);
+
+                var result = new Box()
+                {
+                    UpperLeft = upperLeft,
+                    UpperRight = upperRight,
+                    BottomLeft = bottomLeft,
+                    BottomRight = bottomRight
+                };
+
+                return result;
+            }
+
+            public static double CalculatePerimeter(double width, double height)
+            {
+                return (2 * width + 2 * height);
+            }
+
+            public static double CalculateArea(double width, double height)
+            {
+                return (width * height);
+            }
+        }
+
+
+        static void Main()
+        {
+            var inputData = Console.ReadLine();
+            var listOfBoxes = new List<Box>();
+
+            while (inputData != "end")
+            {
+                listOfBoxes.Add(Box.Parse(inputData));
+                inputData = Console.ReadLine();
+            }
+
+            foreach (var box in listOfBoxes)
+            {
+                Console.WriteLine($"Box: {box.Width}, {box.Height}");
+                Console.WriteLine($"Perimeter: {Box.CalculatePerimeter(box.Width, box.Height)}");
+                Console.WriteLine($"Area: {Box.CalculateArea(box.Width, box.Height)}");
+            }
+        }
+    }
+}
