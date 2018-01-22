73c176b14b870477b6845ce2a6d27266397c9f2b
pirocorp <pirocorp@abv.bg>
2018-01-22 22:29:00 +0100

    Andrey is the guy who gives us food and drinks at the game bar. He likes to play billiard. Since you are nice guy you want to help him play more of his favorite game. You decide to create a program which will help him to take orders faster and generate billing information. First you will receive an integer - the amount of entities with prices (separated by “-”). Then you will receive a list of clients. For every customer, you will receive what to buy and how much. When you receive the command “end of clients” you should display information about every client as described below. After that, print how much total money were spent while Andrey was playing billiard. Constraints • If an entity is added more than once you should override the previous price. • If a buyer tries to buy an entity that does not exist - you should ignore that line. • Buyers should be ordered by their name in ascending order. • All floating-point digits must be rounded to 2 digits after the decimal separator. • In the end, every buyer’s respective bill should be summed. • The quantities will be integers. The prices will be floating-point numbers.

---
+++ 12. Objects and Classes - Exercises/13. Andrey and Billiard/Andrey and Billiard.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Andrey_and_Billiard
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var priceList = new Dictionary<string, decimal>();
+            var inputData = string.Empty;
+            var clientList = new List<Customer>();
+            GeneratePriceList(n, priceList);
+            inputData = Console.ReadLine();
+            GenerateClientList(inputData, priceList, clientList);
+
+            foreach (var client in clientList.OrderBy(x => x.Name))
+            {
+                var clientName = client.Name;
+                var productsBought = client.BoughtProducts;
+                var bill = client.Bill(priceList);
+                Console.WriteLine($"{clientName}");
+                productsBought.ToList().ForEach(x => Console.WriteLine($"-- {x.Key} - {x.Value}"));
+                Console.WriteLine($"Bill: {bill:f2}");
+            }
+
+            Console.WriteLine($"Total bill: {clientList.Sum(x => x.Bill(priceList)):f2}");
+        }
+
+        private static void GenerateClientList(string inputData, Dictionary<string, decimal> priceList, List<Customer> clientList)
+        {
+            while (inputData != "end of clients")
+            {
+                var tokens = inputData.Split(new[] {'-', ','}, StringSplitOptions.RemoveEmptyEntries);
+                var clientName = tokens[0];
+                var productName = tokens[1];
+                var quantaty = int.Parse(tokens[2]);
+
+                if (priceList.ContainsKey(productName))
+                {
+                    if (!clientList.Any(x => x.Name == clientName))
+                    {
+                        var currentClient = new Customer()
+                        {
+                            Name = clientName,
+                            BoughtProducts = new Dictionary<string, int>()
+                        };
+
+                        clientList.Add(currentClient);
+                    }
+
+                    if (clientList.Count > 0)
+                    {
+                        var currentClient = clientList.Where(x => x.Name == clientName).First();
+
+                        if (!currentClient.BoughtProducts.ContainsKey(productName))
+                        {
+                            currentClient.BoughtProducts[productName] = 0;
+                        }
+
+                        currentClient.BoughtProducts[productName] += quantaty;
+                    }
+                }
+
+                inputData = Console.ReadLine();
+            }
+        }
+
+        private static void GeneratePriceList(int n, Dictionary<string, decimal> priceList)
+        {
+            string inputData;
+            for (int i = 0; i < n; i++)
+            {
+                inputData = Console.ReadLine();
+                var tokens = inputData.Split(new[] {'-'}, StringSplitOptions.RemoveEmptyEntries);
+                var productName = tokens[0];
+                var price = decimal.Parse(tokens[1]);
+
+                if (!priceList.ContainsKey(productName))
+                {
+                    priceList[productName] = price;
+                }
+
+                priceList[productName] = price;
+            }
+        }
+    }
+
+}
---
+++ 12. Objects and Classes - Exercises/13. Andrey and Billiard/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Andrey_and_Billiard
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public Dictionary<string, int> BoughtProducts { get; set; }
+
+        public decimal Bill(Dictionary<string, decimal> priceList)
+        {
+            var result = 0m;
+
+            foreach (var product in BoughtProducts)
+            {
+                var productName = product.Key;
+                var productQuantaty = product.Value;
+                var productPrice = priceList[productName];
+
+                result += productPrice * productQuantaty;
+            }
+
+            return result;
+        }
+    }
+}
