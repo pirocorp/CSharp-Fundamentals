666d289171a0aefb08fe354d9d4243fefad5bec7
pirocorp <pirocorp@abv.bg>
2018-01-22 19:59:15 +0100

    Define a class Student, which holds the following information about students: name, list of grades and average grade (calculated property, read-only). A single grade will be in range [2…6], e.g. 3.25 or 5.50. Read a list of students and print the students that have average grade ≥ 5.00 ordered by name (ascending), then by average grade (descending). Print the student name and the calculated average grade.

---
+++ 12. Objects and Classes - Exercises/10. Average Grades/Average Grades.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.Average_Grades
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split(' ').ToList();
+                var name = tokens[0];
+                var listOfGrades = tokens.Skip(1).Select(double.Parse).ToList();
+                var currentStudent = new Student()
+                {
+                    Name = name,
+                    Grades = listOfGrades
+                };
+                students.Add(currentStudent);
+            }
+
+            var result = students
+                .Where(x => x.AverageGrade >= 5)
+                .OrderBy(x => x.Name)
+                .ThenByDescending(x => x.AverageGrade)
+                .ToList();
+
+            result.ForEach(x => Console.WriteLine($"{x.Name} -> {x.AverageGrade:f2}"));
+        }
+    }
+}
