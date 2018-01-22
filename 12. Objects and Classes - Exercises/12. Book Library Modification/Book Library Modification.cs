cee127ff2550bd5d6caffff4a38c52939d773e33
pirocorp <pirocorp@abv.bg>
2018-01-22 21:20:05 +0100

    Use the classes from the previous problem and make a program that read a list of books and print all titles released after given date ordered by date and then by title lexicographically. The date is given in the format “dd.MM.yyyy” at the last line in the input.

---
+++ 12. Objects and Classes - Exercises/12. Book Library Modification/Book Library Modification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using _11.Book_Library;
+
+namespace _12.Book_Library_Modification
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var books = new List<Book>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentBook = Book.Parse(Console.ReadLine());
+                books.Add(currentBook);
+            }
+
+            var format = "dd.MM.yyyy";
+            var startDate = DateTime.ParseExact(Console.ReadLine(), format, CultureInfo.InvariantCulture);
+
+            books.Where(x => x.ReleaseDate > startDate)
+                .OrderBy(x => x.ReleaseDate)
+                .ThenBy(x => x.Title)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x.Title} -> {x.ReleaseDate.Day}.{x.ReleaseDate.Month:d2}.{x.ReleaseDate.Year}"));
+        }
+    }
+}
