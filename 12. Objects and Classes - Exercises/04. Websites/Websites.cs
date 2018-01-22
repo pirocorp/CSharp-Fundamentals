ac04983b51c54b5db157bc07d04b6089f0e1efda
pirocorp <pirocorp@abv.bg>
2018-01-22 13:46:29 +0100

    You have been tasked to create an ordered database of websites. For the task you will need to create a class Website, which will have a Host, a Domain and Queries. The Host and the Domain are simple strings. The Queries, is Collections of strings. You will be given several input lines in the following format: {host} | {domain} | {query1,query2. . .} Note: There will always be a host and a domain, but there might NOT be ANY queries. The input sequence ends, when you receive the command “end”. Then you must print all websites in the following format: https://www.{host}.{domain}/query?=[{query1]&[{query2}]&[query3]. . . In case there are NO queries, just print: https://www.{host}.{domain}

---
+++ 12. Objects and Classes - Exercises/04. Websites/Websites.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Websites
+{
+    class Websites
+    {
+        public class Website
+        {
+            public string Host { get; set; }
+            public string Domain { get; set; }
+            public List<string> Queries { get; set; }
+
+            public static Website Parse(string inputData)
+            {
+                var tokens = inputData.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+                var host = tokens[0];
+                var domain = tokens[1];
+                var queries = new List<string>();
+
+                if (tokens.Length > 2)
+                {
+                    queries = tokens[2].Split(new []{ ',' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToList();
+                }
+
+                var result = new Website()
+                {
+                    Host = host,
+                    Domain = domain,
+                    Queries = queries
+                };
+
+                return result;
+            }
+        }
+
+        static void Main()
+        {
+            var inputData = Console.ReadLine();
+            var websites = new List<Website>();
+
+            while (inputData != "end")
+            {
+                websites.Add(Website.Parse(inputData));
+                inputData = Console.ReadLine();
+            }
+
+            foreach (var website in websites)
+            {
+                if (website.Queries.Count > 0)
+                {
+                    var queries = website.Queries.Select(x => $"[{x}]").ToList();
+                    Console.WriteLine($"https://www.{website.Host}.{website.Domain}/query?={String.Join("&", queries)}");
+                }
+                else
+                {
+                    Console.WriteLine($"https://www.{website.Host}.{website.Domain}");
+                }
+                
+            }
+        }
+    }
+}
