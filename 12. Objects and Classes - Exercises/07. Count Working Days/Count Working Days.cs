6b51bc40356a56a0ef113c8e1da4f4eec325a10a
pirocorp <pirocorp@abv.bg>
2018-01-22 18:21:35 +0100

    Write a program that reads two dates in format dd-MM-yyyy and prints the number of working days between these two dates inclusive. Non-working days are: • All days that are Saturday or Sunday. • All days that are official holidays in Bulgaria: o New Year Eve (1 Jan) o Liberation Day (3 March) o Worker’s day (1 May) o Saint George’s Day (6 May) o Saints Cyril and Methodius Day (24 May) o Unification Day (6 Sept) o Independence Day (22 Sept) o National Awakening Day (1 Nov) o Christmas (24, 25 and 26 Dec) All days not mentioned above are working and should count.

---
+++ 12. Objects and Classes - Exercises/07. Count Working Days/Count Working Days.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Count_Working_Days
+{
+    class Program
+    {
+        static void Main()
+        {
+            var format = "dd-MM-yyyy";
+            var startDateString = Console.ReadLine();
+            var endDateString = Console.ReadLine();
+            var startDate = DateTime.ParseExact(startDateString, format, CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(endDateString, format, CultureInfo.InvariantCulture);
+
+
+
+            var workingDaysCounter = 0;
+
+            for (var i = startDate; i <= endDate; i = i.AddDays(1))
+            {
+                if (!IsHoliday(i))
+                {
+                    workingDaysCounter++;
+                }
+            }
+
+            Console.WriteLine(workingDaysCounter);
+        }
+
+        public static bool IsHoliday(DateTime date)
+        {
+            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return true;
+            }
+
+            DateTime[] officialHolidays =
+            {
+                new DateTime(2016, 1, 1),
+                new DateTime(2016, 3, 3),
+                new DateTime(2016, 5, 1),
+                new DateTime(2016, 5, 6),
+                new DateTime(2016, 5, 24),
+                new DateTime(2016, 9, 6),
+                new DateTime(2016, 9, 22),
+                new DateTime(2016, 11, 1),
+                new DateTime(2016, 12, 24), 
+                new DateTime(2016, 12, 25), 
+                new DateTime(2016, 12, 26), 
+            };
+
+            var dateDay = date.Day;
+            var dateMonth = date.Month;
+
+            for (int i = 0; i < officialHolidays.Length; i++)
+            {
+                var currentHolidayDay = officialHolidays[i].Day;
+                var currentHolidayMonth = officialHolidays[i].Month;
+
+                if (dateDay == currentHolidayDay && dateMonth == currentHolidayMonth)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
