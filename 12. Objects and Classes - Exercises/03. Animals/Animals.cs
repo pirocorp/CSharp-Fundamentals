3b7876b58ab18fa089cdf4a1d29035d4e4654cee
pirocorp <pirocorp@abv.bg>
2018-01-22 12:32:01 +0100

    You have been given the task to create classes for several sophisticated animals. Create a class Dog which has a Name (string), Age (int) and NumberOfLegs (int). Create a class Cat which has a Name (string), Age (int) and IntelligenceQuotient (int). Create a class Snake which has a Name (string), Age(int) and CrueltyCoefficient (int). Create a method in each class which is called public void ProduceSound(). The method should print on the console a string depending on the class: • If it’s a Dog, you should print “I’m a Distinguishedog, and I will now produce a distinguished sound! Bau Bau.” • It it’s a Cat, you should print “I’m an Aristocat, and I will now produce an aristocratic sound! Myau Myau.” • If it’s a Snake, you should print “I’m a Sophistisnake, and I will now produce a sophisticated sound! Honey, I’m home.” Now for the real deal. You will receive several input commands, which will register animals or make them produce sounds, until you receive the command “I’m your Huckleberry”. The commands will be in the following format: {class} {name} {age} {parameter} The class will be either “Dog”, “Cat” or “Snake”. The name will be a simple string, which can contain any ASCII character BUT space. The age will be an integer. The parameter, will be an integer. Depending on the class it would either be number of legs, IQ, or cruelty coefficient. Register each animal, and keep them in collections, by your choice, so that you can ACCESS THEM BY NAME. You will most likely need 3 collections, to store the different animals inside them. Between the register commands you might receive a command in the following format: talk {name} You must then make the animal with the given name, produce a sound.
    When you receive the ending command, you should print every animal in the following format:
    • If it’s a Dog, you should print “Dog: {name}, Age: {age}, Number Of Legs: {numberOfLegs}”
    • It it’s a Cat, you should print “Cat: {name}, Age: {age}, IQ: {intelligenceQontient}”
    • If it’s a Snake, you should print “Snake: {name}, Age: {age}, Cruelty: {crueltyCoefficient}”
    Print first the Dogs, then the Cats, and lastly – The Snakes.
    Constraints
    • You can assume that there will be no duplicate names (even in different animals).
    • All input data will be valid. There will be no invalid input lines.
    • The name in the talk command, will always be existent.

---
+++ 12. Objects and Classes - Exercises/03. Animals/Animals.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Animals
+{
+    class Animals
+    {
+        public class Dog
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int NumberOfLegs { get; set; }
+
+            public static Dog Parse(string inputData)
+            {
+                var tokens = inputData.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                
+                var result = new Dog()
+                {
+                    Name = tokens[0],
+                    Age = int.Parse(tokens[1]),
+                    NumberOfLegs = int.Parse(tokens[2])
+                };
+                
+                return result;
+            }
+
+            public static void ProduceSound()
+            {
+                Console.WriteLine("I'm a Distinguishedog, and I will now produce a distinguished sound! Bau Bau.");
+            }
+        }
+
+        public class Cat
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int IntelligenceQuotient { get; set; }
+
+            public static Cat Parse(string inputData)
+            {
+                var tokens = inputData.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var result = new Cat()
+                {
+                    Name = tokens[0],
+                    Age = int.Parse(tokens[1]),
+                    IntelligenceQuotient = int.Parse(tokens[2])
+                };
+
+                return result;
+            }
+
+            public static void ProduceSound()
+            {
+                Console.WriteLine("I'm an Aristocat, and I will now produce an aristocratic sound! Myau Myau.");
+            }
+        }
+
+        public class Snake
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int CrueltyCoefficient { get; set; }
+
+            public static Snake Parse(string inputData)
+            {
+                var tokens = inputData.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var result = new Snake()
+                {
+                    Name = tokens[0],
+                    Age = int.Parse(tokens[1]),
+                    CrueltyCoefficient = int.Parse(tokens[2])
+                };
+
+                return result;
+            }
+
+            public static void ProduceSound()
+            {
+                Console.WriteLine("I'm a Sophistisnake, and I will now produce a sophisticated sound! Honey, I'm home.");
+            }
+        }
+
+        static void Main()
+        {
+            var inputData = Console.ReadLine();
+            var dogsDataBase = new List<Dog>();
+            var catDataBase = new List<Cat>();
+            var snakeDataBase = new List<Snake>();
+            var animalNamesType = new Dictionary<string, string>(); //<name, type>
+
+            while (inputData != "I'm your Huckleberry")
+            {
+                var tokens = inputData.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                var className = tokens[0];
+                var classData = String.Join(" ", tokens.Skip(1));
+
+                switch (className)
+                {
+                    case "Dog":
+                        var currentDog = Dog.Parse(classData);
+                        dogsDataBase.Add(currentDog);
+                        var dogNname = tokens[1];
+                        animalNamesType[dogNname] = "Dog";
+                        break;
+                    case "Cat":
+                        var currentCat = Cat.Parse(classData);
+                        catDataBase.Add(currentCat);
+                        var catName = tokens[1];
+                        animalNamesType[catName] = "Cat";
+                        break;
+                    case "Snake":
+                        var currentSnake = Snake.Parse(classData);
+                        snakeDataBase.Add(currentSnake);
+                        var snakeName = tokens[1];
+                        animalNamesType[snakeName] = "Snake";
+                        break;
+                    case "talk":
+                        var name = tokens[1];
+                        var typeOfAnimal = animalNamesType[name];
+
+                        switch (typeOfAnimal)
+                        {
+                            case "Dog":
+                                Dog.ProduceSound();
+                                break;
+                            case "Cat":
+                                Cat.ProduceSound();
+                                break;
+                            case "Snake":
+                                Snake.ProduceSound();
+                                break;
+                        }
+                        break;
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+            foreach (var currentDog in dogsDataBase)
+            {
+                Console.WriteLine($"Dog: {currentDog.Name}, Age: {currentDog.Age}, Number Of Legs: {currentDog.NumberOfLegs}");
+            }
+
+            foreach (var currentCat in catDataBase)
+            {
+                Console.WriteLine($"Cat: {currentCat.Name}, Age: {currentCat.Age}, IQ: {currentCat.IntelligenceQuotient}");
+            }
+
+            foreach (var currentSnake in snakeDataBase)
+            {
+                Console.WriteLine($"Snake: {currentSnake.Name}, Age: {currentSnake.Age}, Cruelty: {currentSnake.CrueltyCoefficient}");
+            }
+
+            //foreach (var animal in animalNamesType)
+            //{
+            //    var animalName = animal.Key;
+            //    var animalType = animal.Value;
+
+            //    switch (animalType)
+            //    {
+            //        case "Dog":
+            //            var currentDog = dogsDataBase.Where(x => x.Name == animalName).First();
+            //            Console.WriteLine($"Dog: {currentDog.Name}, Age: {currentDog.Age}, Number Of Legs: {currentDog.NumberOfLegs}");
+            //            break;
+            //        case "Cat":
+            //            var currentCat = catDataBase.Where(x => x.Name == animalName).First();
+            //            Console.WriteLine($"Cat: {currentCat.Name}, Age: {currentCat.Age}, IQ: {currentCat.IntelligenceQuotient}");
+            //            break;
+            //        case "Snake":
+            //            var currentSnake = snakeDataBase.Where(x => x.Name == animalName).First();
+            //            Console.WriteLine($"Snake: {currentSnake.Name}, Age: {currentSnake.Age}, Cruelty: {currentSnake.CrueltyCoefficient}");
+            //            break;
+            //    }
+            //}
+        }
+    }
+}
