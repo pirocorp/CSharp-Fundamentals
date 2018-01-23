8767beb21d1315a0cf358096bbfc1ff3868b17f4
pirocorp <pirocorp@abv.bg>
2018-01-23 19:12:04 +0100

    Fixed

---
+++ 13. Files Directories and Exceptions - Lab/2. Line Numbers/Line Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Line_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            var numberedLines = lines.Select((line, index) => $"{index + 1}. {line}");
+            File.WriteAllLines("output.txt", numberedLines);
+        }
+    }
+}
