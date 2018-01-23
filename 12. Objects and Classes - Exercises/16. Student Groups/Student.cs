589e4e0f4e94c42d75b6b4dbc2eb0529611a8fa3
pirocorp <pirocorp@abv.bg>
2018-01-23 15:40:27 +0100

    Fixed

--- 12. Objects and Classes - Exercises/16. Student Groups/Student.cs
+++ 12. Objects and Classes - Exercises/16. Student Groups/Student.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _16.Student_Groups
 {
