15abcc0e378e6e523d7cd3490dddb985b9dfca40
pirocorp <pirocorp@abv.bg>
2018-01-23 19:03:56 +0100

    Write a program that reads a text file and writes its every odd line in another file. Line numbers starts from 0.

---
+++ 13. Files Directories and Exceptions - Lab/1. Odd Lines/Odd Lines.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Odd_Lines
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lines = File.ReadAllLines("input.txt");
+            var oddLines = lines.Where((line, index) => index % 2 == 1);
+            File.WriteAllLines("Output.txt", oddLines);
+        }
+    }
+}
