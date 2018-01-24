98230239720d0c1714a977edb1e045304f5d6c81
pirocorp <pirocorp@abv.bg>
2018-01-24 13:36:45 +0100

    You are given a folder named “TestFolder”. Get the size of all files in the folder, which are NOT directories. The result should be written to another text file in Megabytes.

---
+++ 13. Files Directories and Exceptions - Lab/04. Merge Files/Merge Files.cs
@@ -0,0 +1,10 @@
+﻿namespace _04.Merge_Files
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ 13. Files Directories and Exceptions - Lab/05. Folder Size/Folder Size.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace _05.Folder_Size
+{
+    class Program
+    {
+        static void Main()
+        {
+            var files = Directory.GetFiles("TestFolder");
+            double sum = 0;
+
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+            File.WriteAllText("оutput.txt", sum.ToString());
+        }
+    }
+}
