325036170567b2fa15f67b859ea45b55dcd027f2
pirocorp <pirocorp@abv.bg>
2018-01-24 17:51:07 +0100

    You have been tasked to create a program which represents a Console interface for creating HTML files. Every HTML file naturally holds the following elements: “<!DOCTYPE html>  <html>  <head>  </head>  <body>  </body>  </html>” You will need to add them at the end in order to form the file...
    You will start receiving input lines in the following format:
    {tag} {content}
    You should generate a string from every input line – like this: <{tag}>{content}</{tag}> …
    If the tag is “title” you should add the generated string between the <head> and </head> tags with a tabulation (“\t”) before it.
    If you receive the “title” tag MORE than ONCE, you should CHANGE its value.
    In any other case you should APPEND the generated string between the <body> and </body> tags with a tabulation (“\t”) before it.
    When you receive the command “exit” the input ends. The content you have generated should be stored in a file called “index.html” (.html extension).

---
+++ 13. Files Directories and Exceptions - Exercises/02. HTML Contents/HTML Contents.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.HTML_Contents
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputLines = File.ReadAllLines("Input.txt");
+            var body = new StringBuilder();
+            var title = string.Empty;
+
+            for (int i = 0; i < inputLines.Length; i++)
+            {
+                if (inputLines[i] == "exit")
+                {
+                    break;
+                }
+
+                var currentElements = inputLines[i].Split(' ');
+
+                if (currentElements[0] == "title")
+                {
+                    title = currentElements[1];
+                    continue;
+                }
+
+                body.AppendLine($"\t<{currentElements[0]}>{currentElements[1]}</{currentElements[0]}>");
+            }
+
+            var result = new StringBuilder();
+
+            result.AppendLine($"<!DOCTYPE html>");
+            result.AppendLine($"<html>");
+            result.AppendLine($"<head>");
+            result.AppendLine($"<title>{title}</title>");
+            result.AppendLine($"</head>");
+            result.AppendLine($"<body>");
+            result.AppendLine(body.ToString().TrimEnd());
+            result.AppendLine($"</body>");
+            result.AppendLine($"</html>");
+
+            File.WriteAllText("index.html", result.ToString());
+        }
+    }
+}
+
