b9e3a8ba0db4a448983b8f9df1d5c4425d910430
pirocorp <pirocorp@abv.bg>
2018-01-24 17:20:16 +0100

    You will receive a folder called input, with various files with custom extensions. You may add or remove the files as you wish, but they are the only way to test your code. Write a program which accepts a single input line from the Console, holding an extension … like: “txt”, “bmp”, “rar” etc. Print the NAMES AND EXTENSIONS of all files, which have the given extension.

---
+++ 13. Files Directories and Exceptions - Exercises/01. Filter Extensions/Filter Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace _01.Filter_Extensions
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var dir = new DirectoryInfo("../../input");
+            var result = dir.GetFiles($"*.{input}");
+
+            foreach (var file in result)
+            {
+                Console.WriteLine($"{file.Name}");
+            }
+        }
+    }
+}
