c56c717218e953285eef85a9366000d65ac17164
pirocorp <pirocorp@abv.bg>
2018-01-24 22:12:21 +0100

    You have been tasked to create a File Database for several stocked products at a universal shop. A product has a Type (string), Name (string), Price (decimal) and Quantity (integer). The type of the product can be – “Food”, “Electronics”, “Domestics”. The name of the product may consist of any ASCII character, except space. The price of the product will be a floating-point number with up to 20 digits after the decimal point. The quantity of the product will be an integer in range [0, 1000]. The software program you must build should be a Console interface. You will receive several input lines, containing information about products, in the following format: {name} {type} {price} {quantity}
    You should store every product, with its respective properties.
    If you receive a product NAME, which already exists AND has the SAME TYPE, you should REPLACE its price and quantity, with the given ones.
    The products are stored virtually, in your program’s memory – they are called ACTIVE products.
    When you receive the command “stock”, in the input, you must stock all products, you have, in a file.
    When you receive the command “analyze”, in the input, you must print all STOCKED products, in alphabetical order, by their TYPE, each printed in the following format:
    “{type}, Product: {name}
     Price: ${price}, Amount Left: {quantity}”
    In case there are NO products print “No products stocked”.
    When you receive the command “sales”, in the input, you must print all types of ACTIVE products, and the income, earned if all products and their quantities from that TYPE are sold. In other words, you need to calculate for every product from the respective type, its quantity * price. You must then sum all sums, from the products – that’s the INCOME.
    The output should be formatted like this:
    “{firstType}: ${income}
     {secondType}: ${income}
     {thirdType}: ${income}”
    The types must be ordered in descending order, by their total income. If one of the types, has NO products, DO NOT PRINT IT.
    ALL PRICES, must be FORMATTED to the second digit, after the decimal point.
    The input ends when you receive the command “exit”. You do NOT print anything, you do NOT store anything on files. . .
    You just exit the program.
    Note
    You only STOCK products in the external FILE, when you receive the command “stock”. Do NOT stock products at the end of the program execution.
    When you start the program, you should check if you have any stocked products, and if you do, you should load them into your database.

---
+++ 13. Files Directories and Exceptions - Exercises/05. Products/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Products
+{
+    public class Product
+    {
+        public Product(string type, string name, decimal price, int quantity)
+        {
+            Type = type;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public static Product Parse(string inputData)
+        {
+            var tokens = inputData.Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+            var type = tokens[1];
+            var name = tokens[0];
+            var price = decimal.Parse(tokens[2]);
+            var quantity = int.Parse(tokens[3]);
+            
+            return new Product(type, name, price, quantity);
+        }
+    }
+}
---
+++ 13. Files Directories and Exceptions - Exercises/05. Products/Products.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Products
+{
+    class Products
+    {
+        private static string dataBaseFile = "../../dataBase.txt";
+
+        static void Main()
+        {
+            var dataBase = new List<Product>();
+            InitializeDataBaseFromFile(dataBase);
+            var inputData = Console.ReadLine();
+
+            while (inputData != "exit")
+            {
+                if (inputData == "stock")
+                {
+                    WriteDataBaseToFile(dataBase);
+                }
+                else if (inputData == "analyze")
+                {
+                    PrintAllProductsFromFile();
+                }
+                else if (inputData == "sales")
+                {
+                    PrintAllActiveProducts(dataBase);
+                }
+                else
+                {
+                    var currentProduct = Product.Parse(inputData);
+
+                    if (dataBase.Any(x => x.Name == currentProduct.Name && x.Type == currentProduct.Type))
+                    {
+                        var product = dataBase.Find(x =>
+                            x.Name == currentProduct.Name && x.Type == currentProduct.Type);
+                        product.Price = currentProduct.Price;
+                        product.Quantity = currentProduct.Quantity;
+                    }
+                    else
+                    {
+                        dataBase.Add(currentProduct);
+                    }
+
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+        }
+
+        private static void InitializeDataBaseFromFile(List<Product> dataBase)
+        {
+            try
+            {
+                var productsFromFile = File.ReadAllLines(dataBaseFile)
+                    .Select(Product.Parse)
+                    .ToList();
+
+                dataBase = productsFromFile;
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        private static void PrintAllActiveProducts(List<Product> dataBase)
+        {
+            var typeOfProducts = dataBase.Select(x => x.Type).Distinct().ToArray();
+            var sumByType = new Dictionary<string, decimal>();
+
+            for (var i = 0; i < typeOfProducts.Length; i++)
+            {
+                var currentType = typeOfProducts[i];
+                var currentSum = dataBase.Where(x => x.Type == currentType).Sum(x => x.Price * x.Quantity);
+                sumByType.Add(currentType, currentSum);
+            }
+
+            sumByType = sumByType.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var type in sumByType)
+            {
+                if (type.Value != 0)
+                {
+                    Console.WriteLine($"{type.Key}: ${type.Value:F2}");
+                }
+            }
+        }
+
+        private static void PrintAllProductsFromFile()
+        {
+            try
+            {
+                var productsFromFile = File.ReadAllLines(dataBaseFile)
+                    .Select(Product.Parse)
+                    .OrderBy(x => x.Type)
+                    .ToList();
+
+                foreach (var product in productsFromFile)
+                {
+                    Console.WriteLine($"{product.Type}, Product: {product.Name}");
+                    Console.WriteLine($"Price: ${product.Price:F2}, Amount Left: {product.Quantity}");
+                }
+            }
+            catch 
+            {
+                Console.WriteLine($"No products stocked");
+            }
+        }
+
+        private static void WriteDataBaseToFile(List<Product> dataBase)
+        {
+            var result = new StringBuilder();
+
+            for (var i = 0; i < dataBase.Count; i++)
+            {
+                result.AppendLine($"{dataBase[i].Name} {dataBase[i].Type} {dataBase[i].Price} {dataBase[i].Quantity}");
+            }
+
+            File.WriteAllText(dataBaseFile, result.ToString().Trim());
+        }
+    }
+}
