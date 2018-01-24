956a3d1d6d3b36e9355695a5d3e3e08cb8bf22ae
pirocorp <pirocorp@abv.bg>
2018-01-24 18:31:11 +0100

    •	register {username} {password} {confirmPassword} •	login {username} {password} •	logout If you receive the register command, you must store the user in your database of users, with the given password. •	If there is already a user with the given username, you must print “The given username already exists.”, and ignore the command. •	If the password and confirmPassword, do NOT match, print on the console “The two passwords must match.”, and ignore the command. If you receive the login command, you must log in the user with the given username and password. •	If there is already a logged in user, you must print “There is already a logged in user.”, and ignore the command. •	If there is NO user, with the given username you must print “There is no user with the given username.”, and ignore the command. •	If the password is does NOT match the one with which the user was registered, you must print “The password you entered is incorrect.”, and ignore the command. If you receive the logout command, you must logout the, currently logged in, user. •	If there is NO currently logged in user, you must print “There is no currently logged in user.”, and ignore the command. When you receive the command “exit”, the input sequence ends. You must store the current database of REGISTERED users, in a file called “users.txt”. The way you store them is up to you. You must load it, every time the program is ran.

---
+++ 13. Files Directories and Exceptions - Exercises/03. User Database/User Database.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _03.User_Database
+{
+    class Program
+    {
+        private static string dbPath = "users.txt";
+        private static Dictionary<string, string> users = new Dictionary<string, string>();
+        private static bool isLogged = false;
+
+        static void Main()
+        {
+            if (!File.Exists(dbPath))
+            {
+                File.Create(dbPath).Close();
+            }
+
+            var usersFromFile = File.ReadAllLines(dbPath);
+
+            for (var i = 0; i < usersFromFile.Length; i++)
+            {
+                var currentUser = usersFromFile[i].Split(' ');
+                var currentUsername = currentUser[0];
+                var currentPasword = currentUser[1];
+
+                users[currentUsername] = currentPasword;
+            }
+
+            var commands = File.ReadAllLines("Input.txt");
+
+            for (var i = 0; i < commands.Length; i++)
+            {
+                var currentCommand = commands[i].Split(' ');
+
+                switch (currentCommand[0])
+                {
+                    case "register":
+                        var username = currentCommand[1];
+                        var password = currentCommand[2];
+                        var confirmPassword = currentCommand[3];
+                        Register(username, password, confirmPassword);
+                        break;
+                    case "login":
+                        username = currentCommand[1];
+                        password = currentCommand[2];
+                        Login(username, password);
+                        break;
+                    case "logout":
+                        Logout();
+                        break;
+                }
+            }
+        }
+
+        private static void Logout()
+        {
+            if (!isLogged)
+            {
+                Console.WriteLine($"There is no currently logged in user.");
+                return;
+            }
+
+            isLogged = false;
+        }
+
+        private static void Login(string username, string password)
+        {
+            if (!users.ContainsKey(username))
+            {
+                Console.WriteLine($"There is no user with the given username.");
+                return;
+            }
+
+            if (users[username] != password)
+            {
+                Console.WriteLine($"The password you entered is incorrect.");
+                return;
+            }
+
+            if (isLogged)
+            {
+                Console.WriteLine($"There is already a logged in user.");
+                return;
+            }
+
+            isLogged = true;
+        }
+
+        private static void Register(string username, string password, string confirmPassword)
+        {
+            if (users.ContainsKey(username))
+            {
+                Console.WriteLine($"The given username already exists.");
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                Console.WriteLine($"The two passwords must match.");
+                return;
+            }
+
+            users[username] = password;
+            var currentUser = new[] { $"{username} {password}" };
+            File.AppendAllLines(dbPath, currentUser);
+        }
+    }
+}
