de17572b21d355fe5243c26d3637b01cfa1bc839
pirocorp <pirocorp@abv.bg>
2018-01-24 13:28:17 +0100

    Write a program that reads a list of words from the file words.txt and finds how many times each of the words is contained in another file text.txt. Matching should be case-insensitive. The result should be written to another text file. Sort the words by frequency in descending order.

--- 13. Files Directories and Exceptions - Lab/03. Word Count/Word Count.cs
+++ 13. Files Directories and Exceptions - Lab/03. Word Count/Word Count.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _3.Word_Count
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var words = File.ReadAllText("words.txt").ToLower().Split();
+            var text = File.ReadAllText("input.txt").ToLower()
+                .Split(new char[] { '\n', '\r', ' ', '.', ',', '!', '?', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            var wordCount = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                wordCount[word] = 0;
+            }
+
+            foreach (var word in text)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+            }
+
+            wordCount = wordCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            var currentLines = new string[wordCount.Count];
+            var i = 0;
+
+            foreach (var word in wordCount)
+            {
+                var currentLine = $"{word.Key} - {word.Value}";
+                currentLines[i] = currentLine;
+                i++;
+            }
+
+            File.AppendAllLines("Output.txt", currentLines);
         }
     }
 }
