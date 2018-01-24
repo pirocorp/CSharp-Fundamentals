02e1a75930863452936accb1486504e86daa7c16
pirocorp <pirocorp@abv.bg>
2018-01-24 19:17:21 +0100

    You have been tasked to distribute a directory (folder) of files with various extensions to different folders. The files should be distributed by their file extension. You need to group all the files, which have the same extension, into a folder named: “{extension} + s” In other words: all “.txt” files must be put in a folder called “txts”. The resulting folders should be put in a folder “output”.

--- 13. Files Directories and Exceptions - Exercises/04. Re-Directory/04. Re-Directory.cs
+++ 13. Files Directories and Exceptions - Exercises/04. Re-Directory/04. Re-Directory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,27 @@ namespace _04.Re_Directory
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var inputDir = new DirectoryInfo("../../input");
+            var files = inputDir.GetFiles().Select(x => x.Extension).Distinct().ToArray();
+            Directory.CreateDirectory("../../output");
+
+            for (var i = 0; i < files.Length; i++)
+            {
+                var currentExtension = files[i];
+                Directory.CreateDirectory($"../../output/{currentExtension.Remove(0, 1)}s");
+                var filesToMove = inputDir
+                    .GetFiles()
+                    .Where(x => x.Extension == currentExtension)
+                    .ToList();
+
+                foreach (var file in filesToMove)
+                {
+                    File.Copy(file.FullName, $"../../output/{currentExtension.Remove(0, 1)}s/{file.Name}");
+                }
+            }
+
         }
     }
 }
