bfe80bcb0e43dd3c4994ff04d980946093a4a780
pirocorp <pirocorp@abv.bg>
2018-01-24 13:51:10 +0100

    Write a program that reads the contents of two text files and merges them together into a third one.

--- 13. Files Directories and Exceptions - Lab/04. Merge Files/Merge Files.cs
+++ 13. Files Directories and Exceptions - Lab/04. Merge Files/Merge Files.cs
@@ -1,10 +1,42 @@
-﻿namespace _04.Merge_Files
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _04.Merge_Files
 {
     class Program
     {
         static void Main()
         {
+            var linesFileOne = File.ReadAllLines("FileOne.txt");
+            var linesFileTwo = File.ReadAllLines("FileTwo.txt");
+            var count = Math.Min(linesFileOne.Length, linesFileTwo.Length);
+            var resultetLines = new List<string>();
 
+            for (var i = 0; i < count; i++)
+            {
+                resultetLines.Add(linesFileOne[i]);
+                resultetLines.Add(linesFileTwo[i]);
+            }
+
+            if (linesFileOne.Length > linesFileTwo.Length)
+            {
+                for (int i = count; i < linesFileOne.Length; i++)
+                {
+                    resultetLines.Add(linesFileOne[i]);
+                }
+            }
+
+            if (linesFileTwo.Length > linesFileOne.Length)
+            {
+                for (int i = count; i < linesFileTwo.Length; i++)
+                {
+                    resultetLines.Add(linesFileTwo[i]);
+                }
+            }
+
+            File.WriteAllLines("Output.txt", resultetLines);
         }
     }
 }
