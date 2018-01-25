5e1b148ae503ea1feac003c88c040e105b4132af
pirocorp <pirocorp@abv.bg>
2018-01-25 15:09:30 +0100

    You will receive an unknown number of lines. On each line, you will receive array with 3 elements. The first element will be string and represents the name of the person. The second element will be a string and will represent the ID of the person. The last element will be an integer and represents the age of the person. When you receive the command “End”, stop taking input and print all the people, ordered by age.

---
+++ 13. Files Directories and Exceptions - Exercises/16. Order by Age/Order by Age.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _16.Order_by_Age
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputData = Console.ReadLine();
+            var peoples = new List<Person>();
+
+            while (inputData != "End")
+            {
+                peoples.Add(Person.Parse(inputData));
+                inputData = Console.ReadLine();
+            }
+
+            peoples = peoples.OrderBy(x => x.Age).ToList();
+
+            foreach (var person in peoples)
+            {
+                Console.WriteLine($"{person.Name} with ID: {person.Id} is {person.Age} years old.");
+            }
+        }
+    }
+}
---
+++ 13. Files Directories and Exceptions - Exercises/16. Order by Age/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _16.Order_by_Age
+{
+    public class Person
+    {
+        public Person(string name, string id, int age)
+        {
+            Name = name;
+            Id = id;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+
+        public static Person Parse(string inputData)
+        {
+            var tokens = inputData.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var name = tokens[0];
+            var id = tokens[1];
+            var age = int.Parse(tokens[2]);
+
+            return new Person(name, id, age);
+        }
+    }
+}
