0e2ee043a019b97b6a14889ca230d660a4273b50
pirocorp <pirocorp@abv.bg>
2018-01-25 10:20:05 +0100

    Write a program that creates an array containing all letters from the alphabet (a-z). Read a lowercase word from the console and print the index of each of its letters in the letters array.

---
+++ 13. Files Directories and Exceptions - Exercises/07. Index of Letters/Index of Letters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Index_of_Letters
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputLines = File.ReadAllLines("Input.txt");
+            var resultedString = new StringBuilder();
+
+            for (int i = 0; i < inputLines .Length; i++)
+            {
+                var chars = inputLines[i].ToCharArray();
+
+                for (var j = 0; j < chars.Length; j++)
+                {
+                    var result = chars[j] - 'a';
+                    resultedString.AppendLine($"{chars[j]} -> {result}");
+                }
+
+                resultedString.AppendLine(Environment.NewLine);
+            }
+
+            File.WriteAllText("Output.txt", resultedString.ToString());
+        }
+    }
+}
