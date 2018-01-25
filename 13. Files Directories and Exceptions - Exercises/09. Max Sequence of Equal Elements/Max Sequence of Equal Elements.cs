126ba406373f21245885134bdf0496af5d9d119a
pirocorp <pirocorp@abv.bg>
2018-01-25 10:42:06 +0100

    Read a list of integers and find the longest sequence of equal elements. If several exist, print the leftmost.

---
+++ 13. Files Directories and Exceptions - Exercises/09. Max Sequence of Equal Elements/Max Sequence of Equal Elements.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputLines = File.ReadAllLines("Input.txt");
+
+            for (var i = 0; i < inputLines.Length; i++)
+            {
+                var numbers = inputLines[i]
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                var maxSequenceOfEqualElements = GetMaxSequenceOfEqualElements(numbers);
+                var result = new[] { $"{String.Join(" ", maxSequenceOfEqualElements)}" };
+                File.AppendAllLines("Output.txt", result);
+            }
+
+        }
+
+        private static List<int> GetMaxSequenceOfEqualElements(List<int> arr)
+        {
+            var longestSequence = new List<int>();
+            var currentSequence = new List<int>();
+            currentSequence.Add(arr[0]);
+
+            for (int i = 1; i < arr.Count; i++)
+            {
+                var currentNum = arr[i];
+                var searchNum = currentSequence[0];
+                if (currentNum == searchNum)
+                {
+                    currentSequence.Add(currentNum);
+                }
+                else
+                {
+                    if (currentSequence.Count > longestSequence.Count)
+                    {
+                        longestSequence = new List<int>(currentSequence);
+                    }
+
+                    currentSequence.Clear();
+                    currentSequence.Add(currentNum);
+                }
+
+                if (currentSequence.Count > longestSequence.Count)
+                {
+                    longestSequence = new List<int>(currentSequence);
+                }
+
+            }
+
+            return longestSequence;
+        }
+    }
+}
