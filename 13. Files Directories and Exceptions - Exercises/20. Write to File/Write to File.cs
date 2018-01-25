1081765dc750541a3f709b22cf30d0ecdbfe2877
pirocorp <pirocorp@abv.bg>
2018-01-25 19:29:23 +0100

    Read the same file, as in the previous task, but this time write everything, except the punctuation marks to a new file. Again, consider as punctuations only: “.”, “,”, “!”, “?” and “:”. Submit the content of the file in judge.

---
+++ 13. Files Directories and Exceptions - Exercises/20. Write to File/Write to File.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Write_to_File
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputLines = File.ReadAllLines("sample_text.txt").ToList();
+            var listOfChars = ".,!?:".ToCharArray();
+            inputLines = inputLines
+                .Select(x => String.Join("", x.Split(listOfChars, StringSplitOptions.RemoveEmptyEntries)))
+                .ToList();
+            File.WriteAllLines("../../output.txt", inputLines);
+        }
+    }
+}
