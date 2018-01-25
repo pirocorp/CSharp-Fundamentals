b58d06c8535a9b85d58e802dacc3e336c751c491
pirocorp <pirocorp@abv.bg>
2018-01-25 10:51:47 +0100

    You are given a sequence of strings, each on a new line. Every odd line on the console is representing a resource (e.g. Gold, Silver, Copper, and so on), and every even – quantity. Your task is to collect the resources and print them each on a new line. Print the resources and their quantities in format: {resource} –> {quantity} The quantities inputs will be in the range [1 … 2 000 000 000]

---
+++ 13. Files Directories and Exceptions - Exercises/10. A Miner Task/A Miner Task.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.A_Miner_Task
+{
+    class Program
+    {
+        static void Main()
+        {
+            var resources = new Dictionary<string, long>();
+            var allLines = File.ReadAllLines("Input.txt");
+
+            for (int i = 0; i < allLines.Length; i += 2)
+            {
+                var resource = allLines[i];
+
+                if (resource == "stop")
+                {
+                    break;
+                }
+
+                var inputLine = allLines[i + 1];
+                var quantity = 0;
+
+                if (!int.TryParse(inputLine, out quantity))
+                {
+                    break;
+                }
+
+                if (!resources.ContainsKey(resource))
+                {
+                    resources[resource] = 0;
+                }
+
+                resources[resource] += quantity;
+            }
+
+            resources.ToList().ForEach(x => File.AppendAllLines("Output.txt", new []{ $"{x.Key} -> {x.Value}" }));
+        }
+    }
+}
