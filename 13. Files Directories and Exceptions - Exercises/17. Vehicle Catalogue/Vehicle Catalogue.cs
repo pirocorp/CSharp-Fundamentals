1740fe6fd965a547799aeaa6f73ac6cf2d59648c
pirocorp <pirocorp@abv.bg>
2018-01-25 15:57:48 +0100

    You have to make a catalogue for vehicles. You will receive two types of vehicle – car or truck. Until you receive the command “End” you will receive lines of input in the format: {typeOfVehicle} {model} {color} {horsepower} After the “End” command, you will start receiving models of vehicles. Print for every received vehicle its data in the format: Type: {typeOfVehicle} Model: {modelOfVehicle} Color: {colorOfVehicle} Horsepower: {horsepowerOfVehicle} When you receive the command “Close the Catalogue”, stop receiving input and print the average horsepower for the cars and for the trucks in the format: {typeOfVehicles} have average horsepower of {averageHorsepower}. The average horsepower is calculated by dividing the sum of horsepower for all vehicles of the type by the total count of vehicles from the same type. Format the answer to the 2nd decimal point. Constraints • The type of vehicle will always be car or truck. • You will not receive the same model twice. • The received horsepower will be integer in the interval [1…1000] • You will receive at most 50 vehicles. • Single whitespace will be used for separator.

---
+++ 13. Files Directories and Exceptions - Exercises/17. Vehicle Catalogue/Vehicle Catalogue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _17.Vehicle_Catalogue
+{
+    class Program
+    {
+        static void Main()
+        {
+            var vehicles = new List<Vehicle>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "End")
+            {
+                vehicles.Add(Vehicle.Parse(inputData));
+                inputData = Console.ReadLine();
+            }
+
+            inputData = Console.ReadLine();
+
+            while (inputData != "Close the Catalogue")
+            {
+                var result = vehicles.Find(x => x.Model == inputData);
+                Console.WriteLine($"Type: {(result.Type.ToLower() == "car"?"Car":"Truck")}");
+                Console.WriteLine($"Model: {result.Model}");
+                Console.WriteLine($"Color: {result.Color}");
+                Console.WriteLine($"Horsepower: {result.Horsepower}");
+                inputData = Console.ReadLine();
+            }
+
+            var carTypeVehicles = vehicles.Where(x => x.Type.ToLower() == "car").ToList();
+            var carAverageHorsepower = carTypeVehicles.Sum(x => x.Horsepower) / (double)carTypeVehicles.Count;
+            var truckTypeVehicles = vehicles.Where(x => x.Type.ToLower() == "truck").ToList();
+            var truckAverageHorsepower = truckTypeVehicles.Sum(x => x.Horsepower) / (double)truckTypeVehicles.Count;
+            Console.WriteLine($"Cars have average horsepower of: {(double.IsNaN(carAverageHorsepower) ? "0.00" : $"{carAverageHorsepower:F2}")}.");
+            Console.WriteLine($"Trucks have average horsepower of: {(double.IsNaN(truckAverageHorsepower)?"0.00":$"{truckAverageHorsepower:F2}")}.");
+        }
+    }
+}
---
+++ 13. Files Directories and Exceptions - Exercises/17. Vehicle Catalogue/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _17.Vehicle_Catalogue
+{
+    public class Vehicle
+    {
+        public Vehicle(string type, string model, string color, int horsepower)
+        {
+            Type = type;
+            Model = model;
+            Color = color;
+            Horsepower = horsepower;
+        }
+
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Horsepower { get; set; }
+
+        public static Vehicle Parse(string inputData)
+        {
+            var tokens = inputData.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var type = tokens[0];
+            var model = tokens[1];
+            var color = tokens[2];
+            var horsepower = int.Parse(tokens[3]);
+
+            return new Vehicle(type, model, color, horsepower);
+        }
+    }
+}
