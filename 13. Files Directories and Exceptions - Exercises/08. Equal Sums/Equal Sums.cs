1e672fe237bca846e0ff81d61af6264df3c22238
pirocorp <pirocorp@abv.bg>
2018-01-25 10:34:07 +0100

    Write a program that determines if there exists an element in the array such that the sum of the elements on its left is equal to the sum of the elements on its right. If there are no elements to the left / right, their sum is considered to be 0. Print the index that satisfies the required condition or “no” if there is no such index.

---
+++ 13. Files Directories and Exceptions - Exercises/08. Equal Sums/Equal Sums.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Equal_Sums
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputLines = File.ReadAllLines("Input.txt");
+
+            for (int i = 0; i < inputLines.Length; i++)
+            {
+                var currentOutput = new[] {String.Empty};
+
+                var numbers = inputLines[i]
+                    .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var isFound = false;
+
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    var leftSum = LeftSum(j, numbers);
+                    var rightSum = RightSum(j, numbers);
+
+                    if (leftSum == rightSum)
+                    {
+                        currentOutput[0] = j.ToString();
+                        isFound = true;
+                        break;
+                    }
+                }
+
+                if (!isFound)
+                {
+                    currentOutput[0] = "no";
+                }
+
+                File.AppendAllLines("Output.txt", currentOutput);
+            }
+        }
+
+        private static int RightSum(int position, int[] numbers)
+        {
+            var sum = 0;
+
+            for (int i = numbers.Length - 1; i > position; i--)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+
+        private static int LeftSum(int position, int[] numbers)
+        {
+            var sum = 0;
+
+            for (var i = 0; i < position; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+    }
+}
