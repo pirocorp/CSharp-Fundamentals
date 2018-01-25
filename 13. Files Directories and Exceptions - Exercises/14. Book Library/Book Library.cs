e5117f73df8178f0c2d165d55c3438de31b2cb37
pirocorp <pirocorp@abv.bg>
2018-01-25 13:09:34 +0100

    To model a book library, define classes to hold a book and a library. The library must have a name and a list of books. The books must contain the title, author, publisher, release date, ISBN-number and price. Read a list of books, add them to the library and print the total sum of prices by author, ordered descending by price and then by author’s name lexicographically. Books in the input will be in format {title} {author} {publisher} {release date} {ISBN} {price}.

---
+++ 13. Files Directories and Exceptions - Exercises/14. Book Library/Book Library.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _14.Book_Library
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputLines = File.ReadAllLines("Input.txt");
+            var books = new List<Book>();
+
+            for (int i = 0; i < inputLines.Length; i++)
+            {
+                var currentBook = Book.Parse(inputLines[i]);
+                books.Add(currentBook);
+            }
+
+            var authors = books.Select(x => x.Author).Distinct().ToList();
+            var result = new Dictionary<string, decimal>();
+
+            foreach (var author in authors)
+            {
+                var sum = books.Where(x => x.Author == author).Sum(x => x.Price);
+                result[author] = sum;
+            }
+
+            result = result.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var kvp in result)
+            {
+                File.AppendAllLines("Output.txt", new []{ $"{kvp.Key} -> {kvp.Value:f2}" });
+            }
+        }
+    }
+}
---
+++ 13. Files Directories and Exceptions - Exercises/14. Book Library/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace _14.Book_Library
+{
+    public class Book
+    {
+        public Book(string title, string author, string publisher, DateTime releaseDate, string isbn, decimal price)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            ReleaseDate = releaseDate;
+            Isbn = isbn;
+            Price = price;
+        }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Isbn { get; set; }
+        public decimal Price { get; set; }
+
+        public static Book Parse(string inputData)
+        {
+            var tokens = inputData.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var title = tokens[0];
+            var author = tokens[1];
+            var publisher = tokens[2];
+            var format = "dd.MM.yyyy";
+            var releaseDate = DateTime.ParseExact(tokens[3], format, CultureInfo.InvariantCulture);
+            var isbn = tokens[4];
+            var price = decimal.Parse(tokens[5]);
+            var newBook = new Book(title, author, publisher, releaseDate, isbn, price);
+            return newBook;
+        }
+    }
+}
