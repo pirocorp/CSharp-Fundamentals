1167cceb90e423565ae0db294c60fcd1cfc28228
pirocorp <pirocorp@abv.bg>
2018-01-26 18:15:42 +0100

    Write a program that reads a string from the console, reverses it and prints the result back at the console.

---
+++ 14. Strings and Text Processing - Lab/01. Reverse String/Reverse String.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Reverse_String
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(new string(Console.ReadLine().Reverse().ToArray()));
+        }
+    }
+}
