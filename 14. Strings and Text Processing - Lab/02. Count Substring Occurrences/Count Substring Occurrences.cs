a07d53fd6286605930d23eb52d023ccb793f5b51
pirocorp <pirocorp@abv.bg>
2018-01-26 18:22:22 +0100

    Write a program to find how many times a given string appears in a given text as substring. The text is given at the first input line. The search string is given at the second input line. The output is an integer number. Please ignore the character casing. Overlapping between occurrences is allowed.

---
+++ 14. Strings and Text Processing - Lab/02. Count Substring Occurrences/Count Substring Occurrences.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.Count_Substring_Occurrences
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().ToLower();
+            var pattern = Console.ReadLine().ToLower();
+            var counter = 1;
+            var index = input.IndexOf(pattern);
+
+            while ((index = input.IndexOf(pattern, index + 1)) != -1)
+            {
+                counter++;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
