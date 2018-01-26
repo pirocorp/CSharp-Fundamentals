b3c0620c39f3f0e636c1f41c946ff4d3a6816856
pirocorp <pirocorp@abv.bg>
2018-01-26 18:50:19 +0100

    Write a program that extracts from a given text all palindromes, e.g. ABBA, lamal, exe and prints them on the console on a single line, separated by comma and space. Use spaces, commas, dots, question marks and exclamation marks as word delimiters. Print only unique palindromes, sorted lexicographically.

---
+++ 14. Strings and Text Processing - Lab/04. Palindromes/Palindromes.cs
@@ -0,0 +1,30 @@
+﻿namespace _04.Palindromes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Palindromes
+    {
+        static void Main()
+        {
+            var delimeterList = new []{' ', '.', ',', '!', '?'};
+            var words = Console.ReadLine().Split(delimeterList, StringSplitOptions.RemoveEmptyEntries);
+            var resultedList = new List<string>();
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                var currentWord = words[i];
+                var reversedCurrentWord = new string(currentWord.Reverse().ToArray());
+
+                if (currentWord == reversedCurrentWord)
+                {
+                    resultedList.Add(currentWord);
+                }
+            }
+
+            resultedList = resultedList.Distinct().OrderBy(x => x).ToList();
+            Console.WriteLine(String.Join(", ", resultedList));
+        }
+    }
+}
