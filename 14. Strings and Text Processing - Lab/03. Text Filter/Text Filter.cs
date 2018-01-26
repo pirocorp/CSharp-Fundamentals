fee0d81725318976b136251bf1b676380c89bc52
pirocorp <pirocorp@abv.bg>
2018-01-26 18:28:20 +0100

    Write a program that takes a text and a string of banned words. All words included in the ban list should be replaced with asterisks "*", equal to the word's length. The entries in the ban list will be separated by a comma and space ", ". The ban list should be entered on the first input line and the text on the second input line.

---
+++ 14. Strings and Text Processing - Lab/03. Text Filter/Text Filter.cs
@@ -0,0 +1,23 @@
+﻿namespace _03.Text_Filter
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var banWords = Console.ReadLine().Split(new []{',', ' '}, StringSplitOptions.RemoveEmptyEntries);
+            var text = Console.ReadLine();
+
+            foreach (var banWord in banWords)
+            {
+                if (text.Contains(banWord))
+                {
+                    text = text.Replace(banWord, new string('*', banWord.Length));
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
