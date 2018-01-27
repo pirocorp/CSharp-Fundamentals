41592e09be0af2fe0314062b46b920be15cf04a3
pirocorp <pirocorp@abv.bg>
2018-01-27 14:06:52 +0100

    Stringifying a JSON was easy? You want something harder? Well… JSON parse is your problem. You will receive a JSON collection of students. You need to parse it to Student objects. Input The input will consist of a single line containing the students’ info. The students will be given in the following format: • [{student1},{student2}. . .] Each student will be given in the following format: • {name:"{name}",age:{age},grades:[{grade1}, {grade2}. . .]} Output Parse the input data and print each student in the following format: • {name} : {age} -> {grade1}, {grade2}, {grade3}... In cases where a student has NO grades, print “None”.

---
+++ 14. Strings and Text Processing - Exercises/04. JSON Parse/JSON Parse.cs
@@ -0,0 +1,36 @@
+﻿namespace _04.JSON_Parse
+{
+    using System;
+    using System.Collections.Generic;
+    using _03.JSON_Stringify;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var students = new List<Student>();
+            var inputStudents = Console.ReadLine()
+                .Split(new[] {"},{"}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim("[{".ToCharArray()))
+                .Select(x => x.Trim("}".ToCharArray()))
+                .ToArray();
+
+            var lastElement = inputStudents[inputStudents.Length - 1];
+            lastElement = lastElement.Substring(0, lastElement.Length - 2);
+            inputStudents[inputStudents.Length - 1] = lastElement;
+
+            for (int i = 0; i < inputStudents.Length; i++)
+            {
+                var currentStudent = Student.JsonParse(inputStudents[i]);
+                students.Add(currentStudent);
+            }
+
+            foreach (var student in students)
+            {
+                var grades = student.Grades.Count > 0 ? String.Join(", ", student.Grades) : "None";
+                Console.WriteLine($"{student.Name} : {student.Age} -> {grades}");
+            }
+        }
+    }
+}
---
+++ 14. Strings and Text Processing - Exercises/04. JSON Parse/Student.cs
@@ -0,0 +1,76 @@
+﻿namespace _03.JSON_Stringify
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Student
+    {
+        public Student(string name, int age, List<double> grades)
+        {
+            Name = name;
+            Age = age;
+            Grades = grades;
+        }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Grades = new List<double>();
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<double> Grades { get; set; }
+
+        public static Student Parse(string inputData)
+        {
+            var tokens = inputData.Split(new[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
+            var studentData = tokens[0]
+                .Trim()
+                .Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToArray();
+
+            var studentName = studentData[0];
+            var studentAge = int.Parse(studentData[1]);
+
+            if (tokens.Length == 1)
+            {
+                return new Student(studentName, studentAge);
+            }
+
+            var grades = tokens[1]
+                .Trim()
+                .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .Select(double.Parse)
+                .ToList();
+
+            return new Student(studentName, studentAge, grades);
+        }
+
+        //name:"{name}",age:{age},grades:[{grade1}, {grade2}. . .]
+        public static Student JsonParse(string inputData)
+        {
+            var tokens = inputData.Split(new[] {':'}, StringSplitOptions.RemoveEmptyEntries);
+            var name = tokens[1].Split('\"').Skip(1).First();
+            var age = int.Parse(tokens[2].Split(',').First());
+            var stringGrades = tokens[3];
+
+            if (stringGrades.Length > 2)
+            {
+                var grades = stringGrades.Trim('[').Trim(']').Split(',').Select(double.Parse).ToList();
+                return new Student(name, age, grades);
+            }
+
+            return new Student(name, age);
+        }
+
+        public override string ToString()
+        {
+            return $"{{name:\"{this.Name}\",age:{this.Age},grades:[{String.Join(", ", this.Grades)}]}}";
+        }
+    }
+}
