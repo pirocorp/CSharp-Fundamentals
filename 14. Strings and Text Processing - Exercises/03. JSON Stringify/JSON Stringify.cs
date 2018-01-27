11a09134019592935ce0c97941da2c1f3605eed9
pirocorp <pirocorp@abv.bg>
2018-01-27 12:56:33 +0100

    JSON is a syntax for storing and exchanging data. It’s derived from JavaScript Object Notation. You have been tasked to create a database of students, and store them in the JSON format. First, create a Student class, which will represent our data model – it will hold our data. The Student should have a Name (string), an Age (integer), and Grades (Collection of integer numbers). This will be enough to store our data. We’ll need a collection of Students, since we will store a lot of students. And now let’s see the input and output formats. Input The input will consist of several input lines in the following format: {name} : {age} -> {grade1}, {grade2}, {grade3}. . . Store every student with their name, age and given grades. The name will be a string which can contain any ASCII characters except space (“ “), “:”, “-”, “>” and “,”. The age will be an integer. The grades will be integers. When you receive the command “stringify”, the input sequence ends. Output You should print the collection of students in the following format: [{student1},{student2}. . .] Each student must be printed in the following format: • {name:"{name}",age:{age},grades:[{grade1}, {grade2}. . .]} See the examples for more info.

---
+++ 14. Strings and Text Processing - Exercises/03. JSON Stringify/JSON Stringify.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.JSON_Stringify
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputData = Console.ReadLine();
+            var students = new List<Student>();
+
+            while (inputData != "stringify")
+            {
+                students.Add(Student.Parse(inputData));
+                inputData = Console.ReadLine();
+            }
+
+            Console.WriteLine($"[{String.Join(",", students)}]");
+        }
+    }
+}
---
+++ 14. Strings and Text Processing - Exercises/03. JSON Stringify/Student.cs
@@ -0,0 +1,59 @@
+﻿namespace _03.JSON_Stringify
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Student
+    {
+        public Student(string name, int age, List<double> grades)
+        {
+            Name = name;
+            Age = age;
+            Grades = grades;
+        }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Grades = new List<double>();
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<double> Grades { get; set; }
+
+        public static Student Parse(string inputData)
+        {
+            var tokens = inputData.Split(new[] {"->"}, StringSplitOptions.RemoveEmptyEntries);
+            var studentData = tokens[0]
+                .Trim()
+                .Split(new []{ ':' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToArray();
+
+            var studentName = studentData[0];
+            var studentAge = int.Parse(studentData[1]);
+
+            if (tokens.Length == 1)
+            {
+                return new Student(studentName, studentAge);
+            }
+
+            var grades = tokens[1]
+                .Trim()
+                .Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .Select(double.Parse)
+                .ToList();
+
+            return new Student(studentName, studentAge, grades);
+        }
+
+        public override string ToString()
+        {
+            return $"{{name:\"{this.Name}\",age:{this.Age},grades:[{String.Join(", ", this.Grades)}]}}";
+        }
+    }
+}
