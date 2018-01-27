6e2ba4b4ab217498eb7391f17248e4a7643e1fb6
pirocorp <pirocorp@abv.bg>
2018-01-27 11:48:54 +0100

    Write a program, which receives a string and prints the index of a given letter in the string. The tricky part is that you will have to find not the first letter, but the nth letter.

---
+++ 14. Strings and Text Processing - Exercises/01. Find the Letter/Find the Letter.cs
@@ -0,0 +1,38 @@
+﻿namespace _01.Find_the_Letter
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+            var inputCommand = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var leterToSearch = inputCommand[0];
+            var letterOccurrence = int.Parse(inputCommand[1]);
+
+            var occurrence = 0;
+            var indexOfOccurrence = -1;
+            var currentOccurrence = -1;
+
+            while (occurrence < letterOccurrence || inputString.IndexOf(leterToSearch, currentOccurrence + 1) > 0)
+            {
+                currentOccurrence = inputString.IndexOf(leterToSearch, currentOccurrence + 1);
+
+                if (++occurrence == letterOccurrence)
+                {
+                    indexOfOccurrence = currentOccurrence;
+                }
+            }
+
+            if (indexOfOccurrence < 0)
+            {
+                Console.WriteLine($"Find the letter yourself!");
+            }
+            else
+            {
+                Console.WriteLine(indexOfOccurrence);
+            }
+        }
+    }
+}
