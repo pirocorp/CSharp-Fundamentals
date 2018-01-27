84d80bfe22a4a85307a78aa9b5ef277e6f1e603d
pirocorp <pirocorp@abv.bg>
2018-01-27 12:01:49 +0100

    Placeholders in C# are really comfortable for use, right? How about you implement them to see just how easy they are.

---
+++ 14. Strings and Text Processing - Exercises/02. Placeholders/Placeholders.cs
@@ -0,0 +1,27 @@
+﻿namespace _02.Placeholders
+{
+    using System;
+
+    class Placeholders
+    {
+        static void Main()
+        {
+            var readLine = Console.ReadLine();
+
+            while (readLine != "end")
+            {
+                var inputData = readLine.Split(new[] {" -> "}, StringSplitOptions.RemoveEmptyEntries);
+                var inputString = inputData[0];
+                var dataForPlaceholders = inputData[1].Split(new []{ ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (var i = 0; i < dataForPlaceholders.Length; i++)
+                {
+                    inputString = inputString.Replace("{" + $"{i}" + "}", dataForPlaceholders[i]);
+                }
+
+                Console.WriteLine(inputString);
+                readLine = Console.ReadLine();
+            }
+        }
+    }
+}
