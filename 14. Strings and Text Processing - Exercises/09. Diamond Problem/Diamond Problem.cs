506b0036b164116515a24b0aff17c6e17c03eefd
pirocorp <pirocorp@abv.bg>
2018-01-28 22:37:33 +0100

    In the programming languages, which permit multiple inheritance, the diamond problem is a very common problem. In our task, the diamond problem is a bit more… money driven. Your task is to write a program, which finds all diamonds in a string and calculates the carats of each diamond. Each diamond will start with the character ‘<’. After that, it will be followed by several random characters (contents of the diamond). The diamond will end with the character ‘>’. The carat value of the diamond is equal to the sum of all the digits in the contents of the diamond. Example: “<2big32diamond>”  2 + 3 + 2  7 carats If the given string contains one or more diamonds, print for each found diamond the following output: • Found {caratValueOfTheDiamond} carat diamond If in the given string cannot be found any diamond, print: • Better luck next time

---
+++ 14. Strings and Text Processing - Exercises/09. Diamond Problem/Diamond Problem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Diamond_Problem
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+            var currentIndex = -1;
+            bool noDimond = true;
+
+            while (currentIndex < inputString.Length)
+            {
+                currentIndex++;
+                var startIndex = inputString.IndexOf("<", currentIndex);
+                var endIndex = inputString.IndexOf(">", currentIndex);
+
+                if (startIndex < endIndex && startIndex >= 0 && endIndex >= 0)
+                {
+                    var currentDimond = inputString.Substring(startIndex, endIndex - startIndex);
+                    var dimondValue = GetValue(currentDimond);
+                    noDimond = false;
+                    Console.WriteLine($"Found {dimondValue} carat diamond");
+                    currentIndex = endIndex + 1;
+                }
+            }
+
+            if (noDimond)
+            {
+                Console.WriteLine("Better luck next time");
+            }
+
+        }
+
+        private static object GetValue(string currentDimond)
+        {
+            var value = currentDimond.Where(x => Char.IsDigit(x)).Select(x => int.Parse(new string(x, 1))).Sum();
+            return value;
+        }
+    }
+}
