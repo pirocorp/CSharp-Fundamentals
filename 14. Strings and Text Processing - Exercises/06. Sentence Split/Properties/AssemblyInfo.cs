55af8b4582118f1262aa58cbd4bd91ca5c6467c5
pirocorp <pirocorp@abv.bg>
2018-01-28 17:57:04 +0100

    Splitting the input by a given delimiter is easy, especially when you are told in the problem descriptions, that the elements will not contain the delimiter. What if, however, they do. Input You will be given a sentence, which may contain any ASCII character. Then you will be given a delimiter, which may also contain any ASCII character. Output Your task is to split the sentence by the given delimiter, and print each of the elements, in the following format: • [{element1}, {element2}, {element3}. . .] See the examples for more info.

--- 14. Strings and Text Processing - Exercises/06. Sentence Split/Properties/AssemblyInfo.cs
+++ 14. Strings and Text Processing - Exercises/06. Sentence Split/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("05. Points Counter")]
+[assembly: AssemblyTitle("06. Sentence Split")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("05. Points Counter")]
+[assembly: AssemblyProduct("06. Sentence Split")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6c20eb86-f866-440e-9e3e-256cc4669845")]
+[assembly: Guid("801d3baf-b1ca-4b64-8d02-8fadc6e142f2")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ 14. Strings and Text Processing - Exercises/06. Sentence Split/Sentence Split.cs
@@ -0,0 +1,14 @@
+﻿namespace _06.Sentence_Split
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var delimiters = Console.ReadLine();
+            Console.WriteLine("[" + string.Join(", ", input.Split(new[] { delimiters }, StringSplitOptions.None)) + "]");
+        }
+    }
+}
