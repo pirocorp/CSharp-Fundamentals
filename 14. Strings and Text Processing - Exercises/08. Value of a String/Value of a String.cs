6ea1b07c89f37eef63fc88a987db19c34ba68a58
pirocorp <pirocorp@abv.bg>
2018-01-28 20:43:47 +0100

    Write a program which finds the sum of the ASCII codes of the letters in a given string.  Your tasks will be a bit harder, because you will have to find the sum of either the lowercase or the uppercase letters. On the first line, you will receive the string. On the second line, you will receive one of two possible inputs: • If you receive “UPPERCASE”  find the sum of all uppercase English letters in the previously received string •  If you receive “LOWERCASE”  find the sum of all lowercase English letters in the previously received string You should not sum the ASCII codes of any characters, which is not letters. At the end print the sum in the following format: • The total sum is: {sum}

---
+++ 14. Strings and Text Processing - Exercises/08. Value of a String/Value of a String.cs
@@ -0,0 +1,28 @@
+﻿namespace _08.Value_of_a_String
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+            var command = Console.ReadLine();
+
+            var lowercase = inputString.Where(x => char.IsLower(x)).Sum(x => x);
+            var uppercase = inputString.Where(x => char.IsUpper(x)).Sum(x => x);
+
+            switch (command)
+            {
+                case "LOWERCASE":
+                    Console.WriteLine($"The total sum is: {lowercase}");
+                    break;
+                case "UPPERCASE":
+                    Console.WriteLine($"The total sum is: {uppercase}");
+                    break;
+            }
+
+        }
+    }
+}
