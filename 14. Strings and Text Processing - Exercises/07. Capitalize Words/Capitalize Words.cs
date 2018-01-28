637d600115e4a2ced5bea0ffc58f103876d43717
pirocorp <pirocorp@abv.bg>
2018-01-28 20:13:02 +0100

    Create a program, which receives sentences of words, and capitalizes the words in them. The sentences will be grammatically correct. You must extract each word from the sentences, and you must capitalize it. The capitalization of a word is the process of making the first letter Capital, and every other letter – lowercase. You should read input lines, until you receive the command “end”. You might receive SEVERAL sentences on 1 input line. After you’ve read a sentence, you extract the words, capitalize them, and print them, separated by a space and a comma. ONLY then should you read the next sentence.

---
+++ 14. Strings and Text Processing - Exercises/07. Capitalize Words/Capitalize Words.cs
@@ -0,0 +1,52 @@
+﻿namespace _07.Capitalize_Words
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var words = new List<String>();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "end")
+            {
+                var sentence = inputData.Split(new[] { ' ', '.', ',', ':', ';', '?', '!', '-', '_' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                sentence = WordsCapitalization(sentence);
+                words.AddRange(sentence);
+                Console.WriteLine(String.Join(", ", sentence));
+                inputData = Console.ReadLine();
+            }
+
+            //Console.WriteLine(String.Join(", ", words));
+        }
+
+        private static List<string> WordsCapitalization(List<string> sentence)
+        {
+            var currentWords = new List<String>();
+            var words = new List<string>();
+
+            for (var i = 0; i < sentence.Count; i++)
+            {
+                currentWords = sentence[i].Split(new []{ ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                for (int j = 0; j < currentWords.Count; j++)
+                {
+                    var currentWord = Capitalization(currentWords[j]);
+                    words.Add(currentWord);
+                }
+            }
+            
+            return words;
+        }
+
+        private static string Capitalization(string s)
+        {
+            var result = s.ToLower();
+            result = new String(result.ToUpper().Take(1).ToArray()) + new String(result.Skip(1).ToArray());
+            return result.Trim();
+        }
+    }
+}
