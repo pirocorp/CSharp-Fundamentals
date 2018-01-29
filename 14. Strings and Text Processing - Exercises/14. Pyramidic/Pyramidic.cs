219cdeabdb6cdf4fbb022c24e6c91b1894844086
pirocorp <pirocorp@abv.bg>
2018-01-29 17:31:43 +0100

    You will be given N – an integer. On the next N input lines, you will be given N strings, which may consist of any ASCII character. Your task is to find the BIGGEST pyramid formation of occurrences of a SINGLE CHARACTER, throughout the strings. The pyramid is formed by finding a character on a line, then finding 3 consecutive (next to each, other) occurrences of the same character on the next line, then finding 5 consecutive occurrences on the next line and so on. . .

---
+++ 14. Strings and Text Processing - Exercises/14. Pyramidic/Pyramidic.cs
@@ -0,0 +1,61 @@
+﻿namespace _14.Pyramidic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Pyramidic
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var characterOccurances = new Dictionary<char, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var inputLine = Console.ReadLine();
+
+                for (int j = 0; j < inputLine.Distinct().ToArray().Length; j++)
+                {
+                    var currentChar = inputLine.Distinct().ToArray()[j];
+                    CountCurrentChar(currentChar, characterOccurances, inputLine);
+                }
+            }
+
+            var maxPyramid = characterOccurances.OrderByDescending(x => x.Value).First();
+            var count = maxPyramid.Value;
+            var character = maxPyramid.Key;
+
+            for (int i = 0; i <= count; i += 2)
+            {
+                Console.WriteLine(new string(character, i + 1));
+            }
+        }
+
+        private static void CountCurrentChar(char currentChar, Dictionary<char, int> characterOccurances, string inputLine)
+        {
+            var count = 0;
+
+            for (int i = 0; i < inputLine.Length; i++)
+            {
+                if (inputLine[i] == currentChar)
+                {
+                    count++;
+                }
+            }
+
+            if (!characterOccurances.ContainsKey(currentChar))
+            {
+                characterOccurances[currentChar] = 1;
+                return;
+            }
+
+            if (characterOccurances.ContainsKey(currentChar) && characterOccurances[currentChar] + 2 <= count)
+            {
+                characterOccurances[currentChar] += 2;
+            }
+
+        }
+    }
+}
