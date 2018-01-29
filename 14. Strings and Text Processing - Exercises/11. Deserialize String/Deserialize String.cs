8b8bc43705b08146157aeef6147fabc8faff0991
pirocorp <pirocorp@abv.bg>
2018-01-29 11:40:09 +0100

    Write a program, which takes the output from the previous task and turns it back into a string. Until you receive the line “end”, you will receive several lines of input on the console, in the following format: • “{letter}:{index1}/{index2}/{index…}/{indexN}” Your task is to take every letter and its index and form a string out of them.

---
+++ 14. Strings and Text Processing - Exercises/11. Deserialize String/Deserialize String.cs
@@ -0,0 +1,42 @@
+﻿namespace _11.Deserialize_String
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var charListOfOccurances = new Dictionary<char, List<int>>();
+
+            while (inputLine != "end")
+            {
+                var tokens = inputLine.Split(':');
+                var character = tokens[0].First();
+                var listOfOccurances = tokens[1].Split('/').Select(int.Parse).ToList();
+                charListOfOccurances[character] = listOfOccurances;
+                inputLine = Console.ReadLine();
+            }
+
+            var numberOfAllCharactersInString = charListOfOccurances.Values.Sum(x => x.Count);
+            var charArray = new char[numberOfAllCharactersInString];
+
+            foreach (var characer in charListOfOccurances)
+            {
+                var currentChar = characer.Key;
+                var currentCharOccurances = characer.Value;
+
+                for (int i = 0; i < currentCharOccurances.Count; i++)
+                {
+                    var currentIndex = currentCharOccurances[i];
+                    charArray[currentIndex] = currentChar;
+                }
+            }
+
+            var result = new string(charArray);
+            Console.WriteLine(result);
+        }
+    }
+}
