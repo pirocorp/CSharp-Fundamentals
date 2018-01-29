edca48a1b2a2e88c5401c2205ed8f7cf9f412bfe
pirocorp <pirocorp@abv.bg>
2018-01-29 11:20:44 +0100

    You have been tasked to serialize a string. The serialization is done in a special way, in which a character from that string is saved with the indexes at which it is found. The input will consist of a single input line, containing a single string, which may consist of ANY ASCII character. Your task is to serialize the string in the following way: {char}:{index1}/{index2}/{index3} The char will be the character, and the indexes, will be the indexes it is found at in the string. Note: This problem is a string problem, and should ONLY use strings in its solution.

--- 14. Strings and Text Processing - Exercises/10. Serialize String/Properties/AssemblyInfo.cs
+++ 14. Strings and Text Processing - Exercises/10. Serialize String/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("09. Diamond Problem")]
+[assembly: AssemblyTitle("10. Serialize String")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("09. Diamond Problem")]
+[assembly: AssemblyProduct("10. Serialize String")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("166bf45c-ebc9-468e-825a-24054fdc4911")]
+[assembly: Guid("7420f2a7-98a9-4180-ae85-56f2fab58749")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ 14. Strings and Text Processing - Exercises/10. Serialize String/Serialize String.cs
@@ -0,0 +1,30 @@
+﻿namespace _10.Serialize_String
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+            var charOccurances = new Dictionary<char, List<int>>();
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (!charOccurances.ContainsKey(inputString[i]))
+                {
+                    charOccurances[inputString[i]] = new List<int>();
+                }
+
+                charOccurances[inputString[i]].Add(i);
+            }
+
+            foreach (var character in charOccurances)
+            {
+                var listOfOccurances = character.Value;
+                Console.WriteLine($"{character.Key}:{String.Join("/", listOfOccurances)}");
+            }
+        }
+    }
+}
