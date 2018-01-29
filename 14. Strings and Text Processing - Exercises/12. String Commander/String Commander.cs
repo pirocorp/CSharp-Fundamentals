4125e4ee87530bd027ab2858295b134085a3e4c5
pirocorp <pirocorp@abv.bg>
2018-01-29 12:30:42 +0100

    Strings can be hard to manipulate because of their immutable nature. As a master of strings, you have to write a program, which will help the less experienced manipulate them. On the first line, you will receive the string, which you have to manipulate. On the next input lines, until you receive the command "end", you’ will receive a series of commands in one of the following formats: • "Left {count} times" – this command moves all elements left count times. On each roll, the first element is placed at the end of the string. • "Right {count} times" – this command moves all elements left count times. On each roll, the last element is placed at the beginning of the string. • "Insert {index} {string}" – insert the given string at the index. • "Delete {startIndex} {endIndex}" – delete the element from the startIndex (inclusive) to the endIndex (inclusive) At the end, print the string after all modifications. Input • The first input line will hold the string, which we have to manipulate. • The next lines will hold commands in the described formats. • The input ends with the keyword "end". Output • After receiving the "end" command, print the string after all manipulations. Constraints • All commands, indices and counts will be in the correct format and inside the string. You do not have to check them explicitly.

---
+++ 14. Strings and Text Processing - Exercises/12. String Commander/String Commander.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.String_Commander
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+            var inputData = Console.ReadLine();
+
+            while (inputData != "end")
+            {
+                var tokens = inputData.Split(' ');
+                var command = tokens[0];
+
+                switch (command)
+                {
+                    case "Left":
+                        var count = int.Parse(tokens[1]);
+                        count = count % inputString.Length;
+                        inputString = Left(inputString, count);
+                        break;
+                    case "Right":
+                        count = int.Parse(tokens[1]);
+                        count = count % inputString.Length;
+                        inputString = Right(inputString, count);
+                        break;
+                    case "Insert":
+                        var index = int.Parse(tokens[1]);
+                        var str = tokens[2];
+                        inputString = inputString.Insert(index, str);
+                        break;
+                    case "Delete":
+                        var startIndex = int.Parse(tokens[1]);
+                        var endIndex = int.Parse(tokens[2]);
+                        count = (endIndex - startIndex) + 1;
+                        inputString = inputString.Remove(startIndex, count);
+                        break;
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+            Console.WriteLine(inputString);
+        }
+
+        private static string Right(string inputString, int count)
+        {
+            var result = inputString;
+
+            for (int i = 0; i < count; i++)
+            {
+                var lastElement = result[result.Length - 1];
+                result = lastElement + result.Substring(0, result.Length - 1);
+            }
+
+            return result;
+        }
+
+        private static string Left(string inputString, int count)
+        {
+            var result = inputString;
+
+            for (int i = 0; i < count; i++)
+            {
+                result = result.Substring(1);
+                var firstElement = inputString[i];
+                result = result + firstElement;
+            }
+
+            return result;
+        }
+    }
+}
