3c5e247c5e56af8134a4a8288ef6eb5807c93a47
pirocorp <pirocorp@abv.bg>
2018-01-30 14:24:21 +0100

    Create a method that takes two strings as arguments and returns the sum of their character codes multiplied (multiply str1.charAt (0) with str2.charAt (0) and add to the total sum). Then continue with the next two characters. If one of the strings is longer than the other, add the remaining character codes to the total sum without multiplication.

---
+++ 14. Strings and Text Processing - Exercises/19. Character Multiplier/Character Multiplier.cs
@@ -0,0 +1,39 @@
+﻿namespace _19.Character_Multiplier
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputData = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var firstString = inputData[0];
+            var secondString = inputData[1];
+            var totalSum = 0L;
+
+            var count = Math.Min(firstString.Length, secondString.Length);
+
+            for (int i = 0; i < count; i++)
+            {
+                totalSum += firstString[i] * secondString[i];
+            }
+
+            if (firstString.Length > secondString.Length)
+            {
+                for (int i = count; i < firstString.Length; i++)
+                {
+                    totalSum += firstString[i];
+                }
+            }
+            else if (secondString.Length > firstString.Length)
+            {
+                for (int i = count; i < secondString.Length; i++)
+                {
+                    totalSum += secondString[i];
+                }
+            }
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
