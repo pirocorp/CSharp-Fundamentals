ab93c8627713febb05322266913f6dfebf1e859c
pirocorp <pirocorp@abv.bg>
2018-01-30 14:15:39 +0100

    Write a program that converts a string to a sequence of Unicode character literals.

---
+++ 14. Strings and Text Processing - Exercises/18. Unicode Characters/Unicode Characters.cs
@@ -0,0 +1,21 @@
+﻿namespace _18.Unicode_Characters
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+
+            var chars = inputString
+                .Select(c => (int)c)
+                .Select(c => $"\\u{c:x4}")
+                .ToArray();
+
+            var result = string.Join("", chars);
+            Console.WriteLine(result);
+        }
+    }
+}
