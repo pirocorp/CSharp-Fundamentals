f549cd8f24d819df58d74d2096c871e554340b6f
pirocorp <pirocorp@abv.bg>
2018-01-30 13:46:43 +0100

    Some refact

--- 14. Strings and Text Processing - Exercises/17. Convert from Base-N to Base-10/Convert from Base-N to Base-10.cs
+++ 14. Strings and Text Processing - Exercises/17. Convert from Base-N to Base-10/Convert from Base-N to Base-10.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _17.Convert_from_Base_N_to_Base_10
+﻿namespace _17.Convert_from_Base_N_to_Base_10
 {
+    using System;
+    using System.Numerics;
+
     class Program
     {
         static void Main()
