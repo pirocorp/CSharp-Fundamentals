0599d3600eb15119d81194a75ab7b678e1800f35
pirocorp <pirocorp@abv.bg>
2018-01-30 13:45:36 +0100

    Write a program that takes a base-N number and converts it to a base-10 number (0 to 1050), where 2 <= N <= 10. The input consists of 1 line containing two numbers separated by a single space. The first number is the base N to which you have to convert. The second one is the base N number to be converted. Do not use any built in converting functionality, try to write your own algorithm.

---
+++ 14. Strings and Text Processing - Exercises/17. Convert from Base-N to Base-10/Convert from Base-N to Base-10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Convert_from_Base_N_to_Base_10
+{
+    class Program
+    {
+        static void Main()
+        {
+            var tokens = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var numberBase = BigInteger.Parse(tokens[0]);
+            var numberInBase = BigInteger.Parse(tokens[1]);
+            BigInteger currentNumberInDec = 0;
+            var power = 0;
+
+            while (numberInBase > 0)
+            {
+                currentNumberInDec += (numberInBase % 10) * BigInteger.Pow(numberBase, power);
+                numberInBase = numberInBase / 10;
+                power++;
+            }
+
+            Console.WriteLine(currentNumberInDec);
+        }
+    }
+}
