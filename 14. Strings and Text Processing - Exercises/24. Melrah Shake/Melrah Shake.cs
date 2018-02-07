5349909c55093aac553ec0a2ac715de3751d360f
pirocorp <pirocorp@abv.bg>
2018-02-07 14:16:46 +0100

    You are given a string of random characters, and a pattern of random characters. You need to “shake off” (remove) all of the border occurrences of that pattern, in other words, the very first match and the very last match of the pattern you find in the string. When you successfully shake off a match, you remove from the pattern the character which corresponds to the index equal to the pattern’s length / 2. Then you continue to shake off the border occurrences of the new pattern until the pattern becomes empty or until there is less than the - needed for shake, matches in the remaining string. In case you have found at least two matches, and you have successfully shaken them off, you print “Shaked it.” on the console. Otherwise you print “No shake.”, the remains of the main string, and you end the program. See the examples for more info. Input • The input will consist only of two lines. • On the first line, you will get a string of random characters. • On the second line, you will receive the pattern and that ends the input sequence. Output • You must print “Shaked it.” for every time you successfully do the melrah shake. • If the melrah shake fails, you print “No shake.”, and on the next line you print what has remained of the main string. Constraints • The two strings may contain ANY ASCII character. • Allowed time/memory: 250ms/16MB.

---
+++ 14. Strings and Text Processing - Exercises/24. Melrah Shake/Melrah Shake.cs
@@ -0,0 +1,36 @@
+﻿namespace _24.Melrah_Shake
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var stringInput = Console.ReadLine();
+            var pattern = Console.ReadLine();
+
+            while (stringInput.Length > 0 && pattern.Length > 0)
+            {
+                var firstIndex = stringInput.IndexOf(pattern);
+                var lastIndex = stringInput.LastIndexOf(pattern);
+
+                if (firstIndex >= 0 && lastIndex >= 0 && firstIndex != lastIndex)
+                {
+                    Console.WriteLine($"Shaked it.");
+
+                    stringInput = stringInput.Remove(firstIndex, pattern.Length);
+                    lastIndex = stringInput.LastIndexOf(pattern);
+                    stringInput  = stringInput.Remove(lastIndex, pattern.Length);
+                    pattern = pattern.Remove(pattern.Length / 2, 1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"No shake.");
+            Console.WriteLine(stringInput);
+        }
+    }
+}
