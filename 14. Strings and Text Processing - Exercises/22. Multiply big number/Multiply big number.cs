0e3521375611413081892fea8aa7e918b5fd0e64
pirocorp <pirocorp@abv.bg>
2018-02-07 12:36:22 +0100

    You are given two lines – the first one can be a really big number (0 to 1050). The second one will be a single digit number (0 to 9). You must display the product of these numbers. Note: do not use the BigInteger or BigDecimal classes for solving this problem.

---
+++ 14. Strings and Text Processing - Exercises/22. Multiply big number/Multiply big number.cs
@@ -0,0 +1,46 @@
+﻿namespace _22.Multiply_big_number
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            var num1Chars = Console.ReadLine().TrimStart(new[] { '0' }).ToCharArray();
+            var num2Chars = int.Parse(Console.ReadLine());
+
+            var resultList = new List<string>();
+            var inMemory = 0;
+
+            for (var i = num1Chars.Length - 1; i >= 0; i--)
+            {
+                var currentResult = int.Parse(new string(num1Chars[i], 1)) * num2Chars + inMemory;
+                inMemory = currentResult / 10;
+                currentResult %= 10;
+                resultList.Add($"{currentResult}");
+            }
+
+            if (inMemory > 0)
+            {
+                resultList.Add($"{inMemory}");
+            }
+
+            var result = string.Empty;
+
+            for (var i = resultList.Count - 1; i >= 0; i--)
+            {
+                result += resultList[i];
+            }
+
+            result = result.TrimStart(new[] {'0'});
+
+            if (result == string.Empty)
+            {
+                result = "0";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
