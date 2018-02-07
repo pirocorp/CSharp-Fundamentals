5f8b2ca97964bddcc6e926f6af2a35ed020cb93a
pirocorp <pirocorp@abv.bg>
2018-02-07 11:23:16 +0100

    Write a method that takes as input two strings, and returns Boolean if they are exchangeable or not. Exchangeable are words where the characters in the first string can be replaced to get the second string. Example: "egg" and "add" are exchangeable, but "aabbccbb" and "nnooppzz" are not. (First 'b' corresponds to 'o', but then it also corresponds to 'z'). The two words may not have the same length, if such is the case they are exchangeable only if the longer one doesn't have more types of characters then the shorter one ("Clint" and "Eastwaat" are exchangeable because 'a' and 't' are already mapped as 'l' and 'n', but "Clint" and "Eastwood" aren't exchangeable because 'o' and 'd' are not contained in "Clint").

---
+++ 14. Strings and Text Processing - Exercises/20. Magic exchangeable words/Magic exchangeable words.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Magic_exchangeable_words
+{
+    class Program
+    {
+        static void Main()
+        {
+            var tokens = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var string1 = tokens[0];
+            var string2 = tokens[1];
+
+            var result = IsExchangeable(string1, string2);
+            Console.WriteLine(result.ToString().ToLower());
+        }
+
+        private static bool IsExchangeable(string string1, string string2)
+        {
+            var chars = new Dictionary<char, char>();
+            var minLength = Math.Min(string1.Length, string2.Length);
+
+            for (var i = 0; i < minLength; i++)
+            {
+                var currentCharacter = string1[i];
+
+                if (!chars.ContainsKey(currentCharacter))
+                {
+                    chars[currentCharacter] = string2[i];
+                }
+                else
+                {
+                    if (chars[currentCharacter] != string2[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            var distinctElementsInFIrstString = string1.ToCharArray().Distinct().ToArray().Length;
+            var distinctElementsInSecondString = string2.ToCharArray().Distinct().ToArray().Length;
+
+            if (distinctElementsInFIrstString != distinctElementsInSecondString)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
