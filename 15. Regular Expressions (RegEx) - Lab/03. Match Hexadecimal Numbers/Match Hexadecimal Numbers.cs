4e08e1292a848667da71da036f828dd54af678e7
pirocorp <pirocorp@abv.bg>
2018-02-08 12:22:04 +0100

    Write a program, which finds all valid hexadecimal numbers in a string and print them space-separated. Compose the Regular Expression A valid hexadecimal number follows these conditions: • Can have “0x” in front of it (not required) • Has one or more hexadecimal digits after it (0-9 and A-F). • Doesn’t have anything on either of its sides (use \b).

---
+++ 15. Regular Expressions (RegEx) - Lab/03. Match Hexadecimal Numbers/Match Hexadecimal Numbers.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.Match_Hexadecimal_Numbers
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var pattern = @"\b(?:0x)?[0-9A-F]+\b";
+            var numbersString = Console.ReadLine();
+            var numbers = Regex.Matches(numbersString, pattern)
+                .Cast<Match>()
+                .Select(x => x.Value)
+                .ToArray();
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
