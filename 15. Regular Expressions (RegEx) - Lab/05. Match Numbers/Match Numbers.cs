9d3b494c78bb0b360dcf01e21bf71e622ed1d989
pirocorp <pirocorp@abv.bg>
2018-02-08 13:07:52 +0100

    A number has the following characteristics: • Has either whitespace before it or the start of the string (match either ^ or what’s called a positive lookbehind). The entire syntax for the beginning of your RegEx might look something like “(^|(?<=\s))”. • The number might or might not be negative, so it might have a hyphen on its left side (“-“). • Consists of one or more digits. • Might or might not have digits after the decimal point • The decimal part (if it exists) consists of a period (“.”) and one or more digits after it. Use a capturing group. • Has either whitespace before it or the end of the string (match either $ or what’s called a positive lookahead). The syntax for the end of the RegEx might look something like “($|(?=\s))”. Let’s see how we would translate the above rules into a regular expression: • First off, we need to establish what needs to exist before our number. We can’t use \b here, since it includes “-“, which we need to match negative numbers. Instead, we’ll use a positive lookbehind, which matches if there’s something immediately behind it. We’ll match if we’re either at the start of the string (^), or if there’s any whitespace behind the string:

---
+++ 15. Regular Expressions (RegEx) - Lab/05. Match Numbers/Match Numbers.cs
@@ -0,0 +1,22 @@
+﻿namespace _05.Match_Numbers
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+            var numbersString = Console.ReadLine();
+            var numbers = Regex.Matches(numbersString, pattern);
+
+            foreach (Match number in numbers)
+            {
+                Console.Write(number.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
