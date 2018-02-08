2f6c4adeee897cc018590e83c3e61644f036cbf3
pirocorp <pirocorp@abv.bg>
2018-02-08 20:25:05 +0100

    You will be given sequences of playing cards. Your task is to print the valid ones of them, separated by a comma and a space. The VALID playing cards have: • power – 2, 3, 4… 10, J, Q, K, A. • suit – S, H, D, C A card is formed by its power and its suit in the following format: {power}{suit}. . ., but ONLY those with valid power and valid suit, should be considered VALID. So a valid car would look like this: KS, 10S, 2D, 3D. Input The input will consist of a single line, containing a sequence of cards. Output The output should be a single line, containing the valid cards, separated by a comma and a space.

---
+++ 15. Regular Expressions (RegEx) - Exercises/01. Cards/Cards.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.Cards
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Cards
+    {
+        static void Main()
+        {
+            var pattern = @"(^|(?<=[A-Z]))(?<power>[AKQJ]|10|[2-9])(?<suit>[SHDC])";
+
+            var inputString = Console.ReadLine();
+            var matches = Regex.Matches(inputString, pattern);
+            var cards = matches.Cast<Match>().Select(x => x.Value).ToArray();
+            Console.WriteLine(String.Join(", ", cards));
+        }
+    }
+}
