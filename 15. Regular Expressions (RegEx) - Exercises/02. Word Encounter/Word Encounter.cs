61be854d379fd7f7760f66176e60c69f7b481624
pirocorp <pirocorp@abv.bg>
2018-02-08 21:59:50 +0100

    You will be given a filter, in the form of a string of two characters – the first being an ASCII character, and the second – a digit. You will then receive a sequence of sentences. You must extract all words from those sentences, and print only the words that contain the given filter character, at least N times – N being the filter digit. There are 2 types of sentences – Valid and Invalid. The valid ones, always start with a capital letter, and always end with one of the following characters: “.”, “!”, “?”. Invalid sentences, should be ignored. Note: You WILL NOT be given more than 1 sentence on a single input line. The input sequence ends, when you receive the command “end”. After that you must print all the filtered words, you’ve gathered. The valid words must be printed, on a single line, separated by a comma and a space.

---
+++ 15. Regular Expressions (RegEx) - Exercises/02. Word Encounter/Word Encounter.cs
@@ -0,0 +1,47 @@
+﻿namespace _02.Word_Encounter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var filter = Console.ReadLine();
+            var letter = filter[0];
+            var digit = int.Parse(new string(filter[1], 1));
+            var inputSentence = Console.ReadLine();
+            var validSentence = @"(?<!.)[A-Z].*[.!?](?!.)"; //negative lookbehind and negative lookahead (nothing before and after)
+            var letters = new string(letter, digit).ToCharArray();
+            var wordPattern = string.Empty;
+
+            if (digit > 0)
+            {
+                wordPattern = $"\\w*{string.Join("\\w*", letters)}\\w*";
+            }
+            else
+            {
+                wordPattern = @"\w+";
+            }
+
+            var allMatchedWords = new List<string>();
+
+            while (inputSentence != "end")
+            {
+                var isMatch = Regex.IsMatch(inputSentence, validSentence);
+
+                if (isMatch)
+                {
+                    var words = Regex.Matches(inputSentence, wordPattern).Cast<Match>().Select(x => x.Value).ToList();
+                    allMatchedWords.AddRange(words);
+                }
+
+                inputSentence = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", allMatchedWords));
+        }
+    }
+}
