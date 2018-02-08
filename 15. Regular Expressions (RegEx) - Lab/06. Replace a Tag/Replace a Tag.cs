3046fa40f72fcbe7c05ddff32c69120b4c961b8c
pirocorp <pirocorp@abv.bg>
2018-02-08 13:27:00 +0100

    Write a program that replaces in a HTML document given as string all the tags <a href=…>…</a> with corresponding tags [URL href=…>…[/URL]. Read an input, until you receive the “end” command. Print the lines on the console, but with the <a> tags replaced.

---
+++ 15. Regular Expressions (RegEx) - Lab/06. Replace a Tag/Replace a Tag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.Replace_a_Tag
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+            var replacement = @"[URL href=$1]$2[/URL]";
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != "end")
+            {
+                var replaced = Regex.Replace(inputLine, pattern, replacement);
+                Console.WriteLine(replaced);
+                inputLine = Console.ReadLine();
+            }
+
+        }
+    }
+}
