75452ea38f04d2d6b59fc149b20b1ecfb3bb3680
pirocorp <pirocorp@abv.bg>
2018-02-09 14:52:30 +0100

    You just bought brand new cannon, but you are not very good at calculating where your bullets will fall. Instead of just shooting blindly you decided to make a simulation, which is expensive and a bit dangerous, you decided to simulate cannon shots using strings. On the first line, you will receive an array of integers. First element – n will be the distance of the shot and the second element – m will be the count of elements, which the shot will destroy. On the second line, you will receive a string. Every cannon in the string will be marked with "\<<<". You have to skip next n characters after the cannon and destroy the next m characters. If you find another cannon in your target  take as target only until the last character before the new cannon and start new shot with the new cannon. Your task is to print all destroyed targets separated with "/\". Output Print all destroyed targets separated with "/\ ".

---
+++ 15. Regular Expressions (RegEx) - Exercises/03. Booming Cannon/Booming Cannon.cs
@@ -0,0 +1,73 @@
+﻿namespace _03.Booming_Cannon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var distance = numbers[0];
+            var countOfElements = numbers[1];
+            var inputString = Console.ReadLine();
+            var cannonPatern = @"\\<<<";
+            var isMatch = Regex.IsMatch(inputString, cannonPatern);
+            var matches = new [] {-1};
+            var resultedStrings = new List<string>();
+
+            if (isMatch)
+            {
+                matches = Regex.Matches(inputString, cannonPatern)
+                    .Cast<Match>()
+                    .Select(x => x.Index)
+                    .ToArray();
+            }
+
+            if (matches[0] >= 0)
+            {
+                for (int i = 0; i < matches.Length; i++)
+                {
+                    var currentMatchIndex = matches[i];
+                    var currentStartIndex = currentMatchIndex + cannonPatern.Length;
+                    var currentSubStringStartIndex = currentStartIndex + distance - 1;
+
+                    if (currentSubStringStartIndex >= inputString.Length)
+                    {
+                        break;
+                    }
+
+                    var currentCountOfElements = countOfElements;
+
+                    if ((i + 1) < matches.Length)
+                    {
+                        if (currentSubStringStartIndex + currentCountOfElements >= matches[i + 1])
+                        {
+                            currentCountOfElements = Math.Max(matches[i + 1] - currentSubStringStartIndex, 0);
+                        }
+                    }
+
+                    if (currentSubStringStartIndex + currentCountOfElements >= inputString.Length)
+                    {
+                        currentCountOfElements = Math.Max(inputString.Length - currentSubStringStartIndex, 0);
+                    }
+
+                    var currentSubString = inputString.Substring(currentSubStringStartIndex, currentCountOfElements);
+                    resultedStrings.Add(currentSubString);
+                }
+            }
+            //else
+            //{
+            //    Console.WriteLine("Not found");
+            //}
+
+            resultedStrings = resultedStrings.Where(x => x.Length > 0).ToList();
+            Console.WriteLine(String.Join("/\\", resultedStrings));
+        }
+    }
+}
