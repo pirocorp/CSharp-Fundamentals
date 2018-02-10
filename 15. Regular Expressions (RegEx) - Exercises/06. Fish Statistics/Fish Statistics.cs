bf1975f1d9111cf64a91bf86408a7d3306958e1b
pirocorp <pirocorp@abv.bg>
2018-02-10 13:31:35 +0100

    This fish has a tail length of 1, a body length of 4 and has the status “Awake”, since its eye is open. One ASCII character represents 2 centimeters in real life. By those standards, this fish has a tail length of 2 cm and a body length of 8 cm. There are various types of tails, bodies and statuses, which are described below: • Tail types: o Tail longer than 5 “<” characters  Long o Tail longer than 1 “<” characters  Medium o Tail, which is 1 “<” character long  Short o Nonexistent tail  None • Body types: o Body longer than 10 “(” characters  Long o Body longer than 5 “(” characters  Medium o Any other length  Short • Statuses: o '  Awake o -  Asleep o x  Dead The input will contain a variable amount of fish, separated by any sequence of ASCII characters. There’s a possibility you might receive input, which has no fish – in this case, just print “No fish found.”, and end the program.

---
+++ 15. Regular Expressions (RegEx) - Exercises/06. Fish Statistics/Fish Statistics.cs
@@ -0,0 +1,100 @@
+﻿namespace _06.Fish_Statistics
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var pattern = @"(?<tail>>*)<(?<body>\(+)(?<status>['\-x])>";
+            var inputData = Console.ReadLine();
+            var fishIsFound = Regex.IsMatch(inputData, pattern);
+
+            if (fishIsFound)
+            {
+                var fishes = Regex.Matches(inputData, pattern).Cast<Match>().ToList();
+                var currentFishNumber = 0;
+
+                foreach (var fish in fishes)
+                {
+                    currentFishNumber++;
+                    ProcessFish(fish, currentFishNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No fish found.");
+            }
+        }
+
+        private static void ProcessFish(Match fish, int currentFishNumber)
+        {
+            var fishTail = fish.Groups["tail"].Value;
+            var fishBody = fish.Groups["body"].Value;
+            var fishStatus = fish.Groups["status"].Value;
+
+            var tailType = string.Empty;
+
+            if (fishTail.Length > 5)
+            {
+                tailType = "Long";
+            }
+            else if (fishTail.Length > 1)
+            {
+                tailType = "Medium";
+            }
+            else if (fishTail.Length == 1)
+            {
+                tailType = "Short";
+            }
+            else
+            {
+                tailType = "None";
+            }
+
+            var bodyType = string.Empty;
+
+            if (fishBody.Length > 10)
+            {
+                bodyType = "Long";
+            }
+            else if (fishBody.Length > 5)
+            {
+                bodyType = "Medium";
+            }
+            else
+            {
+                bodyType = "Short";
+            }
+
+            var status = string.Empty;
+
+            if (fishStatus == "'")
+            {
+                status = "Awake";
+            }
+            else if (fishStatus == "-")
+            {
+                status = "Asleep";
+            }
+            else if (fishStatus == "x")
+            {
+                status = "Dead";
+            }
+
+            Console.WriteLine($"Fish {currentFishNumber}: {fish.Value}");
+            if (fishTail.Length * 2 == 0)
+            {
+                Console.WriteLine($"Tail type: {tailType}");
+            }
+            else
+            {
+                Console.WriteLine($"Tail type: {tailType} ({fishTail.Length * 2} cm)");
+            }
+            Console.WriteLine($"Body type: {bodyType} ({fishBody.Length * 2} cm)");
+            Console.WriteLine($"Status: {status}");
+        }
+    }
+}
