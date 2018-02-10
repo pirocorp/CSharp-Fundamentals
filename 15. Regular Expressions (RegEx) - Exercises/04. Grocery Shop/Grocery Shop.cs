40d0ae40f26a7beae805640d1d6ce806489b1c8a
pirocorp <pirocorp@abv.bg>
2018-02-10 11:10:17 +0100

    You are given the task to write a program for a shop, which calculates the total price of products. The problem is the old system was full of bugs and you need to decrypt the data which you receive. We know the following about the input data: • The product name starts with English capital letter and all other letters should be lower-case. • After that the product name should be followed by colon – ‘:’ • Price of the product, which should be floating point number with exactly two digits after the decimal point. If you receive input, which does not follow the rules above  ignore it. If you receive new price for already existing product  rewrite the previous values. At the end print the prices for every product. Order the products by price in descending order. Input You will receive indefinite count of string until you receive the command "bill". Output Print all prices ordered by price in descending order. Use the following format: {nameOfTheProduct} costs {priceOfTheProduct} Format the price to the 2nd decimal place.

---
+++ 15. Regular Expressions (RegEx) - Exercises/04. Grocery Shop/Grocery Shop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _04.Grocery_Shop
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pattern = @"^(?<product>[A-Z][a-z]+):(?<price>\d+\.\d{2})$";
+            var inputLine = Console.ReadLine();
+            var inventory = new Dictionary<string, decimal>();
+
+            while (inputLine != "bill")
+            {
+                var matches = Regex.Matches(inputLine, pattern);
+
+                foreach (Match product in matches)
+                {
+                    var productName = product.Groups["product"].Value;
+                    var productPrice = decimal.Parse(product.Groups["price"].Value);
+
+                    if (!inventory.ContainsKey(productName))
+                    {
+                        inventory[productName] = 0;
+                    }
+
+                    inventory[productName] = productPrice;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            inventory = inventory.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in inventory)
+            {
+                Console.WriteLine($"{item.Key} costs {item.Value:F2}");
+            }
+        }
+    }
+}
