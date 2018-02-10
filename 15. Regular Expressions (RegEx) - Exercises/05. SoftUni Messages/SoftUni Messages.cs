838018f625c75122da56a479e7b8246c5776da6c
pirocorp <pirocorp@abv.bg>
2018-02-10 12:38:46 +0100

    After one of the beer fests at SoftUni (called also Friday by the people outside SoftUni) Vladi made a secret messaging system, which will help the SoftUni team to safely communicate. The problem is he lost the code which decrypted the messages and now he needs you to write it again. All he knows is the pattern, which he used to decrypt the messages. Valid messages should be in the following format: • Have length of m, where m is the number, which is given after every input line • Have one or more digits before the message • Consist only of Latin alphabet letters • Have at least one number after the message • Have no letters after the message After you find the message you need to decrypt it. Take all number before and after the message and consider them as indices. If they exist in the message form a string from the characters at those indices in the message. If they do not exist – just ignore them. Input • On every odd line, you will receive an encrypted message • On every even line, you will receive the searched length for the message from the previous line. • When the line “Decrypt!” is entered, the input sequence ends. Output • For every valid message print: {message} = {verificationCode} Constraints • The input lines can consist of ANY ASCII character. • There will be NO such cases as an encrypted message without a number before it. • The number will be a valid integer in the range [0, 100]. • You will receive only positive numbers as indices in the string.

---
+++ 15. Regular Expressions (RegEx) - Exercises/05. SoftUni Messages/SoftUni Messages.cs
@@ -0,0 +1,51 @@
+﻿namespace _05.SoftUni_Messages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+
+            while (inputString != "Decrypt!")
+            {
+                var lengthOfMessage = Console.ReadLine();
+                var pattern = $@"^\d+(?<message>[a-zA-Z]{{{lengthOfMessage}}})[^a-zA-Z\s]*(\d+)$";
+                var match = Regex.Match(inputString, pattern);
+                var allInput = match.Groups[0].Value;
+                var message = match.Groups["message"].Value;
+                var indexes = new List<int>();
+                var indexMatches = Regex.Matches(allInput, "\\d")
+                    .Cast<Match>()
+                    .Select(x => x.Value)
+                    .Select(int.Parse)
+                    .ToList();
+                indexes.AddRange(indexMatches);
+
+                var result = string.Empty;
+
+                for (var i = 0; i < indexes.Count; i++)
+                {
+                    var currentIndex = indexes[i];
+
+                    if (currentIndex < message.Length)
+                    {
+                        var currentChar = message[currentIndex];
+                        result += currentChar.ToString();
+                    }
+                }
+
+                if (result != string.Empty)
+                {
+                    Console.WriteLine($"{message} = {result}");
+                }
+
+                inputString = Console.ReadLine();
+            }
+        }
+    }
+}
