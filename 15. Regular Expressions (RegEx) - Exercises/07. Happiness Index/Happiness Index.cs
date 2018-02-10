ab73c3ad54aaf1812c542ca649c15fae9962e7f0
pirocorp <pirocorp@abv.bg>
2018-02-10 14:34:47 +0100

    You will be given a string, consisting of one or several emoticons and random garbage characters in-between them. Your task is to count the happy and sad emoticons and calculate the happiness index of the string. The index is calculated by this formula: {happyEmoticonsCount} / {sadEmoticonsCount}. The happiness index is then rounded to the second decimal place. Two emoticons will never touch. The emoticons of all the emotion types are as follows: • Happy: :), :D, ;), :*, :], ;], :}, ;}, (:, *:, c:, [:, [; • Sad: :(, D:, ;(, :[, ;[, :{, ;{, ):, :c, ]:, ]; After you calculate the happiness index, print the final emoticon score, following this format: • Happiness index greater than or equal to 2  :D • Happiness index greater than 1  :) • Happiness index equal to 1  :| • Happiness index smaller than 1  :(

---
+++ 15. Regular Expressions (RegEx) - Exercises/07. Happiness Index/Happiness Index.cs
@@ -0,0 +1,39 @@
+﻿namespace _07.Happiness_Index
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var happyEmojiPattern = @"(\:\)|\:D|\;\)|\:\*|\:\]|\;\]|\:\}|\;\}|\(\:|\*\:|c\:|\[\:|\[\;)";
+            var sadEmojiPattern = @"(\:\(|D\:|\;\(|\:\[|\;\[|\:\{|\;\{|\)\:|\:c|\]\:|\]\;)";
+            var inputLine = Console.ReadLine();
+            var happyCount = Regex.Matches(inputLine, happyEmojiPattern).Count;
+            var sadCount = Regex.Matches(inputLine, sadEmojiPattern).Count;
+            var happinessIndex = happyCount / (double) sadCount;
+            var emoji = string.Empty;
+
+            if (happinessIndex >= 2)
+            {
+                emoji = ":D";
+            }
+            else if (happinessIndex > 1)
+            {
+                emoji = ":)";
+            }
+            else if (happinessIndex == 1)
+            {
+                emoji = ":|";
+            }
+            else
+            {
+                emoji = ":(";
+            }
+
+            Console.WriteLine($"Happiness index: {happinessIndex:F2} {emoji}");
+            Console.WriteLine($"[Happy count: {happyCount}, Sad count: {sadCount}]");
+        }
+    }
+}
