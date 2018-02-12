358719513cd2ad29466cf54cb738ace7062f6f72
pirocorp <pirocorp@abv.bg>
2018-02-12 17:10:19 +0100

    The input comes from the console on a variable number of lines and ends when the keyword "END" is received. For each row of the input, the query string contains field=value pairs. Within each pair, the field name and value are separated by an equals sign, '='. The series of pairs are separated by an ampersand, '&'. The question mark is used as a separator and is not part of the query string. A URL query string may contain another URL as value. The input data will always be valid and in the format described. There is no need to check it explicitly. Output For each input line, print on the console a line containing the processed string as follows:  key=[value]nextkey=[another value] ... etc. Multiple whitespace characters should be reduced to one inside value/key names, but there shouldn’t be any whitespaces before/after extracted keys and values. If a key already exists, the value is added with comma and space after other values of the existing key in the current string.  See the examples below. Constraints • SPACE is encoded as '+' or "%20". Letters (A-Z and a-z), numbers (0-9), the characters '*', '-', '.', '_' and other non-special symbols are left as-is. • Allowed working time: 0.1 seconds. Allowed memory: 16 MB.

---
+++ 15. Regular Expressions (RegEx) - Exercises/15. Query Mess/Query Mess.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _15.Query_Mess
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputData = Console.ReadLine().Split(new []{'?'}, StringSplitOptions.RemoveEmptyEntries);
+            var convertWhiteSpacesRegex = new Regex(@"((%20)|\+)");
+            var reduceWhiteSpaceRegex = new Regex(@"\s+");
+
+            while (inputData[0] != "END")
+            {
+                var kvps = new List<string>();
+
+                foreach (var input in inputData)
+                {
+                    var currentKvp = input.Split(new[] {'&'}, StringSplitOptions.RemoveEmptyEntries);
+                    kvps.AddRange(currentKvp);
+                }
+
+                var result = new Dictionary<string, List<string>>();
+
+                for (var i = 0; i < kvps.Count; i++)
+                {
+                    var currentKvp = kvps[i].Split(new[] {'='}, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (currentKvp.Length < 2)
+                    {
+                        continue;
+                    }
+
+                    var key = currentKvp[0];
+                    key = convertWhiteSpacesRegex.Replace(key, " ");
+                    key = reduceWhiteSpaceRegex.Replace(key, " ");
+                    key = key.Trim();
+                    var value = currentKvp[1];
+                    value = convertWhiteSpacesRegex.Replace(value, " ");
+                    value = reduceWhiteSpaceRegex.Replace(value, " ");
+                    value = value.Trim();
+
+                    if (!result.ContainsKey(key))
+                    {
+                        result[key] = new List<string>();
+                    }
+
+                    result[key].Add(value);
+                }
+
+                foreach (var item in result)
+                {
+                    var key = item.Key;
+                    var values = item.Value;
+
+                    Console.Write($"{key}=[{string.Join(", ", values)}]");
+                }
+
+                Console.WriteLine();
+
+                inputData = Console.ReadLine().Split(new[] { '?' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
