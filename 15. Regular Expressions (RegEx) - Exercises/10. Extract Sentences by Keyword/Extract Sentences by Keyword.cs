97b07c544e09a36354bdbadaa4efe3ca27aef30d
pirocorp <pirocorp@abv.bg>
2018-02-12 14:05:18 +0100

    Write a program that extracts all sentences that contain a particular word from a string (case-sensitive). • Assume that the sentences are separated from each other by the character "." or "!" or "?". • The words are separated by a non-letter character. • Note that a substring is different than a word. The sentence “I am a fan of Motorhead” does not contain the word “to”. It contains the substring “to”, which is not what we need. • Print the result text without the separators between the sentences ("." or "!" or "?").

---
+++ 15. Regular Expressions (RegEx) - Exercises/10. Extract Sentences by Keyword/Extract Sentences by Keyword.cs
@@ -0,0 +1,26 @@
+﻿namespace _10.Extract_Sentences_by_Keyword
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var filter = Console.ReadLine();
+            var filterRegex = new Regex($@"\b{filter}\b");
+            var sentenceRegex = new Regex ("(?<sentance>.+?)[.!?]");
+            var inputData = Console.ReadLine();
+            var sentances = sentenceRegex.Split(inputData).Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            foreach (var sentance in sentances)
+            {
+                if (filterRegex.IsMatch(sentance))
+                {
+                    Console.WriteLine(sentance.Trim());
+                }
+            }
+        }
+    }
+}
