6057e36f4ae251ab2803990b2c1e0b65745ae51c
pirocorp <pirocorp@abv.bg>
2018-02-12 12:08:48 +0100

    Write a program, which receives lines of URLs. Until you receive the command “git push”, your task is to parse the URLs, which come in the following format: https://github.com/{user}/{repo}/commit/{hash},{message},{additions},{deletions} Valid input will follow this format. If any input doesn’t follow this format, ignore it. The validation rules are as follows: • The username may contain alphanumeric characters (Latin letters and digits) and hyphens (-). • The repository name may contain letters, hyphens and underscores. • The commit hash will be a SHA-1 hash, and as such, it may contain only hexadecimal characters (digits and letters A through F). Since SHA-1 hashes are always 20 bytes long, the commit hash will also always be 40 characters long. • The message may contain any character, except the new line character. • The additions and deletions are numbers and may only contain digits. While parsing the input, you also need to categorize it for Mr. Branch. Every user has his own repos (a user cannot have two repos with the same name) and every repo has a list of commits. The users are sorted alphabetically by name. The repos are also sorted alphabetically by name. The commits are sorted by order of insertion. After you receive the command “git push”, your task is to print information about the commits in the following format:

---
+++ 15. Regular Expressions (RegEx) - Exercises/08. Commits/Commit.cs
@@ -0,0 +1,32 @@
+﻿namespace _08.Commits
+{
+    using System.Text.RegularExpressions;
+
+    public class Commit
+    {
+        public Commit(string hashMessage, string message, int additions, int deletions)
+        {
+            HashMessage = hashMessage;
+            Message = message;
+            Additions = additions;
+            Deletions = deletions;
+        }
+
+        public string HashMessage { get; set; }
+        public string Message { get; set; }
+        public int Additions { get; set; }
+        public int Deletions { get; set; }
+
+        public static Commit Parse(string inputData)
+        {
+            var pattern = @"^(?<hash>[a-fA-F0-9]{40}),(?<message>.+),(?<additions>\d+),(?<deletions>\d+)$";
+            var match = Regex.Match(inputData, pattern);
+            var hashMessage = match.Groups["hash"].Value;
+            var message = match.Groups["message"].Value;
+            var additions = int.Parse(match.Groups["additions"].Value);
+            var deletions = int.Parse(match.Groups["deletions"].Value);
+            var commitObject = new Commit(hashMessage, message, additions, deletions);
+            return commitObject;
+        }
+    }
+}
---
+++ 15. Regular Expressions (RegEx) - Exercises/08. Commits/Commits.cs
@@ -0,0 +1,73 @@
+﻿namespace _08.Commits
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Commits
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var pattern = @"^https:\/\/github\.com\/(?<user>[a-zA-Z0-9\-]+)\/(?<repo>[a-zA-Z\-_]+)\/commit\/(?<commit>[a-fA-F0-9]{40}\,.+?,\d+,\d+)$";
+            var users = new SortedDictionary<string, SortedDictionary<string, List<Commit>>>(); //<user, <repo, commits>>
+
+            while (inputLine != "git push")
+            {
+                var match = Regex.Match(inputLine, pattern);
+                var user = match.Groups["user"].Value;
+                var repo = match.Groups["repo"].Value;
+                var commitString = match.Groups["commit"].Value;
+
+                if (commitString == string.Empty)
+                {
+                    inputLine = Console.ReadLine();
+                    continue;
+                }
+
+                var commit = Commit.Parse(commitString);
+
+                if (!users.ContainsKey(user))
+                {
+                    users[user] = new SortedDictionary<string, List<Commit>>();
+                }
+
+                if (!users[user].ContainsKey(repo))
+                {
+                    users[user][repo] = new List<Commit>();
+                }
+
+                users[user][repo].Add(commit);
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (var user in users)
+            {
+                var username = user.Key;
+                var repos = user.Value;
+                Console.WriteLine($"{username}:");
+
+                foreach (var repo in repos)
+                {
+                    var repoName = repo.Key;
+                    var commits = repo.Value;
+                    Console.WriteLine($"  {repoName}:");
+
+                    foreach (var commit in commits)
+                    {
+                        var hash = commit.HashMessage;
+                        var message = commit.Message;
+                        var additionsCount = commit.Additions;
+                        var deletionsCount = commit.Deletions;
+                        Console.WriteLine($"    commit {hash}: {message} ({additionsCount} additions, {deletionsCount} deletions)");
+                    }
+
+                    var totalAdditionsCount = commits.Sum(x => x.Additions);
+                    var totalDeletionsCount = commits.Sum(x => x.Deletions);
+                    Console.WriteLine($"    Total: {totalAdditionsCount} additions, {totalDeletionsCount} deletions");
+                }
+            }
+        }
+    }
+}
