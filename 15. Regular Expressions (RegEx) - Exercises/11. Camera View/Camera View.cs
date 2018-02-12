20bbde2fcbee93eaacd517f87bef9e6de0a06e6a
pirocorp <pirocorp@abv.bg>
2018-02-12 14:28:35 +0100

    You are an amateur photographer and you want to calculate what will be seen in your pictures. On the first line, you will receive an array of integers with exactly two elements: First element – m will be the elements, which you have to skip. The second element – n will be the elements, which you have to take. On the next line, you will receive a string, in which every camera will be marked with "|<". Skip the next m elements immediately after the camera and take the next n elements. If you encounter new camera in the view  stop the current camera and start new view with the newly found.

---
+++ 15. Regular Expressions (RegEx) - Exercises/11. Camera View/Camera View.cs
@@ -0,0 +1,42 @@
+﻿namespace _11.Camera_View
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var nums = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var skip = int.Parse(nums[0]);
+            var take = int.Parse(nums[1]);
+            var inputString = Console.ReadLine();
+            var tokens = Regex.Split(inputString, @"\|<");
+
+            if (tokens.Length <= 1)
+            {
+                //Console.WriteLine("Nothing found");
+                return;
+            }
+
+            var results = new List<string>();
+
+            for (var i = 1; i < tokens.Length; i++)
+            {
+                if (tokens[i].Length > 0)
+                {
+                    var result = new string(tokens[i].Skip(skip).Take(take).ToArray());
+                    results.Add(result);
+                }
+            }
+
+            results = results.Where(x => x.Length != 0).ToList();
+            if (results.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", results));
+            }
+        }
+    }
+}
