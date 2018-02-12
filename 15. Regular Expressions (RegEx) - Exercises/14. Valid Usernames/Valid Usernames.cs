c71cba9426d751229d5d16eb9a05f930d889cf1e
pirocorp <pirocorp@abv.bg>
2018-02-12 16:09:24 +0100

    You are given a line of usernames, separated by one of the following symbols: “ ”, “/”, “\”, “(“, “)”. First you have to export all valid usernames. A valid username starts with a letter and can only contain letters, digits and underscores “_”. It cannot be less than 3 or more than 25 symbols long. Your task is to sum the length of every 2 consecutive valid usernames and print the 2 valid usernames with biggest sum of their lengths, on the console, each on a separate line. Input The input comes from the console. One line will hold all the data. It will hold usernames, divided by the symbols: “ “, “/”, “\”, “(“, “)”. The input data will always be valid and in the format described. There is no need to check it explicitly. Output Print the 2 consecutive valid usernames with the biggest sum of their lengths on the console, each on a separate line. If there are 2 or more couples of usernames with the same sum of their lengths, print the left most. Constraints • The input line will hold characters in the range [0 … 9999]. • The usernames should start with a letter and can contain only letters, digits and “_”. • The username cannot be less than 3 or more than 25 symbols long. • Time limit: 0.5 sec. Memory limit: 16 MB.

---
+++ 15. Regular Expressions (RegEx) - Exercises/14. Valid Usernames/Valid Usernames.cs
@@ -0,0 +1,48 @@
+﻿namespace _14.Valid_Usernames
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var delimeterList = @" /\()".ToCharArray();
+            var possibleUsernames = Console.ReadLine()
+                .Split(delimeterList, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToArray();
+            var usernameRegex = new Regex(@"^[A-Za-z][A-Za-z0-9_]{2,25}$");
+            var verifiedUsernames = new List<string>();
+
+            for (var i = 0; i < possibleUsernames.Length; i++)
+            {
+                var currentUsername = possibleUsernames[i];
+
+                if (usernameRegex.IsMatch(currentUsername))
+                {
+                    verifiedUsernames.Add(currentUsername);
+                }
+            }
+
+            var indexOfBest = -1;
+            var bestSum = 0;
+
+            for (var i = 0; i < verifiedUsernames.Count - 1; i++)
+            {
+                var currentSum = verifiedUsernames[i].Length + verifiedUsernames[i + 1].Length;
+
+                if (currentSum > bestSum)
+                {
+                    bestSum = currentSum;
+                    indexOfBest = i;
+                }
+            }
+
+            Console.WriteLine(verifiedUsernames[indexOfBest]);
+            Console.WriteLine(verifiedUsernames[indexOfBest + 1]);
+        }
+    }
+}
