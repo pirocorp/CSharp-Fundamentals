62f712a6e6c4075ae9a532dd7fc8d9a065fe0e10
pirocorp <pirocorp@abv.bg>
2018-02-12 13:20:56 +0100

    Write a program to extract all email addresses from a given text. The text comes at the only input line. Print the emails on the console, each at a separate line. Emails are considered to be in format <user>@<host>, where: • <user> is a sequence of letters and digits, where '.', '-' and '_' can appear between them. • <host> is a sequence of at least two words, separated by dots '.'. Each word is sequence of letters and can have hyphens '-' between the letters.

---
+++ 15. Regular Expressions (RegEx) - Exercises/09. Extract Emails/Extract Emails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _09.Extract_Emails
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pattern = @"\b(?<![\-,.])[a-z0-9]+[\-._]*[a-z0-9]+@([a-z]+-*[a-z]+)(\.[a-z]+-*[a-z]+)+";
+            var inputData = Console.ReadLine();
+            var matches = Regex.Matches(inputData, pattern);
+            var result = matches.Cast<Match>().Select(x => x.Value).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
