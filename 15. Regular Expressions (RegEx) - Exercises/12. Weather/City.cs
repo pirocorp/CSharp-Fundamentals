f030d64c97f6b2cb229a23c4a65c0f87a2f9b1cc
pirocorp <pirocorp@abv.bg>
2018-02-12 15:15:14 +0100

    You have to make a weather forecast about the weather depending on strings, which you receive from the console. Every string consists of data about the city, average temperature and weather type. You will receive strings until you receive the command “end”. Every valid weather forecast consists of: • Two Latin capital letters, which represent the code of the city • Immediately followed by a floating-point number, which will represent the average temperature. Numbers without a floating point are not considered valid. • Followed by the type of weather, which will consist of uppercase and lowercase Latin letters, starts immediately after the temperature and ends at the first occurrence of the sign ‘|’ If you receive input, which does not follow the rules above – ignore it. If you receive a new temperature and/or type of weather for a city, which already exists – rewrite the previous values. At the end, print the temperature and weather type for every city. Order the cities by average temperature in ascending order. Input You will receive strings until you receive the command “end”. Output Print all cities ordered by average temperature in ascending order. Use the following format: “{nameOfTheCity} => {averageTemperature} => {typeOfWeather}” Format the temperature to the 2nd decimal place. Constraints • The average temperature will be in the interval [0.00…50.00] • The floating-point numbers will have at most 2 digits after the floating point.

---
+++ 15. Regular Expressions (RegEx) - Exercises/12. Weather/City.cs
@@ -0,0 +1,16 @@
+﻿namespace _12.Weather
+{
+    public class City
+    {
+        public City(string cityCode, double temperature, string typeOfWeather)
+        {
+            CityCode = cityCode;
+            Temperature = temperature;
+            TypeOfWeather = typeOfWeather;
+        }
+
+        public string CityCode { get; set; }
+        public double Temperature { get; set; }
+        public string TypeOfWeather { get; set; }
+    }
+}
---
+++ 15. Regular Expressions (RegEx) - Exercises/12. Weather/Weather.cs
@@ -0,0 +1,53 @@
+﻿namespace _12.Weather
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Weather
+    {
+        static void Main()
+        {
+            var pattern = @"(?<cityCode>[A-Z]{2})(?<averageTemperature>\d{1,2}\.\d{1,2})(?<typeOfWeather>[A-Za-z]+)\|";
+            var inputLine = Console.ReadLine();
+            var cities = new List<City>();
+
+            while (inputLine != "end")
+            {
+                var currentCity = Regex.Match(inputLine, pattern);
+
+                if (currentCity.Length > 0)
+                {
+                    var cityCode = currentCity.Groups["cityCode"].Value;
+                    var averageTemperature = double.Parse(currentCity.Groups["averageTemperature"].Value);
+                    var typeOfWeather = currentCity.Groups["typeOfWeather"].Value;
+
+                    if (!cities.Any(x => x.CityCode == cityCode))
+                    {
+                        var city = new City(cityCode, averageTemperature, typeOfWeather);
+                        cities.Add(city);
+                    }
+
+                    var cityInList = cities.First(x => x.CityCode == cityCode);
+                    cityInList.Temperature = averageTemperature;
+                    cityInList.TypeOfWeather = typeOfWeather;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            cities = cities.OrderBy(x => x.Temperature).ToList();
+
+            for (var i = 0; i < cities.Count; i++)
+            {
+                var currentCity = cities[i];
+                var nameOfTheCity = currentCity.CityCode;
+                var averageTemperature = currentCity.Temperature;
+                var typeOfWeather = currentCity.TypeOfWeather;
+
+                Console.WriteLine($"{nameOfTheCity} => {averageTemperature:F2} => {typeOfWeather}");
+            }
+        }
+    }
+}
