d77259567acf5d4cf0b703e8113e822f7ade9c73
pirocorp <pirocorp@abv.bg>
2018-02-12 18:45:38 +0100

    This problem is from the JavaScript Basics Exam (9 January 2015). You may check your solution here. You are in Cherny Vit now and there are 12km to Anchova Bichkiya Hut. You need to get there by car. But there is so much snow that you need to use car chains. In order to put them on the wheels correctly, you need to read the manual. But it is encrypted… As input you will receive an HTML document as a single string. You need to get the text from all the <p> tags and replace all characters which are not small letters and numbers with a space " ". After that you must decrypt the text – all letters from a to m are converted to letters from n to z (a  n; b  o; … m  z). The letters from n to z are converted to letters from a to m (n  a; o  b; … z  m). All multiple spaces should then be replaced by only one space. For example, if we have "<div>Santa</div><p>znahny # grkg ()&^^^&12</p>" we extract "znahny # grkg ()&^^^&12". Every character that is not a small letter or a number is replaced with a space ("znahny grkg       12"). We convert each small letter as described above ("znahny grkg       12"  "manual text       12") and replace all multiple spaces with only one space ("manual text 12"). Finally, we concatenate the decrypted text from all <p></p> tags (in this case, it's only one). And there you go – you have the manual ready to read! Input The input is read from the console and consists of just one line – the string with the HTML document. The input data will always be valid and in the format described. There is no need to check it explicitly. Output Print on a single line the decrypted text of the manual. See the given examples below.

---
+++ 15. Regular Expressions (RegEx) - Exercises/16. Use Your Chains, Buddy/Use Your Chains, Buddy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _16.Use_Your_Chains__Buddy
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputData = Console.ReadLine();
+            var inputRegex = new Regex(@"<p>(?<inputString>.+?)</p>", RegexOptions.CultureInvariant);
+            var matches = inputRegex.Matches(inputData);
+            var inputStrings = matches.Cast<Match>().Select(x => x.Groups["inputString"].Value).ToArray();
+            var inputString = string.Join("", inputStrings);
+            var replaceWithSpace = @"[^a-z0-9]";
+            inputString = Regex.Replace(inputString, replaceWithSpace, " ");
+            inputString = Regex.Replace(inputString, @"\s+", " ").Trim();
+
+            var result = new StringBuilder();
+
+            for (var i = 0; i < inputString.Length; i++)
+            {
+                var currentChar = inputString[i];
+
+                if (currentChar >= 'a' && currentChar <= 'm')
+                {
+                    currentChar += (char)13;
+                }
+                else if (currentChar >= 'n' && currentChar <= 'z')
+                {
+                    currentChar -= (char)13;
+                }
+
+                result.Append(currentChar);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
