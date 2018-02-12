77e6d3786f7a386a4f039fd43ff85bc68ea11c79
pirocorp <pirocorp@abv.bg>
2018-02-12 15:45:21 +0100

    You will be given a key string and a text string. The key string will contain a start key and an end key. The start key starts at the beginning of the string and ends at the first occurrence of one of the symbols – “|”, “<” or “\”. The end key starts at the last occurrence of one of these symbols and ends when the string ends. Both keys can contain only Latin alphabet letters. When you extract both keys search for them in the text string and extract every string, which is between them. Concatenate all collected strings and print the result. If the result string is empty print “Empty result”.

---
+++ 15. Regular Expressions (RegEx) - Exercises/13. Key Replacer/Key Replacer.cs
@@ -0,0 +1,43 @@
+﻿namespace _13.Key_Replacer
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var keyString = Console.ReadLine();
+            var keysPattern = @"(?<startKey>^[A-Za-z]+?)[|<\\].*[|<\\](?<endKey>[A-Za-z]+$)";
+            var keys = Regex.Match(keyString, keysPattern);
+            var startKey = keys.Groups["startKey"].Value;
+            var endKey = keys.Groups["endKey"].Value;
+            var textPattern = $@"{startKey}(?<stringResult>.*?){endKey}";
+            var textString = Console.ReadLine();
+            var strings = Regex.Matches(textString, textPattern)
+                .Cast<Match>()
+                .Select(x => x.Groups["stringResult"].Value)
+                .ToArray();
+
+            var resultedStringBuilder = new StringBuilder();
+
+            for (var i = 0; i < strings.Length; i++)
+            {
+                var currentString = strings[i];
+                resultedStringBuilder.Append(currentString);
+            }
+
+            var result = resultedStringBuilder.ToString();
+            if (result.Length == 0)
+            {
+                Console.WriteLine($"Empty result");
+            }
+            else
+            {
+                Console.WriteLine(resultedStringBuilder.ToString());
+            }
+        }
+    }
+}
