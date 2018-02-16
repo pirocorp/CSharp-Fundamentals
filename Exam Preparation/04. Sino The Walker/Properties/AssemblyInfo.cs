e3425a4f93827b454756bbc9b6fcdafa1673b6cf
pirocorp <pirocorp@abv.bg>
2018-02-16 17:51:35 +0100

    Sino is one of those people that lives in SoftUni. He leaves every now and then, but when he leaves he always takes a different route, so he needs to know how much time it will take for him to go home. Your job is to help him with the calculations. You will receive the time that Sino leaves SoftUni, the steps taken and time for each step in seconds. You need to print the exact time that he will arrive at home in the format specified. Input / Constrains • The first line will be the time Sino leaves SoftUni in the following format: HH:mm:ss • The second line will contain the number of steps that he needs to walk to his home. This number will be an integer in range [0…2,147,483,647] • On the final line you will receive the time in seconds for each step. This number will be an integer in range [0…2,147,483,647] Output • Print the time of arrival at home in the following format: o Time Arrival: {hours}:{minutes}:{seconds} • If hours, minutes or seconds are a single digit number, add a zero in front. • If, for example, hours are equal to zero, print a 00 in their place. The same is true for minutes or seconds. • Time of day starts at 00:00:00 and ends at 23:59:59

--- Exam Preparation/04. Sino The Walker/Properties/AssemblyInfo.cs
+++ Exam Preparation/04. Sino The Walker/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("03. Dragon Army")]
+[assembly: AssemblyTitle("04. Sino The Walker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("03. Dragon Army")]
+[assembly: AssemblyProduct("04. Sino The Walker")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4d03ac10-7412-4ac1-9e78-700f043c9690")]
+[assembly: Guid("681157c1-833f-4114-9604-1a7113c744d6")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Exam Preparation/04. Sino The Walker/Sino The Walker.cs
@@ -0,0 +1,27 @@
+﻿namespace _04.Sino_The_Walker
+{
+    using System;
+    using System.Globalization;
+
+    class Program
+    {
+        static void Main()
+        {
+            const string format = @"HH:mm:ss";
+            var inputData = Console.ReadLine();
+            var timeOfDeparture = DateTime.ParseExact(inputData, format, CultureInfo.InvariantCulture);
+            var stepsToHome = long.Parse(Console.ReadLine());
+            var secondsPerStep = long.Parse(Console.ReadLine());
+            var totalElpasedSeconds = stepsToHome * secondsPerStep;
+            var secondsLeft = totalElpasedSeconds % 60;
+            var totalElpasedMinutes = totalElpasedSeconds / 60;
+            var minutesLeft = totalElpasedMinutes % 60;
+            var hours = totalElpasedMinutes / 60;
+            var hoursLeft = hours % 24;
+            var timeOfArrival = timeOfDeparture.AddSeconds(secondsLeft);
+            timeOfArrival = timeOfArrival.AddMinutes(minutesLeft);
+            timeOfArrival = timeOfArrival.AddHours(hoursLeft);
+            Console.WriteLine($"Time Arrival: {timeOfArrival.Hour:D2}:{timeOfArrival.Minute:D2}:{timeOfArrival.Second:D2}");
+        }
+    }
+}
