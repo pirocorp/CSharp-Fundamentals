a74aa075ed9b9881c0b1e6d948974cca88f91001
pirocorp <pirocorp@abv.bg>
2018-02-16 10:20:41 +0100

    He will give you her email and your task is to subtract the sum of the characters after the ‘@’ from the sum of the characters before the ‘@’. If the result is equal or greater than 0 – he will write her email, otherwise he will not. Input You will receive single line with the email of the girl. Output If the result is equal or greater than 0 print: • Call her! Otherwise print: • She is not the one.

---
+++ 15. Regular Expressions (RegEx) - Exercises/18. Email me/Email me.cs
@@ -0,0 +1,24 @@
+﻿namespace _18.Email_me
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var email = Console.ReadLine();
+            var tokens = email.Split(new[] {'@'}, StringSplitOptions.RemoveEmptyEntries);
+            var result = tokens[0].Sum(x => x) - tokens[1].Sum(x => x);
+
+            if (result >= 0)
+            {
+                Console.WriteLine("Call her!");
+            }
+            else
+            {
+                Console.WriteLine($"She is not the one.");
+            }
+        }
+    }
+}
