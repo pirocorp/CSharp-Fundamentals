25656b2420420c325a12469200a532982c98f27b
pirocorp <pirocorp@abv.bg>
2018-02-16 11:42:51 +0100

    His punches are marked with ‘>’. Immediately after the mark, there will be an integer, which signifies the strength of the punch. You should remove x characters (where x is the strength of the punch), starting after the punch character (‘>’). If you find another punch mark (‘>’) while you’re deleting characters, you should add the strength to your previous punch. When all characters are processed, print the string without the deleted characters. You should not delete the punch character – ‘>’, but you should delete the integers, which represent the strength. Input You will receive single line with the string, which is used by Pesho for training. Output Print what is left from the string after Pesho’s punches. Constraints • You will always receive a strength for the punches • The path will consist only of letters from the Latin alphabet, integers and the char ‘>’ • The strength of the punches will be in the interval [0…9]

---
+++ 15. Regular Expressions (RegEx) - Exercises/19. Karate Strings/Karate Strings.cs
@@ -0,0 +1,40 @@
+﻿namespace _19.Karate_Strings
+{
+    using System;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main() // 100/100
+        {
+            var input = new StringBuilder(Console.ReadLine());
+
+            const char punch = '>';
+            var punchStrength = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[i] != punch)
+                {
+                    continue;
+                }
+
+                punchStrength += int.Parse(input[i + 1].ToString());
+                var index = i + 1;
+
+                while (index < input.Length && punchStrength > 0)
+                {
+                    if (input[index] == punch)
+                    {
+                        break;
+                    }
+
+                    input.Remove(index, 1);
+                    punchStrength--;
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
