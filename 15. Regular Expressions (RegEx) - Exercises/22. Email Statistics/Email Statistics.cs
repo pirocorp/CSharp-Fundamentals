3c135db69b560744443a0ae4c768fdaa03becf37
pirocorp <pirocorp@abv.bg>
2018-02-16 13:06:14 +0100

    You will receive n emails from the console. Some of these emails will be invalid. In order one email to be valid it should pass the following conditions: • The username of the user should be at least 5 characters long and consist only of uppercase and lowercase Latin letters. • The username should be followed immediately by ‘@’. • The domain part should consist of two parts: o The mail server, which should contain only lowercase Latin letters and should be at least 3 letters long. o The top-level domain, which can be one of the following: .com, .bg or .org At the end, print data in the format described in the output section. Input • On the first line, you will receive n – the count of emails. • On the next n lines, you will receive emails. Output Print the domains in the format: {1st domain}: ### {1st username} ### {2nd username} … ### {nth username} … {nth domain} ### {1st username} … ### {nth username} Order the domains by the counts of usernames in the domain in descending order. If they are equal, print them in the order, in which they were received. Order the usernames by the time of receiving. If you receive two of the same username for one domain – ignore it.

---
+++ 15. Regular Expressions (RegEx) - Exercises/22. Email Statistics/Email Statistics.cs
@@ -0,0 +1,54 @@
+﻿namespace _22.Email_Statistics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var emails = new Dictionary<string, List<string>>(); //Domain, userList
+            var pattern = @"^(?<username>[A-Za-z]{5,})@(?<domain>[a-z]{3,}(?:\.com|\.bg|\.org))$";
+            var regex = new Regex(pattern);
+
+            for (var i = 0; i < n; i++)
+            {
+                var inputLine = Console.ReadLine();
+
+                if (regex.IsMatch(inputLine))
+                {
+                    var match = regex.Match(inputLine);
+                    var domain = match.Groups["domain"].Value;
+                    var username = match.Groups["username"].Value;
+
+                    if (!emails.ContainsKey(domain))
+                    {
+                        emails[domain] = new List<string>();
+                    }
+
+                    if (!emails[domain].Contains(username))
+                    {
+                        emails[domain].Add(username);
+                    }
+                }
+            }
+
+            emails = emails.OrderByDescending(x => x.Value.Count).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var domain in emails)
+            {
+                var domainName = domain.Key;
+                var lisOfUsers = domain.Value;
+                Console.WriteLine($"{domainName}:");
+
+                foreach (var user in lisOfUsers)
+                {
+                    Console.WriteLine($"### {user}");
+                }
+            }
+        }
+    }
+}
