2d59476fb107b6c6bb84634c0e65dbe35b34d976
pirocorp <pirocorp@abv.bg>
2018-02-16 12:14:14 +0100

    You have written new secret way to transmit coded messages. You will receive the input in the format: {firstLetterOfTheMessage}|{secondLetterOfTheMessage}|…|{nthLetterOfTheMessage} Each part of the message will consist only of ‘0’ and ‘1’. Each part of the message will transform into a character from the printable range of the ASCII table [32…126 (space…~)]. The transformation for each part happens in the following way: • Each 0 adds 3 to the total sum. • Each 1 adds 5 to the total sum. • Every time you receive a sequence of equal digits, the sum increases by the count of the equal digits. The sum should give you the ASCII code of a character. The final message consists of all deciphered signs. Example: 10101010101010101  The message has nine ones and eight zeroes. There are no consecutive equal digits, which means the total is 8 * 3 + 9 * 5 = 69    the letter ‘E’. Example 2: 1110011111111  The message has eleven ones and three zeroes. This sums up to 11 * 5 + 2 * 3 = 61 On top of that we have three sequences with equal digits: • The first three digits are ones, so we add 3 to the sum (the current sum equals 61 + 3 = 64) • The next two digits are zeroes, so we add 2 to the sum (the current sum equals 64 + 2 = 66) • The next eight digits are ones, so we add 8 to the sum (the current sum equals 66 + 8 = 74). • We reached the end of the string, and the final ASCII code is 74 ‘J’. Input You will receive a single line with the letters from the message. They will be separated with single pipe – ‘|’ Output Print only the deciphered message. Constraints • Each coded letter will consist of either ‘1’ or ‘0’. • The ASCII codes will be in the interval [32…126].

---
+++ 15. Regular Expressions (RegEx) - Exercises/20. Morse Code Upgraded/Morse Code Upgraded.cs
@@ -0,0 +1,40 @@
+﻿namespace _20.Morse_Code_Upgraded
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            const string pattern = @"(?<cryptedLetter>\d+)";
+            var inputLine = Console.ReadLine();
+            var cryptedLetters = Regex.Matches(inputLine, pattern)
+                .Cast<Match>()
+                .Select(x => x.Groups["cryptedLetter"].Value)
+                .ToArray();
+            var result = new StringBuilder();
+
+            for (var i = 0; i < cryptedLetters.Length; i++)
+            {
+                var regex = new Regex("(?<zeroGroup>0{2,})|(?<oneGroup>1{2,})");
+                char currentChar = CharDecode(cryptedLetters[i], regex);
+                result.Append(currentChar);
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static char CharDecode(string cryptedLetter, Regex regex)
+        {
+            var sum = 0;
+            var zeros = cryptedLetter.Count(x => x == '0');
+            var ones = cryptedLetter.Count(x => x == '1');
+            var consecutiveEqualDigits = regex.Matches(cryptedLetter).Cast<Match>().Select(x => x.Length).ToArray().Sum();
+            sum = zeros * 3 + ones * 5 + consecutiveEqualDigits;
+            return (char)sum;
+        }
+    }
+}
