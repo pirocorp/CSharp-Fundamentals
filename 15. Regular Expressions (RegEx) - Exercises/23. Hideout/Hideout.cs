fe64ffd58b574e44980cdad7e7be1dc1bb534418
pirocorp <pirocorp@abv.bg>
2018-02-16 13:39:52 +0100

    You are a detective from Scotland Yard and you need to find the hideout of a very dangerous group of criminals. You will receive a map in the form of a string and after that you will receive clues from the intelligence. On the next unknown amount of lines, you will receive arrays containing two elements: • The first element will be the character, which marks the hideout. • The second element will be the minimum count of characters, which you need to search. The array will be in format: “{searchedCharacter} {minimumCount}”. If you cannot find a hideout  continue reading the next two lines. If you find a hideout  stop the program and print the index where the hideout starts and the length of the hideout. Input • On the first line, you will receive the map, which will contain random strings. • On the next unknown amount of lines, you will receive arrays o The first element is the searched character o The second element is the minimum count, which should be searched Output If you find the hideout, print: “Hideout found at index {indexOfTheFirstChar} and it is with size {lengthOfTheFoundString}!”

---
+++ 15. Regular Expressions (RegEx) - Exercises/23. Hideout/Hideout.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _23.Hideout
+{
+    class Hideout
+    {
+        static void Main()
+        {
+            var inputLine = Console.ReadLine();
+
+            while (true)
+            {
+                var tokens = Console.ReadLine().Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+                var character = tokens[0];
+                var count = int.Parse(tokens[1]);
+                var search = $"\\{character}{{{count},}}";
+                var regex = new Regex(search);
+
+                if (regex.IsMatch(inputLine))
+                {
+                    var indexAtFound = regex.Match(inputLine).Index;
+                    var sizeOfFound = GetSize(inputLine.Substring(indexAtFound));
+                    Console.WriteLine($"Hideout found at index {indexAtFound} and it is with size {sizeOfFound}!");
+                    break;
+                }
+            }
+        }
+
+        private static int GetSize(string substring)
+        {
+            var result = 1;
+            var character = substring[0];
+
+            for (var i = 1; i < substring.Length; i++)
+            {
+                if (substring[i] == character)
+                {
+                    result++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
