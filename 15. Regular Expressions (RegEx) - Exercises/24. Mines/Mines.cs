1f76abf2ad3380a71ee2172ca7566d711303455b
pirocorp <pirocorp@abv.bg>
2018-02-16 15:47:04 +0100

    The mines will be in format <{firstCharacter}{secondCharacter}>. When you encounter a mine, you should destroy all its characters. The mine also destroys n characters to the left and right of itself. n is determined by the absolute value of the subtraction of the ASCII codes of the first character and the second characters. Replace the destroyed characters with underscores – ‘_’. Example: we received the following string: bewareOf<AF>TheMines The mine is <AF>. The power of the mine will equal |A (65) – F (70)| = 5. When the mine explodes, we have the following string: bew______________nes (Legend: red – mine, green ¬– blast radius) Input You will receive single line with the string, which you need to check for mines. Output Print the string after the explosions. Constraints • The length of the text will be in the range [1...500]. • Mine explosions will not overlap with other bombs.

---
+++ 15. Regular Expressions (RegEx) - Exercises/24. Mines/Mines.cs
@@ -0,0 +1,29 @@
+﻿namespace _24.Mines
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Mines
+    {
+        static void Main()
+        {
+            var minePattern = @"<(?<mine>.{2})>";
+            var inputLine = Console.ReadLine();
+            var mines = Regex.Matches(inputLine, minePattern).Cast<Match>().ToArray();
+
+            for (var i = mines.Length -1; i >= 0; i--)
+            {
+                var currentMine = mines[i];
+                var minePower = Math.Abs(currentMine.Groups["mine"].Value[0] - currentMine.Groups["mine"].Value[1]);
+                var startIndex = Math.Max(0, currentMine.Index - minePower);
+                var endIndex = Math.Min(inputLine.Length, (currentMine.Index + currentMine.Length + minePower));
+                var lenght = endIndex - startIndex;
+                inputLine = inputLine.Remove(startIndex, lenght);
+                inputLine = inputLine.Insert(startIndex, new string('_', lenght));
+            }
+
+            Console.WriteLine(inputLine);
+        }
+    }
+}
