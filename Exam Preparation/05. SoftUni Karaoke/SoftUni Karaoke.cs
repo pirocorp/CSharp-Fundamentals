dfb9e246426dd94aa1dd62d290ea5cb1c2633444
pirocorp <pirocorp@abv.bg>
2018-02-16 18:37:54 +0100

    • On the first line, you will receive a list with all participants that applied for performance. • On the second line, you will receive the list with all available songs. • On the next lines, until the "dawn" command, you will get the names of people, the song that they are performing on stage and the award they get from the audience. However, not every time the plan goes according to schedule. In other words, everyone (listed or not) can go on stage and perform a song that is not even available and he still gets awards from the audience. However, you should record only the awards for listed participants that perform songs available in the list. In case someone is not listed or sings a song that is not available you should not record neither the participant, nor his award. When the "dawn" comes, you need to print all participants, the count of the unique awards they received and all unique awards. Participants should be sorted by number of awards in descending order and then by participant name alphabetically. Awards should be sorted in alphabetical order. Input • On the first line, you will receive list with all participants that applied for performance in the format: "{participant}, {participant} … {participant}" • On the second line, you will get all available songs in the in the format: "{song}, {song} … {song}" • On the next lines, until the "dawn" command you will receive every stage performance in the format: "{participant}, {song}, {award}" • Performances and songs will be separated by a comma and single or multiple white spaces. Output • Print all participants along with the number of unique awards they won in the format: o "{participant}: {award count} awards" o "--{award}" • Print participants sorted by unique awards count in descending order. If two participants have the same unique award count, sort them alphabetically by name • Print unique awards for every participant sorted alphabetically • If there are no awards, print "No awards" Constrains • The number of total participants will be in range [1 … 100] • The number of total songs will be in range [1 … 100] • The input will always end with the "dawn" command

---
+++ Exam Preparation/05. SoftUni Karaoke/SoftUni Karaoke.cs
@@ -0,0 +1,62 @@
+﻿namespace _05.SoftUni_Karaoke
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            const string splitPattern = @"\,\s+";
+            var participants = Regex.Split(Console.ReadLine(), splitPattern).ToList();
+            var songs = Regex.Split(Console.ReadLine(), splitPattern).ToList();
+            var inputLine = Console.ReadLine();
+            var awards = new Dictionary<string, List<string>>(); //Participant, awards
+
+            while (inputLine != "dawn")
+            {
+                var tokens = Regex.Split(inputLine, splitPattern);
+                var participant = tokens[0];
+                var song = tokens[1];
+                var award = tokens[2];
+
+                if (participants.Contains(participant) && songs.Contains(song))
+                {
+                    if (!awards.ContainsKey(participant))
+                    {
+                        awards[participant] = new List<string>();
+                    }
+
+                    awards[participant].Add(award);
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            awards = awards
+                .OrderByDescending(x => x.Value.Distinct().ToList().Count)
+                .ThenBy(m => m.Key)
+                .ToDictionary(x => x.Key, x => x.Value.OrderBy(z => z).Distinct().ToList());
+
+            foreach (var participant in awards)
+            {
+                var participantName = participant.Key;
+                var participantAwards = participant.Value;
+                var numberOfAwards = participantAwards.Count;
+                Console.WriteLine($"{participantName}: {numberOfAwards} awards");
+
+                foreach (var award in participantAwards)
+                {
+                    Console.WriteLine($"--{award}");
+                }
+            }
+
+            if (awards.Count == 0)
+            {
+                Console.WriteLine("No awards");
+            }
+        }
+    }
+}
