22ce45f60861636c7c14f4ffee10f28f3289d018
pirocorp <pirocorp@abv.bg>
2018-02-16 12:33:14 +0100

    Write a program which takes a string message as input and replaces all numbers with the letter immediately after the number. Input You will receive a single line with the message, which you need to correct. Output Print only the corrected message.

---
+++ 15. Regular Expressions (RegEx) - Exercises/21. Only Letters/Only Letters.cs
@@ -0,0 +1,28 @@
+﻿namespace _21.Only_Letters
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var pattern = @"(?<digit>\d+)(?<letter>[a-zA-Z])";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(inputLine);
+            var digits = matches.Cast<Match>().Select(x => x.Groups["digit"].Value).ToArray();
+            var letters = matches.Cast<Match>().Select(x => x.Groups["letter"].Value).ToArray();
+
+            for (var i = 0; i < digits.Length; i++)
+            {
+                var currentDigit = digits[i];
+                var currentLetter = letters[i];
+                inputLine = Regex.Replace(inputLine, currentDigit, currentLetter);
+            }
+
+            Console.WriteLine(inputLine);
+        }
+    }
+}
