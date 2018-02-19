7ec3334b7470886fd842af9821565cbde86ef8ae
pirocorp <pirocorp@abv.bg>
2018-02-19 20:10:34 +0100

    Some refact

--- Exam Preparation/09. Ladybugs/Ladybugs.cs
+++ Exam Preparation/09. Ladybugs/Ladybugs.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _09.Ladybugs
+﻿namespace _09.Ladybugs
 {
+    using System;
+    using System.Linq;
+
     class Ladybugs
     {
         static void Main()
