4d52f979f69cdab2bb2c1cd0a6aa23e614c59f22
pirocorp <pirocorp@abv.bg>
2018-02-19 16:30:02 +0100

    You are given a collection of tickets separated by commas and spaces. You need to check every one of them if it has a winning combination of symbols. A valid ticket should have exactly 20 characters. The winning symbols are '@', '#', '$' and '^'. But in order for a ticket to be a winner, the same winning symbol should uninterruptedly repeat for at least 6 times in both the tickets left half and the tickets right half. For example, a valid winning ticket should be something like this: "Cash$$$$$$Ca$$$$$$sh" The left half "Cash$$$$$$" contains "$$$$$$", which is also contained in the tickets right half "Ca$$$$$$sh". A winning ticket should contain symbols repeating up to 10 times in both halves, which is considered a Jackpot (for example: "$$$$$$$$$$$$$$$$$$$$"). Input The input will be read from the console. The input consists of a single line containing all tickets separated by commas and one or more white spaces in the format: • "{ticket}, {ticket}, … {ticket}" Output Print the result for every ticket in the order of their appearance, each on a separate line in the format: • Invalid ticket - "invalid ticket" • No match - "ticket "{ticket}" - no match" • Match with length 6 to 9 - "ticket "{ticket}" - {match length}{match symbol}" • Match with length 10 - "ticket "{ticket}" - {match length}{match symbol} Jackpot!" Constrains • Number of tickets will be in range [0 … 100] Examples

---
+++ Exam Preparation/07. Winning Ticket/Winning Ticket.cs
@@ -0,0 +1,52 @@
+﻿namespace _07.Winning_Ticket
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            const string winPattern = @".*?(?<win>[\@]{6,10}|[\#]{6,10}|[\^]{6,10}|[\$]{6,10}).*?";
+
+            var tickets = Console.ReadLine().Split(", \t".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToArray();
+            var winRegex = new Regex(winPattern);
+
+            for (var i = 0; i < tickets.Length; i++)
+            {
+                var currentTicket = tickets[i];
+
+                if (currentTicket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+                var leftPart = currentTicket.Substring(0, 10);
+                var rightPart = currentTicket.Substring(10, 10);
+                var leftMatch = winRegex.Match(leftPart).Groups["win"].Value;
+                var rightMatch = winRegex.Match(rightPart).Groups["win"].Value;
+
+                if (string.IsNullOrEmpty(leftMatch) || string.IsNullOrEmpty(rightMatch) || leftMatch[0] != rightMatch[0])
+                {
+                    Console.WriteLine($"ticket \"{currentTicket}\" - no match");
+                    continue;
+                }
+
+                var commonLenght = Math.Min(leftMatch.Length, rightMatch.Length);
+
+                if (commonLenght == 10)
+                {
+                    Console.WriteLine($"ticket \"{currentTicket}\" - {commonLenght}{leftMatch[0]} Jackpot!");
+                }
+                else if (commonLenght >= 6)
+                {
+                    Console.WriteLine($"ticket \"{currentTicket}\" - {commonLenght}{rightMatch[0]}");
+                }
+            }
+        }
+    }
+}
