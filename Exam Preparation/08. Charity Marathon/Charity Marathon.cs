580654cf1b7739b715a7740296204230530c1d55
pirocorp <pirocorp@abv.bg>
2018-02-19 19:11:01 +0100

    Every year a charity marathon takes place in your town in which all major companies are obliged to make donations depending on the total kilometers ran by runners in a number of days. And this year you have been chosen to create the software for it. The marathon can last for variable number days and a variable number of runners can participate in it on a track that can have a variable length. However, the track that can take only a limited number of runners per day. If the runners that want to take part are more than the capacity, then the number of runners that will run will be equal to the maximum capacity of the track. The amount of money raised per kilometer is voted in advance by all companies and the final money per kilometer is calculated by an average of all votes. The goal is simple, create a program that calculates the total money raised through the marathon. Input • On the first line of input you will get the length of the marathon in days • On the second line of input you will get the number of runners that will participate • On the third line you will get the average number of laps every runner makes • On the fourth line you will get the length of the track • On the fifth line you will get the capacity of the track • On the sixth line you will get the amount of money donated per kilometer Output • Print the money raised, rounded by the second digit after the decimal point from the charity marathon in the format: "Money raised: {money}" Constraints • Marathon day count will be an integer in the range [0 … 365] • Runner count will be an integer in the range [0 … 2,147,483,647] • Average number of laps will be an integer in the range [0 … 100] • Lap length will be an integer in the range [0 … 2,147,483,647] • Track capacity will be an integer in the range [0 … 1000] • Money per kilometer will all be a floating point number

---
+++ Exam Preparation/08. Charity Marathon/Charity Marathon.cs
@@ -0,0 +1,24 @@
+﻿namespace _08.Charity_Marathon
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var days = int.Parse(Console.ReadLine());
+            var participants = long.Parse(Console.ReadLine());
+            var laps = int.Parse(Console.ReadLine());
+            var lenghtOfTrack = long.Parse(Console.ReadLine());
+            var capacity = int.Parse(Console.ReadLine());
+            var donatedMoneyPerKm = decimal.Parse(Console.ReadLine());
+
+            var maxCapacity = days * capacity;
+            var currentCapacity = Math.Min(participants, maxCapacity);
+            var totalKm = laps * (decimal) lenghtOfTrack * currentCapacity / 1000;
+            var totalMoney = totalKm * donatedMoneyPerKm;
+            Console.WriteLine($"Money raised: {totalMoney:F2}");
+
+        }
+    }
+}
