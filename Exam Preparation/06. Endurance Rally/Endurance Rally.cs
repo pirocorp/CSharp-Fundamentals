1d360dc4c603aa83dd52efe78694b219a1ffa0e3
pirocorp <pirocorp@abv.bg>
2018-02-19 14:35:26 +0100

    The goal of the Endurance Rally is, simply, to finish the race. You are given the names of the participants, the track layout and the checkpoint indexes. The starting fuel of each participant is equal to the ASCII code of the first character of his name. Track layout consists of zones represented by numbers, given on a single line separated by a single space. Every number represents the fuel given or the fuel taken by the current zone of the track: • If the current zone is a checkpoint, the value of the zone is added to the driver's fuel. • If the current zone is not a checkpoint, the value of the zone is subtracted from the driver's fuel. Zones are indexed. Indexes are sequential and always start from zero (like an array). The checkpoints are numbers, representing indexes that show if a zone of the track is a checkpoint. For example, you are given checkpoints 0, 3 and 5, that means that zones at index 0, 3 and 5 of the track are checkpoints and therefore provide fuel for the driver. Given this information, you need to check if a driver can finish and print the fuel that he is left with. If a driver can't finish you need to print the zone he managed to reach. Input The input will be read from the console. The input consists of exactly three lines: • The first line holds the drivers' names separated by a space: "{driver 1} {driver 2} … {driver N}" • On the second line is the track layout (zones) in the form of numbers separated by a space: "{zone 0} {zone 1} … {zone N}" • On the third line are the checkpoint indexes also separated by a space: "{index 0} {index 1} … {index N}" Output Print all drivers in the order of their appearance, each on a separate line: • If the driver finished, print his name and fuel left to the second digit after the decimal point in the format: "{driver name} - fuel left {fuel points}" • If the driver could not finish, print: "{driver name} - reached {zone index}" Constrains • Drivers count will be in the range [0 … 200] • Zone fuel will be a floating-point number • Checkpoints will be integers in the range [-231 … 231 - 1]

---
+++ Exam Preparation/06. Endurance Rally/Endurance Rally.cs
@@ -0,0 +1,61 @@
+﻿namespace _06.Endurance_Rally
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var participants = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var trackLayout = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            var checkPointsIndexes = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            for (var i = 0; i < participants.Length; i++)
+            {
+                var currentParticipant = participants[i];
+                var startingFuel = (double)currentParticipant[0];
+                var kvp = ProcessFuel(startingFuel, trackLayout, checkPointsIndexes);
+                var reached = kvp.Key;
+                var fuelLeft = kvp.Value;
+
+                if (reached == trackLayout.Length - 1)
+                {
+                    Console.WriteLine($"{currentParticipant} - fuel left {fuelLeft:F2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{currentParticipant} - reached {reached + 1}");
+                }
+            }
+        }
+
+        private static KeyValuePair<int, double> ProcessFuel(double startingFuel, double[] trackLayout, int[] checkPointsIndexes)
+        {
+            var reached = - 1;
+
+            for (var i = 0; i < trackLayout.Length; i++)
+            {
+                if (checkPointsIndexes.Contains(i))
+                {
+                    startingFuel += trackLayout[i];
+                }
+                else
+                {
+                    startingFuel -= trackLayout[i];
+                }
+
+                if (startingFuel <= 0)
+                {
+                    break;
+                }
+
+                reached = i;
+            }
+
+            startingFuel = Math.Max(0, startingFuel);
+            return new KeyValuePair<int, double>(reached, startingFuel);
+        }
+    }
+}
