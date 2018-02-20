3ae041b203cca5effd44bcba27982c5f13e81d43
pirocorp <pirocorp@abv.bg>
2018-02-20 11:17:05 +0100

    Your job, however is not so exciting. You are assigned to sign in all the participants in the nether realm's mighty battle's demon book, which of course is sorted alphabetically. A demon's name contains his health and his damage. The sum of the asci codes of all characters (excluding numbers (0-9), arithmetic symbols ('+', '-', '*', '/') and delimiter dot ('.')) gives a demon's total health. The sum of all numbers in his name forms his base damage. Note that you should consider the plus '+' and minus '-' signs (e.g. +10 is 10 and -10 is -10). However, there are some symbols ('*' and '/') that can further alter the base damage by multiplying or dividing it by 2 (e.g. in the name "m15*/c-5.0", the base damage is 15 + (-5.0) = 10 and then you need to multiply it by 2 (e.g. 10 * 2 = 20) and then divide it by 2 (e.g. 20 / 2 = 10)). So, multiplication and division are applied only after all numbers are included in the calculation and in the order they appear in the name. You will get all demons on a single line, separated by commas and zero or more blank spaces. Sort them in alphabetical order and print their names along their health and damage. Input The input will be read from the console. The input consists of a single line containing all demon names separated by commas and zero or more spaces in the format: "{demon name}, {demon name}, … {demon name}" Output Print all demons sorted by their name in ascending order, each on a separate line in the format: • "{demon name} - {health points} health, {damage points} damage" Constraints • A demon's name will contain at least one character • A demon's name cannot contain blank spaces ' ' or commas ',' • A floating point number will always have digits before and after its decimal separator • Number in a demon's name is considered everything that is a valid integer or floating point number (with dot '.' used as separator). For example, all these are valid numbers: '4', '+4', '-4', '3.5', '+3.5', '-3.5'

---
+++ Exam Preparation/10. Nether Realms/Demon.cs
@@ -0,0 +1,16 @@
+﻿namespace _10.Nether_Realms
+{
+    public class Demon
+    {
+        public Demon(string demonName, int demonHealth, double demonDamage)
+        {
+            DemonName = demonName;
+            DemonHealth = demonHealth;
+            DemonDamage = demonDamage;
+        }
+
+        public string DemonName { get; set; }
+        public int DemonHealth { get; set; }
+        public double DemonDamage { get; set; }
+    }
+}
---
+++ Exam Preparation/10. Nether Realms/Nether Realms.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _10.Nether_Realms
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string splitPattern = @"(\s*\,+\s*)";
+            var splitRegex = new Regex(splitPattern);
+            var demons = splitRegex.Split(Console.ReadLine()).Where(s => s.Length > 0).Where(x => !splitRegex.IsMatch(x)).ToArray();
+            const string healthPattern = @"(?<healthCharacters>[^0-9+\-*\/.])";
+            var healthRegex = new Regex(healthPattern);
+            var demonsHealth = demons.Select(x => healthRegex.Matches(x).Cast<Match>().Sum(y => y.Value[0])).ToArray();
+            const string digitsPattern = @"[+\-]?\d+(\.\d+)*";
+            var digitsRegex = new Regex(digitsPattern);
+            var demonsBaseDamage = demons.Select(x =>
+                digitsRegex.Matches(x).Cast<Match>().Select(y => y.Value).Select(double.Parse).Sum(s => s)).ToArray();
+            var modifingDamagePattern = @"[*\/]";
+            var modifingDamageRegex = new Regex(modifingDamagePattern);
+            var demonsDamageModifier = demons.Select(x =>
+                string.Join("", modifingDamageRegex.Matches(x).Cast<Match>().Select(m => m.Value).ToArray())).ToArray();
+            var demonsDamage = new double[demonsBaseDamage.Length];
+
+            for (var i = 0; i < demonsDamage.Length; i++)
+            {
+                var currentBaseDamage = demonsBaseDamage[i];
+                var currentDamageModifier = demonsDamageModifier[i];
+                var currentDamage = currentBaseDamage;
+
+                if (string.IsNullOrEmpty(currentDamageModifier))
+                {
+                    demonsDamage[i] = currentDamage;
+                    continue;
+                }
+
+                for (var j = 0; j < currentDamageModifier.Length; j++)
+                {
+                    var currentChar = currentDamageModifier[j];
+                    if (currentChar == '*')
+                    {
+                        currentDamage *= 2;
+                    }
+                    else if (currentChar == '/')
+                    {
+                        currentDamage /= 2;
+                    }
+                }
+
+                demonsDamage[i] = currentDamage;
+            }
+
+            var demonsResult = new List<Demon>();
+
+            for (var i = 0; i < demons.Length; i++)
+            {
+                var currentDemonName = demons[i];
+                var currentDemonHealth = demonsHealth[i];
+                var currentDemonDamage = demonsDamage[i];
+                var currentDemon = new Demon(currentDemonName, currentDemonHealth, currentDemonDamage);
+                demonsResult.Add(currentDemon);
+            }
+
+            demonsResult = demonsResult.OrderBy(x => x.DemonName).ToList();
+
+            foreach (var demon in demonsResult)
+            {
+                var name = demon.DemonName;
+                var health = demon.DemonHealth;
+                var damage = demon.DemonDamage;
+                Console.WriteLine($"{name} - {health} health, {damage:F2} damage");
+            }
+        }
+    }
+}
