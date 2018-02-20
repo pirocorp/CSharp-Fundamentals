8a433214954a5f86e385a7eb89e5f7de516b4471
pirocorp <pirocorp@abv.bg>
2018-02-20 22:28:43 +0100

    You will be given a series of strings on a single line, separated by one or more whitespaces. These represent the collection you’ll be working with. On the next input lines, until you receive the command "end", you’ll receive a series of commands in one of the following formats: • "reverse from [start] count [count]" – this instructs you to reverse a portion of the array – just [count] elements starting at index [start]; • "sort from [start] count [count]" – this instructs you to sort a portion of the array - [count] elements starting at index [start]; • "rollLeft [count] times" – this instructs you to move all elements in the array to the left [count] times. On each roll, the first element is placed at the end of the array; • "rollRight [count] times" – this instructs you to move all elements in the array to the right [count] times. On each roll, the last element is placed at the beginning of the array; If any of the provided indices or counts is invalid (non-existent index or negative count), you should print a message on the console "Invalid input parameters." and keep the collection unchanged. After you’re done, print the resulting array in the following format: "[arr0, arr1, …, arrN-1]". The examples should help you understand the task better. Input • The input data should be read from the console. • The first input line will hold a series of strings, separated by one or more whitespaces. • The next lines will hold commands in the described formats (exactly). • The input ends with the keyword "end". • The input data will always be valid and in the format described. There is no need to check it explicitly. Output • The output should be printed on the console. • Each time an invalid parameters are received, print the following line: "Invalid input parameters." • After receiving the "end" command, print the resulting array on the console in the format specified above. Constraints • The count of strings in the collection will be in the range [1 … 50]. • The number of commands will be in the range [1 … 20]. • All commands will be in the described format; an invalid command is a command containing invalid [start] or [count], there won’t be any missing or misspelled words. • [start] and [count] will be integers in the range [-231 … 231 - 1]. • Allowed working time for your program: 0.1 seconds. Allowed memory: 16 MB.

---
+++ Exam Preparation/13. Command Interpreter/Command Interpreter.cs
@@ -0,0 +1,117 @@
+﻿namespace _13.Command_Interpreter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var strings = Regex.Split(Console.ReadLine(), @"\s+").Where(s => s.Length > 0).ToList();
+            var command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                var tokens = command.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                var currentCommand = tokens[0];
+
+                switch (currentCommand)
+                {
+                    case "reverse":
+                        var start = int.Parse(tokens[2]);
+                        var count = int.Parse(tokens[4]);
+                        var startIsInside = start >= 0 && start < strings.Count;
+                        var countIsInside = start + count <= strings.Count && count >= 0;
+
+                        if (startIsInside && countIsInside)
+                        {
+                            strings.Reverse(start, count);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input parameters.");
+                        }
+
+                        break;
+                    case "sort":
+                        start = int.Parse(tokens[2]);
+                        count = int.Parse(tokens[4]);
+                        startIsInside = start >= 0 && start < strings.Count;
+                        countIsInside = start + count <= strings.Count && count >= 0;
+
+                        if (startIsInside && countIsInside)
+                        {
+                            strings.Sort(start, count, StringComparer.InvariantCulture);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input parameters.");
+                        }
+
+                        break;
+                    case "rollLeft":
+                        count = int.Parse(tokens[1]) % strings.Count;
+
+                        if (count >= 0)
+                        {
+                            RollLeft(strings, count);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input parameters.");
+                        }
+
+                        break;
+                    case "rollRight":
+                        count = int.Parse(tokens[1]) % strings.Count;
+
+                        if (count >= 0)
+                        {
+                            RollRight(strings, count);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input parameters.");
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"[{string.Join(", ", strings)}]");
+        }
+
+        private static void RollRight(List<string> strings, int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                var lastElement = strings[strings.Count - 1];
+
+                for (var j = strings.Count - 1; j > 0; j--)
+                {
+                    strings[j] = strings[j - 1];
+                }
+
+                strings[0] = lastElement;
+            }
+        }
+
+        private static void RollLeft(List<string> strings, int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                var firstElement = strings[0];
+
+                for (var j = 0; j < strings.Count - 1; j++)
+                {
+                    strings[j] = strings[j + 1];
+                }
+
+                strings[strings.Count - 1] = firstElement;
+            }
+        }
+    }
+}
