64b4ec2138f0702d129c599f094f8dda051c18f1
pirocorp <pirocorp@abv.bg>
2018-02-21 20:10:10 +0100

    The array may be manipulated by one of the following commands • exchange {index} – splits the array after the given index, and exchanges the places of the two resulting sub-arrays. E.g. [1, 2, 3, 4, 5] -> exchange 2 -> result: [4, 5, 1, 2, 3] o If the index is outside the boundaries of the array, print “Invalid index” • max even/odd– returns the INDEX of the max even/odd element -> [1, 4, 8, 2, 3] -> max odd -> print 4 • min even/odd – returns the INDEX of the min even/odd element -> [1, 4, 8, 2, 3] -> min even > print 3 o If there are two or more equal min/max elements, return the index of the rightmost one o If a min/max even/odd element cannot be found, print “No matches” • first {count} even/odd– returns the first {count} elements -> [1, 8, 2, 3] -> first 2 even -> print [8, 2] • last {count} even/odd – returns the last {count} elements -> [1, 8, 2, 3] -> last 2 odd -> print [1, 3] o If the count is greater than the array length, print “Invalid count” o If there are not enough elements to satisfy the count, print as many as you can. If there are zero even/odd elements, print an empty array “[]” • end – stop taking input and print the final state of the array Input • The input data should be read from the console. • On the first line, the initial array is received as a line of integers, separated by a single space • On the next lines, until the command “end” is received, you will receive the array manipulation commands • The input data will always be valid and in the format described. There is no need to check it explicitly. Output • The output should be printed on the console. • On a separate line, print the output of the corresponding command • On the last line, print the final array in square brackets with its elements separated by a comma and a space • See the examples below to get a better understanding of your task Constraints • The number of input lines will be in the range [2 … 50]. • The array elements will be integers in the range [0 … 1000]. • The number of elements will be in the range [1 .. 50] • The split index will be an integer in the range [-231 … 231 – 1] • first/last count will be an integer in the range [1 … 231 – 1] • There will not be redundant whitespace anywhere in the input • Allowed working time for your program: 0.1 seconds. Allowed memory: 16 MB.

---
+++ Exam Preparation/17. Array Manipulator/Array Manipulator.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Array_Manipulator
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var inputLine = Console.ReadLine().ToLower();
+
+            while (inputLine != "end")
+            {
+                ProcessNumbers(inputLine, numbers);
+                inputLine = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine($"[{string.Join(", ", numbers)}]");
+        }
+
+        private static void ProcessNumbers(string inputLine, int[] numbers)
+        {
+            var tokens = inputLine.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var command = tokens[0];
+
+            switch (command)
+            {
+                case "exchange":
+                    var index = int.Parse(tokens[1]);
+
+                    if (index < 0 || index >= numbers.Length)
+                    {
+                        Console.WriteLine($"Invalid index");
+                        return;
+                    }
+
+                    ProcessExchangeCommand(index, numbers);
+                    break;
+                case "max":
+                    var evenOrOdd = tokens[1];
+                    index = GetIndexOfMaxEvenOrOddElement(numbers, evenOrOdd);
+
+                    if (index == -1)
+                    {
+                        Console.WriteLine("No matches");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{index}");
+                    }
+                    break;
+                case "min":
+                    evenOrOdd = tokens[1];
+                    index = GetIndexOfMinEvenOrOddElement(numbers, evenOrOdd);
+
+                    if (index == -1)
+                    {
+                        Console.WriteLine("No matches");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{index}");
+                    }
+                    break;
+                case "first":
+                    var count = int.Parse(tokens[1]);
+                    if (count > numbers.Length)
+                    {
+                        Console.WriteLine("Invalid count");
+                        return;
+                    }
+                    evenOrOdd = tokens[2];
+                    PrintFirstNElements(numbers, count, evenOrOdd);
+                    break;
+                case "last":
+                    count = int.Parse(tokens[1]);
+                    if (count > numbers.Length)
+                    {
+                        Console.WriteLine("Invalid count");
+                        return;
+                    }
+                    evenOrOdd = tokens[2];
+                    PrintLastNElements(numbers, count, evenOrOdd);
+                    break;
+            }
+        }
+
+        private static void PrintLastNElements(int[] numbers, int count, string evenOrOddElement)
+        {
+            var evenOrOdd = evenOrOddElement == "even" ? 0 : 1;
+            var result = numbers.Where(x => x % 2 == evenOrOdd).Reverse().Take(count).ToArray();
+            result = result.Reverse().ToArray();
+            Console.WriteLine($"[{string.Join(", ", result)}]");
+        }
+
+        private static void PrintFirstNElements(int[] numbers, int count, string evenOrOddElement)
+        {
+            var evenOrOdd = evenOrOddElement == "even" ? 0 : 1;
+            var result = numbers.Where(x => x % 2 == evenOrOdd).Take(count).ToArray();
+            Console.WriteLine($"[{string.Join(", ", result)}]");
+        }
+
+        private static int GetIndexOfMinEvenOrOddElement(int[] numbers, string evenOrOddElement)
+        {
+            var index = -1;
+            var evenOrOdd = evenOrOddElement == "even" ? 0 : 1;
+
+            var minIndex = -1;
+            var currentMin = int.MaxValue;
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                var currentElement = numbers[i];
+
+                if (currentMin >= currentElement && currentElement % 2 == evenOrOdd)
+                {
+                    minIndex = i;
+                    currentMin = currentElement;
+                }
+            }
+
+            index = minIndex;
+            return index;
+        }
+
+        private static int GetIndexOfMaxEvenOrOddElement(int[] numbers, string evenOrOddElement)
+        {
+            var index = -1;
+            var evenOrOdd = evenOrOddElement == "even" ? 0 : 1;
+
+            var maxIndex = -1;
+            var currentMax = int.MinValue;
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                var currentElement = numbers[i];
+
+                if (currentMax <= currentElement && currentElement % 2 == evenOrOdd)
+                {
+                    maxIndex = i;
+                    currentMax = currentElement;
+                }
+            }
+
+            index = maxIndex;
+            return index;
+        }
+
+        private static void ProcessExchangeCommand(int index, int[] numbers)
+        {
+            var firstPart = new int[index + 1];
+            var secondPart = new int[numbers.Length - (index + 1)];
+
+            for (var i = 0; i < firstPart.Length; i++)
+            {
+                firstPart[i] = numbers[i];
+            }
+
+            for (var i = firstPart.Length; i < numbers.Length; i++)
+            {
+                secondPart[i - firstPart.Length] = numbers[i];
+            }
+
+            for (var i = 0; i < secondPart.Length; i++)
+            {
+                numbers[i] = secondPart[i];
+            }
+
+            for (var i = 0; i < firstPart.Length; i++)
+            {
+                numbers[i + secondPart.Length] = firstPart[i];
+            }
+        }
+    }
+}
