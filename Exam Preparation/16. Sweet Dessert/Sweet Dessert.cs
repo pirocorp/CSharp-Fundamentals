deafcfc20cf1edc01ab6877e5185628bf05f087b
pirocorp <pirocorp@abv.bg>
2018-02-21 17:48:16 +0100

    Ivancho and his girlfriend are throwing a party. She plans to cook her favorite dessert. She asks Ivancho to buy the needed products. The number of desserts depends on how many people will be coming. She can prepare the dessert in portions of 6. If there are 5 guests coming, she will still cook 6 portions, 10 guests – cook 12. The products for the dessert are bananas, eggs and berries. For a set of 6 she needs 2 bananas, 4 eggs and 0.2 kilos berries. You will be given the amount of money Ivancho has, the number of guests and the prices of the products. You have to help Ivancho calculate if the cash he has is enough to buy all of the products, or how much more money he needs. Input The input data should be read from the console. It will consist of exactly 5 lines: • The amount of cash Ivancho has – floating-point number in range [0.00…1,000,000,000.00] • The number of guests – integer in range [0…1,000,000,000] • The price of bananas for a single unit – floating-point number in range [0.00…1,000.00] • The price of eggs for a single unit – floating-point number in range [0.00…1,000.00] • The price of berries for a kilo – floating-point number in range [0.00…1,000.00] The input data will always be valid and in the format described. There is no need to check it explicitly. Output The output should be printed on the console. • If the calculated price of the products is less or equal to the money Ivancho has: o “Ivancho has enough money - it would cost {the cost of the products}lv.” • If the calculated price of the products is more than the money Ivancho has: o  “Ivancho will have to withdraw money - he will need {neededMoney}lv more.” • All prices must be rounded to two digits after the decimal point.

---
+++ Exam Preparation/16. Sweet Dessert/Sweet Dessert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.Sweet_Dessert
+{
+    class Program
+    {
+        static void Main()
+        {
+            var amountOfCash = decimal.Parse(Console.ReadLine());
+            var numberOfGuests = int.Parse(Console.ReadLine());
+            var priceOfBananas = decimal.Parse(Console.ReadLine());
+            var priceOfEggs = decimal.Parse(Console.ReadLine());
+            var priceOfBerries = decimal.Parse(Console.ReadLine());
+            var numberOfSets = (decimal)Math.Ceiling(numberOfGuests / 6.0);
+            var pricePerSet = priceOfBananas * 2 + 4 * priceOfEggs + 0.2m * priceOfBerries;
+            var costOfProducts = pricePerSet * numberOfSets;
+
+            var cashleft = costOfProducts - amountOfCash;
+
+            if (amountOfCash >= costOfProducts)
+            {
+                Console.WriteLine($"Ivancho has enough money - it would cost {costOfProducts:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivancho will have to withdraw money - he will need {cashleft:F2}lv more.");
+            }
+        }
+    }
+}
