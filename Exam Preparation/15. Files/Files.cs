58af1de06d65afa1d5fb8d6ffd62438609926ef1
pirocorp <pirocorp@abv.bg>
2018-02-21 12:40:38 +0100

    You are given number of files with their full file paths and file sizes. You need to print all file names with a given extension that are present in a given root directory sorted by their file size in descending order. If two files have same size, order them by alphabetical order. If a file name (file name + extension) appears more than once in a given root, save only its latest value. If a file name appears in more than one root, they are treated as different files. If there aren't any files that correspond to the query, print "No". Input / Constrains • On the first line of input you will get N the number of files to be read from the console. • On the next N lines, you receive the actual files in the format “root\folder\filename.extension;filesize”. • There may be more than one folder, e.g. folders can be deeply nested. • On the last line you receive a query string in format “{extension} in {root}”. You need to print all files with the given extension that are in present in the given root. Output • You need to print all files sorted by their size in descending order. • If two files have the same size, order them by alphabetical order. • Files should be printed in the given format "filename.extension - filesize KB". • If there aren't any movies that correspond to the query, print "No".

---
+++ Exam Preparation/15. Files/Files.cs
@@ -0,0 +1,60 @@
+﻿namespace _15.Files
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Files
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var files = new Dictionary<string, long>();
+
+            for (var i = 0; i < n; i++)
+            {
+                var inputData = Console.ReadLine().Split(new[] {';'}, StringSplitOptions.RemoveEmptyEntries);
+                var fileFullName = inputData[0].Trim();
+                var size = long.Parse(inputData[1]);
+
+                if (!files.ContainsKey(fileFullName))
+                {
+                    files[fileFullName] = size;
+                }
+
+                files[fileFullName] = size;
+            }
+
+            var command = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var extension = command[0];
+            var extensionRegex = new Regex($@"\.{extension}$", RegexOptions.Compiled);
+            const string filenamePattern = @"\\(?<filename>[^\\]+\.[^\\]+)(?!\\)$";
+            var filenameRegex = new Regex(filenamePattern, RegexOptions.Compiled);
+            var root = command[2];
+            var rootRegex = new Regex($@"^{root}\\");
+            var resultFiles = files
+                .Where(x => rootRegex.IsMatch(x.Key))
+                .Where(x => extensionRegex.IsMatch(x.Key))
+                .Select(x =>
+                {
+                    var currentX = new KeyValuePair<string, long>(filenameRegex.Match(x.Key).Groups["filename"].Value, x.Value);
+                    return currentX;
+                })
+                .OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key)
+                .ToArray();
+
+            if (resultFiles.Length == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            foreach (var file in resultFiles)
+            {
+                Console.WriteLine($"{file.Key} - {file.Value} KB");
+            }
+        }
+    }
+}
