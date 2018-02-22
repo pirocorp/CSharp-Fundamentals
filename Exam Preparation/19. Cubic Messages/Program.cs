336be0b3bb6cf0aff7ec9794fee8f6c0a1420426
pirocorp <pirocorp@abv.bg>
2018-02-22 15:16:47 +0100

    You will begin receiving lines of input, which will consist of random ASCII characters – Cubic’s encrypted lines. After each line you will receive a number – the length of the message he sent. Cubic might send false messages, in an act to confuse his “enemies”. You must capture only the messages that follow a certain format. According to that format the valid messages: • Consist of m characters, where m is the integer entered after each encrypted line. • Has only digits before itself in the encrypted line • Consists only of English alphabet letters • Has no English alphabet letters after itself in the encrypted line Any message that does not follow the, specified above, rules, is invalid, and you must ignore it. After you find all valid messages, you need to find their verification code. Every message has its own verification code, which Cubic gives in order to verify the message. Take all the digits before the message and all the digits after the message and consider them as indexes. If they are valid existing indexes in the message, form a string with those indexes taking characters from the message. If an index is nonexistent, put a space there. The string you form up is the verification code for the current message. Input • The input will always come in the form of 2 lines, except when it is the line terminating the input sequence. • The first input line will contain random ASCII characters, and the second – a number. • When the line “Over!” is entered, the input sequence ends. Output • The output is simple. You must print all the valid messages you’ve found, each on a new line, and their verification codes, if they have such. • The format of output is “{message} == {verificationCode}”. Constraints • The input lines can consist of ANY ASCII character. • There will be NO such cases as an encrypted message without a number before it. • The number will be a valid integer in the range [0, 100]. • Allowed time/memory: 100ms/16MB

---
+++ Exam Preparation/19. Cubic Messages/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _19.Cubic_Messages
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var digitRegex = new Regex(@"\d", RegexOptions.Compiled);
+
+            while (inputLine != "Over!")
+            {
+                var number = int.Parse(Console.ReadLine());
+                var pattern = $@"^\d+(?<message>[a-zA-Z]{{{number}}})[^a-zA-Z]*$";
+                var match = Regex.Match(inputLine, pattern);
+
+                if (!match.Success)
+                {
+                    inputLine = Console.ReadLine();
+                    continue;
+                }
+
+                var cryptedMessage = match.Groups["message"].Value;
+                var allDigitInMessage = digitRegex.Matches(inputLine)
+                    .Cast<Match>()
+                    .Select(x => x.Value)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var verificationCode = GetVerificationCode(cryptedMessage, allDigitInMessage);
+                Console.WriteLine($"{cryptedMessage} == {verificationCode}");
+                inputLine = Console.ReadLine();
+            }
+        }
+
+        private static string GetVerificationCode(string cryptedMessage, int[] allDigitInMessage)
+        {
+            var result = new StringBuilder();
+
+            for (var i = 0; i < allDigitInMessage.Length; i++)
+            {
+                var currentDigit = allDigitInMessage[i];
+                var currentChar = ' ';
+
+                if (currentDigit >= 0 && currentDigit < cryptedMessage.Length)
+                {
+                    currentChar = cryptedMessage[currentDigit];
+                }
+
+                result.Append(currentChar);
+            }
+
+            return result.ToString();
+        }
+    }
+}
