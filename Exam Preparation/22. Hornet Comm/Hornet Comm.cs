0803a2c567ef9273b1b26fc45982c66a32244fd7
pirocorp <pirocorp@abv.bg>
2018-02-24 14:25:14 +0100

    You will be given data of several tracked comm. channels, which you must decrypt. The input data will come in the following format: {firstQuery} <-> {secondQuery} If the first query consists only of digits and the second one consists of digits and / or letters, it is a private message. If the first query consists of anything but digits, and the second one consists of letters and / or digits, it is a broadcast. Any input that does NOT follow the format, specified above, should be IGNORED. If the given data is a private message, the first query is the recipient’s code, and the second query is the message. You must reverse the recipient’s code and store it along with the message. If the given data is a broadcast, the first query is the message, and the second query is the frequency. You must take the frequency and make all capital letters – small and all small letters – capital. Then you must store it, along with the message. You must keep all input broadcasts and messages after you decrypt them. When you receive the command “Hornet is Green”, the input sequences ends, and you must print the stored broadcasts and messages. Input • The input comes in the form of several input lines in the format specified above. • The input ends when you receive the command “Hornet is Green”. Output • As output you must print all broadcasts and messages, printing first the broadcasts, in the following format: o Broadcasts: o {frequency} -> {message} o . . . o Messages: o {recipient} -> {message} o . . . • If there are no messages, or no broadcasts, print “None” in their place. Constrains • The input lines may consist of any ASCII character. • There will be NO more than 1000 lines of input. • All data must be printed in order of input.

---
+++ Exam Preparation/22. Hornet Comm/Hornet Comm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _22.Hornet_Comm
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string privateMessagePattern = @"^(?<recipientCode>\d+)(?: <-> )(?<message>[A-Za-z0-9]+)$";
+            var privateMessageRegex = new Regex(privateMessagePattern, RegexOptions.Compiled);
+            const string broadcastPattern = @"^(?<message>[^0-9]+)(?: <-> )(?<frequency>[A-Za-z0-9]+)$";
+            var broadcastRegex = new Regex(broadcastPattern, RegexOptions.Compiled);
+            var inputLine = Console.ReadLine();
+            var privateMessages = new List<KeyValuePair<string, string>>(); // recipient message
+            var broadcasts = new List<KeyValuePair<string, string>>(); // frequency message
+
+            while (inputLine != "Hornet is Green")
+            {
+                var privateMessage = privateMessageRegex.Match(inputLine);
+                var broadcast = broadcastRegex.Match(inputLine);
+
+                if (privateMessage.Success)
+                {
+                    var recipientCode = new string(privateMessage.Groups["recipientCode"].Value.Reverse().ToArray());
+                    var message = privateMessage.Groups["message"].Value;
+                    var currentPrivateMessage = new KeyValuePair<string, string>(recipientCode, message);
+                    privateMessages.Add(currentPrivateMessage);
+                    inputLine = Console.ReadLine();
+                    continue;
+                }
+
+                if (broadcast.Success)
+                {
+                    var message = broadcast.Groups["message"].Value;
+                    var frequencyChars = broadcast.Groups["frequency"].Value.Select(x =>
+                    {
+                        if (char.IsUpper(x))
+                        {
+                            var newX = char.ToLower(x);
+                            return newX;
+                        }
+
+                        if (char.IsLower(x))
+                        {
+                            var newX = char.ToUpper(x);
+                            return newX;
+                        }
+
+                        return x;
+                    }).ToArray();
+
+                    var frequency = new string(frequencyChars);
+                    var currentBroadCast = new KeyValuePair<string, string>(frequency, message);
+                    broadcasts.Add(currentBroadCast);
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Broadcasts:");
+
+            if (broadcasts.Count == 0)
+            {
+                Console.WriteLine("None");
+            }
+
+            foreach (var broadcast in broadcasts)
+            {
+                var frequency = broadcast.Key;
+                var message = broadcast.Value;
+                Console.WriteLine($"{frequency} -> {message}");
+            }
+
+            Console.WriteLine($"Messages:");
+
+            if (privateMessages.Count == 0)
+            {
+                Console.WriteLine("None");
+            }
+
+            foreach (var message in privateMessages)
+            {
+                var recipient = message.Key;
+                var privateMessage = message.Value;
+                Console.WriteLine($"{recipient} -> {privateMessage}");
+            }
+        }
+    }
+}
