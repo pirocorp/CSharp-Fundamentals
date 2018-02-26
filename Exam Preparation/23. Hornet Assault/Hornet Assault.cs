2a433bad322df2e71712b48458a040a4fd0a9c1d
pirocorp <pirocorp@abv.bg>
2018-02-26 11:07:49 +0100

    You will be given a sequence of integers, separated by a space. The integers will represent the beehives and the amount of bees in them. You will then receive another sequence of integers, which will represent the hornets, and their power. The power indicates how many bees a hornet can kill. The hornets must start attacking the beehives one by one. If the bees are more or equal to the summed-up power of the hornets, the first (entered) hornet, currently alive, dies in the assault of the current beehive. When the hornets assault a beehive, they kill an amount of bees, equal to their summed-up power. This means that you must decrease the integer of the beehive, with the value of the summed-up power, of the live hornets. After you’ve successfully assaulted all beehives, you must print the result of the winning side. If there are ANY beehives with live bees inside them, you must print them. If there aren’t, you must print the live hornets. Input • On the first line of input you will receive a sequence of integers, separated by spaces – the beehives. • On the second line of input you will receive a sequence of integers, separated by spaces – the hornets. Output • Depending on the case of printing you must either print the live beehives, or the live hornets. • They are sequences of integers, so in both cases you must print them separated by a space. Constrains • The input will consist only of integers in range [0; 1,000,000]. • There will be NO STALEMATE situations. • The input sequences may consist of up to 3000 elements.

---
+++ Exam Preparation/23. Hornet Assault/Hornet Assault.cs
@@ -0,0 +1,51 @@
+﻿namespace _23.Hornet_Assault
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var beehives = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+            var hornets = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+            var hornetSummedUpPower = hornets.Sum();
+
+            while (beehives.Count > 0 && beehives[0] < hornetSummedUpPower)
+            {
+                beehives.RemoveAt(0);
+            }
+
+            for (var i = 0; i < beehives.Count; i++)
+            {
+                var currentBeeHive = beehives[i];
+                var currentHornetSummedUpPower = hornets.Sum();
+
+                if (currentBeeHive >= currentHornetSummedUpPower)
+                {
+                    if (hornets.Count != 0)
+                    {
+                        hornets.RemoveAt(0);
+                    }
+                }
+
+                beehives[i] = beehives[i] - currentHornetSummedUpPower;
+
+                if (beehives[i] <= 0)
+                {
+                    beehives.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            if (beehives.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", beehives));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", hornets));
+            }
+        }
+    }
+}
