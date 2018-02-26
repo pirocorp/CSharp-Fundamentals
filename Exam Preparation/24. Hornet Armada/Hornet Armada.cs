233d8f17ae1ec76d55c6bffe5a50acb330bdc19c
pirocorp <pirocorp@abv.bg>
2018-02-26 11:28:07 +0100

    You will be given N – an integer. On the next N lines you will be given input containing information about soldiers in the following format: {lastActivity} = {legionName} -> {soldierType}:{soldierCount} The last activity is an integer. The legion name and soldier type, will both be strings. The soldier count will be an integer. You must store every legion with its activity, and every soldier type with its count, in its legion. If a given legion already exists, you must add the new soldier type, with its count. If the soldier type exists ALSO, you should just add the soldier count. IN BOTH cases, stated above, you should update the last activity, with the newly entered one, ONLY if the entered one is GREATER than the previous one. After you’ve read all N input lines, you will receive a line in one of the following formats: • {activity}\{soldierType} • {soldierType} In the first case, you must print all legions, and the count of soldiers they have from the given soldier type, who’s last activity is LOWER than the given activity. The legions must be printed in descending order by soldier count. In the second case, you must print all legions which have the given soldier type, with last activity, and legion name. The legions must be printed in descending order of their activity. Input • On the first line you will receive N – the integer. • On the next N lines you will receive data about soldiers and legions. • On the last line you will receive one of the two commands, which will determine the output. Output • If you are given the last activity and soldier type on the last command, you must print the legions in this format: - {legionName} -> {soldierCount} • If you are given only the soldier type on the last command, you must print the legions in this format: - {lastActivity} : {legionName} Constrains • The first integer – N, will be in range [0; 10,000]. • The legion names and soldier types may consist of any ASCII character, except “=”, “-”, “>”, “:”, “ ”(space). • The soldier count and last activity will be integers in range [0; 1,000,000,000]. • All input data will be exactly as stated above. There will be NO invalid input lines. • Data which has NO specified order must be sorted in order of input.

---
+++ Exam Preparation/24. Hornet Armada/Hornet Armada.cs
@@ -0,0 +1,80 @@
+﻿namespace _04.Hornet_Armada
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, int> legionsWithActivity = new Dictionary<string, int>();
+            Dictionary<string, Dictionary<string, long>> legionsWithSoldiers = new Dictionary<string, Dictionary<string, long>>(); 
+
+            var n = int.Parse(Console.ReadLine());
+
+            var inputPattern = @"(\d+)\s\=\s(.+)\s\-\>\s(.+)\:(\d+)";
+            Regex inputRegex = new Regex(inputPattern);
+
+            for (int i = 0; i < n; i++)
+            {
+                var inputMatch = inputRegex.Match(Console.ReadLine());
+
+                var lastActivity = int.Parse(inputMatch.Groups[1].Value); //EXTRACT THE LAST ACTIVITY
+                var legionName = inputMatch.Groups[2].Value; //EXTRACT THE LEGION NAME
+                var soldierType = inputMatch.Groups[3].Value; //EXTRACT THE SOLDIER TYPE
+                var soldierCount = int.Parse(inputMatch.Groups[4].Value); //EXTRACT THE SOLDIER COUNT
+
+                if (!legionsWithActivity.ContainsKey(legionName))
+                {
+                    legionsWithActivity.Add(legionName, lastActivity);
+                    legionsWithSoldiers.Add(legionName, new Dictionary<string, long>());
+                }
+
+                if (lastActivity > legionsWithActivity[legionName])
+                {
+                    legionsWithActivity[legionName] = lastActivity;
+                }
+
+                if (!legionsWithSoldiers[legionName].ContainsKey(soldierType))
+                {
+                    legionsWithSoldiers[legionName].Add(soldierType, 0);
+                }
+
+                legionsWithSoldiers[legionName][soldierType] += soldierCount;
+            }
+
+
+            var queryParams = Console.ReadLine().Split('\\');
+
+            if (queryParams.Length > 1)
+            {
+                var activity = int.Parse(queryParams[0]);
+                var soldierType = queryParams[1];
+
+                foreach (var legionEntry in legionsWithSoldiers
+                    .Where(legion => legion.Value.ContainsKey(soldierType))
+                    .OrderByDescending(legion => legion.Value[soldierType]))
+                {
+                    if (legionsWithActivity[legionEntry.Key] < activity)
+                    {
+                        Console.WriteLine("{0} -> {1}", legionEntry.Key, legionsWithSoldiers[legionEntry.Key][soldierType]);
+                    }
+                }
+            }
+            else
+            {
+                var soldierType = queryParams[0];
+
+                foreach (var legionEntry in legionsWithActivity.OrderByDescending(legion => legion.Value))
+                {
+                    if (legionsWithSoldiers[legionEntry.Key].ContainsKey(soldierType))
+                    {
+                        Console.WriteLine("{0} : {1}", legionEntry.Value, legionEntry.Key);
+                    }
+                }
+            }
+        }
+    }
+}
