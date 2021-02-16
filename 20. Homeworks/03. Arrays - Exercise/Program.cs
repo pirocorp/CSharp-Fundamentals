eef5b7ee30c2a1deb8a09baecb8fafb92c7a3832
Zdravko Zdravkov <zdr0@yahoo.com>
2021-02-16 15:55:20 +0100

    03. Arrays - Exercise

---
+++ 20. Homeworks/03. Arrays - Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Arrays___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
