e64ba5627b297937b4d5a333fe62bfdf5e0fc887
Zdravko Zdravkov <zdr0@yahoo.com>
2021-02-17 13:19:24 +0100

    05. Lists - Exercise

---
+++ 20. Homeworks/05. Lists - Exercise/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _05._Lists___Exercise
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Task01();
+            //Task02();
+            //Task03();
+            //Task04();
+            //Task05();
+            //Task06();
+            //Task07();
+            //Task08();
+            //Task09();
+            //Task10();
+        }
+
+        private static void Task01()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task02()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task03()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task04()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task05()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task06()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task07()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task08()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task09()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task10()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
