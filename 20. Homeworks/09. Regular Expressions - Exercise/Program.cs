4eaf3731a0bfe91bc169ee2d683f46cc6013beb2
Zdravko Zdravkov <zdr0@yahoo.com>
2021-02-19 13:28:11 +0100

    09. Regular Expressions - Exercise

---
+++ 20. Homeworks/09. Regular Expressions - Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09._Regular_Expressions___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
