d6d87fa4fb1d92e63a8ebd34c5497f3567c60612
Zdravko Zdravkov <zdr0@yahoo.com>
2021-02-19 15:58:24 +0100

    09. Regular Expressions - Exercise

--- 20. Homeworks/09. Regular Expressions - Exercise/Program.cs
+++ 20. Homeworks/09. Regular Expressions - Exercise/Program.cs
@@ -1,12 +1,152 @@
-﻿using System;
-
-namespace _09._Regular_Expressions___Exercise
+﻿namespace _09._Regular_Expressions___Exercise
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            //Task01();
+            //Task02();
+            //Task03();
+            Task04();
+            //Task05();
+            //Task06();
+        }
+
+        private static void Task01()
+        {
+            var pattern = "(>>)(?<furniture>[A-Za-z]+)(<<)(?<price>\\d+(\\.\\d+)?)!(?<quantity>\\d+)";
+            var regex = new Regex(pattern);
+
+            var furnitures = new List<string>();
+            var sum = 0M;
+
+            string input;
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+                var match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    var furniture = match.Groups["furniture"].Value;
+                    var price = decimal.Parse(match.Groups["price"].Value);
+                    var quantity = int.Parse(match.Groups["quantity"]?.Value ?? "0");
+
+                    furnitures.Add(furniture);
+                    sum += price * quantity;
+                }
+            }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (var furniture in furnitures)
+            {
+                Console.WriteLine(furniture);
+            }
+
+            Console.WriteLine($"Total money spend: {sum:F2}");
+        }
+
+        private static void Task02()
+        {
+            var participants = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).ToDictionary(x => x.Trim(), x => 0);
+            var letters = new Regex("[A-Za-z]+[^A-Za-z]*?");
+            var digits = new Regex("[0-9][^0-9]*?");
+
+            string input;
+            while ((input = Console.ReadLine()) != "end of race")
+            {
+                var name = GetName(letters, input);
+                var sum = GetDistance(digits, input);
+
+                if (participants.ContainsKey(name))
+                {
+                    participants[name] += sum;
+                }
+            }
+
+            var i = 0;
+            var positions = new[] { "1st", "2nd", "3rd" };
+            participants
+                .OrderByDescending(x => x.Value)
+                .Take(3)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{positions[i++]} place: {x.Key.Trim()}"));
+        }
+
+        private static int GetDistance(Regex digits, string input)
+        {
+            var matches = digits.Matches(input);
+
+            var sum = 0;
+
+            foreach (Match match in matches)
+            {
+                sum += int.Parse(match.Value);
+            }
+
+            return sum;
+        }
+
+        private static string GetName(Regex letters, string input)
+        {
+            var matches = letters.Matches(input);
+            var sb = new StringBuilder();
+
+            foreach (Match x in matches)
+            {
+                sb.Append(x.Value.Trim());
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        private static void Task03()
+        {
+            var pattern = ".*?%(?<customer>[A-Z][a-z]+)%.*?<(?<product>\\w+)>.*?\\|(?<count>\\d+)\\|.*?(?<price>\\d+(\\.\\d+)?)\\$";
+            var regex = new Regex(pattern);
+
+            var income = 0M;
+
+            string input;
+            while ((input = Console.ReadLine()) != "end of shift")
+            {
+                var match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    var customer = match.Groups["customer"].Value;
+                    var product = match.Groups["product"].Value;
+                    var count = int.Parse(match.Groups["count"].Value);
+                    var price = decimal.Parse(match.Groups["price"].Value);
+
+                    var total = price * count;
+                    Console.WriteLine($"{customer}: {product} - {total:F2}");
+
+                    income += total;
+                }
+            }
+
+            Console.WriteLine($"Total income: {income:F2}");
+        }
+
+        private static void Task04()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task05()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void Task06()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
