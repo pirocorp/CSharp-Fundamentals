8e6260cd96d09c198a7bb71a43f1a9d79703cb19
Zdravko Zdravkov <pirocorp@abv.bg>
2021-02-26 20:17:49 +0100

    01. National Court

---
+++ MidExamPreps/ExamPrep2/01. National Court/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._National_Court
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                var a = int.Parse(Console.ReadLine());
+                var b = int.Parse(Console.ReadLine());
+                var c = int.Parse(Console.ReadLine());
+
+                var count = int.Parse(Console.ReadLine());
+
+                var sum = a + b + c;
+
+                var h = Math.Ceiling(count / (double)sum);
+
+                h += Math.Max(0, Math.Ceiling(h / 3) - 1);
+                Console.WriteLine($"Time needed: {Math.Ceiling(h)}h.");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
